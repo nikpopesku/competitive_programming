4620ede5ab7c0ec0fda1d00aa5c1c4bcbc2ae78e
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-17 13:43:09 +0800

    c++/300-399/354_russian_doll_envelopes.cpp

--- c++/300-399/354_russian_doll_envelopes.cpp
+++ c++/300-399/354_russian_doll_envelopes.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bits/stdc++.h>
 
-using std::cout, std::endl, std::vector, std::sort;
+
+using std::cout, std::endl, std::vector, std::sort, std::max;
 
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(), envelopes.end(), myfunction);
+        vector<int> height {};
 
+        for (size_t i = 0; i < envelopes.size(); i++) {
+            height.push_back(envelopes[i][1]);
+        }
+
+        vector<int>dp {};
+
+
+        for (size_t i = 0; i < height.size(); i++) {
+            auto lower = lower_bound(dp.begin(), dp.end(), height[i]);
+            if (lower == dp.end) {
+                dp.push_back(height[i]);
+            } else {
+                dp[lower-dp.begin()] = height[i];
+            }
+
+        }
+
+
+        return dp.size();
     }
 private:
-    static bool myfunction (vector<int> i,vector<int> j) { return (i[0] + i[1]) < (j[0] + j[1]); }
+    static bool myfunction (const vector<int> &i, const vector<int> &j) {
+        if (i[0] < j[0]) {
+            return true;
+        }
+        if (j[0] > i[0]) {
+            return true;
+        }
+
+        if (i[1] > j[1]) {
+            return true;
+        }
+
+        return false;
+    }
 };
 
 int main() {
