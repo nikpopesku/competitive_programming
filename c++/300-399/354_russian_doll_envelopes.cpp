b26ea76f7a6fd1897da5579607fbd663a51e890a
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-17 13:00:46 +0800

    c++/300-399/354_russian_doll_envelopes.cpp

---
+++ c++/300-399/354_russian_doll_envelopes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+
+using std::cout, std::endl, std::vector;
+
+class Solution {
+public:
+    int maxEnvelopes(const vector<vector<int>>& envelopes) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    cout << s.maxEnvelopes({{5,4},{6,4},{6,7},{2,3}}) << endl;
+}
