9f9f821c63b0b6edb591d765fdc3352c689ebd87
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-17 13:13:32 +0800

    c++/300-399/354_russian_doll_envelopes.cpp

--- c++/300-399/354_russian_doll_envelopes.cpp
+++ c++/300-399/354_russian_doll_envelopes.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-
-using std::cout, std::endl, std::vector;
+using std::cout, std::endl, std::vector, std::sort;
 
 class Solution {
 public:
-    int maxEnvelopes(const vector<vector<int>>& envelopes) {
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        sort(envelopes.begin(), envelopes.end(), myfunction);
 
     }
+private:
+    static bool myfunction (vector<int> i,vector<int> j) { return (i[0] + i[1]) < (j[0] + j[1]); }
 };
 
 int main() {
     auto s = Solution();
-    cout << s.maxEnvelopes({{5,4},{6,4},{6,7},{2,3}}) << endl;
+    auto a = vector{vector<int>{5,4},vector<int>{6,4},vector<int>{6,7},vector<int>{2,3}};
+    cout << s.maxEnvelopes(a) << endl;
 }
