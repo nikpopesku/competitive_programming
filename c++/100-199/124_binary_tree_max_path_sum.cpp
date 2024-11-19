b5e75caa6bb188b360acf22cb185afe19283a055
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-19 13:38:34 +0800

    c++/100-199/124_binary_tree_max_path_sum.cpp

--- c++/100-199/124_binary_tree_max_path_sum.cpp
+++ c++/100-199/124_binary_tree_max_path_sum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "../library/treenode.h"
 #include <unordered_map>
+#include <climits>
 
 
 using std::cout, std::endl;
@@ -9,22 +10,22 @@ using std::cout, std::endl;
 class Solution {
 public:
     int maxPathSum(TreeNode *root) {
-        if (root == nullptr) return 0;
+        if (root == nullptr) return INT_MIN;
         if (dp.find(root->val) != dp.end()) return dp[root->val];
 
         if (root->left == nullptr and root->right == nullptr) {
             dp[root->val] = root->val;
         }
 
-        const int left = maxPathSum(root->left);
-        const int right = maxPathSum(root->right);
+        const long long left = maxPathSum(root->left);
+        const long long right = maxPathSum(root->right);
         dp[root->val] = max({left, right, left + right + root->val, root->val, left + root->val, right + root->val});
 
         return dp[root->val];
     }
 
 private:
-    static int max(const std::initializer_list<int> list) {
+    static int max(const std::initializer_list<long long> list) {
         int max_value = *list.begin();
         for (const auto elem: list) {
             if (elem > max_value) {
@@ -35,11 +36,11 @@ private:
         return max_value;
     }
 
-    std::unordered_map<int, int> dp{};
+    std::unordered_map<int, long long> dp{};
 };
 
 int main() {
     auto s = Solution();
-    cout << s.maxPathSum(new TreeNode(-10, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))) <<
-            endl;
+    // cout << s.maxPathSum(new TreeNode(-10, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)))) << endl;
+    cout << s.maxPathSum(new TreeNode(-3)) << endl;
 }
