f9f75a1b9e1925f2049830ab5d53d137f5962019
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-19 13:00:48 +0800

    c++/100-199/124_binary_tree_max_path_sum.cpp

---
+++ c++/100-199/124_binary_tree_max_path_sum.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <unordered_map>
+#include "library/treenode.h"
+
+
+using std::cout, std::endl;
+
+
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    // const auto a = new TreeNode(3, new TreeNode(2, nullptr, new TreeNode(3)),
+    //                             new TreeNode(3, nullptr, new TreeNode(1)));
+    const auto a = new TreeNode(3, new TreeNode(4, new TreeNode(1), new TreeNode(3)),
+                                new TreeNode(5, nullptr, new TreeNode(1)));
+    cout << s.rob(a) << endl;
+    delete a;
+}
