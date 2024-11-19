12e8a402dc12db2501a4e2cf127966a22ef02b3f
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-19 16:49:08 +0800

    c++/100-199/124_binary_tree_max_path_sum.cpp

---
+++ c++/300-399/309_best_time_buy_stock_cooldown.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <vector>
+
+
+using std::cout, std::endl, std::vector;
+
+class Solution {
+public:
+    int maxProfit(const vector<int>& prices) {
+        return 4;
+    }
+};
+
+int main() {
+    auto s = Solution();
+    cout << s.maxProfit({1,2,3,0,2}) << endl;
+}
