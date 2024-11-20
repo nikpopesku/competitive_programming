fbc511b12d6957c76bfdd2c03f74056166ce0317
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-20 13:22:47 +0800

    c++/300-399/309_best_time_buy_stock_cooldown.cpp

--- c++/300-399/309_best_time_buy_stock_cooldown.cpp
+++ c++/300-399/309_best_time_buy_stock_cooldown.cpp
@@ -1,18 +1,26 @@
 
 #include <iostream>
 #include <vector>
+#include <climits>
 
 
-using std::cout, std::endl, std::vector;
+using std::cout, std::endl, std::vector, std::max;
 
 class Solution {
 public:
     int maxProfit(const vector<int>& prices) {
-        vector<int> dp (prices.size()+1, 0);
+        int sold = INT_MIN;
+        int held = INT_MIN;
+        int reset = 0;
 
-        return 4;
+        for (auto &p: prices) {
+            int pre_sold = sold;
+            sold = held + p;
+            held = max(held, reset - p);
+            reset = max(reset, pre_sold);
+        }
 
-
+        return max(sold, reset);
     }
 };
 
