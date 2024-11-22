fc13da23ca84bb30269df27d23328bc3ba79ae12
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-22 14:22:02 +0800

    c++/200-299/265_paint_house2.cpp

---
+++ c++/200-299/265_paint_house2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using std::vector, std::min;
+
+class Solution {
+public:
+    int minCostII(vector<vector<int>>& costs) {
+        const size_t m = costs.size();
+        const size_t n = costs[0].size();
+        vector dp (m+1, vector(n, 0));
+
+        for (size_t i = 1; i <= m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                vector<int> others {};
+                for (size_t k = 0; k < j; k++) {
+                    others.push_back(dp[i-1][k]);
+                }
+                for (size_t k = j+1; k < n; k++) {
+                    others.push_back(dp[i-1][k]);
+                }
+                dp[i][j] = minValue(others) + costs[i-1][j];
+            }
+        }
+
+        return minValue(dp[m]);
+    }
+private:
+    int minValue(vector<int>& dp) {
+        int response = dp[0];
+        for (const auto &val: dp) {
+            if (val < response) response = val;
+        }
+
+        return response;
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    std::cout << s.minCostII({{17,2,17},{16,16,5},{14,3,19}}) << std::endl;
+}
