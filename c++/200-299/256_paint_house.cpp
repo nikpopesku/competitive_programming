ba0389e03ee143393789bbdf367479851c7e62f6
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-22 13:49:49 +0800

    c++/200-299/256_paint_house.cpp

--- c++/200-299/256_paint_house.cpp
+++ c++/200-299/256_paint_house.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using std::vector, std::min;
 
 class Solution {
 public:
     int minCost(const vector<vector<int>>& costs) {
-        vector<int> red(costs.size() + 1, 0);
-        vector<int> blue(costs.size() + 1, 0);
-        vector<int> green(costs.size() + 1, 0);
+        const size_t n = costs.size();
+        vector red(n + 1, 0);
+        vector blue(n + 1, 0);
+        vector green(n + 1, 0);
         red[1] = costs[0][0];
         blue[1] = costs[0][1];
         green[1] = costs[0][2];
 
-        return 4;
+        for (int i = 2; i <= n; i++) {
+            red[i] = min(blue[i-1], green[i-1]) + costs[i-1][0];
+            blue[i] = min(red[i-1], green[i-1]) + costs[i-1][1];
+            green[i] = min(red[i-1], blue[i-1]) + costs[i-1][2];
+        }
+
+        return min(red[n], blue[n], green[n]);
     }
 };
 
