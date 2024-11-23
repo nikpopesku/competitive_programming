f98f5b0ac49bc7a3ec03a1eed644ee9865a8106d
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-23 14:11:29 +0800

    c++/100-199/123_best_time_buy_stock3.cpp

---
+++ c++/100-199/123_best_time_buy_stock3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <vector>
+#include <climits>
+
+
+using std::cout, std::endl, std::vector, std::max;
+
+class Solution {
+public:
+    int maxProfit(const vector<int>& prices) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    cout << s.maxProfit({3,3,5,0,0,3,1,4}) << endl;
+}
