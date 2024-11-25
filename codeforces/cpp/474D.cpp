a37122c690b9d260e6c11379e4f983561ecd25f5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-25 16:18:20 +0800

    codeforces/cpp/474D.cpp

--- codeforces/cpp/474D.cpp
+++ codeforces/cpp/474D.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 int main()
 {
@@ -6,12 +9,21 @@ int main()
     int k;
     int min_flower;
     int max_flower;
+    vector dp (10001, 0);
 
     std::cin >> t >> k;
 
+    vector<int> response (t);
+
     for (int i = 0; i < t; i++) {
         std::cin >> min_flower >> max_flower;
+        response.push_back(6);
     }
+
+    for (const auto i: response) {
+        std::cout << i << ' ';
+    }
+
 
     return 0;
 }
