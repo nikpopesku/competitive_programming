7816322428e9c8e10e06e9f4879152618222f5e6
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-25 15:39:30 +0800

    codeforces/cpp/474D.cpp

--- codeforces/cpp/474D.cpp
+++ codeforces/cpp/474D.cpp
@@ -4,8 +4,14 @@ int main()
 {
     int t;
     int k;
+    int min_flower;
+    int max_flower;
 
     std::cin >> t >> k;
+
+    for (int i = 0; i < t; i++) {
+        std::cin >> min_flower >> max_flower;
+    }
 
     return 0;
 }
