aa277796f4f010955d6687e53bca5634dba2b434
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-27 18:33:36 +0800

    codeforces/cpp/1900-1999/1900B.cpp

--- codeforces/cpp/1900-1999/1900B.cpp
+++ codeforces/cpp/1900-1999/1900B.cpp
@@ -3,38 +3,40 @@
 
 using std::vector;
 
-int main()
-{
+int main() {
     int t;
     int a, b, c;
 
 
     std::cin >> t;
-    vector response (t, vector<int>(3, 0));
+    vector response(t, vector(3, 0));
 
     for (int i = 0; i < t; i++) {
         std::cin >> a >> b >> c;
 
         if (std::abs(b - c) % 2 == 0 and std::abs(b - c) / 2 < a) {
-          response[i][0] = 1;
+            response[i][0] = 1;
         }
 
         if (std::abs(a - c) % 2 == 0 and std::abs(a - c) / 2 < b) {
-          response[i][1] = 1;
+            response[i][1] = 1;
         }
 
         if (std::abs(a - b) % 2 == 0 and std::abs(a - b) / 2 < c) {
-          response[i][2] = 1;
+            response[i][2] = 1;
         }
-
     }
 
     for (auto &row: response) {
-      for (const auto &elem: row) {
-        std::cout << elem;
-      }
+        for (int i = 0; i < row.size(); i++) {
+            std::cout << row[i];
 
-      std::cout << std::endl;
+            if (i < row.size() - 1) {
+                std::cout << ' ';
+            }
+        }
+
+        std::cout << std::endl;
     }
 
 
