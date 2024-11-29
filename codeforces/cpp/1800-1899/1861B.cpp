b6ea16e7e6ad50f661d3c499c282ee22735f1ed7
npopesku <nikolay.popesku@unbiased.co.uk>
2024-11-29 17:34:09 +0800

    codeforces/cpp/1800-1899/1861B.cpp

--- codeforces/cpp/1800-1899/1861B.cpp
+++ codeforces/cpp/1800-1899/1861B.cpp
@@ -13,20 +13,24 @@ int main() {
 
     for (int i = 0; i < t; i++) {
         std::cin>>a>>b;
+        size_t most_right_zero_a = 0, most_right_zero_b = 0, most_left_one_a = a.size() - 1, most_left_one_b = b.size() - 1;
 
-        if (a == b) {
+        for (int j=1; j < a.size() - 1; j++) {
+            if (a[j] == '0' and j > most_right_zero_a) most_right_zero_a = j;
+            if (a[j] == '1' and j < most_left_one_a) most_left_one_a = j;
+            if (b[j] == '0' and j > most_right_zero_b) most_right_zero_b = j;
+            if (b[j] == '1' and j < most_left_one_b) most_left_one_b = j;
+        }
+
+        if (most_right_zero_a == most_right_zero_b or most_left_one_a == most_left_one_b) {
             response[i] = "YES"s;
         }
     }
 
 
     for (int j = 0; j < response.size(); j++) {
-        std::cout << response[j];
-
-        if (j < response.size() - 1) std::cout << ' ';
+        std::cout << response[j] << std::endl;
     }
-
-    std::cout << std::endl;
 
     return 0;
 }
