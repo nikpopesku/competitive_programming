e6b3ce34918cbd60c14650bd26c4a52e084f2b53
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-03 14:23:46 +0800

    codeforces/cpp/2000-2099/2025A.cpp

---
+++ codeforces/cpp/2000-2099/2025A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (auto i = 0; i < t; i++) {
+        std::string number;
+        std::cin >> number;
+        std::vector<int> numbers{};
+
+        for (const char digit: number) {
+            if (digit == '1') {
+                puts("13");
+                break;
+            }
+            if (digit == '3') {
+                puts("31");
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
