b561fb47eb81ac67417459b4c0cb1ffa77d6f23f
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-03 12:52:42 +0800

    codeforces/cpp/1800-1899/1861A.cpp

--- codeforces/cpp/1800-1899/1861A.cpp
+++ codeforces/cpp/1800-1899/1861A.cpp
@@ -18,14 +18,10 @@ std::vector<std::string> get_numbers(const std::string& number, const int start,
     return response;
 }
 
-std::string find(const std::array<int, 999999999> primes, const std::string& number) {
-
-}
-
 int main() {
     int t;
     std::cin >> t;
-    std::array<int, 999999999> primes{};
+    std::array<int, 9999> primes{};
 
     for (auto i = 0; i < primes.size(); i++) {
         primes[i] = i;
@@ -34,32 +30,33 @@ int main() {
     for (auto i = 2; i < primes.size(); i++) {
         if (primes[i] == -1) continue;
 
-        const auto number = primes[i] + primes[i];
+        auto number = primes[i] + primes[i];
 
         while (number < primes.size()) {
             primes[number] = -1;
+            number = primes[i];
         }
     }
 
-    for (auto i = 0; i < t; i++) {
-        std::string number;
-        std::cin >> number;
-        std::vector<int> numbers {};
-
-        for (int left = 2; left < 10; left++) {
-            for (auto start = 0; start < 9 - left; start++) {
-                for (auto &elem: get_numbers(number, start, left)) {
-                    numbers.push_back(std::stoi(elem));
-                }
-            }
-        }
-
-        for (auto &elem: numbers) {
-            if (primes[elem] != -1) {
-                puts(reinterpret_cast<const char *>(static_cast<char>(elem)));
-            }
-        }
-    }
+    // for (auto i = 0; i < t; i++) {
+    //     std::string number;
+    //     std::cin >> number;
+    //     std::vector<int> numbers {};
+    //
+    //     for (int left = 2; left < 10; left++) {
+    //         for (auto start = 0; start < 9 - left; start++) {
+    //             for (auto &elem: get_numbers(number, start, left)) {
+    //                 numbers.push_back(std::stoi(elem));
+    //             }
+    //         }
+    //     }
+    //
+    //     for (auto &elem: numbers) {
+    //         if (primes[elem] != -1) {
+    //             puts(reinterpret_cast<const char *>(static_cast<char>(elem)));
+    //         }
+    //     }
+    // }
 
     return 0;
 }
