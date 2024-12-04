1b41b3c3609999da6d1742a7079280bd80e19434
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-04 16:50:45 +0800

    codeforces/cpp/1400-1499/1476A.cpp

--- codeforces/cpp/1800-1899/1809B.cpp
+++ codeforces/cpp/1800-1899/1809B.cpp
@@ -13,11 +13,13 @@ int main() {
 
         if (n == 1)  {
             std::cout << 0 << std::endl;
-        } else if (n <= 6) {
-            std::cout << 2 << std::endl;
+        } else if (n <= 4) {
+            std::cout << 1 << std::endl;
+        } else if (n <= 10) {
+            std::cout << 3 << std::endl;
         } else {
-            const long long k = sqrt(n - 3) - 2;
-            std::cout << 2 * (k + 1) << std::endl;
+            const long long k = (sqrt(9 + 4 * n) - 5) / 2;
+            std::cout << k + 3 << std::endl;
         }
 
 
