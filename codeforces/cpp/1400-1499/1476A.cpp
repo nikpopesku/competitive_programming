0c7fdae910dcc94299e7845b4f550c34f0f8ee85
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-04 14:39:44 +0800

    codeforces/cpp/1400-1499/1476A.cpp

--- codeforces/cpp/1400-1499/1476A.cpp
+++ codeforces/cpp/1400-1499/1476A.cpp
@@ -8,6 +8,19 @@ int main() {
         int n, k;
         std::cin >> n >> k;
 
+        const int initial_k = k;
+        while (k < n) {
+            k += initial_k;
+        }
+
+        int start = 1;
+        while (start <= k) {
+            if (n * start >= k) {
+                std::cout << start << std::endl;
+                break;
+            }
+            start++;
+        }
     }
 
     return 0;
