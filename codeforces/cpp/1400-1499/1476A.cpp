ecaabc71d9455450e1767f40906655783638b890
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-04 14:24:33 +0800

    codeforces/cpp/1400-1499/1476A.cpp

---
+++ codeforces/cpp/1400-1499/1476A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (auto i = 0; i < t; i++) {
+        int n, k;
+        std::cin >> n >> k;
+
+    }
+
+    return 0;
+}
