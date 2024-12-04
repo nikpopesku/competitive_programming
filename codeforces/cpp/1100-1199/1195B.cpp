f2f3ef05d231896e21a3ecccfba3585afdd55aa3
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-04 13:36:41 +0800

    codeforces/cpp/1100-1199/1195B.cpp

--- codeforces/cpp/1100-1199/1195B.cpp
+++ codeforces/cpp/1100-1199/1195B.cpp
@@ -5,10 +5,9 @@ int main() {
     int n, k;
     std::cin >> n >> k;
 
-    const int x = (sqrt(8*k + 1) - 1) / 2;
-    const int response = n - x;
+    const int x = (sqrt(8*(k + n) + 9) - 3) / 2;
 
-    std::cout << response << std::endl;
+    std::cout << n - x << std::endl;
 
     return 0;
 }
