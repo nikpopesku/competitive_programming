598763a3f5de3cc8846b1ec4596ca4cc75a539aa
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-08 17:36:59 +0800

    codeforces/cpp/2000-2099/2009C.cpp

---
+++ codeforces/cpp/2000-2099/2009C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (auto i = 0; i < t; i++) {
+        int x, y, k;
+        std::cin >> x >> y >> k;
+
+        if (y >= x) {
+            int steps = y / k;
+            if (y % k) steps += 1;
+            std::cout << steps * 2 << std::endl;
+        } else {
+            int steps = x / k;
+            if (x % k) steps += 1;
+            std::cout << 2 * x - 1 << std::endl;
+        }
+    }
+
+    return 0;
+}
