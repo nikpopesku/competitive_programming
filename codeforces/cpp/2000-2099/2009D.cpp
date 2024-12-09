d227262cc1d4175275a7bf7ecfb8d6fbd76f298f
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-09 17:13:48 +0800

    codeforces/cpp/2000-2099/2009D.cpp

--- codeforces/cpp/2000-2099/2009D.cpp
+++ codeforces/cpp/2000-2099/2009D.cpp
@@ -32,26 +32,26 @@ int main() {
         std::sort(points1.begin(), points1.end(), comp);
         std::sort(points0.begin(), points0.end(), comp);
 
-        long long counter = 0;
-        for (auto k1 = 0; k1 < points.size() - 2; k1++) {
-            for (auto k2 = k1 + 1; k2 < points.size() - 1; k2++) {
-                for (auto k3 = k2 + 1; k3 < points.size(); k3++) {
-                    double line1 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k2]), 2) + std::pow(
-                                       std::get<1>(points[k1]) - std::get<1>(points[k2]), 2);
-                    double line2 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k3]), 2) + std::pow(
-                                       std::get<1>(points[k1]) - std::get<1>(points[k3]), 2);
-                    double line3 = std::pow(std::get<0>(points[k2]) - std::get<0>(points[k3]), 2) + std::pow(
-                                       std::get<1>(points[k2]) - std::get<1>(points[k3]), 2);
-                    const double max_elem = std::max(std::max(line1, line2), line3);
-                    const double min_elem = std::min(std::min(line1, line2), line3);
-                    if (max_elem - min_elem == line1 or max_elem - min_elem == line2 or max_elem - min_elem == line3) {
-                        counter += 1;
-                    }
-                }
-            }
-        }
-
-        std::cout << counter << std::endl;
+        // long long counter = 0;
+        // for (auto k1 = 0; k1 < points.size() - 2; k1++) {
+        //     for (auto k2 = k1 + 1; k2 < points.size() - 1; k2++) {
+        //         for (auto k3 = k2 + 1; k3 < points.size(); k3++) {
+        //             double line1 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k2]), 2) + std::pow(
+        //                                std::get<1>(points[k1]) - std::get<1>(points[k2]), 2);
+        //             double line2 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k3]), 2) + std::pow(
+        //                                std::get<1>(points[k1]) - std::get<1>(points[k3]), 2);
+        //             double line3 = std::pow(std::get<0>(points[k2]) - std::get<0>(points[k3]), 2) + std::pow(
+        //                                std::get<1>(points[k2]) - std::get<1>(points[k3]), 2);
+        //             const double max_elem = std::max(std::max(line1, line2), line3);
+        //             const double min_elem = std::min(std::min(line1, line2), line3);
+        //             if (max_elem - min_elem == line1 or max_elem - min_elem == line2 or max_elem - min_elem == line3) {
+        //                 counter += 1;
+        //             }
+        //         }
+        //     }
+        // }
+        //
+        // std::cout << counter << std::endl;
     }
 
     return 0;
