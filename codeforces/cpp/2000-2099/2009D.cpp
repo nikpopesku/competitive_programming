82eaa3383ff86b781249e28bde150231f14ad595
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-09 17:35:59 +0800

    codeforces/cpp/2000-2099/2009D.cpp

--- codeforces/cpp/2000-2099/2009D.cpp
+++ codeforces/cpp/2000-2099/2009D.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
-#include <cmath>
 #include <bits/stdc++.h>
 
 bool comp(const std::tuple<int, int> &a, const std::tuple<int, int> &b) {
@@ -17,7 +15,8 @@ int main() {
         std::cin >> n;
         std::vector<std::tuple<int, int>> points0{};
         std::vector<std::tuple<int, int>> points1{};
-        std::unordered_map<std::tuple<int, int>, int> points {};
+
+        std::unordered_set<std::string> points;
         for (auto j = 0; j < n; j++) {
             int x, y;
             std::cin >> x >> y;
@@ -26,32 +25,32 @@ int main() {
             } else {
                 points0.emplace_back(x, y);
             }
-            points.insert({std::tuple(x, y), 1});
+            points.insert(std::to_string(x) + "_" + std::to_string(y));
         }
 
-        std::sort(points1.begin(), points1.end(), comp);
-        std::sort(points0.begin(), points0.end(), comp);
+        std::ranges::sort(points1, comp);
+        std::ranges::sort(points0, comp);
 
-        // long long counter = 0;
-        // for (auto k1 = 0; k1 < points.size() - 2; k1++) {
-        //     for (auto k2 = k1 + 1; k2 < points.size() - 1; k2++) {
-        //         for (auto k3 = k2 + 1; k3 < points.size(); k3++) {
-        //             double line1 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k2]), 2) + std::pow(
-        //                                std::get<1>(points[k1]) - std::get<1>(points[k2]), 2);
-        //             double line2 = std::pow(std::get<0>(points[k1]) - std::get<0>(points[k3]), 2) + std::pow(
-        //                                std::get<1>(points[k1]) - std::get<1>(points[k3]), 2);
-        //             double line3 = std::pow(std::get<0>(points[k2]) - std::get<0>(points[k3]), 2) + std::pow(
-        //                                std::get<1>(points[k2]) - std::get<1>(points[k3]), 2);
-        //             const double max_elem = std::max(std::max(line1, line2), line3);
-        //             const double min_elem = std::min(std::min(line1, line2), line3);
-        //             if (max_elem - min_elem == line1 or max_elem - min_elem == line2 or max_elem - min_elem == line3) {
-        //                 counter += 1;
-        //             }
-        //         }
-        //     }
-        // }
-        //
-        // std::cout << counter << std::endl;
+        long long counter = 0;
+        for (auto k1 = 0; k1 < points0.size() - 1; k1++) {
+            for (auto k2 = k1 + 1; k2 < points0.size(); k2++) {
+                const int x1 = std::get<0>(points1[k1]);
+                const int x2 = std::get<0>(points1[k2]);
+                if (auto got = points.find(std::to_string(x1) + "_1"); got != points.end()) {
+                    counter += 1;
+                }
+                if (auto got = points.find(std::to_string(x2) + "_1"); got != points.end()) {
+                    counter += 1;
+                }
+                if (x2 == x1 + 4) {
+                    if (auto got = points.find(std::to_string(x1 + 2) + "_1"); got != points.end()) {
+                        counter += 1;
+                    }
+                }
+            }
+        }
+
+        std::cout << counter << std::endl;
     }
 
     return 0;
