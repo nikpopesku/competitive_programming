a26d9b177b88f4af74373cf4c49b20968f4e76dd
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-18 21:52:53 +0800

    codeforces/cpp/2000-2099/2009E.cpp

--- codeforces/cpp/2000-2099/2009E.cpp
+++ codeforces/cpp/2000-2099/2009E.cpp
@@ -5,12 +5,12 @@ long long calc(int k, int m, int n) {
     long long s2 = k + n - 1;
 
     if (m > k) {
-        s1 = (k + m) * m / 2;
+        s1 = (k + m) * (m - k + 1) / 2;
     }
 
 
     if (m + 1 < k + n - 1) {
-        s2 = (m + 1 + k + n - 1) * (k + n - 2 - m) / 2;
+        s2 = (m + 1 + k + n - 1) * (k + n - 1 - m) / 2;
     }
 
     return std::abs(s1 - s2);
@@ -26,19 +26,21 @@ int main() {
 
         int left = k;
         int right = k + n - 1;
-        int m;
+        int m = left;
 
         while (left + 1 < right) {
-            m = left + (right - left) / 2;
+            m = left + (right - left - 1) / 2;
 
-            if (calc(k, m, n) > calc(k, m-1, n)) {
+            if (calc(k, m, n) > calc(k, m+1, n)) {
+                left = m;
+            } else if (calc(k, m, n) < calc(k, m-1, n)) {
                 right = m;
             } else {
-                left = m + 1;
+                break;
             }
         }
 
-        std::cout << std::abs(calc(k, left, n)) << std::endl;
+        std::cout << std::abs(calc(k, m, n)) << std::endl;
     }
 
     return 0;
