7243a3231b6dfcf348e0b5627c34cfc23ca0f1b1
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-19 14:11:47 +0800

    codeforces/cpp/1800-1899/1850D.cpp

--- codeforces/cpp/1800-1899/1850D.cpp
+++ codeforces/cpp/1800-1899/1850D.cpp
@@ -1,18 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
-
-template<typename T>
-typename std::vector<T>::iterator
-insert_sorted(std::vector<T> &vec, T const &item) {
-    return vec.insert
-            (
-                    std::upper_bound(vec.begin(), vec.end(), item),
-                    item
-            );
-}
-
+#include <queue>
 
 int main() {
     int t, n, k;
@@ -23,27 +10,30 @@ int main() {
         std::cin >> n >> k;
 
         int problem;
-        std::vector<int> numbers;
+        std::priority_queue<int> pq;
 
         for (int j = 0; j < n; j++) {
             std::cin >> problem;
-            insert_sorted(numbers, problem);
+            pq.push(problem);
         }
 
         int largest_group = 1;
         int current_group = 1;
-        for (int j = 1; j < numbers.size(); j++) {
-            if (numbers[j] - numbers[j - 1] <= k) {
+        int previous_number = pq.top();
+        pq.pop();
+        while (!pq.empty()) {
+            if (previous_number - pq.top() <= k) {
                 current_group += 1;
             } else {
                 largest_group = std::max(largest_group, current_group);
                 current_group = 1;
             }
+            previous_number = pq.top();
+            pq.pop();
         }
 
         largest_group = std::max(largest_group, current_group);
-
-        std::cout << numbers.size() - largest_group << std::endl;
+        std::cout << n - largest_group << std::endl;
     }
 
     return 0;
