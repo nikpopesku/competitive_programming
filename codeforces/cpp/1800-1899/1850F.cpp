21cd0ee35924b06a1980255cffdf5da1e9e422c5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-21 21:55:05 +0800

    codeforces/cpp/1800-1899/1850F.cpp

--- codeforces/cpp/1800-1899/1850F.cpp
+++ codeforces/cpp/1800-1899/1850F.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <algorithm>
 
 int main() {
     int t;
-    long long n;
-    long long c;
+    int n;
+
 
     std::cin >> t;
 
     for (int i = 0; i < t; i++) {
-        std::cin >> n >> c;
-        std::vector<long long> sides (n);
-        for (int j = 0; j < n; j++) std::cin >> sides[j];
+        std::cin >> n;
 
-        long long left = 1;
-        long long right = 1e9;
+        std::vector multipliers (n+1, std::vector<int> {});
 
-
-        while (left <= right) {
-            long long w = left + (right - left) / 2;
-            long long sum = 0;
-
-            for (int j = 0; j < n; j++) {
-                sum += (sides[j] + 2 * w) * (sides[j] + 2 * w);
-
-                if (sum > c) break;
-            }
-
-
-            if (sum == c) {
-                std::cout << w << std::endl;
-                break;
-            } else if (sum > c) {
-                right = w - 1;
-            } else {
-                left = w + 1;
+        for (int j = 1; j < n; j++) {
+            int value = j;
+            while (value <= n) {
+                multipliers[value].push_back(value);
+                value += j;
             }
         }
+
+        int leap;
+        std::vector leaps (n+1, 0);
+
+        for (int j = 0; j < n; j++) {
+            std::cin >> leap;
+
+            if (leap > n) {
+                continue;
+            }
+
+            for (auto & elem: multipliers[leap]) {
+                leaps[elem] += 1;
+            }
+        }
+
+        std::cout << *std::max_element(leaps.begin(), leaps.end()) << std::endl;
     }
 
     return 0;
