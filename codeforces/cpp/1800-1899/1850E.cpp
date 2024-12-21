431c0aecfa469102584dfc5c8ef1e8873f5fa438
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-21 17:45:10 +0800

    codeforces/cpp/1800-1899/1850E.cpp

--- codeforces/cpp/1800-1899/1850E.cpp
+++ codeforces/cpp/1800-1899/1850E.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <climits>
 
 int main() {
     int t;
@@ -12,18 +13,32 @@ int main() {
         std::cin >> n >> c;
 
         long long sum = 0;
-        long long double_sum = 0;
 
         for (int j = 0; j < n; j++) {
             std::cin >> side;
 
             sum += side;
-            double_sum += side * side;
+            c -= side * side;
+
+            long long left = 1;
+            long long right = 100;
+
+            while (left <= right) {
+                long long w = left + (right - left) / 2;
+
+                long long expression = 4 * n * w * w + 4 * sum * w;
+
+                if (expression == c) {
+                    std::cout << w << std::endl;
+
+                    break;
+                } else if (expression > c) {
+                    right = w - 1;
+                } else {
+                    left = w + 1;
+                }
+            }
         }
-
-        long long response = (-sum + sqrt(sum * sum + n * (c - double_sum))) / (2 * n);
-
-        std::cout << response << std::endl;
     }
 
     return 0;
