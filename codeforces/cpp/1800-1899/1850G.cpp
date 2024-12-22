7144ca652e0e5fa99bc203b72be1b94d6b19055e
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 20:47:36 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
 
 int main() {
     int t;
@@ -13,43 +12,16 @@ int main() {
 
         std::vector points_x(n, 0);
         std::vector points_y(n, 0);
-        std::unordered_map<int, int> x{};
-        std::unordered_map<int, int> y{};
-
 
         int response = 0;
 
         for (int j = 0; j < n; j++) {
             std::cin >> points_x[j] >> points_y[j];
-
-            if (auto value = x.find(points_x[j]); value != x.end()) {
-                x[value->first] += 1;
-            } else {
-                x[points_x[j]] = 1;
-            }
-
-            if (auto value = x.find(points_y[j]); value != y.end()) {
-                y[value->first] += 1;
-            } else {
-                y[points_y[j]] = 1;
-            }
-        }
-
-        for (auto &elem: x) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
-            }
-        }
-
-        for (auto &elem: y) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
-            }
         }
 
         for (int j = 0; j < points_x.size() - 1; j++) {
             for (int k = j + 1; k < points_x.size(); k++) {
-                if (points_y[k] != points_y[j] and std::abs(points_x[j] - points_x[k]) == std::abs(points_y[j] - points_y[k])) {
+                if (std::abs(points_x[j] - points_x[k]) == std::abs(points_y[j] - points_y[k]) or points_x[j] == points_x[k] or points_y[j] == points_y[k]) {
                     response += 2;
                 }
             }
