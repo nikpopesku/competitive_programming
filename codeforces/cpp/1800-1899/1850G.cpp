6fc18cc993ef80eba242bb319bc0c1625b9e67de
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 21:52:44 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -25,39 +25,38 @@ int main() {
         for (int j = 0; j < n; j++) {
             std::cin >> x >> y;
 
-            if (same_x.find(x) != same_x.end()) {
-                same_x[x] += 1;
-                value = same_x[x];
-                response += value * (value - 1) - (value - 1) * (value - 2);
-            } else {
-                same_x[x] = 1;
+            if (same_x.find(x) == same_x.end()) {
+                same_x[x] = 0;
             }
+            same_x[x] += 1;
+            value = same_x[x];
+            response += value * (value - 1) - (value - 1) * (value - 2);
 
-            if (same_y.find(y) != same_y.end()) {
-                same_y[y] += 1;
-                value = same_y[y];
-                response += value * (value - 1) - (value - 1) * (value - 2);
-            } else {
-                same_y[y] = 1;
+
+            if (same_y.find(y) == same_y.end()) {
+                same_y[y] = 0;
             }
+            same_y[y] += 1;
+            value = same_y[y];
+            response += value * (value - 1) - (value - 1) * (value - 2);
+
 
             coordinate = x - y;
-            if (positive_45.find(coordinate) != positive_45.end()) {
-                positive_45[coordinate] += 1;
-                value = positive_45[coordinate];
-                response += value * (value - 1) - (value - 1) * (value - 2);
-            } else {
-                positive_45[coordinate] = 1;
+            if (positive_45.find(coordinate) == positive_45.end()) {
+                positive_45[coordinate] = 0;
             }
+            positive_45[coordinate] += 1;
+            value = positive_45[coordinate];
+            response += value * (value - 1) - (value - 1) * (value - 2);
+
 
             coordinate = x + y;
-            if (negative_45.find(coordinate) != negative_45.end()) {
-                negative_45[coordinate] += 1;
-                value = negative_45[coordinate];
-                response += value * (value - 1) - (value - 1) * (value - 2);
-            } else {
-                negative_45[coordinate] = 1;
+            if (negative_45.find(coordinate) == negative_45.end()) {
+                negative_45[coordinate] = 0;
             }
+            negative_45[coordinate] += 1;
+            value = negative_45[coordinate];
+            response += value * (value - 1) - (value - 1) * (value - 2);
         }
 
         std::cout << response << std::endl;
