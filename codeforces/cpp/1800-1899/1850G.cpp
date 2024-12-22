67d3921ae901121d0ad00f37d62012bd835946c5
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 16:50:35 +0800

    codeforces/cpp/1800-1899/1850G.cpp

---
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int t;
+    int n;
+    int x, y;
+
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        std::cin >> n;
+
+
+        for (int j = 0; j < n; j++) {
+            std::cin >> x >> y;
+        }
+    }
+
+    return 0;
+}
