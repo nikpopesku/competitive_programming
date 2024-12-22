55516d77f266db4ddb62c32daab1f4ab05a5576e
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 21:54:20 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -25,35 +25,23 @@ int main() {
         for (int j = 0; j < n; j++) {
             std::cin >> x >> y;
 
-            if (same_x.find(x) == same_x.end()) {
-                same_x[x] = 0;
-            }
             same_x[x] += 1;
             value = same_x[x];
             response += value * (value - 1) - (value - 1) * (value - 2);
 
 
-            if (same_y.find(y) == same_y.end()) {
-                same_y[y] = 0;
-            }
             same_y[y] += 1;
             value = same_y[y];
             response += value * (value - 1) - (value - 1) * (value - 2);
 
 
             coordinate = x - y;
-            if (positive_45.find(coordinate) == positive_45.end()) {
-                positive_45[coordinate] = 0;
-            }
             positive_45[coordinate] += 1;
             value = positive_45[coordinate];
             response += value * (value - 1) - (value - 1) * (value - 2);
 
 
             coordinate = x + y;
-            if (negative_45.find(coordinate) == negative_45.end()) {
-                negative_45[coordinate] = 0;
-            }
             negative_45[coordinate] += 1;
             value = negative_45[coordinate];
             response += value * (value - 1) - (value - 1) * (value - 2);
