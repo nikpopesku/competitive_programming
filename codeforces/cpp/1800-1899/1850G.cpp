df19c3b3f1d88f839b7c4fd25780f372aae0ed55
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 21:34:36 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -25,50 +25,47 @@ int main() {
 
             if (auto value = same_x.find(x); value != same_x.end()) {
                 same_x[value->first] += 1;
+
+                response += same_x[value->first] * (same_x[value->first] - 1);
+                if (same_x[value->first] > 2) {
+                    response -= (same_x[value->first] - 1) * (same_x[value->first] - 2);
+                }
             } else {
                 same_x[x] = 1;
             }
 
             if (auto value = same_y.find(y); value != same_y.end()) {
                 same_y[value->first] += 1;
+
+                response += same_y[value->first] * (same_y[value->first] - 1);
+                if (same_y[value->first] > 2) {
+                    response -= (same_y[value->first] - 1) * (same_y[value->first] - 2);
+                }
             } else {
                 same_y[y] = 1;
             }
 
             if (auto value = positive_45.find(x - y); value != positive_45.end()) {
                 positive_45[value->first] += 1;
+
+
+                response += positive_45[value->first] * (positive_45[value->first] - 1);
+                if (positive_45[value->first] > 2) {
+                    response -= (positive_45[value->first] - 1) * (positive_45[value->first] - 2);
+                }
             } else {
                 positive_45[x - y] = 1;
             }
 
             if (auto value = negative_45.find(x + y); value != negative_45.end()) {
                 negative_45[value->first] += 1;
+
+                response += negative_45[value->first] * (negative_45[value->first] - 1);
+                if (negative_45[value->first] > 2) {
+                    response -= (negative_45[value->first] - 1) * (negative_45[value->first] - 2);
+                }
             } else {
                 negative_45[x + y] = 1;
-            }
-        }
-
-        for (auto &elem: same_x) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
-            }
-        }
-
-        for (auto &elem: same_y) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
-            }
-        }
-
-        for (auto &elem: positive_45) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
-            }
-        }
-
-        for (auto &elem: negative_45) {
-            if (elem.second > 1) {
-                response += elem.second * (elem.second - 1);
             }
         }
 
