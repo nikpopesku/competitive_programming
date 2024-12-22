3d26da04a8ede09cf064b69b3c1d6d326072c527
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 22:04:11 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -5,19 +5,13 @@
 int main() {
     int t;
     int n;
-
+    int x, y;
     std::cin >> t;
 
     for (int i = 0; i < t; i++) {
         std::cin >> n;
 
-        int x, y;
-        std::unordered_map<int, long long> same_x{};
-        std::unordered_map<int, long long> same_y{};
-        std::unordered_map<int, long long> positive_45{};
-        std::unordered_map<int, long long> negative_45{};
-
-
+        std::unordered_map<int, long long> same_x, same_y, positive_45, negative_45;
         long long response = 0;
 
         for (int j = 0; j < n; j++) {
@@ -27,14 +21,12 @@ int main() {
             same_y[y] += 1;
             positive_45[x - y] += 1;
             negative_45[x + y] += 1;
-
-            response += same_x[x] * (same_x[x] - 1) - (same_x[x] - 1) * (same_x[x] - 2) +
-                        same_y[y] * (same_y[y] - 1) - (same_y[y] - 1) * (same_y[y] - 2) +
-                        positive_45[x - y] * (positive_45[x - y] - 1) -
-                        (positive_45[x - y] - 1) * (positive_45[x - y] - 2) +
-                        negative_45[x + y] * (negative_45[x + y] - 1) -
-                        (negative_45[x + y] - 1) * (negative_45[x + y] - 2);
         }
+
+        for (auto &elem: same_x) response += elem.second * (elem.second - 1);
+        for (auto &elem: same_y) response += elem.second * (elem.second - 1);
+        for (auto &elem: positive_45) response += elem.second * (elem.second - 1);
+        for (auto &elem: negative_45) response += elem.second * (elem.second - 1);
 
         std::cout << response << std::endl;
     }
