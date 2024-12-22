b7e2be200fb2c853170fe5f7270e118623932218
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-22 22:07:10 +0800

    codeforces/cpp/1800-1899/1850G.cpp

--- codeforces/cpp/1800-1899/1850G.cpp
+++ codeforces/cpp/1800-1899/1850G.cpp
@@ -11,8 +11,8 @@ int main() {
     for (int i = 0; i < t; i++) {
         std::cin >> n;
 
-        std::unordered_map<int, long long> same_x, same_y, positive_45, negative_45;
-        long long response = 0;
+        std::unordered_map<int, int> same_x, same_y, positive_45, negative_45;
+        int response = 0;
 
         for (int j = 0; j < n; j++) {
             std::cin >> x >> y;
