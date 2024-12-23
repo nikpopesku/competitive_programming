9211a33fd1f1e515064141e3ae25e1fefbf14f34
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-23 12:17:03 +0800

    codeforces/cpp/1800-1899/1850H.cpp

--- codeforces/cpp/1800-1899/1850H.cpp
+++ codeforces/cpp/1800-1899/1850H.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <map>
+#include <unordered_map>
 
 int main() {
     int t;
@@ -9,16 +8,29 @@ int main() {
     std::cin >> t;
 
     for (auto i = 0; i < t; i++) {
-        std::cin >> n;
+        std::cin >> n >> m;
 
+        std::string response = "YES";
+        std::unordered_map<int, long long> places;
 
-        for (auto j = 0; j < n; j++) {
-            std::cin >> n >> m;
+        for (auto k = 0; k < m; k++) {
+            std::cin >> a >> b >> d;
 
-            for (auto k = 0; k < m; k++) {
-                std::cin >> a >> b >> d;
+            auto it_a = places.find(a);
+            auto it_b = places.find(b);
+            if (it_a == places.end() and it_b == places.end()) {
+                places[a] = 1;
+                places[b] = 1 + d;
+            } else if (it_a != places.end() and it_b == places.end()) {
+                places[b] = 1 + d;
+            } else if (it_a->second + d != it_b->second) {
+                response = "NO";
+
+                break;
             }
         }
+
+        std::cout << response << std::endl;
     }
 
     return 0;
