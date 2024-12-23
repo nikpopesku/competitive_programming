04f5ceddc0d6dd5ef8e9570704fb71a3d9668a9d
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-23 11:57:45 +0800

    codeforces/cpp/1800-1899/1850H.cpp

---
+++ codeforces/cpp/1800-1899/1850H.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+int main() {
+    int t;
+    int n, m;
+    std::cin >> t;
+
+    for (long long i = 0; i < t; i++) {
+        std::cin >> n;
+
+
+        for (long long j = 0; j < n; j++) {
+            std::cin >> n >> m;
+
+        }
+    }
+
+    return 0;
+}
