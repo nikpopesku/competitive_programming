50d8ede28763d570722a6e48d0d82d07a6a895b2
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-24 15:36:26 +0800

    codeforces/cpp/1800-1899/1850H.cpp

--- codeforces/cpp/1800-1899/1850H.cpp
+++ codeforces/cpp/1800-1899/1850H.cpp
@@ -1,74 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include "bits/stdc++.h"
+using namespace std;
 
+#define ll long long
+
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
+
+#define int long long
 const int N = 2e5 + 5;
-
-
-std::vector<std::pair<int, int>> adj[N];
-std::vector<int> val(N);
-std::vector<bool> visited(N, false);
-
+vector<pair<int, int>> adj[N];
+int val[N], vis[N];
 void dfs(int u) {
-    visited[u] = true;
-
-    for (auto &p: adj[u]) {
-        int b = p.first;
-        int d = p.second;
-
-        if (!visited[b]) {
-            val[b] = val[u] + d;
-            dfs(b);
+    vis[u] = 1;
+    for(auto x: adj[u]) {
+        int v = x.first, w = x.second;
+        if(!vis[v]) {
+            val[v] = val[u] + w;
+            dfs(v);
         }
     }
 }
-
-
-int main() {
-    int t;
-    int n, m;
-    int a, b, d;
-    std::cin >> t;
-
-    for (auto i = 0; i < t; i++) {
-        std::cin >> n >> m;
-
-        std::vector c(n + 1, std::vector<int>(3, 0));
-
-
-        for (int j = 0; j < N; j++) {
-            val[j] = 0;
-            visited[j] = false;
-            adj[j].clear();
-        }
-
-        std::string response = "YES";
-
-        for (auto k = 0; k < m; k++) {
-            std::cin >> a >> b >> d;
-            adj[a].emplace_back(b, d);
-            adj[b].emplace_back(a, -d);
-            c.push_back({a, b, d});
-        }
-
-        for (int j = 1; j <= n; j++) {
-            if (!visited[j]) dfs(j);
-        }
-
-        for (auto &j: c) {
-            a = val[j[0]];
-            b = val[j[1]];
-            d = j[2];
-
-            if (a + d != b) {
-                response = "NO";
-
-                break;
-            }
-        }
-
-        std::cout << response << std::endl;
+void solve() {
+    int n, m; cin >> n >> m;
+    for(int i = 1; i <= n; ++i) {
+        adj[i].clear();
+        vis[i] = 0, val[i] = 0;
     }
+    vector<array<int, 3>> c;
+    for(int i = 1; i <= m; ++i) {
+        int a, b, d; cin >> a >> b >> d;
+        adj[a].pb({b, d});
+        adj[b].pb({a, -d});
+        c.pb({a, b, d});
+    }
+    for(int i = 1; i <= n; ++i) {
+        if(!vis[i]) dfs(i);
+    }
+    for(int i = 1; i <= m; ++i) {
+        int a = c[i - 1][0], b = c[i - 1][1], d = c[i - 1][2];
+        if(val[a] + d != val[b]) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
 
-    return 0;
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
 }
