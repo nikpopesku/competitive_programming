f579ec0c951968ebd0b41a4693824d5c46718d97
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-24 17:47:19 +0800

    codeforces/cpp/2000-2099/2009F.cpp

---
+++ codeforces/cpp/2000-2099/2009F.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (auto i = 0; i < t; i++) {
+        int n, q;
+        std::cin >> n >> q;
+
+    }
+
+    return 0;
+}
