4c4d1d9fe447b84297e792eec6b1c57d9886ab9c
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-26 16:21:39 +0800

    codeforces/cpp/2000-2099/2009F.cpp

--- codeforces/cpp/2000-2099/2009F.cpp
+++ codeforces/cpp/2000-2099/2009F.cpp
@@ -24,8 +24,8 @@ int main() {
         for (auto j = 0; j < q; j++) {
             std::cin >> l >> r;
 
-            long long rotate_left = l / n;
-            long long shift_left = l % n;
+            long long rotate_left = (l - 1) / n;
+            long long shift_left = (l - 1) % n;
 
             long long rotate_right = r / n;
             long long shift_right = r % n;
@@ -33,15 +33,13 @@ int main() {
 
             long long sum_shift_left = 0;
 
-            if (shift_left > 0) {
-                std::rotate(elems.begin(), elems.begin() + rotate_left, elems.end());
+            std::rotate(elems.begin(), elems.begin() + rotate_left, elems.end());
 
-                for (auto k = 0; k < shift_left - 1; k++) {
-                    sum_shift_left += elems[k];
-                }
-
-                std::rotate(elems.rbegin(), elems.rbegin() + rotate_left, elems.rend());
+            for (auto k = 0; k < shift_left; k++) {
+                sum_shift_left += elems[k];
             }
+
+            std::rotate(elems.rbegin(), elems.rbegin() + rotate_left, elems.rend());
 
 
 
