a05c357dcdda69eee538326756143cd73c5ac712
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-26 15:45:06 +0800

    codeforces/cpp/2000-2099/2009F.cpp

--- codeforces/cpp/2000-2099/2009F.cpp
+++ codeforces/cpp/2000-2099/2009F.cpp
@@ -12,7 +12,6 @@ int main() {
 
         std::vector<long long> elems(n, 0);
         std::vector<long long> partial_sum(n * n + 1, 0);
-        int input;
 
         for (auto j = 0; j < n; j++) {
             std::cin >> elems[j];
@@ -22,7 +21,7 @@ int main() {
             int step = j % n;
 
             for (auto k = 1; k <= n; k++) {
-                partial_sum[step * n + k] = partial_sum[step * n + k - 1] + elems[k-1];
+                partial_sum[step * n + k] = partial_sum[step * n + k - 1] + elems[k - 1];
             }
 
             std::rotate(elems.begin(), elems.begin() + 1, elems.end());
@@ -33,7 +32,7 @@ int main() {
         for (auto j = 0; j < q; j++) {
             std::cin >> l >> r;
 
-            std::cout << partial_sum[r] - partial_sum[l-1] << std::endl;
+            std::cout << partial_sum[r] - partial_sum[l - 1] << std::endl;
         }
 
     }
