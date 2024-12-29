da6332022066826f73bf313b57b9832fadab64bb
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-29 20:44:38 +0800

    codeforces/cpp/1900-1999/1999D.cpp

--- codeforces/cpp/1900-1999/1999D.cpp
+++ codeforces/cpp/1900-1999/1999D.cpp
@@ -12,16 +12,27 @@ int main() {
 
         int index_source = 0;
         int index_target = 0;
+        std::string response {};
 
-        while (index_target < t.size() and index_source < s.size()) {
-            if (s[index_source] == t[index_target] or s[index_source] == '?') {
+        while (index_source < s.size()) {
+            if (index_target < t.size() and (s[index_source] == t[index_target] or s[index_source] == '?')) {
+                response += t[index_target];
                 index_target++;
                 index_source++;
+            } else if (index_target == t.size() and s[index_source] == '?') {
+                response += 'a';
+                index_source++;
             } else {
+                response += s[index_source];
                 index_source++;
             }
         }
 
-        std::cout << (index_target == t.size() ? "YES" : "NO") << std::endl;
+        if (index_target == t.size()) {
+            std::cout << "YES" << std::endl;
+            std::cout << response << std::endl;
+        } else {
+            std::cout << "NO" << std::endl;
+        }
     }
 }
