e6e489a336b3581c828bdfc5da397c7d1da123a1
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-30 12:15:24 +0800

    codeforces/cpp/1900-1999/1999E.cpp

--- codeforces/cpp/1900-1999/1999E.cpp
+++ codeforces/cpp/1900-1999/1999E.cpp
@@ -4,26 +4,34 @@ int main() {
     int t;
     int l, r;
     std::cin >> t;
-    int response;
+
 
     for (int i = 0; i < t; i++) {
+        int response = 0;
         std::cin >> l >> r;
 
-        int val = l;
-        response = 0;
+        int start = 0;
+        int end = 0;
 
+        int val = l;
         while (val > 0) {
-            response += 2;
+            start += 1;
             val /= 3;
         }
 
-        for (int j = l + 1; j <= r; j++) {
-            val = j;
-            while (val > 0) {
-                response += 1;
-                val /= 3;
-            }
+        val = r;
+        while (val > 0) {
+            end += 1;
+            val /= 3;
         }
+
+        int current = l;
+        for (auto j = start; j <= end; j++) {
+            response += (std::min(3 * j, r) - current) * j;
+            current = 3 * j - 1;
+        }
+
+        response += start;
 
         std::cout << response << std::endl;
     }
