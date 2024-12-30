21095f76f5e70c07128fcfd250400bd98156fd23
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-30 11:36:12 +0800

    codeforces/cpp/1900-1999/1999E.cpp

--- codeforces/cpp/1900-1999/1999E.cpp
+++ codeforces/cpp/1900-1999/1999E.cpp
@@ -4,9 +4,26 @@ int main() {
     int t;
     int l, r;
     std::cin >> t;
-
+    int response = 0;
 
     for (auto i = 0; i < t; i++) {
         std::cin >> l >> r;
+
+        int val = l;
+
+        while (val > 0) {
+            response += 2;
+            val /= 3;
+        }
+
+        for (int j = l + 1; j <= r; j++) {
+            val = j;
+            while (val > 0) {
+                response += 1;
+                val /= 3;
+            }
+        }
+
+        return response;
     }
 }
