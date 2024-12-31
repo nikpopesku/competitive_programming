7edbc4a9d1f6702e18107dde2c0daa087a1076d4
npopesku <nikolay.popesku@unbiased.co.uk>
2024-12-31 13:55:08 +0800

    codeforces/cpp/1900-1999/1999E.cpp

--- codeforces/cpp/1900-1999/1999E.cpp
+++ codeforces/cpp/1900-1999/1999E.cpp
@@ -14,10 +14,9 @@ int main() {
         steps.push_back(step);
     }
 
-    int index = 0;
-
     for (int i = 0; i < t; i++) {
         int response = 0;
+        int index = 0;
         std::cin >> l >> r;
 
         while (l >= steps[index]) index++;
@@ -31,7 +30,6 @@ int main() {
         }
 
         response += (std::min(steps[index], r + 1) - current) * (index + 1);
-
 
 
         std::cout << response << std::endl;
