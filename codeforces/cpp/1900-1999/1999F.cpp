501ffa11b553c227b6181b675f0538fc374ff3c2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-06 11:52:34 +0800

    codeforces/cpp/1900-1999/1999F.cpp

--- codeforces/cpp/1900-1999/1999F.cpp
+++ codeforces/cpp/1900-1999/1999F.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     int t;
     std::cin >> t;
+    int n, k, value;
 
     for (int i = 0; i < t; i++) {
+        std::cin >> n >> k;
+
+        int count_zero = 0;
+        std::vector<int> vec (n, 1);
+
+        for (int j = 0; j < n; j++) {
+            std::cin >> value;
+            if (value == 0) count_zero++;
+        }
+
+        for (int j = 0; j < count_zero; j++) vec[j] = 0;
     }
 }
