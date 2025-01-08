f8444b627e0142803592608dc09f5781f3d16e1b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-08 15:24:39 +0800

    codeforces/cpp/1900-1999/1926D.cpp

--- codeforces/cpp/1900-1999/1926D.cpp
+++ codeforces/cpp/1900-1999/1926D.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
 
 int main() {
     int t, n;
+    int number;
+    int groups = 0;
+    int reverse;
     std::cin >> t;
-    int N = 2 * 10e5;
-    std::vector digit_sum(N, 0);
-    std::vector<long long> partial_sum(N, 0);
 
-    for (auto i = 1; i <= N; i++) {
-        int j = i;
-        int sum = 0;
-        while (j) {
-            sum += j % 10;
-            j /= 10;
-        }
 
-        digit_sum[i-1] = sum;
-    }
-
-    for (auto i = 1; i <= N; i++) {
-        partial_sum[i] = partial_sum[i-1] + digit_sum[i-1];
-    }
 
     for (auto i = 0; i < t; i++) {
         std::cin >> n;
 
-        std::cout << partial_sum[n] << std::endl;
+        std::unordered_map<int, int> mp{};
+        groups = 0;
+
+        for (auto j = 0; j < n; j++) {
+            std::cin >> number;
+
+            if (mp.find(number) != mp.end()) {
+                mp[number]--;
+                groups++;
+
+                if (mp[number] == 0) {
+                    mp.erase(number);
+                }
+            } else {
+                reverse = ~number;
+
+                if (mp.find(reverse) != mp.end()) {
+                    mp[reverse]++;
+                } else {
+                    mp[reverse] = 1;
+                }
+            }
+        }
+
+        for (auto &elem: mp) {
+            groups += elem.second;
+        }
+
+        std::cout << groups << std::endl;
+
     }
 }
