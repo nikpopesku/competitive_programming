7094c7aa18c52c280c23613f015c910b0e17df9c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-11 17:58:22 +0800

    codeforces/cpp/1900-1999/1926F.cpp

---
+++ codeforces/cpp/1900-1999/1926F.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+    std::string s;
+
+
+    for (auto i = 0; i < t; i++) {
+        for (auto j = 0; j < 7; j++) {
+            std::cin >> s;
+        }
+    }
+}
