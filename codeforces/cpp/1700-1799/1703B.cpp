1fd1981b3b4c6cb6489c52b5c584e3027d5aa751
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-12 15:12:13 +0800

    codeforces/cpp/1900-1999/1703B.cpp

---
+++ codeforces/cpp/1700-1799/1703B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int t, n;
+    std::string s;
+
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        std::cin >> n;
+        std::cin >> s;
+
+
+    }
+}
