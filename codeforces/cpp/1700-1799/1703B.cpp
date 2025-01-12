346d7a18cce9010e8744cd7ee258f48bf0139a6b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-12 15:27:03 +0800

    codeforces/cpp/1900-1999/1703B.cpp

--- codeforces/cpp/1700-1799/1703B.cpp
+++ codeforces/cpp/1700-1799/1703B.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     int t, n;
@@ -10,6 +11,16 @@ int main() {
         std::cin >> n;
         std::cin >> s;
 
+        std::vector<int> alphabet(26, 0);
+        int sum = 0;
 
+        for (auto &ch: s) {
+            int index = int(ch) - int('A');
+            int increase = alphabet[index] == 0 ? 2 : 1;
+            alphabet[index] += increase;
+            sum += increase;
+        }
+
+        std::cout << sum << std::endl;
     }
 }
