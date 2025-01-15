00819f9dde45baffef9b92cf716666159827d66d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-15 15:56:00 +0800

    atcoder/200-299/abc205_b.cpp

--- atcoder/200-299/abc205_b.cpp
+++ atcoder/200-299/abc205_b.cpp
@@ -12,12 +12,12 @@ int main() {
 
         if (value < 1 or value > n) {
             std::cout << "No" << std::endl;
-            break;
+            return 0;
         }
 
         if (mp.find(value) != mp.end()) {
             std::cout << "No" << std::endl;
-            break;
+            return 0;
         }
 
         mp[value] = value;
