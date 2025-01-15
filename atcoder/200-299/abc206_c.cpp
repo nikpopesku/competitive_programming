0b67f063612ccd6d3286c566c44314dd47f2dbfe
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-15 16:53:32 +0800

    atcoder/200-299/abc206_c.cpp

---
+++ atcoder/200-299/abc206_c.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+
+int main() {
+    int n, value;
+    std::cin >> n;
+
+    std::map<int, int> mp;
+    int unique = 0;
+    int duplicates = 0;
+
+    for (auto i = 1; i <= n; i++) {
+        std::cin >> value;
+
+        if (mp.find(value) != mp.end()) {
+            mp[value] += 1;
+
+            continue;
+        }
+
+        mp[value] = 1;
+    }
+
+    if (mp.size() <= 1) {
+        std::cout << "0\n";
+    }
+
+    long long response = 1;
+
+    for (auto& elem: mp) {
+        if (elem.second == 1) {
+            unique++;
+        }
+    }
+
+    for (auto i = 1; i < unique; i++) {
+        response += i;
+    }
+
+
+    for (auto& elem: mp) {
+        if (elem.second > 1) {
+            response *= elem.second;
+        }
+    }
+
+
+    std::cout << response << std::endl;
+}
