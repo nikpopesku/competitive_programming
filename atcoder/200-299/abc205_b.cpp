709ddf6dc8c207895cac5ee74cadb0dc73810717
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-15 15:53:14 +0800

    atcoder/200-299/abc205_b.cpp

---
+++ atcoder/200-299/abc205_b.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+int main() {
+    unsigned int n, value;
+    std::cin >> n;
+
+    std::map<unsigned int, unsigned int> mp;
+
+    for (auto i = 1; i <= n; i++) {
+        std::cin >> value;
+
+        if (value < 1 or value > n) {
+            std::cout << "No" << std::endl;
+            break;
+        }
+
+        if (mp.find(value) != mp.end()) {
+            std::cout << "No" << std::endl;
+            break;
+        }
+
+        mp[value] = value;
+    }
+
+    std::cout << "Yes\n";
+}
