6dbbbd1f983604ee0672b98256446d275cc588ef
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-16 12:24:20 +0800

    atcoder/200-299/abc220_c.cpp

---
+++ atcoder/200-299/abc220_c.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+
+    std::cin >> n;
+    std::vector<int> v(n);
+    long long sum = 0;
+    long long response = 0;
+    long long x;
+
+    for (auto i = 0; i < n; i++) {
+        std::cin >> v[i];
+        sum += v[i];
+    }
+
+    std::cin >> x;
+
+    if (sum > x) {
+        sum = 0;
+        for (auto i = 0; i < n; i++) {
+            sum += v[i];
+
+            if (sum > x) {
+                std::cout << i << std::endl;
+
+                break;
+            }
+        }
+    } else {
+        response = n * (x / sum);
+        x -= sum * (x / sum);
+        sum = 0;
+
+        for (auto i = 0; i < n; i++) {
+            sum += v[i];
+
+            if (sum > x) {
+                std::cout << response + i << std::endl;
+
+                break;
+            }
+        }
+    }
+}
