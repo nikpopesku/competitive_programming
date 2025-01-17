25fa3ea80aa8de0bb8c14d2b269bb113739d55ff
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-17 16:06:49 +0800

    atcoder/200-299/abc196_c.cpp

--- atcoder/200-299/abc196_c.cpp
+++ atcoder/200-299/abc196_c.cpp
@@ -1,33 +1,12 @@
 #include <iostream>
-#include <cmath>
-
-int main() {
-    long long n;
-    std::cin >> n;
-    std::string s = std::to_string(n);
-    long long max_l = (s.size() / 2) * 2;
-    long long response = 0;
-
-    for (auto i = 2; i <= max_l; i += 2) {
-        long long number = 0;
-        int counter = 0;
-
-        while (counter < i) {
-            number = number * 10 + 9;
-            counter++;
+#include <string>
+using namespace std;
+using ll = long long;
+int main(){
+    ll N;
+    cin >> N;
+    for(ll i = 1; ; i++) if(stoll(to_string(i) + to_string(i)) > N){
+            cout << i - 1 << endl;
+            return 0;
         }
-
-        if (number < n) {
-            response += (9 * std::pow(10, i / 2 - 1));
-        } else {
-            long long pw = std::pow(10, i / 2);
-            long long left = n / pw;
-            long long right = n % pw;
-            if (std::min(left, right) >= std::pow(10, i / 2 - 1)) {
-                response += std::min(left, right) - std::pow(10, i / 2 - 1) + 1;
-            }
-        }
-    }
-
-    std::cout << response << std::endl;
 }
