1dd8c1f4639f82483ef4e742b6eb8e6b7e011962
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-22 11:23:44 +0800

    atcoder/200-299/abc194_c.cpp

--- atcoder/200-299/abc194_c.cpp
+++ atcoder/200-299/abc194_c.cpp
@@ -6,17 +6,21 @@ int main() {
     int n;
     std::cin >> n;
     std::vector<int> vc(n);
+    long long response = 0;
+    long long partial_sum = 0;
 
     for (int i = 1; i <= n; i++) {
         std::cin >> vc[i - 1];
+
+        response += vc[i-1] * vc[i-1] * (n - 1);
+        partial_sum += vc[i-1];
     }
 
-    long long response = 0;
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            response += (vc[i] - vc[j]) * (vc[i] - vc[j]);
-        }
+
+    for (int i = 0; i < n; i++) {
+        partial_sum -= vc[i];
+        response -= partial_sum * vc[i];
     }
 
     std::cout << response << std::endl;
