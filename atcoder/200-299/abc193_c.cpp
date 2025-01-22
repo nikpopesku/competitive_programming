3a1c15ca2e839e753540a2847091a0bf2e7fa08e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-22 13:46:22 +0800

    atcoder/200-299/abc193_c.cpp

--- atcoder/200-299/abc193_c.cpp
+++ atcoder/200-299/abc193_c.cpp
@@ -1,46 +1,20 @@
 #include <iostream>
-#include <vector>
+#include <unordered_set>
 
 using ll = long long;
 
 int main() {
-    ll N = 10e10;
-    std::vector<ll> vc;
+    std::unordered_set<ll> s;
     ll n;
     std::cin >> n;
 
-    ll i = 2, j = 2;
-    ll val_i = 2 * 2, val_j = 2 * 2 * 2;
-
-    while (val_i <= N or val_j <= N) {
-        if (val_i < val_j) {
-            if (vc.empty() or vc.back() != val_i) {
-                vc.push_back(val_i);
-            }
-
-            i++;
-            val_i = i * i;
-        } else {
-            if (vc.empty() or vc.back() != val_j) {
-                vc.push_back(val_j);
-            }
-
-            j++;
-            val_j = j * j * j;
+    for (auto i = 2; i * i <= n; i++) {
+        ll value = i * i;
+        while (value <= n) {
+            s.insert(value);
+            value *= i;
         }
     }
 
-    ll left = 0, right = vc.size() - 1;
-
-    while (left < right) {
-        ll m = left + (right - left) / 2;
-
-        if (vc[m + 1] <= n) {
-            left = m + 1;
-        } else {
-            right = m;
-        }
-    }
-
-    std::cout << n - left - 1 << std::endl;
+    std::cout << n - s.size() << std::endl;
 }
