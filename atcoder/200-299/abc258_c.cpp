843155bdfd321c387567a8829a33ee8227b81281
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-23 12:18:57 +0800

    atcoder/200-299/abc258_c.cpp

--- atcoder/200-299/abc258_c.cpp
+++ atcoder/200-299/abc258_c.cpp
@@ -2,23 +2,27 @@
 
 
 int main() {
-    int n, q, type, value;
+    int n, q, type, cur_value, value = 0;
     std::cin >> n >> q;
     std::string s;
     std::cin >> s;
 
     for (auto i = 1; i <= q; i++) {
-        std::cin >> type >> value;
+        std::cin >> type >> cur_value;
 
         if (type == 2) {
-            std::cout << s[value - 1] << std::endl;
+            if (value) {
+                std::string sub = s.substr(n-value, value);
+                s.insert(0, sub);
+                s.resize(n);
+            }
+
+            std::cout << s[cur_value - 1] << std::endl;
         }
 
         if (type == 1) {
-            value = value % n;
-            std::string sub = s.substr(n-value, value);
-            s.insert(0, sub);
-            s.resize(n);
+            value += cur_value;
+            value %= n;
         }
     }
 }
