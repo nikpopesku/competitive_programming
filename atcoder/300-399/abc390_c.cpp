d5f984280288e62dedf9a8b06965527158356f9e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-25 20:59:40 +0800

    atcoder/300-399/abc390_c.cpp

--- atcoder/300-399/abc390_c.cpp
+++ atcoder/300-399/abc390_c.cpp
@@ -3,5 +3,36 @@
 
 
 int main() {
-    std::vect
+    int h, w;
+    std::cin >> h >> w;
+    std::string s;
+    std::vector<std::string> vc;
+    int top = h-1, right = 0, left = w - 1, down = 0;
+
+    for (int i = 0; i < h; i++) {
+        std::cin >> vc[i];
+
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == '#') {
+                if (i > top) top = i;
+                if (i < down) down = i;
+                if (j < left) left = j;
+                if (j > right) right = j;
+            }
+        }
+    }
+
+    std::string response = "Yes";
+
+    for (int i = top; i <= down; i++) {
+        for (int j = left; j <= right; j++) {
+            if (vc[i][j] == '.') {
+                response = "No";
+                break;
+            }
+        }
+        if (response == "No") break;
+    }
+
+    std::cout << response << std::endl;
 }
