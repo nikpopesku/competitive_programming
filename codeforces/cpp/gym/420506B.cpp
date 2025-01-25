06306bba78625f29864159cfa1340cfa32157a56
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-25 13:45:59 +0800

    codeforces/cpp/gym/420506B.cpp

--- codeforces/cpp/gym/420506B.cpp
+++ codeforces/cpp/gym/420506B.cpp
@@ -1,6 +1,28 @@
 #include "iostream"
+#include "vector"
 
 int main() {
-    int n, m, q;
+    int n, m, q, value1, value2;
     std::cin >> n >> m >> q;
+    char type;
+    std::vector<std::vector<int>> vc (n);
+    vc[0] = std::vector<int> (n);
+    for (int i = 0; i < n; i++) vc[0][i] = i;
+
+    for (int i = 0; i < q; i++) {
+        std::cin >> type >> value1 >> value2;
+
+        if (type == 'c') {
+            if (vc[value1].size() >= value2) {
+                std::cout << vc[value1][value2] << std::endl;
+            } else {
+                std::cout << -1 << std::endl;
+            }
+        }
+
+        if (type == 's') {
+            vc[value2].push_back(vc[value1].back());
+            vc[value1].pop_back();
+        }
+    }
 }
