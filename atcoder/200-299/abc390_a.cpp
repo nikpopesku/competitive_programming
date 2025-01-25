3a3b8a2612ed33c3e5a8233c6b12bbccb215fd49
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-25 20:10:23 +0800

    atcoder/200-299/abc390_a.cpp

--- atcoder/200-299/abc390_a.cpp
+++ atcoder/200-299/abc390_a.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
 
 
 int main() {
-    int n;
+    std::vector<int> a(5);
+    bool possible = false;
+
+
+    for (int i = 1; i <= 5; i++) {
+        std::cin >> a[i];
+    }
+
+    if (std::to_string(a[1]) + std::to_string(a[0]) + std::to_string(a[2]) + std::to_string(a[3]) + std::to_string(a[4]) == "12345") {
+        std::cout << "Yes\n";
+        possible = true;
+    }
+
+    if (std::to_string(a[0]) + std::to_string(a[2]) + std::to_string(a[1]) + std::to_string(a[3]) + std::to_string(a[4]) == "12345") {
+        std::cout << "Yes\n";
+        possible = true;
+    }
+
+    if (std::to_string(a[0]) + std::to_string(a[1]) + std::to_string(a[3]) + std::to_string(a[2]) + std::to_string(a[4]) == "12345") {
+        std::cout << "Yes\n";
+        possible = true;
+    }
+
+    if (std::to_string(a[0]) + std::to_string(a[1]) + std::to_string(a[2]) + std::to_string(a[4]) + std::to_string(a[3]) == "12345") {
+        std::cout << "Yes\n";
+        possible = true;
+    }
+
+    if (!possible) std::cout << "No\n";
 }
--- atcoder/200-299/abc390_c.cpp
+++ atcoder/200-299/abc390_c.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <vector>
 
 
 int main() {
-    int n;
+    std::vect
 }
