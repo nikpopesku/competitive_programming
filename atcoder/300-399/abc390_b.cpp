b05518865e150571cbb33e40a4b8ba74f85001d9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-25 20:23:15 +0800

    atcoder/300-399/abc390_b.cpp

--- atcoder/300-399/abc390_b.cpp
+++ atcoder/300-399/abc390_b.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
 
 
 int main() {
     int n;
+    std::cin >> n;
+    std::vector<long long> vc(n);
+    std::string response = "Yes";
+
+    for (int i = 0; i < n; i++) {
+        std::cin >> vc[i];
+    }
+
+    long long factor = vc[1] / vc[0];
+
+    for (int i = 2; i < n; i++) {
+        if (vc[i] / vc[i-1] != factor) {
+            response = "No";
+            break;
+        }
+    }
+
+    std::cout << response << std::endl;
 }
