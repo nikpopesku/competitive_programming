48f6c60e26121179eed04a66db8f63c3f5178a73
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-28 12:35:04 +0800

    cses.fi/1091.cpp

--- cses.fi/1091.cpp
+++ cses.fi/1091.cpp
@@ -4,7 +4,7 @@
 
 
 int main() {
-    int n, m, value;
+    int n, m, value, willing_pay;
     std::cin >> n >> m;
     std::vector<int> price(n);
     std::priority_queue<int> pq;
@@ -21,34 +21,27 @@ int main() {
     }
 
     for (int i = 0; i < m; ++i) {
-        std::cin >> value;
+        std::cin >> willing_pay;
+
+        if (price[0] > willing_pay) {
+            std::cout << -1 << std::endl;
+            continue;
+        }
+
         int left = 0, right = price.size() - 1, mid;
 
-        while (left < right) {
+        while (left <= right) {
             mid = left + (right - left) / 2;
 
-            if (mid == 0 and price[mid] > value) {
-                std::cout << -1 << std::endl;
-                break;
-            } else if (price[mid] <= value and (mid == price.size() - 1 or price[mid + 1] < value)) {
+            if (price[mid] <= willing_pay and (mid == price.size() - 1 or price[mid + 1] > willing_pay)) {
                 std::cout << price[mid] << std::endl;
                 price.erase(price.begin() + mid);
                 break;
-            } else if (price[mid] <= value and left != mid) {
-                left = mid;
-            } else if (price[mid] <= value and left == mid) {
-                right = mid;
+            } else if (price[mid] <= willing_pay) {
+                left = mid + 1;
             } else {
                 right = mid - 1;
             }
-        }
-
-        if (left == right) {
-            if (price[left] <= value) {
-                std::cout << price[left] << std::endl;
-                price.erase(price.begin() + left);
-            }
-            if (price[left] > value) std::cout << -1 << std::endl;
         }
     }
 
