e13d674feead3c71d3446eea83169db430828e42
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-28 13:51:25 +0800

    atcoder/200-299/abc235_c.cpp

--- atcoder/200-299/abc235_c.cpp
+++ atcoder/200-299/abc235_c.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <map>
+#include <vector>
 
 
 int main() {
-    int n, q;
+    int n, q, value, x, k;
+    std::map<int, std::vector<int>> mp;
+    std::cin >> n >> q;
+
+    for (int i = 1; i < n; ++i) {
+        std::cin >> value;
+        mp[value].push_back(i);
+    }
+
+    for (int i = 0; i < q; ++i) {
+        std::cin >> x >> k;
+
+        if (mp[x].size() < k) {
+            std::cout << -1 << std::endl;
+        } else {
+            std::cout << mp[x][k-1] << std::endl;
+        }
+    }
 }
