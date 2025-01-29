7d5d2ef2ca924d1dd366798c14e32828c0c1ad5c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-29 13:52:53 +0800

    atcoder/200-299/abc241_d.cpp

--- atcoder/200-299/abc241_d.cpp
+++ atcoder/200-299/abc241_d.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
-    int q;
+    int q, value, x, k;
     std::cin >> q;
+
+    for (int i = 0; i < q; ++i) {
+        std::cin >> value;
+        if (value == 1) {
+            std::cin >> x;
+        } else {
+            std::cin >> x >> k;
+        }
+    }
 }
