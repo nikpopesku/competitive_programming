e2df15e83d1d4dcc2eb74d6e99d86259cb6712af
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-30 16:41:26 +0800

    cses.fi/1161.cpp

--- cses.fi/1161.cpp
+++ cses.fi/1161.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main() {
     int n, x;
     std::cin >> x >> n;
+    std::vector<int> vc(n);
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> vc[i];
+    }
 }
