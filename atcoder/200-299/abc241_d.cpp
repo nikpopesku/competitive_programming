0d1c9c3ab64a3191e286ba8bb9837e435e69a000
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-30 12:20:20 +0800

    atcoder/200-299/abc241_d.cpp

--- atcoder/200-299/abc241_d.cpp
+++ atcoder/200-299/abc241_d.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <set>
 
 int main() {
-    int q, value, x, k;
+    int q, type, x, k;
     std::cin >> q;
+    std::multiset<int> s;
 
     for (int i = 0; i < q; ++i) {
-        std::cin >> value;
-        if (value == 1) {
+        std::cin >> type;
+        if (type == 1) {
             std::cin >> x;
+            s.insert(x);
         } else {
             std::cin >> x >> k;
+
+            if (type == 2) {
+                auto it = s.begin();
+                while (k > 0) {
+                    if (it != s.end()) ++it;
+                    --k;
+                }
+
+                if (*it <= x) {
+                    std::cout << *it << std::endl;
+                } else {
+                    std::cout << -1 << std::endl;
+                }
+            }
         }
     }
 }
