ff316efdb733034c590ea186a63c47022be016d2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-30 17:00:57 +0800

    cses.fi/1161.cpp

--- cses.fi/1161.cpp
+++ cses.fi/1161.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <vector>
 
+int divide(int start, int end, int x, std::vector<int> partial_sum) {
+    if (start == end) return 0;
+
+    int min_delta = x;
+    int half = partial_sum.back() / 2;
+    int response = 0;
+    int j = 0;
+
+    for (int i = start; i <= end; ++i) {
+        if (std::abs(half - partial_sum[i]) < min_delta) {
+            j = i;
+            min_delta = std::abs(half - partial_sum[i]);
+            response += x;
+        }
+    }
+
+    return response + divide(0, j, partial_sum[j], partial_sum) + divide(j+1, end, partial_sum[j], partial_sum);
+}
+
 int main() {
     int n, x;
     std::cin >> x >> n;
@@ -9,6 +28,9 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         std::cin >> vc[i];
-        partial_sum[i] = (i >= 1 ? partial_sum[i-1] : 0) + vc[i];
     }
+
+    int response = divide(0, n - 1, x, partial_sum);
+
+    std::cout << response << std::endl;
 }
