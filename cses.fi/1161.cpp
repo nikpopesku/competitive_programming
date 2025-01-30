cf2db8e8c23f9335a6490b5e1a68085975769291
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-30 16:44:26 +0800

    cses.fi/1161.cpp

--- cses.fi/1161.cpp
+++ cses.fi/1161.cpp
@@ -5,8 +5,10 @@ int main() {
     int n, x;
     std::cin >> x >> n;
     std::vector<int> vc(n);
+    std::vector<int> partial_sum(n);
 
     for (int i = 0; i < n; ++i) {
         std::cin >> vc[i];
+        partial_sum[i] = (i >= 1 ? partial_sum[i-1] : 0) + vc[i];
     }
 }
