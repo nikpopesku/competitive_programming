ee4035ef6ceea51be8425ed50611622432b48393
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-31 13:33:26 +0800

    cses.fi/1164.cpp

---
+++ cses.fi/1164.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using ll = long long;
+
+int main() {
+    ll n, value, x, response = 0;
+    std::cin >> x >> n;
+    std::priority_queue<int> pq;
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> value;
+        pq.push(-1 * value);
+    }
+
+    while (pq.size() >= 2) {
+        ll left = -1 * pq.top();
+        pq.pop();
+        ll right = -1 * pq.top();
+        pq.pop();
+        value = left + right;
+        response += value;
+        pq.push(-1 * value);
+    }
+
+    std::cout << response << std::endl;
+}
