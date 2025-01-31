3df886d120170bd3a0d9782051d733dc41989300
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-31 16:31:27 +0800

    cses.fi/1164.cpp

--- cses.fi/1164.cpp
+++ cses.fi/1164.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <queue>
+
+struct Person {
+
+    int arrival;
+    int departure;
+
+    Person(int arrival, int departure)
+            : arrival(arrival), departure(departure) {
+    }
+};
+
+struct ComparePerson {
+    bool operator()(Person const &p1, Person const &p2) {
+        return p1.departure < p2.departure;
+    }
+};
 
 int main() {
     int n;
     std::cin >> n;
+    std::vector<int> arrival(n);
+    std::vector<int> departure(n);
+
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arrival[i] >> departure[i];
+    }
 }
