e7d915cc2d9d442b12e2c20c8d9cf1ebcccf5c97
npopesku <nikolay.popesku@unbiased.co.uk>
2025-01-31 13:11:15 +0800

    cses.fi/1161.cpp

--- cses.fi/1161.cpp
+++ cses.fi/1161.cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 
-long long divide(int start, int end, long long x, std::vector<int> partial_sum) {
-    if (start == end) return 0;
-
-    long long min_delta = x;
-    long long half = x / 2;
-    int j = 0;
-
-    int deduct = start > 0 ? partial_sum[start - 1] : 0;
-    for (int i = start; i <= end; ++i) {
-        if (std::abs(half - partial_sum[i] + deduct) < min_delta) {
-            j = i;
-            min_delta = std::abs(half - partial_sum[i] + deduct);
-        }
-    }
-
-    return x + divide(start, j, partial_sum[j] - deduct, partial_sum) +
-           divide(j + 1, end, partial_sum[end] - partial_sum[j], partial_sum);
-}
+using ll = long long;
 
 int main() {
-    int n;
-    long long x;
+    ll n, value, x, response;
     std::cin >> x >> n;
-    std::vector<int> vc(n);
-    std::vector<int> partial_sum(n);
+    std::priority_queue<int> pq;
 
     for (int i = 0; i < n; ++i) {
-        std::cin >> vc[i];
-        partial_sum[i] = (i > 0 ? partial_sum[i - 1] : 0) + vc[i];
+        std::cin >> value;
+        pq.push(-1 * value);
     }
 
-    long long response = divide(0, n - 1, x, partial_sum);
+    while (pq.size() >= 2) {
+        ll left = -1 * pq.top();
+        pq.pop();
+        ll right = -1 * pq.top();
+        pq.pop();
+        value = left + right
+        response += value;
+        pq.push(-1 * value);
+    }
 
     std::cout << response << std::endl;
 }
