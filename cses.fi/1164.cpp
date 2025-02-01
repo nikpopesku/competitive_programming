96d27b47312bba0f3aac01f5590a245609a32404
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-01 13:01:43 +0800

    cses.fi/1164.cpp

--- cses.fi/1164.cpp
+++ cses.fi/1164.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <queue>
 #include <map>
 
 struct Person {
-    int arrival;
-    int departure;
+    int day;
+    std::string type;
 
-    Person(int arrival, int departure)
-            : arrival(arrival), departure(departure) {
+    Person(int day, std::string type)
+            : day(day), type(std::move(type)) {
     }
 };
 
 struct ComparePerson {
     bool operator()(Person const &p1, Person const &p2) {
-        if (p1.departure > p2.departure) return true;
-        if (p1.departure == p2.departure) return p1.arrival > p2.arrival;
+        if (p1.type == p2.type) return p1.day > p2.day;
+        if (p1.day > p2.day) return true;
+        if (p1.day == p2.day) return p1.type == "departure";
 
         return false;
     }
@@ -31,16 +33,16 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         std::cin >> arrival >> departure;
-        pq.emplace(arrival, departure);
+        pq.emplace(arrival, "arrival");
+        pq.emplace(departure, "departure");
     }
 
     while (!pq.empty()) {
         Person p = pq.top();
-        std::cout << p.arrival << ' ' << p.departure << std::endl;
-        counter -= d[p.arrival];
-        counter += 1;
+        std::cout << p.day << ' ' << p.type << std::endl;
+        if (p.type == "arrival") ++counter;
+        if (p.type == "departure") --counter;
         if (counter > max_counter) max_counter = counter;
-        d[p.departure + 1] += 1;
         pq.pop();
     }
 
