5109de4d6d16ec4aef14d8c1affccf924ab5f898
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-01 16:22:05 +0800

    codeforces/cpp/1400-1499/1474C.cpp

---
+++ codeforces/cpp/1400-1499/1474C.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+    int t;
+    std::cin >> t;
+
+    for (auto i = 0; i < t; i++) {
+        int n, k;
+        std::cin >> n >> k;
+
+
+        if (k < n) {
+            const int initial_k = k;
+            const int times = n / k;
+            k = k * times;
+
+            while (k < n) {
+                k += initial_k;
+            }
+        }
+
+        int start = k/n;
+        while (start <= k) {
+            if (n * start >= k) {
+                std::cout << start << std::endl;
+                break;
+            }
+            start++;
+        }
+    }
+
+    return 0;
+}
