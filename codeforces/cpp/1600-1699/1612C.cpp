77c6451b17a7d2dd7adadc361e88c33254c04dfa
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-02 12:53:31 +0800

    codeforces/cpp/1600-1699/1612C.cpp

--- codeforces/cpp/1600-1699/1612C.cpp
+++ codeforces/cpp/1600-1699/1612C.cpp
@@ -30,7 +30,7 @@ int main() {
 
             cout << left + 1 << endl;
         } else {
-            ll left = k + 1, right = 2 * k - 1;
+            ll left = k, right = 2 * k - 1;
 
             while (left < right) {
                 ll m = left + (right - left + 1) / 2;
