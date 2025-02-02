97a7fb54421bc7804066c0abb59a39457cc0ee5a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-02 12:33:41 +0800

    codeforces/cpp/1600-1699/1612C.cpp

--- codeforces/cpp/1600-1699/1612C.cpp
+++ codeforces/cpp/1600-1699/1612C.cpp
@@ -6,8 +6,41 @@ using namespace std;
 
 int main() {
     int t;
-    int k;
-    ll x;
+    ll k, x;
     cin >> t;
     cin >> k >> x;
+
+    ll first_half = (k + 1) * k / 2;
+
+    if (first_half >= x) {
+        ll left = 0, right = k;
+
+        while (left < right) {
+            ll m = left + (right - left + 1) / 2;
+
+            ll value = (1 + m) * m / 2;
+            if (value < x) {
+                left = m;
+            } else {
+                right = m - 1;
+            }
+        }
+
+        cout << left << endl;
+    } else {
+        ll left = 0, right = k;
+
+        while (left < right) {
+            ll m = left + (right - left + 1) / 2;
+
+            ll value = (1 + m) * m / 2;
+            if (value < x) {
+                left = m;
+            } else {
+                right = m - 1;
+            }
+        }
+
+        cout << left << endl;
+    }
 }
