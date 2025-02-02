23437fbf9d5e2d6ce431ce601a532b21c9c99b28
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-02 12:43:36 +0800

    codeforces/cpp/1600-1699/1612C.cpp

--- codeforces/cpp/1600-1699/1612C.cpp
+++ codeforces/cpp/1600-1699/1612C.cpp
@@ -8,37 +8,40 @@ int main() {
     int t;
     ll k, x;
     cin >> t;
-    cin >> k >> x;
 
-    ll first_half = (k + 1) * k / 2;
+    for (int i = 0; i < t; ++i) {
+        cin >> k >> x;
 
-    if (first_half >= x) {
-        ll left = 0, right = k;
+        ll first_half = (k + 1) * k / 2;
 
-        while (left < right) {
-            ll m = left + (right - left + 1) / 2;
+        if (first_half >= x) {
+            ll left = 0, right = k;
 
-            ll value = (1 + m) * m / 2;
-            if (value < x) {
-                left = m;
-            } else {
-                right = m - 1;
+            while (left < right) {
+                ll m = left + (right - left + 1) / 2;
+
+                ll value = (1 + m) * m / 2;
+                if (value < x) {
+                    left = m;
+                } else {
+                    right = m - 1;
+                }
+            }
+        } else {
+            ll left = k + 1, right = 2 * k - 1;
+
+            while (left < right) {
+                ll m = left + (right - left + 1) / 2;
+
+                ll value = k * k - (2 * k - m) * (2 * k - 1 - m) / 2;
+                if (value < x) {
+                    left = m;
+                } else {
+                    right = m - 1;
+                }
             }
         }
-    } else {
-        ll left = k + 1, right = 2 * k - 1;
 
-        while (left < right) {
-            ll m = left + (right - left + 1) / 2;
-
-            ll value = k * k - (2 * k - m) * (2 * k - 1 - m) / 2;
-            if (value < x) {
-                left = m;
-            } else {
-                right = m - 1;
-            }
-        }
+        cout << left << endl;
     }
-
-    cout << left << endl;
 }
