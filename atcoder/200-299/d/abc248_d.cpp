94d9ea410444184c75b8fa46bc3e0c3081d4ba90
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-04 16:59:50 +0800

    atcoder/200-299/d/abc248_d.cpp

--- atcoder/200-299/d/abc248_d.cpp
+++ atcoder/200-299/d/abc248_d.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-int main() {
-    int n, q;
-    cin >> n;
-    vector<int> vc(n);
+int ffind(map<int, vector<int>> mp, int x, int xx) {
+    int left = 0, right = mp[x].size() - 1;
 
-    for (int i = 0; i < n; ++i) {
-        cin >> vc[i];
+    while (left <= right) {
+        int m = left + (right - left) / 2;
+
+        if (mp[x][m] == xx) {
+            return m;
+        } else if (mp[x][m] < xx) {
+            left = m + 1;
+        } else {
+            right = m - 1;
+        }
+    }
+
+    return mp[x][left] <= xx ? mp[x][left] : mp[x][right];
+}
+
+int main() {
+    int n, q, value;
+    cin >> n;
+    map<int, vector<int>> mp;
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> value;
+        mp[value].push_back(i);
     }
 
     cin >> q;
-    int left, right, x;
+    int l, r, x;
 
     for (int i = 0; i < q; ++i) {
-        cin >> left >> right >> x;
+        cin >> l >> r >> x;
 
-        cout << 1 << endl;
+        if (mp[x].empty()) {
+            cout << "0\n";
+            continue;
+        }
+
+        cout << ffind(mp, x, r) - ffind(mp, x, r) + 1 << endl;
     }
 
 }
