a8a4c63f7b4110b60894e7a1b33399962ebcdc5b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-04 15:50:41 +0800

    atcoder/200-299/d/abc248_d.cpp

--- atcoder/200-299/d/abc248_d.cpp
+++ atcoder/200-299/d/abc248_d.cpp
@@ -4,12 +4,21 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n, q;
     cin >> n;
     vector<int> vc(n);
 
     for (int i = 0; i < n; ++i) {
         cin >> vc[i];
+    }
+
+    cin >> q;
+    int left, right, x;
+
+    for (int i = 0; i < q; ++i) {
+        cin >> left >> right >> x;
+
+        cout << 1 << endl;
     }
 
 }
