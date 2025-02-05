345cc4c8f6a1246edcf06e09bb803880d55a976c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-05 20:13:22 +0800

    cses.fi/1085.cpp

--- cses.fi/1085.cpp
+++ cses.fi/1085.cpp
@@ -3,14 +3,51 @@
 
 using namespace std;
 
+#define ll long long
+
+ll count_subarrays(ll val, vector<ll> & vc) {
+    ll response = 0;
+    ll current_sum = 0;
+
+    for (ll i = 0; i < vc.size(); ++i) {
+        if (current_sum + vc[i] <= val) {
+            current_sum += vc[i];
+        } else {
+            ++response;
+            current_sum = vc[i];
+        }
+    }
+
+    if (current_sum > 0) ++response;
+
+    return response;
+}
 
 int main() {
-    int n, k;
+    ll n, k, total_sum = 0, max_elem = 0;
     std::cin >> n >> k;
 
-    std::vector<int> vc(n);
+    std::vector<ll> vc(n);
 
-    for (int i = 0; i < n; ++i) {
+    for (ll i = 0; i < n; ++i) {
         cin >> vc[i];
+        total_sum += vc[i];
+        if (vc[i] > max_elem) max_elem = vc[i];
     }
+
+    ll left = max_elem, right = total_sum;
+
+    while (left < right) {
+        ll m = left + (right - left) / 2;
+
+        ll value = count_subarrays(m, vc);
+
+        if (value < k) {
+            right = m - 1;
+        } else {
+            left = m;
+        }
+    }
+
+    return left;
 }
