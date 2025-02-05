b17591f25b9c2076669833b664793657b1121cec
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-05 14:55:44 +0800

    atcoder/200-299/d/abc248_d.cpp

--- atcoder/200-299/d/abc248_d.cpp
+++ atcoder/200-299/d/abc248_d.cpp
@@ -1,37 +1,17 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-int ffind(map<int, map<int, int>> mp, int x, int xx) {
-    if (xx == 0) return 0;
-
-    int left = 0, right = mp[x].size() - 1;
-
-    while (left < right) {
-        int m = left + (right - left + 1) / 2;
-
-        if (mp[x].count(m) and mp[x][m] == xx) {
-            return m;
-        } else if (mp[x].count(m) and mp[x][m] > xx) {
-            right = m - 1;
-        } else {
-            left = m;
-        }
-    }
-
-    return left;
-}
 
 int main() {
     int n, q, value;
     cin >> n;
-    map<int, map<int, int>> mp;
+    map<int, vector<int>> mp;
 
     for (int i = 1; i <= n; ++i) {
         cin >> value;
-        if (mp[value].empty()) mp[value].emplace(0, 0);
-        mp[value].emplace(mp[value].size(), i);
+        mp[value].push_back(i);
     }
 
     cin >> q;
@@ -45,9 +25,7 @@ int main() {
             continue;
         }
 
-        int pos_r = ffind(mp, x, r), pos_l = ffind(mp, x, l - 1);
-
-        cout << pos_r - pos_l << endl;
+        cout << upper_bound(mp[x].begin(), mp[x].end(), r) - lower_bound(mp[x].begin(), mp[x].end(), l) << endl;
     }
 
 }
