34608e1f98afb3d87ef511b7aa1436dc75bf3e23
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 20:10:20 +0800

    atcoder/200-299/c/abc247_c.cpp

--- atcoder/200-299/c/abc247_c.cpp
+++ atcoder/200-299/c/abc247_c.cpp
@@ -5,12 +5,7 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     string value = "1";
-
-    for (auto i = 2; i <= n; ++i) {
-        value += " " + to_string(i) + " " + value;
-    }
-
+    for (auto i = 2; i <= n; ++i) value += " " + to_string(i) + " " + value;
     cout << value;
 }
