e57904500d03de17ba8373b60ca90cd8fe44cf85
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 16:25:43 +0800

    codeforces/cpp/1600-1699/1610C.cpp

--- codeforces/cpp/1600-1699/1610C.cpp
+++ codeforces/cpp/1600-1699/1610C.cpp
@@ -3,53 +3,36 @@
 #include <set>
 
 using namespace std;
+#define ll long long
+#define fast_io ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+const ll maxn = 2e5 + 100;
 
-bool calc(int k, vector<int> &vc_a, vector<int> &vc_b) {
-    int count = 0;
-    std::set<int> s_b;
+ll q, n, a[maxn], b[maxn];
 
-    for (int i = 0; i < vc_b.size(); ++i) {
-        if (vc_b[i] >= count and count < k) count++;
-        else s_b.insert(i);
+bool ok(ll x){
+    ll c = 0;
+    for(ll i = 0; i < n; i++){
+        if(x - 1 - a[i] <= c && c <= b[i]) c++;
     }
-
-    if (count < k) return false;
-    count = k;
-
-    for (int i = 0; i < vc_a.size(); ++i) {
-        if (vc_a[i] >= count - 1 and count > 0 and s_b.count(i) == 0) --count;
-    }
-
-    return count == 0;
+    return c >= x;
 }
 
-int main() {
-    int t, n;
-    cin >> t;
+int main(){
+    fast_io;
 
-    for (int i = 0; i < t; ++i) {
+    cin >> q;
+    while(q--){
         cin >> n;
-
-        vector<int> vc_a(n);
-        vector<int> vc_b(n);
-
-        for (int j = 0; j < n; ++j) {
-            cin >> vc_a[j] >> vc_b[j];
+        for(ll i = 0; i < n; i++){
+            cin >> a[i] >> b[i];
         }
-
-        int left = 1, right = n;
-
-        while (left < right) {
-            int m = left + (right - left + 1) / 2;
-
-            bool value = calc(m, vc_a, vc_b);
-            if (value) {
-                left = m;
-            } else {
-                right = m - 1;
-            }
+        ll l = -1, r = n + 1, mid;
+        while(r - l > 1){
+            mid = (l + r) >> 1;
+            if(ok(mid)) l = mid;
+            else r = mid;
         }
-
-        cout << left << endl;
+        cout << l << "\n";
     }
+    return 0;
 }
