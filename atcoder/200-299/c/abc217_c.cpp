20836a8794aedb4f2bac11c7b10200aafa51b3b8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 18:36:35 +0800

    atcoder/200-299/c/abc217_c.cpp

--- atcoder/200-299/c/abc217_c.cpp
+++ atcoder/200-299/c/abc217_c.cpp
@@ -7,9 +7,15 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> vc(n);
+    vector<int> vc(n), vc2(n);
 
     for (auto i = 0; i < n; ++i) {
         cin >> vc[i];
     }
+
+    for (auto i = 0; i < n; ++i) {
+        vc2[vc[i] - 1] = i + 1;
+    }
+
+    for (auto i = 0; i < n; ++i) cout << vc2[i] << (i < n -1 ? ' ' : '\n');
 }
