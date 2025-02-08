3c9e93144114f95155f52013dea0aaa5d031a8c1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 19:37:31 +0800

    atcoder/200-299/c/abc247_c.cpp

--- atcoder/200-299/c/abc247_c.cpp
+++ atcoder/200-299/c/abc247_c.cpp
@@ -13,8 +13,8 @@ int main() {
     vc[1] = "121";
     vc[2] = "1213121";
 
-    for (auto i = 3; i < N; ++i) {
-        vc[i] = vc[i - 1] + to_string(i + 4) + vc[i - 1];
+    for (auto i = 3; i < max(N, n); ++i) {
+        vc[i] = vc[i - 1] + to_string(i + 1) + vc[i - 1];
     }
 
     for (auto i = 0; i < vc[n].size(); ++i) {
