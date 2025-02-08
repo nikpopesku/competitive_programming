13dbb323fc850e4b63972bbf083578ab2e67b544
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 19:46:56 +0800

    atcoder/200-299/c/abc247_c.cpp

--- atcoder/200-299/c/abc247_c.cpp
+++ atcoder/200-299/c/abc247_c.cpp
@@ -7,17 +7,11 @@ int main() {
     int n;
     cin >> n;
 
-    vector<string> vc(n+1);
-    vc[0] = "0";
-    vc[1] = "1";
-    vc[2] = "121";
-    vc[3] = "1213121";
+    string value = "1";
 
-    for (auto i = 4; i <= n; ++i) {
-        vc[i] = vc[i - 1] + to_string(i) + vc[i - 1];
+    for (auto i = 2; i <= n; ++i) {
+        value += to_string(i) + value;
     }
 
-    for (size_t i = 0; i < vc[n].size(); ++i) {
-        cout << vc[n][i] << (i < vc[n].size() - 1 ? " " : "\n");
-    }
+    for (size_t i = 0; i < value.size(); ++i) cout << value[i] << (i < value.size() - 1 ? " " : "\n");
 }
