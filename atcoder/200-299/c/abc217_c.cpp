4bd5ea23d2965c11bb659cb0658e3374d8d85151
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 18:36:45 +0800

    atcoder/200-299/c/abc217_c.cpp

--- atcoder/200-299/c/abc217_c.cpp
+++ atcoder/200-299/c/abc217_c.cpp
@@ -17,5 +17,5 @@ int main() {
         vc2[vc[i] - 1] = i + 1;
     }
 
-    for (auto i = 0; i < n; ++i) cout << vc2[i] << (i < n -1 ? ' ' : '\n');
+    for (auto i = 0; i < n; ++i) cout << vc2[i] << (i < n - 1 ? ' ' : '\n');
 }
