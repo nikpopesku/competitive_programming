2fdedb454c504fa6151c9ebc6bfe2527c5212dd9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-08 18:22:32 +0800

    atcoder/200-299/c/abc217_c.cpp

--- atcoder/200-299/c/abc217_c.cpp
+++ atcoder/200-299/c/abc217_c.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
+
+    vector<int> vc(n);
+
+    for (auto i = 0; i < n; ++i) {
+        cin >> vc[i];
+    }
 }
