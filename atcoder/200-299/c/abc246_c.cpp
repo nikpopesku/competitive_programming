314906f08d2364568ec5a7d682da23043a4976a4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-09 14:37:59 +0800

    atcoder/200-299/c/abc246_c.cpp

---
+++ atcoder/200-299/c/abc246_c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll n, k, x;
+    cin >> n >> k >> x;
+
+    vector<ll> vc(n);
+
+    for (auto i = 0; i < n; ++i) {
+        cin >> vc[i];
+    }
+}
