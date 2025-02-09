b1f02537c404adf7ad7f2f996b297a8bf35efcc8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-09 14:46:30 +0800

    atcoder/200-299/c/abc247_c.cpp

--- atcoder/200-299/c/abc246_c.cpp
+++ atcoder/200-299/c/abc246_c.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <math.h>
 
 using namespace std;
 
 #define ll long long
 
 int main() {
-    ll n, k, x;
+    ll n, k, x, value, response = 0;
     cin >> n >> k >> x;
 
-    vector<ll> vc(n);
+    priority_queue<ll> pq;
 
     for (auto i = 0; i < n; ++i) {
-        cin >> vc[i];
+        cin >> value;
+        pq.push(value);
     }
+
+    while (!pq.empty() and k > 0) {
+        value = pq.top();
+        value -= x;
+        if (value < 0) value = 0;
+        pq.pop();
+        if (value > 0) {
+            pq.push(value);
+        }
+        --k;
+    }
+
+
 }
