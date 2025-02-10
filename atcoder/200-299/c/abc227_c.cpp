0ca388bd42b6b694998960570004b25a10b5d8a8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-10 11:40:03 +0800

    atcoder/200-299/c/abc227_c.cpp

--- atcoder/200-299/c/abc227_c.cpp
+++ atcoder/200-299/c/abc227_c.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
-typedef long long ll;
-
-ll n;
+#define ll long long
 
 int main() {
+    ll n;
     cin >> n;
+
+
     ll ret = 0;
-    for (ll a = 1; a * a * a <= n; a++) {
-        for (ll b = a; a * b * b <= n; b++) {
-            ll max_c = n / a / b;
-            ret += max_c - b + 1;
+
+    for (ll a = 1; a * a * a <= n; ++a) {
+        for (ll b = a; a * b * b <= n; ++b) {
+            ll max_c = n / b / a;
+            max_c = max_c - b + 1;
+
+            ret += max_c;
         }
     }
+
     cout << ret << endl;
 }
