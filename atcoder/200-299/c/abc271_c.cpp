6e99ff8ddee1fd7589450a245d6694cb40020091
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-11 15:00:17 +0800

    atcoder/200-299/c/abc271_c.cpp

--- atcoder/200-299/c/abc271_c.cpp
+++ atcoder/200-299/c/abc271_c.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
+#include <queue>
 
+using namespace std;
+
+#define ll long long
 
 int main() {
-    int n, q, type, x;
-    std::string s;
-    std::cin >> n >> q >> s;
-    int beg = 0;
+    int n;
+    cin >> n;
+    ll value;
+    priority_queue<ll> pq_low, pq_high;
 
-    for (auto i = 1; i <= q; i++) {
-        std::cin >> type >> x;
+    for (auto i = 1; i <= n; ++i) {
+        cin >> value;
+        pq_low.push(-1 * value);
+        pq_high.push(value);
+    }
 
-        if (type == 2) {
-            x--;
-            std::cout << s[(beg + x) % n] << '\n';
-        }
+    ll response = 0;
+    bool condition = true;
 
-        if (type == 1) {
-            beg = (beg + n - x) % n;
+    while (condition and !pq_low.empty()) {
+        value = pq_low.top();
+
+        if (value == response + 1) {
+            pq_low.pop();
+            ++response;
+        } else if (value != response + 1) {
+            if (pq_high.empty()) {
+                condition = false;
+                break;
+            }
+            ll value1 = pq_high.top();
+            if (value1 <= value) {
+                condition = false;
+                break;
+            }
+            pq_high.pop();
+            if (pq_high.empty()) {
+                condition = false;
+                break;
+            }
+            ll value2 = pq_high.top();
+            if (value2 <= value) {
+                condition = false;
+                break;
+            }
+            pq_high.pop();
+            response += 1;
         }
     }
+
+    cout << response << endl;
 }
