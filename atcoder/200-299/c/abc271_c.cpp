c636e9688394735c2a9822e4d147d3caaac9cc6d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-11 15:25:48 +0800

    atcoder/200-299/c/abc271_c.cpp

--- atcoder/200-299/c/abc271_c.cpp
+++ atcoder/200-299/c/abc271_c.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include <set>
 
 using namespace std;
 
@@ -9,46 +9,28 @@ int main() {
     int n;
     cin >> n;
     ll value;
-    priority_queue<ll> pq_low, pq_high;
+    multiset<ll> s;
 
     for (auto i = 1; i <= n; ++i) {
         cin >> value;
-        pq_low.push(-1 * value);
-        pq_high.push(value);
+        s.insert(value);
     }
 
     ll response = 0;
-    bool condition = true;
 
-    while (condition and !pq_low.empty()) {
-        value = -1 * pq_low.top();
+    while (!s.empty()) {
+        value = *s.begin();
 
         if (value == response + 1) {
-            pq_low.pop();
-            ++response;
-        } else if (value != response + 1) {
-            if (pq_high.empty()) {
-                condition = false;
-                break;
-            }
-            ll value1 = pq_high.top();
-            if (value1 <= value) {
-                condition = false;
-                break;
-            }
-            pq_high.pop();
-            if (pq_high.empty()) {
-                condition = false;
-                break;
-            }
-            ll value2 = pq_high.top();
-            if (value2 <= value) {
-                condition = false;
-                break;
-            }
-            pq_high.pop();
-            response += 1;
+            s.erase(s.begin());
+        } else {
+            if (s.empty()) break;
+            s.erase(std::prev(s.end()));
+            if (s.empty()) break;
+            s.erase(std::prev(s.end()));
         }
+
+        ++response;
     }
 
     cout << response << endl;
