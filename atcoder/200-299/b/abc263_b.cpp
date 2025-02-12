3238a84c59965f9704dbfce59dedb6c72d42bf13
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-12 16:30:50 +0800

    atcoder/200-299/b/abc263_b.cpp

--- atcoder/200-299/b/abc263_b.cpp
+++ atcoder/200-299/b/abc263_b.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <set>
 
 using namespace std;
 
 int main() {
-    unsigned int n, k, tasteness, max_tasteness = 0, dislike;
-    set<unsigned int> max_tasteness_pool{};
-    std::cin >> n >> k;
+    unsigned int n, value, response = 0;
+    std::cin >> n;
 
     for (auto i = 1; i <= n; i++) {
-        std::cin >> tasteness;
+        std::cin >> value;
 
-        if (tasteness > max_tasteness) {
-            max_tasteness = tasteness;
-            max_tasteness_pool = {};
-            max_tasteness_pool.insert(i);
-        } else if (tasteness == max_tasteness) {
-            max_tasteness_pool.insert(i);
-        }
-    }
-
-    string response = "No";
-
-    for (auto i = 1; i <= k; i++) {
-        std::cin >> dislike;
-
-        if (response == "No" and max_tasteness_pool.count(dislike) > 0) {
-            response = "Yes";
+        if (value == n - 1 and response == 0) {
+            response = i;
         }
     }
 
