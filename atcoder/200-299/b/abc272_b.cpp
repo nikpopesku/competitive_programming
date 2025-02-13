0d77a451403b121dd6d3590552bafd808d04a99a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-13 12:10:12 +0800

    atcoder/200-299/b/abc272_b.cpp

--- atcoder/200-299/b/abc272_b.cpp
+++ atcoder/200-299/b/abc272_b.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
-#include <vector>
+#include <set>
 
 using namespace std;
 
 int main() {
-    unsigned int n, m;
+    unsigned int n, m, value;
     cin >> n >> m;
 
+    set<pair<int, int>> pairs;
 
-    for (int i = 1; i <= m; ++i) {
-        vector<unsigned int> vc(n);
-
-        for (int j = 0; j < n; ++j) {
-            cin >> vc[j];
+    for (int i = 1; i < n - 1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            pairs.insert({i, j});
         }
     }
 
-    cout << 1 << '\n';
+
+    for (int i = 1; i <= m; ++i) {
+        set<unsigned int> s;
+
+        for (int j = 0; j < n; ++j) {
+            cin >> value;
+            s.insert(value);
+        }
+
+        for (auto it1 = s.begin(); it1 != s.end(); ++it1) {
+            for (auto it2 = ++it1; it2 != s.end(); ++it2) {
+                pairs.erase({*it1, *it2});
+            }
+        }
+    }
+
+    cout << (pairs.empty() ? "Yes" : "No") << '\n';
 }
