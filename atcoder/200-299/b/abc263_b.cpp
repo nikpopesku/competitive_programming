f506a1d0a609a459aeb2a08c0bbb43e8712ab025
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-13 11:35:48 +0800

    atcoder/200-299/b/abc263_b.cpp

--- atcoder/200-299/b/abc263_b.cpp
+++ atcoder/200-299/b/abc263_b.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    int n, value, response = 0;
-    std::cin >> n;
-
-    for (auto i = 1; i < n; i++) {
-        std::cin >> value;
-
-        if (value + 1 == n and response == 0) {
-            response = i;
-        }
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    
+    for (int i = 1; i < n; i++) {
+        cin >> p[i];
+        p[i]--;
     }
 
-    std::cout << response << std::endl;
+    int ans = 0;
+    for (int v = n - 1; v; v = p[v]) ans++;
+    cout << ans << '\n';
 }
