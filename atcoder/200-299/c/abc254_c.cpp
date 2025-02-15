94f087ccfb8e11feaf53c17cd86fec9ce97b3357
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-15 18:19:32 +0800

    atcoder/200-299/c/abc254_c.cpp

--- atcoder/200-299/c/abc254_c.cpp
+++ atcoder/200-299/c/abc254_c.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-#define ll long long
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -12,20 +10,20 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<ll> vc(n + 1, 0);
+    vector<int> vc(n + 1, 0);
 
-    for (ll i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         cin >> vc[i];
     }
 
-    for (ll i = 1; i <= n - k; ++i) {
+    for (int i = 1; i <= n - k; ++i) {
         if (vc[i] > vc[i + k]) {
             swap(vc[i], vc[i + k]);
         }
     }
 
     string response = "Yes";
-    for (ll i = 1; i <= n; ++i) {
+    for (int i = 1; i <= n; ++i) {
         if (vc[i] < vc[i - 1]) {
             response = "No";
             break;
