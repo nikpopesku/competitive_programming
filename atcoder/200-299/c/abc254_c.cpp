b2e0dbc9bbd54419ac2b36146c207cffcbeeff09
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-15 18:11:43 +0800

    atcoder/200-299/c/abc254_c.cpp

--- atcoder/200-299/c/abc254_c.cpp
+++ atcoder/200-299/c/abc254_c.cpp
@@ -12,11 +12,27 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<ll> vc(n);
+    vector<ll> vc(n + 1, 0), pos(n + 1, 0);
 
-    for (ll i = 0; i < n; ++i) {
+    for (ll i = 1; i <= n; ++i) {
         cin >> vc[i];
+        pos[vc[i]] = i;
     }
 
-    for (ll i = 0; i < n; ++i) cout << vc[i] << ' ';
+    for (ll i = 1; i <= n - k; ++i) {
+        if (pos[i] != i) {
+            swap(vc[i], vc[i + k]);
+            swap(pos[i], pos[i + k]);
+        }
+    }
+
+    string response = "Yes";
+    for (ll i = 1; i <= n; ++i) {
+        if (i != vc[i]) {
+            response = "No";
+            break;
+        }
+    }
+
+    cout << response << '\n';
 }
