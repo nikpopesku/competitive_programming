925943fe4706fe7f8f4779527f19d45f12919314
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-15 14:55:42 +0800

    atcoder/200-299/c/abc250_c.cpp

--- atcoder/200-299/c/abc250_c.cpp
+++ atcoder/200-299/c/abc250_c.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 #define ll long long
 
 int main() {
-    int n, q;
+    int n, q, value, sw;
     cin >> n >> q;
+
+    map<int, int> mp;
+    for (int i = 1; i <= n; ++i) mp[i] = i;
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> value;
+        if (mp[value] + 1 <= n) {
+            sw = mp[value] + 1;
+        } else {
+            sw = mp[value] - 1;
+        }
+
+        int temp = mp[sw];
+        mp[sw] = value;
+        mp[value] = temp;
+    }
+
+    for (int i = 1; i < n; ++i) cout << mp[i] << ' ';
+    cout << mp[n - 1] << '\n';
 }
