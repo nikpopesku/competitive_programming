68f9d1955855e0a966ac857c8bc87319d5d12c09
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-15 17:56:56 +0800

    atcoder/200-299/c/abc254_c.cpp

--- atcoder/200-299/c/abc254_c.cpp
+++ atcoder/200-299/c/abc254_c.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+#define ll long long
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -10,4 +12,11 @@ int main() {
     int n, k;
     cin >> n >> k;
 
+    vector<ll> vc(n);
+
+    for (ll i = 0; i < n; ++i) {
+        cin >> vc[i];
+    }
+
+    for (ll i = 0; i < n; ++i) cout << vc[i] << ' ';
 }
