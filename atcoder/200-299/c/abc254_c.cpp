c18b02ff690daec4fd18b792cd0b5fcf2915f5e3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-17 12:25:13 +0800

    atcoder/200-299/c/abc254_c.cpp

--- atcoder/200-299/c/abc254_c.cpp
+++ atcoder/200-299/c/abc254_c.cpp
@@ -10,18 +10,21 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    vector<int> vc(n + 1, 0), pos(n+1, 0);
+    vector<int> vc(n + 1, 0);
 
     for (int i = 1; i <= n; ++i) {
         cin >> vc[i];
-        pos[vc[i]] = 1;
     }
 
-    for (int i = 1; i <= n; ++i) {
-        if (vc[i] != i and abs(pos[i] - i) % k == 0) {
-            int value = vc[i];
-            swap(vc[i], vc[pos[i]]);
-            swap(pos[i], pos[value]);
+    for (int i = 1; i <= n - k; ++i) {
+        int j = i;
+        while (j <= n) {
+            if (vc[j] < vc[i]) {
+                swap(vc[i], vc[j]);
+                break;
+            }
+
+            j += k;
         }
     }
 
