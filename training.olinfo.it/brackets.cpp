8d86941995ed686babcc8b4e507d112bf9458aff
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-19 15:35:11 +0800

    training.olinfo.it/brackets.cpp

--- training.olinfo.it/brackets.cpp
+++ training.olinfo.it/brackets.cpp
@@ -1,8 +1,7 @@
 // NOTE: it is recommended to use this even if you don't understand the following code.
 
 #include <cassert>
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 
 // constraints
 #define MAXN 400000
@@ -12,6 +11,7 @@ using namespace std;
 // input data
 int N, K, T, i, t;
 vector<int> P;
+stack<int> st;
 
 int main() {
     //  uncomment the following lines if you want to read/write from files
@@ -19,14 +19,23 @@ int main() {
     //  freopen("output.txt", "w", stdout);
 
     assert(cin >> T);
-    for(t = 0; t < T; t++) {
+    for (t = 0; t < T; t++) {
         assert(cin >> N >> K);
         P.resize(N);
-        for(i = 0; i < N; i++) {
+        for (i = 0; i < N; i++) {
             assert(cin >> P[i]);
         }
+
+        for (i = 0; i < N; ++i) {
+            if (st.empty() or st.top() != P[i]) {
+                st.push(P[i]);
+            } else if (st.top() == P[i]) {
+                st.pop();
+            }
+        }
+
         // write your code here
-        cout << 42 << endl; // print the result
+        cout << (st.empty() ? 1 : 0) << "\n"; // print the result
     }
     return 0;
 }
