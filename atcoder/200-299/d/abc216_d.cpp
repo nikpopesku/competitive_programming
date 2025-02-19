ac66183f1bb12fa08482163e481a569941b99bde
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-19 16:39:14 +0800

    atcoder/200-299/d/abc216_d.cpp

--- atcoder/200-299/d/abc216_d.cpp
+++ atcoder/200-299/d/abc216_d.cpp
@@ -28,7 +28,7 @@ int main() {
     iota(to_parse.begin(), to_parse.end(), 0);
 
     while (!to_parse.empty()) {
-        vector<int> to_add;
+        vector<int> to_add {};
         for (int i = 0; i < to_parse.size(); ++i) {
             if (!cylinder[i].empty()) {
                 value = cylinder[i].top();
@@ -40,6 +40,8 @@ int main() {
                     to_add.push_back(pool[value]);
                     pool.erase(value);
                 }
+
+                cylinder[i].pop();
             }
         }
 
