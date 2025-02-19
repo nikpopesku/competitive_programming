601f2a9a584d005a9531ac23b585382a29a567da
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-19 15:46:39 +0800

    atcoder/200-299/d/abc216_d.cpp

---
+++ atcoder/200-299/d/abc216_d.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+unordered_map<ll, ll> mp;
+
+ll calc(ll n) {
+    if (n == 0) return 1;
+    if (mp.contains(n)) return mp[n];
+
+    mp[n] = calc(n / 2) + calc(n / 3);
+
+    return mp[n];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin >> n;
+
+    cout << calc(n) << "\n";
+}
