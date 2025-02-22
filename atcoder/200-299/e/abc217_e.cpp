3df9c1d978ea9955c74614d7a64295887781cf56
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-22 17:06:31 +0800

    atcoder/200-299/e/abc217_e.cpp

--- atcoder/200-299/e/abc217_e.cpp
+++ atcoder/200-299/e/abc217_e.cpp
@@ -2,33 +2,6 @@
 
 using namespace std;
 
-string type;
-int x;
-deque<int> a;
-priority_queue<int> pq;
-
-void solve() {
-    cin >> type;
-
-    if (type == "1") {
-        cin >> x;
-        a.push_back(x);
-        pq.push(-x);
-    }
-
-    if (type == "2") {
-        cout << a.front() << '\n';
-        a.pop_front();
-    }
-
-    if (type == "3") {
-        a = {};
-        while (!pq.empty()) {
-            a.push_back(-pq.top());
-            pq.pop();
-        }
-    }
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -37,8 +10,49 @@ int main() {
 
     int q;
     cin >> q;
+    string type;
+    int x;
+    deque<int> a;
+    priority_queue<int> pq1, pq2;
+
 
     while (q--) {
-        solve();
+
+        cin >> type;
+
+        if (type == "1") {
+            cin >> x;
+            a.push_back(x);
+            pq1.push(-x);
+        }
+
+        if (type == "2") {
+            int value = a.front();
+            cout << value << '\n';
+            a.pop_front();
+            pq2 = pq1;
+            pq1 = {};
+
+            bool popped = false;
+
+            while (!pq2.empty()) {
+                if (-value == pq2.top() and !popped) {
+                    popped = true;
+                } else {
+                    pq1.push(pq2.top());
+                }
+
+                pq2.pop();
+            }
+        }
+
+        if (type == "3") {
+            a = {};
+            pq2 = pq1;
+            while (!pq2.empty()) {
+                a.push_back(-pq2.top());
+                pq2.pop();
+            }
+        }
     }
 }
