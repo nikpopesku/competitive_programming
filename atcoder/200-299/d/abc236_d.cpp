1b1c9682ea6af9b42eb405f17c4d6acd1070777c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-26 16:26:25 +0800

    atcoder/200-299/d/abc236_d.cpp

--- atcoder/200-299/d/abc236_d.cpp
+++ atcoder/200-299/d/abc236_d.cpp
@@ -3,20 +3,28 @@
 using namespace std;
 
 #define ll long long
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
     ll value;
-    unordered_map<int, ll> mp{};
+    unordered_map<string, ll> mp{};
     int n;
     cin >> n;
 
     for (int i = 0; i < 2 * n; ++i) {
         for (int j = i + 1; j < 2 * n; ++j) {
-            cin >> value;
-            mp[i * 10 + j] = value;
+            cin >> mp[to_string(i) + "_" + to_string(j)];
         }
+    }
+
+    for (int i = 0; i < 2 * n; ++i) {
+        for (int j = i + 1; j < 2 * n; ++j) {
+            cout << mp[to_string(i) + "_" + to_string(j)] << " ";
+        }
+
+        cout << "\n";
     }
 }
