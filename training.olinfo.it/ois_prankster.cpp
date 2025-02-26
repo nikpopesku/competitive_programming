6d3c9cb6b1bedcefe0cbb2a0025c567499e26204
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-26 13:20:55 +0800

    training.olinfo.it/ois_prankster.cpp

--- training.olinfo.it/ois_prankster.cpp
+++ training.olinfo.it/ois_prankster.cpp
@@ -12,7 +12,6 @@ int main() {
     for (int i = 0; i < N; i++) cin >> S[i];
 
     int response = 0;
-    int argument;
     for (int i = 0; i < N; ++i) {
         if (S[i] != 0) {
             int count_1 = 0;
@@ -23,12 +22,12 @@ int main() {
                 if (S[j] == 2) ++count_2;
                 ++j;
             }
-            argument = count_1 > count_2 ? -1 : 1;
+            int argument = count_1 > count_2 ? -1 : 1;
             ++response;
             for (int k = i; k < j; ++k) S[k] = (S[k] + argument) % 3;
             --i;
         }
     }
 
-    cout << response << endl;
+    cout << response << "\n";
 }
