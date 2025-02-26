0ba6412a24a40953aab6c27ab6fcaf3936802ec1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-26 13:38:32 +0800

    training.olinfo.it/ois_prankster.cpp

--- training.olinfo.it/ois_prankster.cpp
+++ training.olinfo.it/ois_prankster.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
+#include <numeric>
 
 int main() {
-    int N;
-    vector<int> S;
-
-    cin >> N;
-    S.resize(N);
-    for (int i = 0; i < N; i++) cin >> S[i];
-
-    int response = 0;
-    for (int i = 0; i < N; ++i) {
-        if (S[i] != 0) {
-            int count_1 = 0;
-            int count_2 = 0;
-            int j = i;
-            while (j < N and S[j] != 0) {
-                if (S[j] == 1) ++count_1;
-                if (S[j] == 2) ++count_2;
-                ++j;
-            }
-            int argument = count_1 > count_2 ? -1 : 1;
-            ++response;
-            for (int k = i; k < j; ++k) S[k] = (S[k] + argument) % 3;
-            --i;
+    int n;
+    std::cin >> n;
+    std::vector<int> s(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> s[i];
+    }
+    if (n <= 1) {
+        std::cout << 0 << std::endl;
+        return 0;
+    }
+    std::vector<int> d(n);
+    d[0] = s[0];
+    for (int i = 1; i < n; ++i) {
+        d[i] = s[i] - s[i - 1];
+    }
+    int operations_count = 0;
+    for (int i = 1; i < n; ++i) {
+        if (d[i] != 0) {
+            operations_count++;
         }
     }
-
-    cout << response << "\n";
+    std::cout << operations_count << std::endl;
+    return 0;
 }
