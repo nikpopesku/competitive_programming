e7db7e13b038862b329e46534bb52c75f7bf8449
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-26 16:23:49 +0800

    atcoder/200-299/d/abc236_d.cpp

--- atcoder/200-299/d/abc236_d.cpp
+++ atcoder/200-299/d/abc236_d.cpp
@@ -2,62 +2,21 @@
 
 using namespace std;
 
+#define ll long long
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n, m, k, value;
-    cin >> n >> m;
-    vector<vector<int>> cylinder(m);
-    unordered_map<int, int> pool;
+    ll value;
+    unordered_map<int, ll> mp{};
+    int n;
+    cin >> n;
 
-    for (int i = 0; i < m; ++i) {
-        cin >> k;
-
-        cylinder[i].resize(k);
-
-        for (int j = 0; j < k; ++j) {
-            cin >> cylinder[i][j];
+    for (int i = 0; i < 2 * n; ++i) {
+        for (int j = i + 1; j < 2 * n; ++j) {
+            cin >> value;
+            mp[i * 10 + j] = value;
         }
     }
-
-    vector<int> to_parse(m);
-    iota(to_parse.begin(), to_parse.end(), 0);
-
-    while (!to_parse.empty()) {
-        vector<int> to_add{};
-        for (auto &j: to_parse) {
-            if (!cylinder[j].empty()) {
-                value = cylinder[j].back();
-
-                if (!pool.contains(value)) {
-                    pool[value] = j;
-                } else {
-                    to_add.push_back(j);
-                    to_add.push_back(pool[value]);
-                    cylinder[j].pop_back();
-                    cylinder[pool[value]].pop_back();
-                    pool.erase(value);
-                }
-            }
-        }
-
-        to_parse = to_add;
-    }
-
-    string response = "No";
-
-    if (pool.empty()) {
-        response = "Yes";
-
-        for (int i = 0; i < m; ++i) {
-            if (!cylinder[i].empty()) {
-                response = "No";
-                break;
-            }
-        }
-    }
-
-    cout << response << "\n";
 }
