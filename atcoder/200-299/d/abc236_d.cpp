aa3df0950c567ab2b7e68169ae7635e18440d4c5
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-27 12:23:46 +0800

    atcoder/200-299/d/abc236_d.cpp

--- atcoder/200-299/d/abc236_d.cpp
+++ atcoder/200-299/d/abc236_d.cpp
@@ -4,15 +4,35 @@ using namespace std;
 
 #define ll long long
 
+ll max_answer = 0;
+
+ll calc(unordered_map<string, ll> &mp, set<int> s, ll answer) {
+    for (auto it1 = s.begin(); it1 != --s.end(); ++it1) {
+        for (auto it2 = ++it1; it2 != s.end(); ++it2) {
+            answer ^= mp[to_string(*it1) + "_" + to_string(*it2)];
+            s.erase(it1);
+            s.erase(it2);
+
+            answer = calc(mp, s, answer);
+        }
+    }
+
+    if (answer > max_answer) max_answer = answer;
+
+    return answer;
+};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    ll value;
+    ll answer = 0;
     unordered_map<string, ll> mp{};
+
     int n;
     cin >> n;
+
 
     for (int i = 0; i < 2 * n; ++i) {
         for (int j = i + 1; j < 2 * n; ++j) {
@@ -25,6 +45,12 @@ int main() {
             cout << mp[to_string(i) + "_" + to_string(j)] << " ";
         }
 
-        cout << "\n";
+        std::vector<int> vec(2 * n);
+        std::iota(vec.begin(), vec.end(), 0);
+        set<int> s;
+        s.insert(vec.begin(), vec.end());
+        calc(mp, s, answer);
+
+        cout << max_answer << "\n";
     }
 }
