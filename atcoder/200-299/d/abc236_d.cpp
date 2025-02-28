47fa1da10bb3476a7d79e73a13197e79df259e11
npopesku <nikolay.popesku@unbiased.co.uk>
2025-02-28 14:47:34 +0800

    atcoder/200-299/d/abc236_d.cpp

--- atcoder/200-299/d/abc236_d.cpp
+++ atcoder/200-299/d/abc236_d.cpp
@@ -4,36 +4,60 @@ using namespace std;
 
 #define ll long long
 
+ll max_answer = 0;
+vector<pair<int, int>> pairs{};
+int used[16];
+int a[16][16];
+int n;
+
+ll calc() {
+    if (pairs.size() == n) {
+        ll answer = 0;
+        for (auto &elem: pairs) answer ^= a[elem.first][elem.second];
+
+        return answer;
+    }
+    int l = 0;
+    while (l < 2 * n) {
+        if (!used[l]) {
+            used[l] = true;
+            break;
+        }
+
+        ++l;
+    }
+
+    for (int r = 0; r < 2 * n; ++r) {
+        if (!used[r]) {
+            used[r] = true;
+            pairs.emplace_back(l, r);
+            max_answer = max(max_answer, calc());
+            pairs.pop_back();
+            used[r] = false;
+        }
+    }
+
+
+    used[l] = false;
+
+    return max_answer;
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-
-    ll max_answer = 0;
-    unordered_map<string, ll> mp{};
-
-    int n;
     cin >> n;
 
 
-    for (int i = 0; i < 2 * n; ++i) {
+    for (int i = 0; i < 2 * n - 1; ++i) {
         for (int j = i + 1; j < 2 * n; ++j) {
-            cin >> mp[to_string(i) + "_" + to_string(j)];
+            cin >> a[i][j];
         }
     }
 
-    std::vector<int> v(2 * n);
-    std::iota(v.begin(), v.end(), 0);
-
-
-    do {
-        ll answer = 0;
-        for (int i = 0; i <= n; i += 2)
-            answer ^= mp[to_string(min(v[i], v[i + 1])) + "_" + to_string(max(v[i], v[i + 1]))];
-        if (answer > max_answer) max_answer = answer;
-    } while (next_permutation(v.begin(), v.end()));
+    calc();
 
     cout << max_answer << "\n";
 }
