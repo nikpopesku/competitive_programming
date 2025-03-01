997af498046e8c9f08edfd995d63054fd2e8ac12
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-01 15:52:47 +0800

    training.olinfo.it/ois_butoaie.cpp

--- training.olinfo.it/ois_butoaie.cpp
+++ training.olinfo.it/ois_butoaie.cpp
@@ -9,19 +9,19 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, K, Q, P;
+    ll N, K, Q, P;
     ll value;
     priority_queue<ll> pq;
 
     cin >> N >> K;
     cin >> P >> Q;
-    for (int i = 0; i < N; i++) {
+    for (ll i = 0; i < N; i++) {
         cin >> value;
         pq.push(value);
     }
 
-    int response = 0;
-    vector<pair<int, int>> power;
+    ll response = 0;
+    vector<pair<ll, ll>> power;
     power.emplace_back(K, P);
     power.emplace_back(N - K, Q);
     if (Q > P) swap(power[0], power[1]);
@@ -29,22 +29,30 @@ int main() {
 
     while (!pq.empty()) {
         vector<ll> parsed{};
+        ll min_value = 1'000'000'000;
 
         for (auto &power_elem: power) {
-            int count = 0;
+            ll count = 0;
             while (count < power_elem.first and !pq.empty()) {
                 value = pq.top();
                 pq.pop();
                 value = max(0LL, value - power_elem.second);
                 if (value > 0) parsed.push_back(value);
+                min_value = min(min_value, value / power_elem.second);
                 ++count;
             }
         }
-
-        for (auto &p: parsed) pq.push(p);
-
-
         ++response;
+
+        if (min_value > 0) {
+            response += min_value;
+        }
+
+        for (auto i = 0; i < parsed.size(); ++i) {
+            ll pp = i < power[0].first ? power[0].second : power[1].second;
+            parsed[i] -= min_value * pp;
+            pq.push(parsed[i]);
+        }
     }
 
     cout << response << "\n";
