7e8ccf196f2847b44ca2cf13e3e97397f4d19b65
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-03 17:56:40 +0800

    cses.fi/1634.cpp

--- cses.fi/1634.cpp
+++ cses.fi/1634.cpp
@@ -9,10 +9,21 @@ int main() {
 
     int n, x;
     cin >> n >> x;
-    vector<int> vc(n, -1);
-    vector<int> dp(x, -1);
+    vector<int> coins(n, -1);
+    vector<int> dp(x + 1, -1);
+    dp[0] = 0;
 
-    for (int i = 0; i < n; ++i) cin >> vc[i];
+    for (int i = 0; i < n; ++i) cin >> coins[i];
+    for (auto &coin: coins) dp[coin] = 1;
 
+    for (int i = 0; i < dp.size(); ++i) {
+        for (auto &c: coins) {
+            if (i - c >= 0) {
+                int current = dp[i] == -1 ? 1'000'000'000 : dp[i];
+                dp[i] = min(current, dp[i - c] + 1);
+            }
+        }
+    }
 
+    cout << dp[x] << "\n";
 }
