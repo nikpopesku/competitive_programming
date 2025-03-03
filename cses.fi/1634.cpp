35fb91c61fb3a85229843f384b3dd504a46a859c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-03 17:38:44 +0800

    cses.fi/1634.cpp

---
+++ cses.fi/1634.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> vc(n, -1);
+    vector<int> dp(x, -1);
+
+    for (int i = 0; i < n; ++i) cin >> vc[i];
+
+
+}
