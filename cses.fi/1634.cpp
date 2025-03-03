92546c8113fc4860c075d4a29176c2765c1f49c4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-03 18:02:43 +0800

    cses.fi/1634.cpp

--- cses.fi/1634.cpp
+++ cses.fi/1634.cpp
@@ -15,9 +15,9 @@ int main() {
 
     for (int i = 0; i < n; ++i) cin >> coins[i];
 
-    for (int i = 0; i < dp.size(); ++i) {
+    for (int i = 1; i < dp.size(); ++i) {
         for (auto &c: coins) {
-            if (i - c >= 0) {
+            if (i - c >= 0 and dp[i - c] >= 0) {
                 int current = dp[i] == -1 ? 1'000'000'000 : dp[i];
                 dp[i] = min(current, dp[i - c] + 1);
             }
