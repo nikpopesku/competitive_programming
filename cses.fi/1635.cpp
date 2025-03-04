5a08c5163de5f47b0d993644b19d42d36aacd078
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-04 13:57:23 +0800

    cses.fi/1635.cpp

--- cses.fi/1635.cpp
+++ cses.fi/1635.cpp
@@ -18,7 +18,7 @@ int main() {
     for (int i = 1; i < dp.size(); ++i) {
         for (auto &c: coins) {
             if (i >= c) {
-                dp[i] += dp[i-c];
+                dp[i] += dp[i - c];
             }
         }
     }
