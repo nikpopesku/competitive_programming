80d4d047571da781ed4888c7cb2838d26ba49327
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-04 14:56:29 +0800

    cses.fi/1638.cpp

--- cses.fi/1638.cpp
+++ cses.fi/1638.cpp
@@ -10,20 +10,22 @@ int main() {
     const int MOD = 1e9 + 7;
     int n;
     cin >> n;
-    vector<vector<int>> vc(n);
+    vector<vector<string>> vc(n);
     vector<vector<int>> dp(n);
     for (int i = 0; i < n; ++i) {
+        vc[i].resize(n);
+        dp[i].resize(n);
         for (int j = 0; j < n; ++j) {
             cin >> vc[i][j];
             dp[i][j] = 0;
         }
     }
 
-    dp[0][0] = vc[0][0] != '*' ? 1 : 0;
+    dp[0][0] = vc[0][0] != "*" ? 1 : 0;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (i > 0 and vc[i - 1][j] != '*') {
+            if (i > 0 and vc[i - 1][j] != "*") {
                 dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
             }
             if (j > 0 and vc[i][j - 1] != '*') {
