7376858c823301e6f372f33260c639269b9c9952
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-04 13:58:58 +0800

    cses.fi/1635.cpp

--- cses.fi/1635.cpp
+++ cses.fi/1635.cpp
@@ -13,7 +13,7 @@ int main() {
     for (int i = 0; i < n; ++i) cin >> coins[i];
 
     vector<int> dp(x + 1, 0);
-    for (auto &c: coins) dp[c] = 1;
+    for (auto &c: coins) if (c <= x) dp[c] = 1;
 
     for (int i = 1; i < dp.size(); ++i) {
         for (auto &c: coins) {
