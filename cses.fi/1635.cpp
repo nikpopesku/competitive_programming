fe37430e30ae342705b235cd14c66d58a61fbf10
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-04 14:18:56 +0800

    cses.fi/1635.cpp

--- cses.fi/1635.cpp
+++ cses.fi/1635.cpp
@@ -2,30 +2,27 @@
 
 using namespace std;
 
-#define ll long long
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    ll n, x;
+    const int MOD = 1e9 + 7;
+    int n, x;
     cin >> n >> x;
-    vector<ll> coins(n);
-    for (ll i = 0; i < n; ++i) cin >> coins[i];
+    vector<int> coins(n);
+    for (int i = 0; i < n; ++i) cin >> coins[i];
 
-    vector<ll> dp(x + 1, 0);
+    vector<int> dp(x + 1, 0);
     for (auto &c: coins) if (c <= x) dp[c] = 1;
 
     for (auto i = 1; i < dp.size(); ++i) {
         for (auto &c: coins) {
             if (i >= c) {
-                dp[i] += dp[i - c];
+                dp[i] = (dp[i] + dp[i - c]) % MOD;
             }
         }
     }
 
-    ll modulo = 10e9 + 7;
-
-    cout << dp[x] % modulo << "\n";
+    cout << dp[x] << "\n";
 }
