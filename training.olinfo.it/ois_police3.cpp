8d2eebbe8dc1a1f08fcd6a34f3fc53e65a688587
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-05 14:24:21 +0800

    training.olinfo.it/ois_police3.cpp

--- training.olinfo.it/ois_police3.cpp
+++ training.olinfo.it/ois_police3.cpp
@@ -1,39 +1,32 @@
+// NOTE: it is recommended to use this even if you don't understand the following code.
+
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main() {
+//  uncomment the following lines if you want to read/write from files
+//  ifstream cin("input.txt");
+//  ofstream cout("output.txt");
+
     int N;
     cin >> N;
 
     vector<int> T(N);
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; i++)
         cin >> T[i];
-    }
 
-    // dp[i][0]: stop at i-th traffic light
-    // dp[i][1]: skip i-th traffic light
     vector<vector<int>> dp(N, vector<int>(2));
+    dp[0][0] = T[0]; //stop
+    dp[0][1] = 0; //go
 
-    // Base case
-    dp[0][0] = T[0]; // Stop at the first traffic light
-    dp[0][1] = 0;    // Skip the first traffic light
-
-    // Fill the DP table
-    for (int i = 1; i < N; ++i) {
-        // If we stop at the i-th traffic light, we could have stopped or skipped the previous one
+    for (int i = 1; i < dp.size(); ++i) {
         dp[i][0] = T[i] + min(dp[i - 1][0], dp[i - 1][1]);
-
-        // If we skip the i-th traffic light, we must have stopped at the previous one
         dp[i][1] = dp[i - 1][0];
     }
 
-    // The answer is the minimum of stopping or skipping the last traffic light
-    int result = min(dp[N - 1][0], dp[N - 1][1]);
-
-    cout << result << endl;
-
+    cout << min(dp[N - 1][0], dp[N - 1][1]) << endl;
     return 0;
 }
