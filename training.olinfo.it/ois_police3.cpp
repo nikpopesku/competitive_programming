d8cdb8fe70a5907d66994c8e1efa1ed401c63f7a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-05 13:54:14 +0800

    training.olinfo.it/ois_police3.cpp

--- training.olinfo.it/ois_police3.cpp
+++ training.olinfo.it/ois_police3.cpp
@@ -10,12 +10,23 @@ int main() {
     for (int i = 0; i < N; i++)
         cin >> T[i];
 
-    vector<int> dp(N, 0);
+    if (N == 1) {
+        cout << "0\n";
+        return 0;
+    }
+
+    if (N == 2) {
+        cout <<  min(T[0], T[1]) << "\n";
+        return 0;
+    }
+
+    vector<int> dp(N);
+    dp[0] = T[0];
     dp[1] = min(T[1], T[0]);
 
     for (int i = 2; i < dp.size(); ++i) {
         dp[i] = min(dp[i-2] + T[i], dp[i-1] + T[i-1]);
     }
 
-    cout << dp[N - 1] << "\n";
+    cout << min(dp[N - 1], dp[N-2]) << "\n";
 }
