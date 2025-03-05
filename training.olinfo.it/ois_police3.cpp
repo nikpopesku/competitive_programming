1d09a99420cbf0f6b059dc3f55200b4bc9ed4396
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-05 14:00:48 +0800

    training.olinfo.it/ois_police3.cpp

--- training.olinfo.it/ois_police3.cpp
+++ training.olinfo.it/ois_police3.cpp
@@ -24,14 +24,14 @@ int main() {
     // Fill the DP table
     for (int i = 1; i < N; ++i) {
         // If we stop at the i-th traffic light, we could have stopped or skipped the previous one
-        dp[i][0] = T[i] + min(dp[i-1][0], dp[i-1][1]);
+        dp[i][0] = T[i] + min(dp[i - 1][0], dp[i - 1][1]);
 
         // If we skip the i-th traffic light, we must have stopped at the previous one
-        dp[i][1] = dp[i-1][0];
+        dp[i][1] = dp[i - 1][0];
     }
 
     // The answer is the minimum of stopping or skipping the last traffic light
-    int result = min(dp[N-1][0], dp[N-1][1]);
+    int result = min(dp[N - 1][0], dp[N - 1][1]);
 
     cout << result << endl;
 
