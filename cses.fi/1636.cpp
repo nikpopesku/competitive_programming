de869d67b04458481c344317eae6b8a9b4af1dd3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-07 17:12:19 +0800

    cses.fi/1636.cpp

--- cses.fi/1636.cpp
+++ cses.fi/1636.cpp
@@ -3,35 +3,28 @@
 using namespace std;
 
 #define ll long long
-
+const ll MOD = 1e9 + 7;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    const ll MOD = 1e9 + 7;
-
-
     ll n, x;
     cin >> n >> x;
     vector<ll> coins(n);
     for (ll i = 0; i < n; ++i) cin >> coins[i];
     sort(coins.begin(), coins.end());
-    queue<pair<ll, ll>> q;
-    ll response = 0;
-    q.emplace(x, 0LL);
 
-    while (!q.empty()) {
-        auto elem = q.front();
-        q.pop();
-        if (elem.first == 0) response = (response + 1) % MOD;
-        for (ll j = elem.second; j < n; ++j) {
-            if (coins[j] > elem.first) break;
+    vector<ll> dp(x + 1, 0);
+    dp[0] = 1;
 
-            q.emplace(elem.first - coins[j], j);
+    for (ll coin : coins) {
+        if (coin > x) break;
+        for (ll sum = coin; sum <= x; ++sum) {
+            dp[sum] = (dp[sum] + dp[sum - coin]) % MOD;
         }
     }
 
-    cout << response % MOD << "\n";
+    cout << dp[x] << "\n";
 }
