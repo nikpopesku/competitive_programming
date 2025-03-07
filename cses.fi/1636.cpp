b5fa9460efed669a08fa47354d6d2579edf61ae4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-07 15:41:05 +0800

    cses.fi/1636.cpp

--- cses.fi/1636.cpp
+++ cses.fi/1636.cpp
@@ -3,30 +3,14 @@
 using namespace std;
 
 #define ll long long
-const ll MOD = 1e9 + 7;
 
-ll calc(ll i, ll left_value, ll &n, vector<ll> &coins) {
-    if (left_value == 0) {
-        return 1;
-    }
-
-    if (left_value < 0) return 0;
-
-    ll response = 0;
-
-    for (ll j = i; j < n; ++j) {
-        if (coins[j] > left_value) break;
-
-        response = (response + calc(j, left_value - coins[j], n, coins)) % MOD;
-    }
-
-    return response % MOD;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    const ll MOD = 1e9 + 7;
 
 
     ll n, x;
@@ -34,6 +18,21 @@ int main() {
     vector<ll> coins(n);
     for (ll i = 0; i < n; ++i) cin >> coins[i];
     sort(coins.begin(), coins.end());
+    queue<pair<ll, ll>> q;
+    ll response = 0;
+    q.emplace(x, 0LL);
 
-    cout << calc(0, x, n, coins) << "\n";
+    while (!q.empty()) {
+        auto elem = q.front();
+        q.pop();
+        if (elem.first < 0) continue;
+        if (elem.first == 0) response = (response + 1) % MOD;
+        for (ll j = elem.second; j < n; ++j) {
+            if (coins[j] > elem.first) break;
+
+            q.emplace(elem.first - coins[j], j);
+        }
+    }
+
+    cout << response << "\n";
 }
