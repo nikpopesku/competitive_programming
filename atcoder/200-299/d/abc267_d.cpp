c9550db1ccfd5e9ea6d805a2a61d713cf46cbc37
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-12 14:08:42 +0800

    atcoder/200-299/d/abc267_d.cpp

--- atcoder/200-299/d/abc267_d.cpp
+++ atcoder/200-299/d/abc267_d.cpp
@@ -1,16 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
-
-#define ll long long
+long long dp[2001][2001];
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    ll N, M;
-    cin >> N >> M;
-
-    cout << 1 << "\n";
+    int n, m;
+    cin >> n >> m;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    dp[0][0] = 0;
+    dp[0][1] = -1000000000000000000ll;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (j == 0) dp[i][0] = 0;
+            else if (j > i) dp[i][j] = -1000000000000000000ll;
+            else dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1] + a[i - 1] * j);
+        }
+    }
+    cout << dp[n][m] << endl;
 }
