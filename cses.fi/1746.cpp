8e7e34a63780c975c2e548be92760f1c2553b17a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-13 11:45:49 +0800

    cses.fi/1746.cpp

---
+++ cses.fi/1746.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    const int MOD = 1e9 + 7;
+    int n, m;
+    cin >> n >> m;
+    vector<ll> num(n);
+    for (int i = 0; i < n; ++i) cin >> num[i];
+
+    vector<vector<ll>> vc(n, vector<ll>(m+1, 0));
+
+
+}
