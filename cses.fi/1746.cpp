a0f3e5909f7d9dfa830af6d2ac68be7ad054e84d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-13 11:54:40 +0800

    cses.fi/1746.cpp

--- cses.fi/1746.cpp
+++ cses.fi/1746.cpp
@@ -15,7 +15,23 @@ int main() {
     vector<ll> num(n);
     for (int i = 0; i < n; ++i) cin >> num[i];
 
-    vector<vector<ll>> vc(n, vector<ll>(m+1, 0));
+    vector<vector<ll>> dp(n, vector<ll>(m + 1, 0));
 
+    for (int i = 0; i < n; ++i) {
+        if (num[i] > 0 and i > 0) {
+            dp[i][num[i]] = (dp[i - 1][num[i] - 1] + dp[i - 1][num[i]] + dp[i - 1][num[i] + 1]) % MOD;
+        }
 
+        if (num[i] > 0 and i == 0) {
+            dp[i][num[i]] = 1;
+        }
+
+        if (num[i] == 0 and i > 0) {
+            dp[i][num[i - 1] + 1] = dp[i - 1][num[i - 1]];
+            dp[i][num[i - 1]] = dp[i - 1][num[i - 1]];
+            dp[i][num[i - 1] - 1] = dp[i - 1][num[i - 1]];
+        }
+    }
+
+    cout << dp[n - 1][num[n - 1]] << "\n";
 }
