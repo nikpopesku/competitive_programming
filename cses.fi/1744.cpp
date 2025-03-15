dd0a199d9976e862a1a984884e4b3949a3e4d305
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-15 14:20:49 +0800

    cses.fi/1744.cpp

--- cses.fi/1744.cpp
+++ cses.fi/1744.cpp
@@ -3,28 +3,27 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    std::cin >> a >> b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    std::vector<std::vector<int>> dp(a + 1, std::vector<int>(b + 1, 0));
+    int a, b;
+    cin >> a >> b;
+    int response = 0;
+    vector dp(a + 1, vector<int>(b + 1, 0));
 
     for (int i = 1; i <= a; ++i) {
         for (int j = 1; j <= b; ++j) {
             if (i == j) {
                 dp[i][j] = 0;
-            } else {
-                dp[i][j] = 1e9; // Initialize with a large value
-                for (int k = 1; k < i; ++k) {
-                    dp[i][j] = std::min(dp[i][j], dp[k][j] + dp[i - k][j] + 1);
-                }
-                for (int k = 1; k < j; ++k) {
-                    dp[i][j] = std::min(dp[i][j], dp[i][k] + dp[i][j - k] + 1);
-                }
+                continue;
             }
+
+            dp[i][j] = 1e9;
+            for (int k = 1; k < i; ++k) dp[i][j] = min(dp[i][j], dp[k][j] + dp[i - k][j] + 1);
+            for (int k = 1; k < j; ++k) dp[i][j] = min(dp[i][j], dp[i][k] + dp[i][j - k] + 1);
         }
     }
 
-    std::cout << dp[a][b] << std::endl;
-
-    return 0;
+    cout << dp[a][b] << "\n";
 }
