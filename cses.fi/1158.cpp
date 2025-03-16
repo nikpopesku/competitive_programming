74e9fd1b511135587b0127f823a1f6034c730dd1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-16 16:48:50 +0800

    cses.fi/1158.cpp

--- cses.fi/1158.cpp
+++ cses.fi/1158.cpp
@@ -14,16 +14,15 @@ int main() {
     for (int i = 0; i < n; ++i) cin >> price[i];
     for (int i = 0; i < n; ++i) cin >> pages[i];
 
-    vector<int> dp(x + 1, 0);
+    vector dp(n + 1, vector<int>(x + 1, 0));
 
-    for (int j = 0; j < n; ++j) {
-        for (int i = 1; i <= x; ++i) {
-            if (dp[i] > 0 and price[j] + i <= x and dp[i] + pages[j] > dp[i + price[j]]) {
-                dp[i + price[j]] = dp[i] + pages[j];
+    for (int i = 0; i <= n; ++i) {
+        for (int j = 0; j <= x; ++j) {
+            if (j - price[i] >= 0) {
+                dp[i][j] =
             }
         }
-        if (price[j] <= x and dp[0] + pages[j] > dp[price[j]]) dp[price[j]] = dp[0] + pages[j];
     }
 
-    cout << *max_element(dp.begin(), dp.end()) << "\n";
+    cout << 1 << "\n";
 }
