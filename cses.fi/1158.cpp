512e246698cb9d5a399a113abcc5faa5e794eaba
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-16 17:01:10 +0800

    cses.fi/1158.cpp

--- cses.fi/1158.cpp
+++ cses.fi/1158.cpp
@@ -9,17 +9,17 @@ int main() {
 
     int n, x;
     cin >> n >> x;
-    vector<int> price(n + 1, 0);
+    vector<int> prices(n + 1, 0);
     vector<int> pages(n + 1, 0);
-    for (int i = 1; i <= n; ++i) cin >> price[i];
+    for (int i = 1; i <= n; ++i) cin >> prices[i];
     for (int i = 1; i <= n; ++i) cin >> pages[i];
 
     vector dp(n + 1, vector<int>(x + 1, 0));
 
-    for (int i = 0; i <= n; ++i) {
-        for (int j = 0; j <= x; ++j) {
-            if (j - price[i] >= 0) {
-                dp[i][j] = max(dp[i][j - price[i]] + pages[i], dp[i - 1][j]);
+    for (int i = 1; i <= n; ++i) {
+        for (int price = 0; price <= x; ++price) {
+            if (price - prices[i] >= 0) {
+                dp[i][price] = max(dp[i][price - prices[i]] + pages[i], dp[i - 1][price]);
             }
         }
     }
