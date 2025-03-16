c30cfcf501e54019e945d95d38d491342ee45549
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-16 14:04:45 +0800

    cses.fi/1158.cpp

--- cses.fi/1158.cpp
+++ cses.fi/1158.cpp
@@ -7,20 +7,13 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    string s1, s2;
-    cin >> s1 >> s2;
-    vector dp(s1.length() + 1, vector<int>(s2.length() + 1, 1e9));
-    dp[0][0] = 0;
+    int n, x;
+    cin >> n >> x;
+    vector<int> price(n, 0);
+    vector<int> pages(n, 0);
 
-    for (size_t i = 1; i <= s1.length(); ++i) dp[i][0] = i;
-    for (size_t j = 1; j <= s2.length(); ++j) dp[0][j] = j;
+    for (int i = 0; i <= n; ++i) cin >> price[i];
+    for (int i = 0; i <= n; ++i) cin>> pages[i];
 
-    for (size_t i = 1; i <= s1.length(); ++i) {
-        for (size_t j = 1; j <= s2.length(); ++j) {
-            dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
-                           dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
-        }
-    }
-
-    cout << dp[s1.length()][s2.length()] << "\n";
+    cout << 1 << "\n";
 }
