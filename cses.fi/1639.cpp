a45ee1ff884332bfd1a0849979f8bcbc383196e3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-16 13:21:42 +0800

    cses.fi/1639.cpp

--- cses.fi/1639.cpp
+++ cses.fi/1639.cpp
@@ -17,7 +17,8 @@ int main() {
 
     for (int i = 1; i <= s1.length(); ++i) {
         for (int j = 1; j <= s2.length(); ++j) {
-            dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
+            dp[i][j] = min(min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
+                           dp[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
         }
     }
 
