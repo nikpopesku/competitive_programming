fdbb8d97acc3bc83260aa27f5721d64940746b82
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-19 16:25:42 +0800

    codeforces/cpp/1700-1799/1765M.cpp

--- codeforces/cpp/1700-1799/1765M.cpp
+++ codeforces/cpp/1700-1799/1765M.cpp
@@ -1,26 +1,14 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
+
+using namespace std;
 
 int main() {
     int t, n;
-    std::string s;
-
-    std::cin >> t;
+    cin >> t;
 
     for (int i = 0; i < t; i++) {
-        std::cin >> n;
-        std::cin >> s;
+        cin >> n;
 
-        std::vector<int> alphabet(26, 0);
-        int sum = 0;
-
-        for (auto &ch: s) {
-            int index = int(ch) - int('A');
-            int increase = alphabet[index] == 0 ? 2 : 1;
-            alphabet[index] += increase;
-            sum += increase;
-        }
-
-        std::cout << sum << std::endl;
+        std::cout << 1 << "\n";
     }
 }
