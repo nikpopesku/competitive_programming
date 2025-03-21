6082b98fff064e90fb5c94fb9942905b6bc8cf3a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-21 16:01:11 +0800

    codeforces/cpp/1700-1799/1764B.cpp

---
+++ codeforces/cpp/1700-1799/1764B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, value;
+        cin >> n;
+
+        vector<int> vc;
+
+        for (int i = 0; i < n ; ++i) {
+            cin >> value;
+            vc.push_back(value);
+        }
+
+        cout << 1 << "\n";
+    }
+}
