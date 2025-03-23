e2ea87e199a89bff087de2c8e4bf79615b926285
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-23 14:09:37 +0800

    codeforces/cpp/1700-1799/1764B.cpp

--- codeforces/cpp/1700-1799/1764B.cpp
+++ codeforces/cpp/1700-1799/1764B.cpp
@@ -15,10 +15,10 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, value;
+        int n;
         cin >> n;
 
-        vector<int> vc;
+        vector<int> vc(n);
 
         for (int i = 0; i < n; ++i) cin >> vc[i];
         int first = *vc.begin();
