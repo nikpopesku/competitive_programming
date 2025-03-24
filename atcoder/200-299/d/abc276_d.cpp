dcc71f6b39b82192118cd3195327afc6e6d87ca0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-24 12:17:57 +0800

    atcoder/200-299/d/abc276_d.cpp

--- atcoder/200-299/d/abc276_d.cpp
+++ atcoder/200-299/d/abc276_d.cpp
@@ -11,10 +11,33 @@ int main() {
     cin >> N;
     vector<int> num(N);
 
+    for (int i = 0; i < N; ++i) cin >> num[i];
+    int commonGcd = num[0];
+    for (int i = 1; i < N; ++i) commonGcd = gcd(commonGcd, num[i]);
+
+    int response = 0;
     for (int i = 0; i < N; ++i) {
-        cin >> num[i];
+        int value = num[i] / commonGcd;
+        while (value > 1) {
+            if (value % 2 == 0) {
+                value /= 2;
+                ++response;
+                continue;
+            }
+            if (value % 3 == 0) {
+                value /= 3;
+                ++response;
+                continue;
+            }
+            if (value % 2 != 0 and value % 3 != 0) break;
+        }
+
+        if (value > 1) {
+            commonGcd = -1;
+            break;
+        }
     }
 
 
-    cout << 1 << "\n";
+    cout << (commonGcd < 0 ? -1 : response) << "\n";
 }
