65fe8707b5e4d9a65ef1b7dcada5ed194e252320
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-25 14:51:28 +0800

    atcoder/200-299/d/abc215_d.cpp

--- atcoder/200-299/d/abc215_d.cpp
+++ atcoder/200-299/d/abc215_d.cpp
@@ -7,11 +7,35 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, M;
+    int N, M, value;
     cin >> N >> M;
-    vector<int> num(N);
+    set<int> a;
 
-    for (int i = 0; i < N; ++i) cin >> num[i];
+    for (int i = 0; i < N; ++i) {
+        cin >> value;
+        a.insert(value);
+    }
 
-    cout << 1 << "\n";
+    set<int> num;
+
+    for (int i = 1; i <= M; ++i) {
+        if (a.find(i) != a.end()) continue;
+
+        num.insert(i);
+    }
+
+    for (auto it = a.begin(); it != a.end(); ++it) {
+        auto it2 = num.begin();
+
+        while (it2 != num.end()) {
+            if (gcd(*it, *it2) != 1) {
+                it2 = num.erase(it2);
+            } else {
+                ++it2;
+            }
+        }
+    }
+
+    cout << num.size() << "\n";
+    for (auto &elem: num) cout << elem << "\n";
 }
