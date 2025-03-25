e0273fc2c85a94a87e10975ed6a0fe93d1ccb4f9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-25 13:54:19 +0800

    atcoder/200-299/d/abc215_d.cpp

---
+++ atcoder/200-299/d/abc215_d.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    vector<int> num(N);
+
+    for (int i = 0; i < N; ++i) cin >> num[i];
+
+    cout << 1 << "\n";
+}
