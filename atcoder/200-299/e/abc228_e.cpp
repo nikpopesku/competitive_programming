61876fad149841649b3da53493c125f9a969af22
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-26 14:10:55 +0800

    atcoder/200-299/e/abc228_e.cpp

--- atcoder/200-299/e/abc228_e.cpp
+++ atcoder/200-299/e/abc228_e.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 #define ll long long
 
-ll modpow(ll x, ll n, ll m) {
+int pow_mod(ll x, ll n, ll m) {
     if (n == 0) return 1 % m;
-    ll u = modpow(x, n / 2, m);
+
+    ll u = pow_mod(x, n / 2, m);
     u = (u * u) % m;
     if (n % 2 == 1) u = (u * x) % m;
 
@@ -14,21 +15,14 @@ ll modpow(ll x, ll n, ll m) {
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
     const ll MOD = 998244353;
-    const ll MOD_MINUS_1 = MOD - 1;
 
     ll N, K, M;
     cin >> N >> K >> M;
-
     if (M % MOD == 0) {
         cout << 0 << endl;
         return 0;
     }
-
-    ll val = modpow(K, N, MOD_MINUS_1);
-    cout << modpow(M, val, MOD) << '\n';
+    ll x = pow_mod(K, N, MOD - 1);
+    cout << pow_mod(M, x, MOD) << endl;
 }
