be35367825cb4e38964ca8e9020e8fde06410b37
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-29 13:56:13 +0800

    cses.fi/1600-1699/1666.cpp

--- cses.fi/1600-1699/1666.cpp
+++ cses.fi/1600-1699/1666.cpp
@@ -10,11 +10,13 @@ int main() {
     int n, m, a, b;
     cin >> n >> m;
     vector<vector<int>> roads(n + 1);
+    vector<pair<int, int>> response;
 
     for (int i = 0; i < m; ++i) {
         cin >> a >> b;
         roads[a].push_back(b);
     }
 
-    cout << 1 << "\n";
+    cout << response.size() << "\n";
+    for (int i = 0; i < response.size(); ++i) cout << response[i].first << " " << response[i].second << "\n";
 }
