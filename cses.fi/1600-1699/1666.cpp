15bed8bc8360d78f1b1895ed195fe7cccb3f9864
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-29 13:36:14 +0800

    cses.fi/1600-1699/1666.cpp

---
+++ cses.fi/1600-1699/1666.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> coins(n);
+    vector<int> dp(x + 1, -1);
+    dp[0] = 0;
+
+    for (int i = 0; i < n; ++i) cin >> coins[i];
+
+    for (int i = 1; i < dp.size(); ++i) {
+        for (auto &c: coins) {
+            if (i - c >= 0 and dp[i - c] >= 0) {
+                int current = dp[i] == -1 ? 1'000'000'000 : dp[i];
+                dp[i] = min(current, dp[i - c] + 1);
+            }
+        }
+    }
+
+    cout << dp[x] << "\n";
+}
