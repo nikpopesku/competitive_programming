5a8390e18f1b80cbb47d1c5f980308b0be8e6bfa
npopesku <nikolay.popesku@unbiased.co.uk>
2025-03-31 12:02:23 +0800

    atcoder/000-099/b/arc065_b.cpp

--- atcoder/000-099/b/arc065_b.cpp
+++ atcoder/000-099/b/arc065_b.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 #define ll long long
 
+void
+dfs(vector<bool> &visited, int city_index, vector<set<int>> &roads, vector<set<int>> &railways, vector<int> &response) {
+    visited[city_index] = true;
+    ++response[city_index];
+
+    for (size_t i = 0; i < roads[city_index].size(); ++i) {
+        if (!visited[i] && railways[city_index].count(i)) {
+            dfs(visited, i, roads, railways, response);
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -11,22 +23,29 @@ int main() {
 
     int N, K, L, city1, city2;
     cin >> N >> K >> L;
-    vector<vector<int>> roads(K);
-    vector<vector<int>> railways(L);
-    vector<int> response(N, 1);
+    vector<set<int>> roads(K);
+    vector<set<int>> railways(L);
+    vector<int> response(N, 0);
+    vector<bool> visited(N, false);
 
     for (int i = 0; i < K; ++i) {
         cin >> city1 >> city2;
 
-        roads[city1].push_back(city2);
-        roads[city2].push_back(city1);
+        roads[city1].insert(city2);
+        roads[city2].insert(city1);
     }
 
     for (int i = 0; i < L; ++i) {
         cin >> city1 >> city2;
 
-        railways[city1].push_back(city2);
-        railways[city2].push_back(city1);
+        railways[city1].insert(city2);
+        railways[city2].insert(city1);
+    }
+
+    for (size_t i = 0; i < visited.size(); ++i) {
+        if (visited[i]) continue;
+
+        dfs(visited, i, roads, railways, response);
     }
 
     for (size_t i = 0; i < response.size(); ++i) cout << response[i] << (i != response.size() - 1 ? " " : "\n");
