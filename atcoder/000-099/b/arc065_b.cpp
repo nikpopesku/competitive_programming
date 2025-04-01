722264c62ebae37f4157a2bd0ec034f60601bcfc
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-01 15:03:52 +0800

    UN-29896: Applied fix

--- atcoder/000-099/b/arc065_b.cpp
+++ atcoder/000-099/b/arc065_b.cpp
@@ -10,6 +10,15 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N, K, L;
+    int N, K, L, road1, road2;
     cin >> N >> K >> L;
+
+    vector<vector<int>> roads(N+1);
+
+    for (int i = 0; i < K; ++i) {
+        cin >> road1 >> road2;
+
+        roads[road1].push_back(road2);
+        roads[road2].push_back(road1);
+    }
 }
