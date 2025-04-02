30868540b5a32fcbb4f9b9139547bf2f4e4c26bd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-02 13:45:40 +0800

    training.olinfo.it/ois_islands.cpp

--- training.olinfo.it/ois_islands.cpp
+++ training.olinfo.it/ois_islands.cpp
@@ -2,24 +2,24 @@
 
 using namespace std;
 
-// constraints
-#define MAXN 1000
 int R, C;
-int M[MAXN][MAXN];
 
-void dfs(int i, int j, vector<vector<bool>> &visited) {
+
+void dfs(int i, int j, vector<vector<bool>> &visited, vector<vector<int>> &M) {
     visited[i][j] = true;
 
     for (int vertical: {1, 0, -1})
         for (int horizontal: {1, 0, -1})
-            if (abs(horizontal) + abs(vertical) < 2 and i + vertical >= 0 and i + vertical < R and j + horizontal >= 0 and j + horizontal < C and
+            if (abs(horizontal) + abs(vertical) < 2 and i + vertical >= 0 and i + vertical < R and
+                j + horizontal >= 0 and j + horizontal < C and
                 !visited[i + vertical][j + horizontal] and M[i + vertical][j + horizontal])
-                dfs(i + vertical, j + horizontal, visited);
+                dfs(i + vertical, j + horizontal, visited, M);
 }
 
 int main() {
     assert(2 == scanf("%d %d", &R, &C));
     vector visited(R, vector<bool>(C, false));
+    vector M(R, vector<int>(C, 0));
 
     for (int i = 0; i < R; i++)
         for (int j = 0; j < C; j++) {
@@ -29,7 +29,7 @@ int main() {
 
     for (int i: {0, R - 1}) {
         for (int j: {0, C - 1}) {
-            if (M[i][j]) dfs(i, j, visited);
+            if (M[i][j]) dfs(i, j, visited, M);
 
             visited[i][j] = true;
         }
@@ -42,7 +42,7 @@ int main() {
             if (visited[i][j]) continue;
             if (!M[i][j]) continue;
 
-            dfs(i, j, visited);
+            dfs(i, j, visited, M);
             ++response;
         }
     }
