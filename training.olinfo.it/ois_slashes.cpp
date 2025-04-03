236b10b16b6f8e07948a803f29c04c5e4d9c12f7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-03 12:32:34 +0800

    training.olinfo.it/ois_slashes.cpp

--- training.olinfo.it/ois_slashes.cpp
+++ training.olinfo.it/ois_slashes.cpp
@@ -9,7 +9,7 @@ void dfs(int i, int j, vector<vector<bool>> &visited, vector<vector<int>> &vc) {
 
     for (auto &dx: {-1, 0, 1}) {
         for (auto &dy: {-1, 0, 1}) {
-            if (abs(dx) + abs(dy) == 1 and i + dx >= 0 and i + dx < 4 * N and j + dy >= 0 and j + dy < 4 * M and
+            if (abs(dx) + abs(dy) == 1 and i + dx >= 0 and i + dx < 3 * N and j + dy >= 0 and j + dy < 3 * M and
                 !visited[i + dx][j + dy] && vc[i + dx][j + dy]) {
                 dfs(i + dx, j + dy, visited, vc);
             }
@@ -26,18 +26,18 @@ int main() {
     for (int i = 0; i < N; i++)
         cin >> S[i];
 
-    vector vc(4 * N, vector<int>(4 * M, 1));
-    vector visited(4 * N, vector<bool>(4 * M, false));
+    vector vc(3 * N, vector<int>(3 * M, 1));
+    vector visited(3 * N, vector<bool>(3 * M, false));
 
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < M; ++j) {
-            for (int vi = i * 4; vi < i * 4 + 4; ++vi) {
-                for (int vj = j * 4; vj < j * 4 + 4; ++vj) {
-                    if (S[i][j] == '\\' && vi - 4 * i == vj - 4 * j) {
+            for (int vi = i * 3; vi < i * 3 + 3; ++vi) {
+                for (int vj = j * 3; vj < j * 3 + 3; ++vj) {
+                    if (S[i][j] == '\\' && vi - 3 * i == vj - 3 * j) {
                         vc[vi][vj] = 0;
                         continue;
                     }
-                    if (S[i][j] == '/' && vi + vj == i * 4 + j * 4 + 3) {
+                    if (S[i][j] == '/' && vi + vj == i * 3 + j * 3 + 2) {
                         vc[vi][vj] = 0;
                     }
                 }
@@ -47,8 +47,8 @@ int main() {
 
     int response = 0;
 
-    for (int i = 0; i < 4 * N; ++i) {
-        for (int j = 0; j < 4 * M; ++j) {
+    for (int i = 0; i < 3 * N; ++i) {
+        for (int j = 0; j < 3 * M; ++j) {
             if (!visited[i][j] and vc[i][j]) {
                 dfs(i, j, visited, vc);
                 ++response;
