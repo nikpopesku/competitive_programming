4b4c48831442763624dd7a9496d843f271c0e54b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-03 11:56:02 +0800

    training.olinfo.it/ois_slashes.cpp

--- training.olinfo.it/ois_slashes.cpp
+++ training.olinfo.it/ois_slashes.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
-// input data
 int N, M;
 
+void dfs(int i, int j, vector<vector<bool>> &visited, vector<vector<int>> &vc) {
+    visited[i][j] = true;
+
+    for (auto &dx: {-1, 0, 1}) {
+        for (auto &dy: {-1, 0, 1}) {
+            if (dx + dy == 1 and i + dx >= 0 and i + dx < 4 * N and j + dy >= 0 and j + dy < 4 * M and
+                !visited[i + dx][j + dy] && vc[i + dx][i + dy]) {
+                dfs(i + dx, j + dy, visited, vc);
+            }
+        }
+    }
+}
 
 int main() {
 
@@ -16,6 +27,8 @@ int main() {
         cin >> S[i];
 
     vector vc(4 * N, vector<int>(4 * M, 1));
+    vector visited(4 * N, vector<bool>(4 * M, false));
+
     for (int i = 0; i < N; ++i) {
         for (size_t j = 0; j < S.size(); ++j) {
             for (int vi = i * 4; vi < i * 4 + 4; ++vi) {
@@ -32,5 +45,16 @@ int main() {
         }
     }
 
-    cout << 1 << endl;
+    int response = 0;
+
+    for (int i = 0; i < 4 * N; ++i) {
+        for (int j = 0; j < 4 * M; ++j) {
+            if (!visited[i][j]) {
+                dfs(i, j, visited, vc);
+                ++response;
+            }
+        }
+    }
+
+    cout << response << "\n";
 }
