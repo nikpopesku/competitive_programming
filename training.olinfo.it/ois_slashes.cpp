0d8b097d31a98b2c0cd17b5628df48d4fbf8ea34
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-03 11:49:19 +0800

    training.olinfo.it/ois_slashes.cpp

--- training.olinfo.it/ois_slashes.cpp
+++ training.olinfo.it/ois_slashes.cpp
@@ -4,17 +4,33 @@ using namespace std;
 
 // input data
 int N, M;
-vector<string> S;
+
 
 int main() {
 
+    vector<string> S;
     cin >> N >> M;
     S.resize(N);
-    for (int i=0; i<N; i++)
+
+    for (int i = 0; i < N; i++)
         cin >> S[i];
 
-    // insert your code here
+    vector vc(4 * N, vector<int>(4 * M, 1));
+    for (int i = 0; i < N; ++i) {
+        for (size_t j = 0; j < S.size(); ++j) {
+            for (int vi = i * 4; vi < i * 4 + 4; ++vi) {
+                for (int vj = j * 4; vj < j * 4 + 4; ++vj) {
+                    if (S[i][j] == '\\' && vi == vj) {
+                        vc[vi][vj] = 0;
+                        continue;
+                    }
+                    if (S[i][j] == '/' && vi + vj == 3) {
+                        vc[vi][vj] = 0;
+                    }
+                }
+            }
+        }
+    }
 
-    cout << 42 << endl; // print the result
-    return 0;
+    cout << 1 << endl;
 }
