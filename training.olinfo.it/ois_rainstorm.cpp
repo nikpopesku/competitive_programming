4c274498dc77e72645f818811c73d1792e5289b2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-04 14:55:39 +0800

    training.olinfo.it/ois_rainstorm.cpp

--- training.olinfo.it/ois_rainstorm.cpp
+++ training.olinfo.it/ois_rainstorm.cpp
@@ -2,51 +2,52 @@
 
 using namespace std;
 
-void dfs(int index, int level, vector<bool> &visited, vector<vector<int>> &adj_list, map<pair<int, int>, int> &R,
+void dfs(const int index, const int level, vector<bool> &visited, const vector<vector<pair<int, int>>> &adj_list,
          int &counter) {
     visited[index] = true;
     ++counter;
 
-    for (auto &neighbour: adj_list[index]) {
-        if (R[{index, neighbour}] >= level and !visited[neighbour]) {
-            dfs(neighbour, level, visited, adj_list, R, counter);
+    for (const auto &elem: adj_list[index]) {
+        int neighbour = elem.first;
+        int neighbour_level = elem.second;
+        if (neighbour_level >= level && !visited[neighbour]) {
+            dfs(neighbour, level, visited, adj_list, counter);
         }
     }
 }
 
 
 int main() {
-    int N, M, i;
+    int N, M;
 
     int val_a, val_b, val_r, min_level = 100000, max_level = 1;
 
-    assert(2 == scanf("%d %d", &N, &M));
-    map<pair<int, int>, int> R;
-    vector<vector<int>> adj_list(N);
+    cin >> N >> M;
+    vector<vector<pair<int, int>>> adj_list(N);
 
-    for (i = 0; i < M; i++) {
+    for (int i = 0; i < M; i++) {
         cin >> val_a >> val_b >> val_r;
-        adj_list[val_a].push_back(val_b);
-        adj_list[val_b].push_back(val_a);
-        R[{val_a, val_b}] = val_r;
-        R[{val_b, val_a}] = val_r;
-        if (val_r > max_level) max_level = val_r;
-        if (val_r < min_level) min_level = val_r;
+        adj_list[val_a].emplace_back(val_b, val_r);
+        adj_list[val_b].emplace_back(val_a, val_r);
+        min_level = min(min_level, val_r);
+        max_level = max(max_level, val_r);
     }
 
     int left = min_level, right = max_level;
+    int result = 1;
 
-    while (left < right) {
+    while (left <= right) {
         int counter = 0;
         int m = left + (right - left) / 2;
         vector<bool> visited(N, false);
-        dfs(0, m, visited, adj_list, R, counter);
+        dfs(0, m, visited, adj_list, counter);
         if (counter == N) {
-            left = m;
+            left = m + 1;
+            result = m;
         } else {
             right = m - 1;
         }
     }
 
-    cout << left << "\n";
+    cout << result << "\n";
 }
