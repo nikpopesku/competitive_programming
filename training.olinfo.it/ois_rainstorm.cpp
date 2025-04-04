882393b2f733a6309ee4e42aa8b7f6b552ca0665
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-04 12:35:27 +0800

    training.olinfo.it/ois_rainstorm.cpp

--- training.olinfo.it/ois_rainstorm.cpp
+++ training.olinfo.it/ois_rainstorm.cpp
@@ -1,60 +1,31 @@
-#include <bits/stdc++.h>
+/*
+ * This template is valid both in C and in C++,
+ * so you can expand it with code from both languages.
+ * NOTE: it is recommended to use this even if you don't
+ * understand the following code.
+ */
 
-using namespace std;
+#include <stdio.h>
+#include <assert.h>
 
-int N, M;
+// constraints
+#define MAXM 100000
 
-void dfs(int i, int j, vector<vector<bool>> &visited, vector<vector<int>> &vc) {
-    visited[i][j] = true;
-
-    for (auto &dx: {-1, 0, 1}) {
-        for (auto &dy: {-1, 0, 1}) {
-            if (abs(dx) + abs(dy) == 1 and i + dx >= 0 and i + dx < 3 * N and j + dy >= 0 and j + dy < 3 * M and
-                !visited[i + dx][j + dy] && vc[i + dx][j + dy]) {
-                dfs(i + dx, j + dy, visited, vc);
-            }
-        }
-    }
-}
+// input data
+int N, M, i;
+int A[MAXM], B[MAXM], R[MAXM];
 
 int main() {
+//  uncomment the following lines if you want to read/write from files
+//  freopen("input.txt", "r", stdin);
+//  freopen("output.txt", "w", stdout);
 
-    vector<string> S;
-    cin >> N >> M;
-    S.resize(N);
+    assert(2 == scanf("%d %d", &N, &M));
+    for(i=0; i<M; i++)
+        assert(3 == scanf("%d %d %d", &A[i], &B[i], &R[i]));
 
-    for (int i = 0; i < N; i++)
-        cin >> S[i];
+    // insert your code here
 
-    vector vc(3 * N, vector<int>(3 * M, 1));
-    vector visited(3 * N, vector<bool>(3 * M, false));
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
-            for (int vi = i * 3; vi < i * 3 + 3; ++vi) {
-                for (int vj = j * 3; vj < j * 3 + 3; ++vj) {
-                    if (S[i][j] == '\\' && vi - 3 * i == vj - 3 * j) {
-                        vc[vi][vj] = 0;
-                        continue;
-                    }
-                    if (S[i][j] == '/' && vi + vj == i * 3 + j * 3 + 2) {
-                        vc[vi][vj] = 0;
-                    }
-                }
-            }
-        }
-    }
-
-    int response = 0;
-
-    for (int i = 0; i < 3 * N; ++i) {
-        for (int j = 0; j < 3 * M; ++j) {
-            if (!visited[i][j] and vc[i][j]) {
-                dfs(i, j, visited, vc);
-                ++response;
-            }
-        }
-    }
-
-    cout << response << "\n";
+    printf("%d\n", 42); // print the result
+    return 0;
 }
