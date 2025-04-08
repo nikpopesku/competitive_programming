486820cea60e7e4d1b4b6bde06e7fcab59433451
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-08 13:00:52 +0800

    atcoder/200-299/d/abc272_d.cpp

--- atcoder/200-299/d/abc272_d.cpp
+++ atcoder/200-299/d/abc272_d.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <cmath>
 
 using namespace std;
 
@@ -13,6 +15,30 @@ int main() {
 
     vector leap(N, vector<int>(N, -1));
     leap[0][0] = 0;
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+
+    while (!q.empty()) {
+        auto elem = q.front();
+        q.pop();
+
+        for (int i = 0; i < N; ++i) {
+            int val = M * M - (elem.first - i) * (elem.first - i);
+            int sq = round(sqrt(val));
+            if (val != sq * sq) continue;
+
+            if (sq - elem.second >= 0 and sq - elem.second < N and leap[i][sq - elem.second] == -1) {
+                leap[i][sq - elem.second] = leap[elem.first][elem.second] + 1;
+                q.push({i, sq - elem.second});
+            }
+            if (sq + elem.second >= 0 and sq + elem.second < N and leap[i][sq + elem.second] == -1) {
+                leap[i][sq + elem.second] = leap[elem.first][elem.second] + 1;
+                q.push({i, sq + elem.second});
+            }
+        }
+    }
+
 
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) cout << leap[i][j] << " ";
