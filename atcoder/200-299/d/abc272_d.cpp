3e2f18d4f372b699feadd2e0e40976aea1c388f8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-08 13:34:19 +0800

    atcoder/200-299/d/abc272_d.cpp

--- atcoder/200-299/d/abc272_d.cpp
+++ atcoder/200-299/d/abc272_d.cpp
@@ -33,6 +33,21 @@ int main() {
     queue<pair<int, int>> q;
     q.push({0, 0});
 
+    while (!q.empty()) {
+        auto [rx, ry] = q.front();
+        q.pop();
+
+        for (const auto & move: moves) {
+            int dx = rx + move.first;
+            int dy = ry + move.second;
+
+            if (dx >= 0 and dx < N and dy >= 0 and dy < N and leap[dx][dy] == -1) {
+                leap[dx][dy] = leap[rx][ry] + 1;
+                q.push({dx, dy});
+            }
+        }
+    }
+
 
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) cout << leap[i][j] << (j != N-1 ? " " : "");
