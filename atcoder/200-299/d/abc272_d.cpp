d336fddb865f1facc645f72224d626fca0691ba2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-08 13:30:47 +0800

    atcoder/200-299/d/abc272_d.cpp

--- atcoder/200-299/d/abc272_d.cpp
+++ atcoder/200-299/d/abc272_d.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <queue>
 
 
 using namespace std;
@@ -11,4 +13,29 @@ int main() {
 
     int N, M;
     cin >> N >> M;
+    vector<pair<int, int>> moves;
+
+    for (int dx = 0; dx * dx <= M; ++dx) {
+        int sq_dy = M - dx * dx;
+        if (sq_dy < 0) continue;
+        int dy = static_cast<int>(sqrt(sq_dy) + 0.5);
+        if (static_cast<long long>(dy) * dy == sq_dy) {
+            moves.push_back({dx, dy});
+            moves.push_back({-dx, -dy});
+            moves.push_back({dx, -dy});
+            moves.push_back({-dx, dy});
+        }
+    }
+
+    vector leap(N, vector<int>(N, -1));
+    leap[0][0] = 0;
+
+    queue<pair<int, int>> q;
+    q.push({0, 0});
+
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) cout << leap[i][j] << (j != N-1 ? " " : "");
+        cout << "\n";
+    }
 }
