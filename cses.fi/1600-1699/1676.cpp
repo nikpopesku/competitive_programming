51bd89f96677bb7549b5f7e2660cfc0fd9c61e04
npopesku <nic1k@yandex.com>
2025-04-12 20:53:54 +0800

    cses.fi/1600-1699/1676.cpp

--- cses.fi/1600-1699/1676.cpp
+++ cses.fi/1600-1699/1676.cpp
@@ -1,9 +1,39 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<numeric>
 
 using namespace std;
 
+class DisjointSetUnion
+{
+public:
+    explicit DisjointSetUnion(const int size): size(size, 1), parent(size)
+    {
+        iota(parent.begin(), parent.end(), 0);
+    }
 
-int main() {
+    [[nodiscard]] int find(int index) const
+    {
+        while (parent[index] != index)
+        {
+            index = parent[index];
+        }
+
+        return index;
+    }
+
+    [[nodiscard]] bool equal(const int a, const int b) const
+    {
+        return find(a) == find(b);
+    }
+
+private:
+    vector<int> size;
+    vector<int> parent;
+};
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
