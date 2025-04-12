a0496a95a76831de5d0f441b6d5e3c1e7eaf7f22
npopesku <nic1k@yandex.com>
2025-04-12 16:25:33 +0800

    cses.fi/1600-1699/1666_dsu.cpp

--- cses.fi/1600-1699/1666_dsu.cpp
+++ cses.fi/1600-1699/1666_dsu.cpp
@@ -1,9 +1,47 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<numeric>
+#include<utility>
 
 using namespace std;
 
+class DisjointSet
+{
+public:
+    explicit DisjointSet(const int size): parents(size), size(size, 1)
+    {
+        iota(parents.begin(), parents.end(), 0);
+    }
 
-int main() {
+    [[nodiscard]] int find(int index) const
+    {
+        while (parents[index] != index)
+        {
+            index = parents[index];
+        }
+
+        return index;
+    }
+
+    [[nodiscard]] bool unite(int a, int b) const
+    {
+        if (find(a) == find(b)) return false;
+
+        if (size[a] < size[b]) swap(a, b);
+
+        parents[b] = a;
+        size[a] += size[b];
+
+        return true;
+    }
+
+private:
+    vector<int> size;
+    vector<int> parents;
+};
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
