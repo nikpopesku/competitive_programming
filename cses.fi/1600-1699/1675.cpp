548ee912e7ea004e08e079184777470e289c45c6
npopesku <nic1k@yandex.com>
2025-04-14 11:58:16 +0800

    cses.fi/1600-1699/1675.cpp

---
+++ cses.fi/1600-1699/1675.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<numeric>
+#include<set>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << 1 << "\n";
+}
