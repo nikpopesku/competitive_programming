cf20f1c5baf227ab4c187f4a8c22f2bad70cd11f
npopesku <nic1k@yandex.com>
2025-04-14 12:07:25 +0800

    cses.fi/1600-1699/1675.cpp

--- cses.fi/1600-1699/1675.cpp
+++ cses.fi/1600-1699/1675.cpp
@@ -1,9 +1,50 @@
 #include<iostream>
+#include<queue>
 #include<vector>
 #include<numeric>
 #include<set>
 
 using namespace std;
+
+class DisjointSetUnion
+{
+public:
+    explicit DisjointSetUnion(const int size): size(size, 1), parent(size)
+    {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    [[nodiscard]] bool equal(const int a, const int b) const
+    {
+        return find(a) == find(b);
+    }
+
+    [[nodiscard]] int find(int a) const
+    {
+        while (a != parent[a]) a = parent[a];
+
+        return a;
+    }
+
+    bool unify(const int a, const int b)
+    {
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if (parent_a == parent_b) return false;
+
+        if (size[parent_a] < size[parent_b]) swap(parent_a, parent_b);
+
+        size[parent_a] += size[parent_b];
+        parent[parent_b] = parent_a;
+
+        return true;
+    }
+
+private:
+    vector<int> size;
+    vector<int> parent;
+};
 
 int main()
 {
@@ -13,6 +54,16 @@ int main()
 
     int n, m;
     cin >> n >> m;
+    int city1, city2, cost;
+    DisjointSetUnion dsu(n + 1);
+
+    priority_queue<tuple<int, int, int>> q;
+
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> city1 >> city2 >> cost;
+        q.emplace(cost, city1, city2);
+    }
 
     cout << 1 << "\n";
 }
