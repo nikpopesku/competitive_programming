a6638f31df65f2be1be07c285a4e89793e246a89
npopesku <nic1k@yandex.com>
2025-04-15 15:14:28 +0800

    atcoder/200-299/d/abc214_d.cpp

---
+++ atcoder/200-299/d/abc214_d.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+    cin >> N >> M;
+    vector<int> a(N);
+
+    set<int> factor;
+
+    for (int i = 0; i < N; ++i) {
+        cin >> a[i];
+
+        for (int j = 2; j * j <= a[i]; ++j) {
+            while (a[i] % j == 0) {
+                factor.insert(j);
+                a[i] /= j;
+            }
+        }
+
+        if (a[i] > 1) factor.insert(a[i]);
+    }
+
+    vector<bool> response(M + 1, true);
+
+    for (auto &elem: factor) {
+        int value = elem;
+        while (value <= M) {
+            if (response[value]) response[value] = false;
+            value += elem;
+        }
+    }
+
+    int counter = 0;
+    for (int i = 1; i <= M; ++i) if (response[i]) ++counter;
+    cout << counter << "\n";
+    for (int i = 1; i <= M; ++i) if (response[i]) cout << i << "\n";
+}
