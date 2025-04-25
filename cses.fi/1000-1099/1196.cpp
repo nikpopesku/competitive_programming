9c6a51655ae52883332f1b9914e7f4a0293bebe9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-25 13:28:37 +0800

    cses.fi/1000-1099/1196.cpp

--- cses.fi/1000-1099/1196.cpp
+++ cses.fi/1000-1099/1196.cpp
@@ -11,40 +11,8 @@ int main()
     ios::sync_with_stdio(false);
     constexpr ll BIG = 1e14;
 
-    int n, m;
-    cin >> n >> m;
-    int a, b;
-    ll c;
-    vector adj_list(n, vector<pair<ll, int>>(0));
-    vector dist(n, BIG);
-    dist[0] = 0;
+    int n, m, k;
+    cin >> n >> m >> k;
 
-    for (int i = 0; i < m; ++i)
-    {
-        cin >> a >> b >> c;
-        adj_list[a - 1].emplace_back(c, b - 1);
-    }
-
-    using T = pair<ll, int>;
-    priority_queue<T, vector<T>, greater<T>> pq;
-
-    pq.emplace(0, 0);
-
-    while (!pq.empty())
-    {
-        auto const [cdist, city] = pq.top();
-        pq.pop();
-        if (cdist != dist[city]) continue;
-
-        for (const auto& [neighbour_distance, neighbour_city] : adj_list[city])
-        {
-            if (cdist + neighbour_distance < dist[neighbour_city])
-            {
-                dist[neighbour_city] = cdist + neighbour_distance;
-                pq.emplace(dist[neighbour_city], neighbour_city);
-            }
-        }
-    }
-
-    for (int i = 0; i < n; ++i) cout << dist[i] << " ";
+    cout << 1 << "\n";
 }
