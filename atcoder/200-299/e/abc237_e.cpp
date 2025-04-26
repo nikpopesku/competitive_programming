bd411bc04e9b58fd29ee5b2cae2002c9883c8082
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-26 13:06:44 +0800

    atcoder/200-299/e/abc237_e.cpp

--- atcoder/200-299/e/abc237_e.cpp
+++ atcoder/200-299/e/abc237_e.cpp
@@ -1,12 +1,47 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    int N, M;
+
+    cin >> N >> M;
+    vector<int> spaces(N + 1);
+    vector<vector<pair<int, int>>> adj(N + 1);
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cin >> spaces[i];
+    }
+
+    int space1, space2;
+
+    for (int j = 0; j < M; ++j)
+    {
+        cin >> space1 >> space2;
+        int cost, cost_inverse;
+
+        if (spaces[space1] >= spaces[space2])
+        {
+            cost = spaces[space1] - spaces[space2];
+            cost_inverse = -2 * cost;
+        }
+        else
+        {
+            cost_inverse = spaces[space2] - spaces[space1];
+            cost = 2 * cost_inverse;
+        }
+
+        adj[space1].push_back({cost, space2});
+        adj[space2].push_back({cost_inverse, space1});
+    }
 
     cout << 1 << '\n';
 }
