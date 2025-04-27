b059dc36e8d077759d7b5fadac62d71bdd83e688
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-27 21:49:56 +0800

    atcoder/100-199/e/abc191_e.cpp

--- atcoder/100-199/e/abc191_e.cpp
+++ atcoder/100-199/e/abc191_e.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <limits>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 #define ll long long
+
+ll INF = numeric_limits<ll>::max() / 2;
 
 int main()
 {
@@ -13,5 +18,59 @@ int main()
     int N, M, A, B, C;
 
     cin >> N >> M;
+    vector<vector<pair<int, int>>> adj(N + 1);
+    vector self_loop(N + 1, INF);
+    vector min_walk(N + 1, -1LL);
 
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> A >> B >> C;
+        if (B == A)
+        {
+            self_loop[i] = min(self_loop[i], static_cast<ll>(C));
+        }
+        else
+        {
+            adj[A].emplace_back(C, B);
+        }
+    }
+
+
+    for (int start_town = 1; start_town <= N; ++start_town)
+    {
+        priority_queue<pair<ll, int>> pq;
+        vector dist(N + 1, INF);
+        ll current_min_walk = self_loop[start_town];
+
+        for (auto [cost, town] : adj[start_town])
+        {
+            if (cost < dist[town])
+            {
+                dist[town] = cost;
+                pq.emplace(cost, town);
+            }
+        }
+
+        while (!pq.empty())
+        {
+            auto [time, town] = pq.top();
+            pq.pop();
+
+            if (time > dist[town]) continue;
+
+            ll new_cost = time + static_cast<ll>(cost);
+
+            if (town == start_town)
+            {
+                current_min_walk = min(current_min_walk, new_cost);
+            }
+        }
+
+        min_walk[start_town] = current_min_walk == INF ? -1 : current_min_walk;
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        cout << min_walk[i] << "\n";
+    }
 }
