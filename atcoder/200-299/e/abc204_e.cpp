aa934a4bd77753ff37f69c83b93446aa8a22dc4c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-04-29 13:51:13 +0800

    atcoder/200-299/e/abc204_e.cpp

---
+++ atcoder/200-299/e/abc204_e.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M;
+
+    cin >> N >> M;
+
+    cout << 1 << "\n";
+}
