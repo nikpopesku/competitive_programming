36fec4f236f4d0b5b576685d6ad1bacac905a992
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-02 15:02:07 +0800

    atcoder/200-299/e/abc204_e.cpp

--- atcoder/200-299/e/abc204_e.cpp
+++ atcoder/200-299/e/abc204_e.cpp
@@ -32,7 +32,7 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    // cout.tie(nullptr); // Not strictly necessary
+    cout.tie(nullptr); // Not strictly necessary
 
     int N;
     int M; // Use int for N, M as per constraints (<= 2*10^5)
