b506c1fe9e3c6b0dc95120771c25f19811774683
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-02 14:49:24 +0800

    atcoder/200-299/e/abc204_e.cpp

--- atcoder/200-299/e/abc204_e.cpp
+++ atcoder/200-299/e/abc204_e.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,5 +15,13 @@ int main()
     int N;
     int M; // Use int for N, M as per constraints (<= 2*10^5)
     cin >> N >> M;
+    int A, B, C, D;
+    vector<vector<tuple<int, int , int>>> adj(N+1);
 
+    for (int i = 0; i < M; ++i)
+    {
+        cin >> A >> B >> C >> D;
+        adj[A].emplace_back(B, C, D);
+        adj[B].emplace_back(A, C, D);
+    }
 }
