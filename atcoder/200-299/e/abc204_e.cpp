29cd6d6a1a24d3a443689de8049a7c2b3e99e5a6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-02 14:56:12 +0800

    atcoder/200-299/e/abc204_e.cpp

--- atcoder/200-299/e/abc204_e.cpp
+++ atcoder/200-299/e/abc204_e.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <limits>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
 #define ll long long
 
+ll INF = numeric_limits<ll>::max() / 3;
+
+ll calculate_best(ll D)
+{
+    return D;
+}
 
 int main()
 {
@@ -16,12 +24,38 @@ int main()
     int M; // Use int for N, M as per constraints (<= 2*10^5)
     cin >> N >> M;
     int A, B, C, D;
-    vector<vector<tuple<int, int , int>>> adj(N+1);
+    vector<vector<tuple<int, int, int>>> adj(N + 1);
 
     for (int i = 0; i < M; ++i)
     {
         cin >> A >> B >> C >> D;
         adj[A].emplace_back(B, C, D);
         adj[B].emplace_back(A, C, D);
+    }
+
+    using T = pair<ll, int>;
+    priority_queue<T, vector<T>, greater<>> pq;
+    pq.emplace(0, 1);
+    vector<ll> time(N + 1, INF);
+
+    while (!pq.empty())
+    {
+        auto& [time_at_city, city] = pq.top();
+        pq.pop();
+
+        for (auto& [neighbour_city, C, D] : adj[city])
+        {
+            ll best_time = calculate_best(D);
+            const ll departure_time = max(best_time, time_at_city);
+            const ll travel_time = C = D / (departure_time + 1);
+
+            ll time_at_neighbour = travel_time + departure_time;
+
+            if (time_at_neighbour < time[neighbour_city])
+            {
+                time[neighbour_city] = time_at_city;
+                pq.emplace(time_at_city, neighbour_city);
+            }
+        }
     }
 }
