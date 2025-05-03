41c42b1d756c256c01e62a2c914d6d9d0fd97da1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-03 16:58:32 +0800

    atcoder/100-199/c/abc184_c.cpp

--- atcoder/100-199/c/abc184_c.cpp
+++ atcoder/100-199/c/abc184_c.cpp
@@ -27,9 +27,10 @@ int main()
         return 0;
     }
 
-    if (r1 + c1 == r2 + c2 || r1 - c1 == r2 - c2 || abs(r1 - r2) + abs(c1 - c2) <= 3)
+    if ((r1 + c1) % 2 == (r2 + c2) % 2 || (r1 - c1) % 2 == (r2 - c2) % 2 || abs(r1 - r2) + abs(c1 - c2) <= 6 ||
+        abs((r1 - c1) - (r2 - c2)) <= 3 || abs((r1 + c1) - (r2 + c2)) <= 3)
     {
-        cout << "1\n";
+        cout << "2\n";
 
         return 0;
     }
