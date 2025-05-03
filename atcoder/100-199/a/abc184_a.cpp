afcaaf727ee8e8b89f646aba02750affe92a237e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-03 13:32:52 +0800

    atcoder/100-199/a/abc184_a.cpp

---
+++ atcoder/100-199/a/abc184_a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, M, A, B, C;
+
+    cin >> N >> M;
+    cout << 1 << "\n";
+}
