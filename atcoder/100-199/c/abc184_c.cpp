7187fddc16aafd55b28bb7d81c3273c7763efe21
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-03 16:52:49 +0800

    atcoder/100-199/c/abc184_c.cpp

--- atcoder/100-199/c/abc184_c.cpp
+++ atcoder/100-199/c/abc184_c.cpp
@@ -2,11 +2,37 @@
 
 using ll = long long;
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
     ll r1, c1, r2, c2;
-    std::cin >> r1 >> c1 >> r2 >> c2;
+    cin >> r1 >> c1 >> r2 >> c2;
+
+    if (r1 == r2 && c1 == c2)
+    {
+        cout << "0\n";
+
+        return 0;
+    }
+
+    if (r1 + c1 == r2 + c2 || r1 - c1 == r2 - c2 || abs(r1 - r2) + abs(c1 - c2) <= 3)
+    {
+        cout << "1\n";
+
+        return 0;
+    }
+
+    if (r1 + c1 == r2 + c2 || r1 - c1 == r2 - c2 || abs(r1 - r2) + abs(c1 - c2) <= 3)
+    {
+        cout << "1\n";
+
+        return 0;
+    }
+
+    cout << "3\n";
 }
