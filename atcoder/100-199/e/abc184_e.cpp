fe27de76095e1d7dcf3b7c892c71ca56ced4037a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-05 11:49:07 +0800

    atcoder/100-199/e/abc184_e.cpp

--- atcoder/100-199/e/abc184_e.cpp
+++ atcoder/100-199/e/abc184_e.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <limits>
+#include <queue>
 #include <vector>
 
 using namespace std;
@@ -16,13 +17,29 @@ int main()
     cin >> H >> W;
     vector grid(H, vector<string>(W));
     vector dist(H, vector(W, INF));
+    priority_queue<pair<pair<int, int>, int>> pq;
+
 
     for (int i = 1; i <= H; ++i)
     {
         for (int j = 1; j <= W; ++j)
         {
             cin >> grid[i][j];
+            if (grid[i][j] == "S")
+            {
+                dist[i][j] = 0;
+                pq.push({{i, j}, 0});
+            }
         }
+    }
+
+    while (!pq.empty())
+    {
+        auto & [coord, distance] = pq.pop();
+        pq.top();
+        auto & [x, y] = coord;
+
+        if (distance > dist[x][y]) continue;
     }
 
     cout << 1 << "\n";
