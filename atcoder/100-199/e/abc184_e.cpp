442640429a11bbdb20eb1079866c0752f2cb48dd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-05 11:36:36 +0800

    atcoder/100-199/e/abc184_e.cpp

---
+++ atcoder/100-199/e/abc184_e.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    cout << 1 << "\n";
+}
