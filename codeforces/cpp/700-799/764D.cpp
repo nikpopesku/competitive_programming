b4bc49daca7eba28fc83108c7a7699cbf88010f1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-13 09:49:23 +0300

    codeforces/cpp/700-799/764D.cpp

---
+++ codeforces/cpp/700-799/764D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, n, k;
+    string s;
+
+    std::cin >> t;
+
+    while (t-- > 0)
+    {
+        cin >> n >> k;
+        cin >> s;
+
+        cout << 1 << "\n";
+    }
+}
