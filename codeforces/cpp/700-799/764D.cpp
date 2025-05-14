95f2bae90a866ae9d1d2e3e17c487947f132eef7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-14 16:14:56 +0300

    codeforces/cpp/700-799/764D.cpp

--- codeforces/cpp/700-799/764D.cpp
+++ codeforces/cpp/700-799/764D.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -14,6 +15,22 @@ int main()
         cin >> n >> k;
         cin >> s;
 
-        cout << 1 << "\n";
+        unordered_map<char, int> ch;
+
+        for (char i : s) ch[i]++;
+
+        int counter = 0;
+
+        for (const auto& it : ch)
+        {
+            if (it.second % 2 == 1) ++counter;
+        }
+
+        int base = (s.size() - counter) / 2;
+        if (base % 2 != 0) --base;
+
+
+        int additional = k <= counter ? 1 : 0;
+        cout << base + additional << "\n";
     }
 }
