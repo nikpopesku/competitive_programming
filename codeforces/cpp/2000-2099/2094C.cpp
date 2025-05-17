5d68ea7f5c189addb76f2213cc34530e9e50815c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-17 15:51:22 +0300

    codeforces/cpp/2000-2099/2094C.cpp

--- codeforces/cpp/2000-2099/2094C.cpp
+++ codeforces/cpp/2000-2099/2094C.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <numeric>
+#include <set>
 #include <vector>
 
 using namespace std;
@@ -13,12 +15,25 @@ int main()
     {
         cin >> n;
         vector vc(2 * n, -1);
+        vector<int> h(2 * n);
+        iota(h.begin(), h.end(), 1);
+        set<int> s (h.begin(), s.end());
+
+
         for (int i = 1; i <= n; ++i)
         {
             for (int j = 1; j <= n; ++j)
             {
                 cin >> vc[i+j];
+                s.erase(vc[i+j]);
             }
+        }
+
+        auto it = s.begin();
+
+        for (int i = 1; i <= 2 * n; ++i)
+        {
+            cout << (vc[i] != -1 ? vc[i] : *it++) << " ";
         }
     }
 }
