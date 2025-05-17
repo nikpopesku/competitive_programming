e282a469791a1b618e5e82a75db9b8df1d5ea543
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-17 16:16:11 +0300

    codeforces/cpp/2000-2099/2094D.cpp

---
+++ codeforces/cpp/2000-2099/2094D.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s, p;
+        cin >> p;
+        cin >> s;
+    }
+}
