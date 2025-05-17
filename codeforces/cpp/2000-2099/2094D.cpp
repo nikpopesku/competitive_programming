23cf944769639c83f6890e0b2bd0a9ed4121b989
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-17 16:45:38 +0300

    codeforces/cpp/2000-2099/2094D.cpp

--- codeforces/cpp/2000-2099/2094D.cpp
+++ codeforces/cpp/2000-2099/2094D.cpp
@@ -12,5 +12,51 @@ int main()
         string s, p;
         cin >> p;
         cin >> s;
+
+        char cur_p_char = p[0];
+        int cur_p_size = 1;
+        int s_index = 0;
+        string response = "YES";
+
+        for (int i = 1; i < p.size(); ++i)
+        {
+            if (p[i] == cur_p_char)
+            {
+                ++cur_p_size;
+                continue;
+            }
+
+            if (s.size() - s_index < cur_p_size)
+            {
+                response = "NO";
+                break;
+            }
+            int cur_s_size = 0;
+
+            for (int j = s_index; j < s.size(); ++j)
+            {
+                if (s[j] == cur_p_char)
+                {
+                    ++cur_s_size;
+                    s_index = j;
+                    continue;
+                }
+
+
+                if (cur_s_size < cur_p_size or cur_s_size > cur_p_size * 2)
+                {
+                    response = "NO";
+                }
+
+                break;
+            }
+
+            cur_p_char = p[i];
+            cur_p_size = 1;
+
+            if (response == "NO") break;
+        }
+
+        cout << response << "\n";
     }
 }
