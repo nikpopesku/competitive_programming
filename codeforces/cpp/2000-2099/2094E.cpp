ca85bb6436f22235f58839d38900e8ab705eadcd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 12:08:58 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-using namespace std;
-#define int long long
+#include <vector>
 
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector cn(30, 0);
+    vector<ll> v(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+
+        for (int j = 0; j < 30; ++j)
+        {
+            cn[j] += (v[i] >> j) & 1;
+        }
+    }
+}
 
 signed main()
 {
