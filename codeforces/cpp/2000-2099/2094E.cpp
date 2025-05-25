a17f24549612d2f1eb806c091915103ba8ce18d0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 12:13:45 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -21,6 +21,29 @@ void solve()
             cn[j] += (v[i] >> j) & 1;
         }
     }
+
+    ll abs_max = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        ll cur_max = 0;
+
+        for (int j = 0; j < 30; ++j)
+        {
+            if (ll val = (v[i] >> j) & 1; val == 1)
+            {
+                cur_max += (n - cn[j]) * (val << j);
+            }
+            else
+            {
+                cur_max += cn[j] * (val << j);
+            }
+        }
+
+        abs_max = max(abs_max, cur_max);
+    }
+
+    cout << abs_max << "\n";
 }
 
 signed main()
