2d13c80e0afbf80ecc846c6a51129986debd7e73
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 11:34:13 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <set>
 
 using namespace std;
 
@@ -10,18 +11,26 @@ ll solve(const int n)
 {
     vector<ll> a(n);
     priority_queue<ll> pq;
+    set<ll> s;
     ll abs_max = 0;
 
     for (int i = 0; i < n; ++i)
     {
         cin >> a[i];
-        pq.push(a[i]);
+        s.insert(a[i]);
+
+        if (!s.contains(a[i]))
+        {
+            pq.push(a[i]);
+        }
     }
 
     while (!pq.empty())
     {
         const auto elem = pq.top();
         pq.pop();
+
+        if (elem * (n - 1) * 2 < abs_max) break;
 
         ll cur_max = 0;
 
