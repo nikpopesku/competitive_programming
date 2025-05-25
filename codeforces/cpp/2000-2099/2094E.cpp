d4a4f37bdc2f1be70a7de7854ad1aaca65d44324
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 12:03:32 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -1,35 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 #define int long long
 
-void solve()
-{
-    int n;
-    cin >> n;
-    int arr[n + 1];
-    vector<int> cnt(30, 0);
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> arr[i];
-        for (int j = 0; j < 30; j++)
-        {
-            cnt[j] += ((arr[i] >> j) & 1);
-        }
-    }
-    int ans = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        int tot = 0;
-        for (int j = 0; j < 30; j++)
-        {
-            bool f = ((arr[i] >> j) & 1);
-            if (f) tot += (1 << j) * (n - cnt[j]);
-            else tot += (1 << j) * cnt[j];
-        }
-        ans = max(ans, tot);
-    }
-    cout << ans << "\n";
-}
 
 signed main()
 {
@@ -38,5 +10,4 @@ signed main()
     int t;
     cin >> t;
     while (t--) solve();
-    return 0;
 }
