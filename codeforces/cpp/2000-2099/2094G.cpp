61bfe7d90209c51b61d8f2ed9c6a615c1b453af7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 18:40:13 +0200

    codeforces/cpp/2000-2099/2094G.cpp

--- codeforces/cpp/2000-2099/2094G.cpp
+++ codeforces/cpp/2000-2099/2094G.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+void calc(const vector<int>& vc)
+{
+    int response = 0;
+    for (int i = 0; i < vc.size(); ++i) response += i * vc[i];
+
+    cout << response << "\n";
+}
+
 void solve()
 {
+    int q, type, value;
+    cin >> q;
+
+    for (int i = 0; i < q; ++i)
+    {
+        vector<int> vc;
+        cin >> type;
+
+        if (type == 2)
+        {
+            ranges::reverse(vc);
+        }
+        else if (type == 3)
+        {
+            cin >> value;
+            vc.push_back(value);
+        }
+        else
+        {
+            rotate(vc.rbegin(), vc.rbegin() + 1, vc.rend());
+        }
+
+        calc(vc);
+    }
 }
 
 int main()
