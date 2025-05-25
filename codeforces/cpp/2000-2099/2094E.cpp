ac07d241e6ebf49515f7c4a10493a4bbfe6c2253
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 11:47:32 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 #define int long long
 
-void solve() {
-    int n; cin >> n;
-    int arr[n+1];
+void solve()
+{
+    int n;
+    cin >> n;
+    int arr[n + 1];
     vector<int> cnt(30, 0);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         cin >> arr[i];
-        for (int j = 0; j < 30; j++) {
+        for (int j = 0; j < 30; j++)
+        {
             cnt[j] += ((arr[i] >> j) & 1);
         }
     }
     int ans = 0;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
+    {
         int tot = 0;
-        for (int j = 0; j < 30; j++) {
+        for (int j = 0; j < 30; j++)
+        {
             bool f = ((arr[i] >> j) & 1);
             if (f) tot += (1 << j) * (n - cnt[j]);
             else tot += (1 << j) * cnt[j];
@@ -25,10 +31,12 @@ void solve() {
     cout << ans << "\n";
 }
 
-signed main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) solve();
     return 0;
 }
