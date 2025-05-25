83c3a51a64b3b83067c021b06ca846d991e2c7f8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 15:59:52 +0200

    codeforces/cpp/2000-2099/2094F.cpp

--- codeforces/cpp/2000-2099/2094F.cpp
+++ codeforces/cpp/2000-2099/2094F.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+void solve()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector vc(n, vector<int>(m));
+
+    if (m % k != 0)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                vc[i][j] = (i * m + j) % k + 1;
+            }
+        }
+    }
+    else if (n % k != 0)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                vc[i][j] = (j * n + i) % k + 1;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < m; ++j)
+            {
+                vc[i][j] = (j + i) % k + 1;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            cout << vc[i][j] << " ";
+        }
+
+        cout << "\n";
+    }
+}
 
 int main()
 {
