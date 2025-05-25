dd5b14bd8f138a35978185c885f1fab448da260d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-25 11:23:55 +0200

    codeforces/cpp/2000-2099/2094E.cpp

--- codeforces/cpp/2000-2099/2094E.cpp
+++ codeforces/cpp/2000-2099/2094E.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -8,13 +9,32 @@ using namespace std;
 ll solve(const int n)
 {
     vector<ll> a(n);
+    priority_queue<ll> pq;
+    ll abs_max = 0;
 
     for (int i = 0; i < n; ++i)
     {
         cin >> a[i];
+        pq.push(a[i]);
     }
 
-    return 5LL;
+    while (!pq.empty())
+    {
+        const auto elem = pq.top();
+        pq.pop();
+
+        ll cur_max = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cur_max += elem ^ a[i];
+        }
+
+        abs_max = max(abs_max, cur_max);
+
+    }
+
+    return abs_max;
 }
 
 int main()
