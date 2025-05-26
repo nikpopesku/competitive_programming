6a3e53043e49485966c5554dd62ff00915b453df
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-26 09:47:12 +0200

    codeforces/cpp/2000-2099/2094G.cpp

--- codeforces/cpp/2000-2099/2094G.cpp
+++ codeforces/cpp/2000-2099/2094G.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 void solve()
 {
-    deque<int> dq;
+    deque<int> dq_normal, dq_reversed;
     int q, type, value, total_sum = 0;
-    int normal_r = 0, reverse_r = 0;
+    int normal_r = 0, reverse_r = 0, cnt = 0;
     cin >> q;
 
     while (q--)
@@ -18,12 +18,18 @@ void solve()
         if (type == 3)
         {
             cin >> value;
+            ++cnt;
 
-            dq.push_back(value);
+            dq_normal.push_back(value);
             total_sum += value;
+            normal_r = value * cnt;
+
+            dq_reversed.push_front(value);
         }
         else if (type == 2)
         {
+            swap(normal_r, reverse_r);
+            swap(dq_normal, dq_reversed);
         }
         else
         {
