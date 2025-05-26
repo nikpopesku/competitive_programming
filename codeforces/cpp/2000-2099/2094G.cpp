c020d6b75c7ca9bf7251a626fe618a53202c5aeb
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-26 09:38:39 +0200

    codeforces/cpp/2000-2099/2094G.cpp

--- codeforces/cpp/2000-2099/2094G.cpp
+++ codeforces/cpp/2000-2099/2094G.cpp
@@ -5,45 +5,6 @@ using namespace std;
 #define int long long
 
 void solve() {
-    int norm = 0, rev = 0;
-    int q; cin >> q;
-    int tot = 0;
-    int n = 0;
-    deque<int> qNorm, qRev;
-    int p = 0;
-    while (q--) {
-        int s; cin >> s;
-        if (s == 1) {
-            int last = qNorm.back();
-            qNorm.pop_back();
-            qNorm.push_front(last);
-            norm += (tot - last);
-            norm -= last * n;
-            norm += last;
-
-            last = qRev.front();
-            qRev.pop_front();
-            qRev.push_back(last);
-            rev -= (tot - last);
-            rev += last * n;
-            rev -= last;
-        }
-        else if (s == 2) {
-            swap(rev, norm);
-            swap(qNorm, qRev);
-        }
-        else if (s == 3) {
-            n++;
-            int k; cin >> k;
-            qNorm.push_back(k);
-            qRev.push_front(k);
-            norm += k * n;
-            rev += tot;
-            rev += k;
-            tot += k;
-        }
-        cout << norm << "\n";
-    }
 }
 
 signed main() {
@@ -51,5 +12,4 @@ signed main() {
     cin.tie(nullptr);
     int t; cin >> t;
     while (t--) solve();
-    return 0;
 }
