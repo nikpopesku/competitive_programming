1f09c6f5eef7361e65f3626507d557ade6312193
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-26 09:42:56 +0200

    codeforces/cpp/2000-2099/2094G.cpp

--- codeforces/cpp/2000-2099/2094G.cpp
+++ codeforces/cpp/2000-2099/2094G.cpp
@@ -4,12 +4,40 @@ using namespace std;
 
 #define int long long
 
-void solve() {
+void solve()
+{
+    deque<int> dq;
+    int q, type, value, total_sum = 0;
+    int normal_r = 0, reverse_r = 0;
+    cin >> q;
+
+    while (q--)
+    {
+        cin >> type;
+
+        if (type == 3)
+        {
+            cin >> value;
+
+            dq.push_back(value);
+            total_sum += value;
+        }
+        else if (type == 2)
+        {
+        }
+        else
+        {
+        }
+
+        cout << normal_r << "\n";
+    }
 }
 
-signed main() {
+signed main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) solve();
 }
