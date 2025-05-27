15d0bbf23d195091714abdcbae39c736a115e1d7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-27 15:20:38 +0200

    codeforces/cpp/2000-2099/2094H.cpp

--- codeforces/cpp/2000-2099/2094H.cpp
+++ codeforces/cpp/2000-2099/2094H.cpp
@@ -2,40 +2,52 @@
 using namespace std;
 #define int long long
 
-void solve() {
+void solve()
+{
     map<int, vector<int>> pos;
     map<int, int> ptr;
-    int n, q; cin >> n >> q;
-    int arr[n+1];
-    for (int i = 1; i <= n; i++) {
+    int n, q;
+    cin >> n >> q;
+    int arr[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
         cin >> arr[i];
         pos[arr[i]].push_back(i);
         ptr[arr[i]] = 0;
     }
     vector<tuple<int, int, int, int>> qr(q);
     int c = 0;
-    for (auto &[l, r, v, i]: qr) {
+    for (auto& [l, r, v, i] : qr)
+    {
         cin >> v >> l >> r;
         i = c++;
     }
     vector<int> anss(q);
     sort(qr.begin(), qr.end());
     int prevL = 1;
-    for (auto [l, r, v, idd]: qr) {
-        for (int j = prevL; j < l; j++) {
+    for (auto [l, r, v, idd] : qr)
+    {
+        for (int j = prevL; j < l; j++)
+        {
             ptr[arr[j]]++;
         }
         prevL = l;
         vector<pair<int, int>> facts;
-        for (int i = 1; i * i <= v; i++) {
-            if (v % i == 0) {
+        for (int i = 1; i * i <= v; i++)
+        {
+            if (v % i == 0)
+            {
                 int fact1 = v / i;
-                if (!(pos[fact1].size() == 0 || ptr[fact1] >= pos[fact1].size() || pos[fact1][ptr[fact1]] > r || fact1 == 1)) {
+                if (!(pos[fact1].size() == 0 || ptr[fact1] >= pos[fact1].size() || pos[fact1][ptr[fact1]] > r || fact1
+                    == 1))
+                {
                     facts.push_back({pos[fact1][ptr[fact1]], fact1});
                 }
                 int fact2 = i;
                 if (fact2 == fact1) continue;
-                if (!(pos[fact2].size() == 0 || ptr[fact2] >= pos[fact2].size() || pos[fact2][ptr[fact2]] > r || fact2 == 1)) {
+                if (!(pos[fact2].size() == 0 || ptr[fact2] >= pos[fact2].size() || pos[fact2][ptr[fact2]] > r || fact2
+                    == 1))
+                {
                     facts.push_back({pos[fact2][ptr[fact2]], fact2});
                 }
             }
@@ -43,7 +55,8 @@ void solve() {
         sort(facts.begin(), facts.end());
         int pr = l;
         int ans = 0;
-        for (auto [idx, val]: facts) {
+        for (auto [idx, val] : facts)
+        {
             int rr = idx - pr;
             ans += v * rr;
             while (v % val == 0) v /= val;
@@ -56,10 +69,12 @@ void solve() {
     for (int i = 0; i < q; i++) cout << anss[i] << "\n";
 }
 
-signed main() {
+signed main()
+{
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) solve();
     return 0;
 }
