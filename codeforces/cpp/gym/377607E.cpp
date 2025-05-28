b3d44a024e17d34f28a8d82fbeefeba030924844
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-28 20:13:56 +0300

    codeforces/cpp/gym/377607E.cpp

--- codeforces/cpp/gym/377607E.cpp
+++ codeforces/cpp/gym/377607E.cpp
@@ -8,11 +8,14 @@ void solve()
     int n, m;
     cin >> n >> m;
     vector<string> phones(n);
+    string s;
 
     for (int i = 0; i < n; ++i)
     {
         cin >> phones[i];
     }
+
+    cin >> s;
 }
 
 int main()
