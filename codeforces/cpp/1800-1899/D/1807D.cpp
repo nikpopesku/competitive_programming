da57b30eb0187c384c8974f32ecc2f152f1d1bfd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-30 13:43:44 +0300

    codeforces/cpp/1800-1899/D/1807D.cpp

--- codeforces/cpp/1800-1899/D/1807D.cpp
+++ codeforces/cpp/1800-1899/D/1807D.cpp
@@ -1,40 +1,31 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
-int main() {
-    int t, n, k;
+using namespace std;
 
-    std::cin >> t;
+void solve()
+{
+    int n, q, l, r, k;
+    cin >> n >> q;
+    vector<int> vc(n);
 
-    for (int i = 0; i < t; i++) {
-        std::cin >> n >> k;
-
-        int problem;
-        std::priority_queue<int> pq;
-
-        for (int j = 0; j < n; j++) {
-            std::cin >> problem;
-            pq.push(problem);
-        }
-
-        int largest_group = 1;
-        int current_group = 1;
-        int previous_number = pq.top();
-        pq.pop();
-        while (!pq.empty()) {
-            if (previous_number - pq.top() <= k) {
-                current_group += 1;
-            } else {
-                largest_group = std::max(largest_group, current_group);
-                current_group = 1;
-            }
-            previous_number = pq.top();
-            pq.pop();
-        }
-
-        largest_group = std::max(largest_group, current_group);
-        std::cout << n - largest_group << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> vc[i];
     }
 
-    return 0;
+    for (int i = 0; i < q; ++i)
+    {
+        cin >> l >> r >> k;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
 }
