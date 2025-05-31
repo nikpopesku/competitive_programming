acd007e1cd145dc2c1aea38a4dedf0eeae3986c4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-05-31 19:50:49 +0300

    codeforces/cpp/1800-1899/G/1807G1.cpp

--- codeforces/cpp/1800-1899/G/1807G1.cpp
+++ codeforces/cpp/1800-1899/G/1807G1.cpp
@@ -8,11 +8,14 @@ void solve()
     int n;
     cin >> n;
     vector<int> c(n);
+    string response = "YES";
 
     for (int i = 0; i < n; ++i)
     {
         cin >> c[i];
     }
+
+    cout << response << "\n";
 }
 
 int main()
