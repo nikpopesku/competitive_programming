7e5394f7edec74a625d61962dab9e7ebd98f42d6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-04 15:28:06 +0300

    codeforces/cpp/1800-1899/G/1807G2.cpp

--- codeforces/cpp/1800-1899/G/1807G2.cpp
+++ codeforces/cpp/1800-1899/G/1807G2.cpp
@@ -1,56 +1,41 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define int long long
+#define ll long long
 
-void solve()
-{
-    int n;
-    cin >> n;
-    vector a(n, 0);
-    for (int i = 0; i < n; ++i)
-    {
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-
-    sort(a.begin(), a.end());
-    if (a[0] != 1)
-    {
+    sort(all(a));
+    if(a[0] != 1) {
         cout << "NO\n";
         return;
     }
-    vector dp(5005, 0);
-    dp[1] = 1;
-
-    for (int i = 1; i < n; ++i)
-    {
-        if (!dp[a[i]])
-        {
+    long long sum = a[0];
+    for(int i = 1; i < n; ++i) {
+        if(sum < a[i]) {
             cout << "NO\n";
             return;
         }
-
-        for (int j = 5000; j >= a[i]; --j)
-        {
-            if (dp[j - a[i]])
-            {
-                dp[j] = 1;
-            }
-        }
+        sum += a[i];
     }
-
     cout << "YES\n";
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int t = 1;
     cin >> t;
-    while (t--)
-    {
+    while(t--) {
         solve();
     }
 }
