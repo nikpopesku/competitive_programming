b2894901dd3770333533db129ddf603e73220b23
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-04 20:28:20 +0300

    codeforces/cpp/1700-1799/1703D.cpp

---
+++ codeforces/cpp/1700-1799/1703D.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> s;
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+}
