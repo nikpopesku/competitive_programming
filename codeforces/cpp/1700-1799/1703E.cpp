fc830cc9a0f169e5dfc85cefe8d4cdb2c8c2c873
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 18:47:00 +0800

    codeforces/cpp/1700-1799/1703E.cpp

--- codeforces/cpp/1700-1799/1703E.cpp
+++ codeforces/cpp/1700-1799/1703E.cpp
@@ -25,7 +25,24 @@ void solve()
     {
         for (int j = 0; j < k; ++j)
         {
-            const int sz = vc[i][j] + vc[i + k][j] + vc[i + k][j + k] + vc[i][j + k];
+            const int sz = vc[i][j] + vc[n - i - 1][j] + vc[n - i - 1][n - j - 1] + vc[i][n - j - 1];
+
+            if (sz == 1 or sz == 3)
+            {
+                response += 1;
+            }
+            else if (sz == 2)
+            {
+                response += 2;
+            }
+        }
+    }
+
+    if (n % 2 == 1)
+    {
+        for (int i = 0; i < k; ++i)
+        {
+            const int sz = vc[k][i] + vc[k][n - i - 1] + vc[i][k] + vc[n - i - 1][k];
 
             if (sz == 1 or sz == 3)
             {
