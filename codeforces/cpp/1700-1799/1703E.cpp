27d0dd251ed2eb49114be3c41d32208c733d76fd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 19:00:57 +0800

    codeforces/cpp/1700-1799/1703E.cpp

--- codeforces/cpp/1700-1799/1703E.cpp
+++ codeforces/cpp/1700-1799/1703E.cpp
@@ -19,39 +19,13 @@ void solve()
         }
     }
 
-    const int k = n / 2;
     int response = 0;
-    for (int i = 0; i < k; ++i)
+    for (int i = 0; i < (n + 1) / 2; ++i)
     {
-        for (int j = 0; j < k; ++j)
+        for (int j = 0; j < n / 2; ++j)
         {
-            const int sz = vc[i][j] + vc[n - i - 1][j] + vc[n - i - 1][n - j - 1] + vc[i][n - j - 1];
-
-            if (sz == 1 or sz == 3)
-            {
-                response += 1;
-            }
-            else if (sz == 2)
-            {
-                response += 2;
-            }
-        }
-    }
-
-    if (n % 2 == 1)
-    {
-        for (int i = 0; i < k; ++i)
-        {
-            const int sz = vc[k][i] + vc[k][n - i - 1] + vc[i][k] + vc[n - i - 1][k];
-
-            if (sz == 1 or sz == 3)
-            {
-                response += 1;
-            }
-            else if (sz == 2)
-            {
-                response += 2;
-            }
+            const int sz = vc[i][j] + vc[j][n - i - 1] + vc[n - i - 1][n - j - 1] + vc[n - j - 1][i];
+            response += min(sz, 4 - sz);
         }
     }
 
