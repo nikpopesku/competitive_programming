62641acd6005d7642496f9886c8bf36253d518ba
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 18:24:29 +0800

    codeforces/cpp/1700-1799/1703E.cpp

--- codeforces/cpp/1700-1799/1703E.cpp
+++ codeforces/cpp/1700-1799/1703E.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector vc(n, vector<int>(n));
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            cin >> vc[i][j];
+        }
+    }
 }
 
 int main()
