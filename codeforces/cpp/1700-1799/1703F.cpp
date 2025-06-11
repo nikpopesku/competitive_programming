0b2cc917da16b0da3579bf0a1cdbca5f5eaa9138
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 21:00:16 +0800

    codeforces/cpp/1700-1799/1703F.cpp

---
+++ codeforces/cpp/1700-1799/1703F.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
