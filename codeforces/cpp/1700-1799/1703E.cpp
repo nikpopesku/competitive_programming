cbc300bba95c610cdba642696cd93792799572e2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 18:34:56 +0800

    codeforces/cpp/1700-1799/1703E.cpp

--- codeforces/cpp/1700-1799/1703E.cpp
+++ codeforces/cpp/1700-1799/1703E.cpp
@@ -8,14 +8,37 @@ void solve()
     int n;
     cin >> n;
     vector vc(n, vector<int>(n));
+    string s;
 
     for (int i = 0; i < n; ++i)
     {
+        cin >> s;
         for (int j = 0; j < n; ++j)
         {
-            cin >> vc[i][j];
+            vc[i][j] = s[j];
         }
     }
+
+    const int k = n / 2;
+    int response = 0;
+    for (int i = 0; i < k; ++i)
+    {
+        for (int j = 0; j < k; ++j)
+        {
+            int s = vc[i][j] + vc[i + k][j] + vc[i + k][j + k] + vc[i][j + k];
+
+            if (s == 1 or s == 3)
+            {
+                response += 1;
+            }
+            else if (s == 2)
+            {
+                response += 2;
+            }
+        }
+    }
+
+    cout << response << "\n";
 }
 
 int main()
