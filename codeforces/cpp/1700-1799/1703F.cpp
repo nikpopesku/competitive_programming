fc3480ae6cd931543ecbf35515a2577ad67a705a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-11 21:25:58 +0800

    codeforces/cpp/1700-1799/1703F.cpp

--- codeforces/cpp/1700-1799/1703F.cpp
+++ codeforces/cpp/1700-1799/1703F.cpp
@@ -3,8 +3,18 @@
 
 using namespace std;
 
+#define int long long
+
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> vc(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> vc[i];
+    }
 }
 
 int main()
