52ecf44dd7eee43dfb064c7620baf3400a78bd44
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-15 12:39:14 +0800

    codeforces/cpp/1700-1799/1703G.cpp

--- codeforces/cpp/1700-1799/1703G.cpp
+++ codeforces/cpp/1700-1799/1703G.cpp
@@ -7,13 +7,15 @@ void solve()
 {
     int n, k;
     cin >> n >> k;
-    vector<int> a(n);
+    vector<int> a(n), partial_sum(n + 1, 0);
 
     int response = 0;
+
 
     for (int i = 0; i < n; ++i)
     {
         cin >> a[i];
+        partial_sum[i + 1] = partial_sum[i] + a[i];
     }
 
     cout << response << "\n";
