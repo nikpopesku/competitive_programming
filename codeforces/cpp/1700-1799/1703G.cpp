e82a1f3be9e3e92a4ed8b46b9d366c2165242389
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-15 15:38:11 +0800

    codeforces/cpp/1700-1799/1703G.cpp

--- codeforces/cpp/1700-1799/1703G.cpp
+++ codeforces/cpp/1700-1799/1703G.cpp
@@ -1,53 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
-#include <algorithm>
 
 using namespace std;
 
-void solve() {
-    int n;
-    long long k;
+
+void solve()
+{
+    int n, k;
     cin >> n >> k;
-    vector<long long> a(n);
-    for (int i = 0; i < n; ++i) {
+    vector<int> a(n);
+
+    long long response = 0, sum = 0;
+
+
+    for (int i = 0; i < n; ++i)
+    {
         cin >> a[i];
     }
 
-    long long max_profit = 0;
-    long long current_profit = 0;
-
-    for (int i = 0; i < n; ++i) {
-        // At each step i, we decide what is the maximum profit we can get
-        // from the subarray a[i...n-1].
-
-        long long spell_profit = 0;
-        // Calculate profit if we use the spell starting from chest i
-        for (int j = i; j < n; ++j) {
-            // The value of chest j is halved (j - i + 1) times
-            // A lookahead of 31 is sufficient since 2^31 > 10^9
-            if (j - i > 31) {
-                break;
-            }
-            spell_profit += a[j] >> (j - i + 1);
+    for (int i = 0; i < n; ++i)
+    {
+        long long now = sum;
+        for (int j = i; j < min(n, i + 32); ++j)
+        {
+            int copy = a[j];
+            copy >>= j - i + 1;
+            now += copy;
         }
 
-        // The total profit up to this point is current_profit.
-        // We can either stop here and take current_profit,
-        // or we can add the spell_profit from this point onwards.
-        max_profit = max(max_profit, current_profit + spell_profit);
+        response = max(response, now);
 
-        // Now, update current_profit for the next iteration by assuming
-        // we pay the key for the current chest.
-        current_profit += a[i] - k;
+        sum += a[i] - k;
     }
 
-    // Final check: maybe the best option was to take some chests normally
-    // and then just stop without using the spell.
-    max_profit = max(max_profit, current_profit);
-
-
-    cout << max_profit << endl;
+    cout << response << "\n";
 }
 
 int main()
