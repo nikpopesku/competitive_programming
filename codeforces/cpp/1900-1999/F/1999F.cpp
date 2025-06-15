dbe28f0c6ed7526250bddf6032668631f6ca0d9d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-15 18:10:24 +0800

    codeforces/cpp/1900-1999/F/1999F.cpp

--- codeforces/cpp/1900-1999/F/1999F.cpp
+++ codeforces/cpp/1900-1999/F/1999F.cpp
@@ -1,39 +1,40 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-const int N = 2e5 + 5, mod = 1e9 + 7;
-int64_t fact[N];
-int64_t pw(int64_t a, int64_t b) {
-    int64_t r = 1;
-    while(b > 0) {
-        if(b & 1) r = (r * a) % mod;
-        b /= 2;
-        a = (a * a) % mod;
+#define ll long long
+constexpr
+ll MODULO = 10e9 + 7;
+
+void solve()
+{
+    int n, k, value;
+    cin >> n >> k;
+    ll zeros = 0;
+    ll answer1 = 1;
+    ll answer2 = 1;
+
+    for (int i = n; i > k; --i) answer1 *= i;
+
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> value;
+        if (value == 0) ++zeros;
     }
-    return r;
+
+    for (ll i = zeros; i > zeros - (k + 1) / 2; --i) answer2 *= i;
+
+    cout << (answer1 - answer2) % MODULO << "\n";
 }
-int64_t C(int64_t n, int64_t k) {
-    if(n < k) return 0LL;
-    return (fact[n] * pw((fact[n - k] * fact[k]) % mod, mod - 2)) % mod;
-}
-int main() {
-    int t; cin >> t;
-    fact[0] = 1;
-    for(int64_t i = 1; i < N; ++i) fact[i] = (fact[i - 1] * i) % mod;
-    while(t--) {
-        int n, k; cin >> n >> k;
-        vector<int> a(n);
-        int ones = 0;
-        for(int i = 0; i < n; ++i) {
-            cin >> a[i];
-            ones += a[i];
-        }
-        //at least k/2+1 ones
-        int64_t ans = 0;
-        for(int cnt_ones = k / 2 + 1; cnt_ones <= min(ones, k); ++cnt_ones) {
-            ans += C(ones, cnt_ones) * C(n - ones, k - cnt_ones) % mod;
-            ans %= mod;
-        }
-        cout << ans << "\n";
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
     }
 }
