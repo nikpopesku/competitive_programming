434136fb8838dceaaf1a068f6a16c2ebf035af66
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-17 12:35:46 +0800

    codeforces/cpp/1900-1999/G/1999G2.cpp

--- codeforces/cpp/1900-1999/G/1999G2.cpp
+++ codeforces/cpp/1900-1999/G/1999G2.cpp
@@ -8,16 +8,23 @@ void solve()
     while (left + 1 < right)
     {
         const int m = left + (right - left + 1) / 2;
-        cout << "? " << m << " " << m << endl;
+        const int new_left = left + (m - left) / 2;
+        const int new_right = m + (right - m) / 2;
+        cout << "? " << new_left << " " << new_right << endl;
         cin >> value;
+        if (value == -1) return;
 
-        if (value == (m + 1) * (m + 1))
+        if (value == new_left * new_right)
         {
-            right = m;
+            left = new_right;
         }
-        else
+        else if (value == (new_left + 1) * (new_right + 1))
         {
-            left = m;
+            left = new_left;
+        } else
+        {
+            left = new_left;
+            right = new_right;
         }
     }
 
