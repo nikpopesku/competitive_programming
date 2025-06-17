41d0b1c6d9d757ea979d3398dffb55d778ab18f9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-17 12:55:48 +0800

    codeforces/cpp/1900-1999/G/1999G2.cpp

--- codeforces/cpp/1900-1999/G/1999G2.cpp
+++ codeforces/cpp/1900-1999/G/1999G2.cpp
@@ -5,11 +5,11 @@ void solve()
 {
     int left = 1, right = 999, value;
 
-    while (left + 1 < right)
+    while (left + 2 < right)
     {
-        const int m = left + (right - left + 1) / 2;
-        const int new_left = left + (m - left) / 2;
-        const int new_right = m + (right - m) / 2;
+        const int delta = (right - left) / 3;
+        const int new_left = left + delta;
+        const int new_right = right - delta;
         cout << "? " << new_left << " " << new_right << endl;
         cin >> value;
         if (value == -1) return;
@@ -27,6 +27,14 @@ void solve()
             left = new_left;
             right = new_right;
         }
+    }
+
+    if (right - left == 2)
+    {
+        cout << "? 1 " << left + 1 << endl;
+        cin >> value;
+
+        if (value != left + 1) { right = left + 1; }
     }
 
     cout << "! " << right << endl;
