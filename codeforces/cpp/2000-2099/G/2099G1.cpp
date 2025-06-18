92cf32f387f554630afb964b62c68b7b7023d1b0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-18 11:34:34 +0800

    codeforces/cpp/2000-2099/G/2099G1.cpp

---
+++ codeforces/cpp/2000-2099/G/2099G1.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+void solve()
+{
+    int n, k, q, l, r;
+    cin >> n >> k >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < q; ++i)
+    {
+        cin >> l >> r;
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
