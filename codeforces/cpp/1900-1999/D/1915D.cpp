250526c10f3a8dabd5aae318733e5bb459770f25
npopesku <nic1k@ya.ru>
2025-06-20 18:53:19 +0800

    codeforces/cpp/1900-1999/D/1915D.cp

--- codeforces/cpp/1900-1999/D/1915D.cpp
+++ codeforces/cpp/1900-1999/D/1915D.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
-#include <cmath>
+#include <set>
 
 using namespace std;
 
 #define ll long long
 
+set<char> vowels = {'a', 'e'};
+set<char> consonants = {'b', 'c', 'd'};
+
+string backtrack(const string & s, const int index, string result)
+{
+    if (index == s.size())
+    {
+        return result;
+    }
+
+    string response1, response2;
+
+    for (int i = index; i < min(static_cast<int>(s.size()), index + 3); ++i)
+    {
+        if (i - index == 1 and consonants.count(s[index]) and vowels.count(s[i]))
+        {
+            response1 = backtrack(s, index + 2, result + "." + s[index] + s[i]);
+        }
+        if (response1.empty() and i - index == 2 and consonants.count(s[index]) and vowels.count(s[i-1]) and consonants.count(s[i]))
+        {
+            response2 = backtrack(s, index + 3, result + "." + s[index] + s[index + 1] + s[index + 2]);
+        }
+    }
+
+    if (!response1.empty()) return response1;
+    if (!response2.empty()) return response2;
+
+    return "";
+}
+
 void solve()
 {
 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    cout << backtrack(s, 2, "") << "\n";
 }
 
 int main()
