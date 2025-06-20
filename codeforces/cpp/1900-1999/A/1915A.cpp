7d7d4bc49a8edb0b73a3f215c13858bc6af5f7c0
npopesku <nic1k@ya.ru>
2025-06-20 11:27:20 +0800

    codeforces/cpp/1900-1999/A/1915A.cpp

---
+++ codeforces/cpp/1900-1999/A/1915A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (a ^ b ^ c) << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
