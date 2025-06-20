ba813778e5ca3a332ead90b2ce166422245ae96d
npopesku <nic1k@ya.ru>
2025-06-20 20:13:12 +0800

    codeforces/cpp/1900-1999/D/1915E.cp

---
+++ codeforces/cpp/1900-1999/E/1915E.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
