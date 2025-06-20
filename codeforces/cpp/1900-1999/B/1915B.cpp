a894114765f23a8cc9b7f70466b5ef1fcf23ca1f
npopesku <nic1k@ya.ru>
2025-06-20 11:44:05 +0800

    codeforces/cpp/1900-1999/A/1915B.cpp

--- codeforces/cpp/1900-1999/B/1915B.cpp
+++ codeforces/cpp/1900-1999/B/1915B.cpp
@@ -6,28 +6,28 @@ using namespace std;
 void solve()
 {
     string s;
-    string response = '';
+    string response;
 
     for (int i = 0; i < 3; ++i)
     {
         set st = {'A', 'B', 'C'};
-        for (int j = 0; j < 3; ++j)
+        cin >> s;
+        if (response.empty())
         {
-            cin >> s;
-            if (response.empty())
+            for (int k = 0; k < 3; ++k)
             {
-                for (int k = 0; k < 3; ++k)
-                {
-                    st.erase(s[k]);
-                }
-
-                if (!st.empty()) response = *st.begin();
+                st.erase(s[k]);
             }
+
+            if (!st.empty()) response = *st.begin();
         }
     }
+
+    cout << response << "\n";
 }
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
 
