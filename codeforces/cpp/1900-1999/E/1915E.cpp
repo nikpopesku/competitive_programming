c8c57c78cbc99a68c45ea211837916d8f52fa61f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-06-23 14:41:03 +0800

    codeforces/cpp/1900-1999/E/1915E.cpp

--- codeforces/cpp/1900-1999/E/1915E.cpp
+++ codeforces/cpp/1900-1999/E/1915E.cpp
@@ -9,6 +9,21 @@ using namespace std;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> v(n), partial_sum_even(n+1, 0), partial_sum_odd(n+1, 0);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+        if (i > 0 and i % 2 == 1) {
+            partial_sum_odd[i] = partial_sum_odd[i-1] + v[i];
+            partial_sum_even[i] = partial_sum_even[i-1];
+        } else if (i > 0) {
+            partial_sum_odd[i] = partial_sum_odd[i-1];
+            partial_sum_even[i] = partial_sum_even[i-1] + v[i];
+        }
+    }
+
 }
 
 
