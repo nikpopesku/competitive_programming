3b66b34144169ecb6fd76b91278a7eb50cbf519d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-01 14:06:15 +0800

    codeforces/cpp/1900-1999/F/1915F.cpp

--- codeforces/cpp/1900-1999/F/1915F.cpp
+++ codeforces/cpp/1900-1999/F/1915F.cpp
@@ -1,80 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-template<typename T>
-class FenwickTree {
-public:
-    explicit FenwickTree(int size) : size_(size), tree_(size + 1, 0) {
-    }
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
-    void update(int index, T value) {
-        for (++index; index <= size_; index += index & -index) {
-            tree_[index] += value;
-        }
-    }
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 
-    T query(int index) {
-        T sum = 0;
-        for (++index; index > 0; index -= index & -index) {
-            sum += tree_[index];
-        }
-        return sum;
-    }
+int t, n;
 
-private:
-    int size_ = 0;
-    vector<T> tree_;
-};
+vector<pair<int, int>> arr;
 
-struct Interval {
-    int start;
-    int end;
-};
+long long ans;
 
-auto solve() {
-    int n;
+ordered_set st;
+
+void solve(){
     cin >> n;
-    vector<Interval> intervals(n);
-    vector<int> end_points;
-    for (int i = 0; i < n; ++i) {
-        cin >> intervals[i].start >> intervals[i].end;
-        end_points.push_back(intervals[i].end);
+
+    arr.assign(n, {});
+
+    for(auto &p : arr) cin >> p.second >> p.first;
+
+    sort(arr.begin(), arr.end());
+
+    ans = 0;
+    st.clear();
+
+    for(auto p : arr){
+        ans += st.size() - st.order_of_key(p.second);
+
+        st.insert(p.second);
     }
 
-    ranges::sort(end_points);
-    end_points.erase(ranges::unique(end_points).begin(), end_points.end());
-
-    for (auto &[start, end]: intervals) {
-        end = ranges::lower_bound(end_points, end) - end_points.begin();
-    }
-
-    ranges::sort(intervals, [](const auto &a, const auto &b) {
-        if (a.start != b.start) {
-            return a.start < b.start;
-        }
-        return a.end > b.end;
-    });
-
-    long long inversions = 0;
-    FenwickTree<int> ft(n);
-    for (int i = 0; i < n; ++i) {
-        inversions += ft.query(n - 1) - ft.query(intervals[i].end - 1);
-        ft.update(intervals[i].end, 1);
-    }
-
-    cout << inversions << "\n";
+    cout << ans << "\n";
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+
     cin >> t;
-    while (t--) {
+
+    while(t--){
         solve();
     }
-    return 0;
 }
