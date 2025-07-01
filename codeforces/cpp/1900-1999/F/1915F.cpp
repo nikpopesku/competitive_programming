15dfe1f0914dd25d966eac40d19e0b45fd28f55e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-01 14:06:23 +0800

    codeforces/cpp/1900-1999/F/1915F.cpp

--- codeforces/cpp/1900-1999/F/1915F.cpp
+++ codeforces/cpp/1900-1999/F/1915F.cpp
@@ -5,29 +5,30 @@ using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 
-typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag,
+    __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 
 int t, n;
 
-vector<pair<int, int>> arr;
+vector<pair<int, int> > arr;
 
 long long ans;
 
 ordered_set st;
 
-void solve(){
+void solve() {
     cin >> n;
 
     arr.assign(n, {});
 
-    for(auto &p : arr) cin >> p.second >> p.first;
+    for (auto &p: arr) cin >> p.second >> p.first;
 
     sort(arr.begin(), arr.end());
 
     ans = 0;
     st.clear();
 
-    for(auto p : arr){
+    for (auto p: arr) {
         ans += st.size() - st.order_of_key(p.second);
 
         st.insert(p.second);
@@ -36,12 +37,13 @@ void solve(){
     cout << ans << "\n";
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     cin >> t;
 
-    while(t--){
+    while (t--) {
         solve();
     }
 }
