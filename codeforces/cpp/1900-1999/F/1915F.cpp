67de62dfe88f7994adedb29ca7d81415892f2110
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-01 13:42:15 +0800

    codeforces/cpp/1900-1999/F/1915F.cpp

--- codeforces/cpp/1900-1999/F/1915F.cpp
+++ codeforces/cpp/1900-1999/F/1915F.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i] >> b[i];
+    }
 }
 
 
