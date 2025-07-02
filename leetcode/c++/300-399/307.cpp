a73607b56ce2da68876182f7cdd11fa4d6786cd0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-02 12:24:46 +0800

    leetcode/c++/300-399/307.cpp

--- leetcode/c++/300-399/307.cpp
+++ leetcode/c++/300-399/307.cpp
@@ -1,24 +1,53 @@
+#include <iostream>
 #include <vector>
 
 using namespace std;
 
 class NumArray {
 public:
-    explicit NumArray(const vector<int> &nums): nums(nums) {
+    explicit NumArray(const vector<int> &nums) : nums(nums) {
+        this->n = static_cast<int>(nums.size());
+        bit.assign(n, 0);
+        for (size_t i = 0; i < bit.size(); i++)
+            add(i, bit[i]);
     }
 
-    void update(int index, int val) {
+    void add(size_t idx, const int delta) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += delta;
     }
 
-    int sumRange(int left, int right) {
+    void update(int idx, int val) {
+        for (; idx < n; idx = idx | (idx + 1))
+            bit[idx] += val;
+    }
+
+    [[nodiscard]] int sumRange(const int l, const int r) const {
+        return sum(r) - sum(l - 1);
+    }
+
+    [[nodiscard]] int sum(int r) const {
+        int ret = 0;
+
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret += bit[r];
+
+        return ret;
     }
 
 private:
+    vector<int> bit = {};
     vector<int> nums = {};
+    int n;
 };
 
 
 int main() {
-    vector v = {1, 3, 5};
+    const vector v = {1, 3, 5};
     auto na = NumArray(v);
+    cout << na.sumRange(0, 2) << "\n";
+
+    na.update(1, 2);
+
+    cout << na.sumRange(0, 2) << "\n";
 }
