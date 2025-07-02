ec8a09d172b87577f1ace40de4fbf7a5866128b2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-02 12:41:51 +0800

    leetcode/c++/300-399/307.cpp

--- leetcode/c++/300-399/307.cpp
+++ leetcode/c++/300-399/307.cpp
@@ -5,21 +5,15 @@ using namespace std;
 
 class NumArray {
 public:
-    explicit NumArray(const vector<int> &nums) : nums(nums) {
-        n = static_cast<int>(nums.size());
-        bit.assign(n, 0);
+    explicit NumArray(vector<int> &nums) : nums(nums) {
+        bit.assign(nums.size(), 0);
 
         for (size_t i = 0; i < bit.size(); i++)
             add(i, nums[i]);
     }
 
-    void add(size_t idx, const int delta) {
-        for (; idx < n; idx = idx | (idx + 1))
-            bit[idx] += delta;
-    }
-
     void update(int idx, const int val) {
-        for (; idx < n; idx = idx | (idx + 1))
+        for (; idx < nums.size(); idx = idx | (idx + 1))
             bit[idx] += val - nums[idx];
         nums[idx] = val;
     }
@@ -27,6 +21,17 @@ public:
     [[nodiscard]] int sumRange(const int l, const int r) const {
         return sum(r) - sum(l - 1);
     }
+
+private:
+    vector<int> bit;
+    vector<int>& nums;
+
+
+    void add(size_t idx, const int delta) {
+        for (; idx < nums.size(); idx = idx | (idx + 1))
+            bit[idx] += delta;
+    }
+
 
     [[nodiscard]] int sum(int r) const {
         int ret = 0;
@@ -36,16 +41,11 @@ public:
 
         return ret;
     }
-
-private:
-    vector<int> bit = {};
-    vector<int> nums = {};
-    int n;
 };
 
 
 int main() {
-    const vector v = {1, 3, 5};
+    vector<int> v = {1, 3, 5};
     auto na = NumArray(v);
     cout << na.sumRange(0, 2) << "\n";
 
