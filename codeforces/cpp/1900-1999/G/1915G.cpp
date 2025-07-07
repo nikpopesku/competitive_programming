118ba1a6d63ed47eaa5726a5c2b199517ac4c1d0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-07 11:26:17 +0800

    codeforces/cpp/1900-1999/G/1915G.cp

--- codeforces/cpp/1900-1999/G/1915G.cpp
+++ codeforces/cpp/1900-1999/G/1915G.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
-auto solve() {
+auto solve()
+{
     int n, m;
     cin >> n >> m;
+    int u, v, w;
+    vector<map<int, int>> adj_list(n);
 
-
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> u >> v >> w;
+        if (adj_list[u].contains(v))
+        {
+            adj_list[u][v] = min(adj_list[u][v], w);
+        }
+        else
+        {
+            adj_list[u][v] = w;
+        }
+    }
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
 }
