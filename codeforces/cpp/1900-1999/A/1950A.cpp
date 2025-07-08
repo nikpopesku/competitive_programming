b24ed1217987dc9d213de8cbcd44526e6c1b0e73
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-08 12:22:40 +0800

    codeforces/cpp/1900-1999/G/1915G.cp

---
+++ codeforces/cpp/1900-1999/A/1950A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a < b and b < c)
+    {
+        cout << "STAIR\n";
+    }
+    else if (a < b and b > c)
+    {
+        cout << "PEAK\n";
+    }
+    else
+    {
+        cout << "NONE\n";
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
---
+++ codeforces/cpp/1900-1999/B/1950B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int n, rest;
+    cin >> n;
+    char elem;
+
+    for (int i = 0; i < 2 * n; ++i)
+    {
+        if (i / 2 % 2 == 0)
+        {
+            rest = 0;
+        }
+        else
+        {
+            rest = 1;
+        }
+        for (int j = 0; j < 2 * n; ++j)
+        {
+            if ((j / 2) % 2 == rest)
+            {
+                elem = '#';
+            }
+            else
+            {
+                elem = '.';
+            }
+
+            cout << elem << " ";
+        }
+
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
