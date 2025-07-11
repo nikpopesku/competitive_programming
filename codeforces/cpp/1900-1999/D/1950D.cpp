19a8a5c8a115690e8cf64ee909bacd720226e33f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-11 11:25:35 +0800

    codeforces/cpp/1900-1999/D/1950D.cpp

--- codeforces/cpp/1900-1999/D/1950D.cpp
+++ codeforces/cpp/1900-1999/D/1950D.cpp
@@ -1,69 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void solve()
-{
-    int n;
-    cin >> n;
-    int i = 2;
-    string response = "YES";
-    vector<int> numbers = {};
-    for (const char j : to_string(n))
-    {
-        if (j - '0' > 1)
-        {
-            response = "NO";
+const int MAX = 100'007;
+const int MOD = 1'000'000'007;
 
-            break;
+vector<int> binary_decimals;
+
+bool ok(int n) {
+    if (n == 1) {return true;}
+    bool ans = false;
+    for (int i : binary_decimals) {
+        if (n % i == 0) {
+            ans |= ok(n / i);
         }
     }
-
-    if (response == "YES")
-    {
-        cout << response << "\n";
-
-        return;
-    }
-
-    response = "YES";
-
-    while (i <= static_cast<int>(sqrt(n)))
-    {
-        if (n % i == 0)
-        {
-            numbers.push_back(i);
-            string s = to_string(i);
-            for (const char j : s)
-            {
-                if (j - '0' > 1)
-                {
-                    response = "NO";
-                    break;
-                }
-            }
-
-            n /= i;
-            i = 2;
-            continue;
-        }
-
-        ++i;
-    }
-    numbers.push_back(n);
-
-    cout << (numbers.size() == 1 ? "NO" : response) << "\n";
+    return ans;
 }
 
-int main()
-{
-    int t;
-    cin >> t;
+void solve() {
+    int n;
+    cin >> n;
+    cout << (ok(n) ? "YES\n" : "NO\n");
+}
 
-    while (t--)
-    {
-        solve();
+int main() {
+    for (int i = 2; i < MAX; i++) {
+        int curr = i;
+        bool bad = false;
+        while (curr) {
+            if (curr % 10 > 1) {bad = true; break;}
+            curr /= 10;
+        }
+        if (!bad) {binary_decimals.push_back(i);}
     }
+    int tt; cin >> tt; for (int i = 1; i <= tt; i++) {solve();}
+    // solve();
 }
