76f3a379b708d8fd25b71a98d85d5ac529712aa2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-11 12:26:17 +0800

    codeforces/cpp/1900-1999/D/1950D.cpp

---
+++ codeforces/cpp/1900-1999/E/1950E.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    if (n == 1)
+    {
+        cout << "1\n";
+        return;
+    }
+
+    int i = 2;
+    int sq = sqrt(n);
+    vector<int> factors;
+
+    while (i <= sq and n > 1)
+    {
+        if (n % i == 0)
+        {
+            factors.push_back(i);
+            n /= i;
+            sq = sqrt(n);
+            i = 2;
+
+            continue;
+        }
+
+        i++;
+    }
+
+    if (n > 1)
+    {
+        factors.push_back(n);
+    }
+
+    cout << "5\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
