aed764f23b7523383bf76a708e93f04c1d89a8f3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-15 12:22:33 +0800

    codeforces/cpp/1900-1999/G/1950G.cpp

--- codeforces/cpp/1900-1999/G/1950G.cpp
+++ codeforces/cpp/1900-1999/G/1950G.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,58 +7,18 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<string> genre(n), artist(n);
+    vector<int> genres(n);
+    vector<int> writers(n);
+
     for (int i = 0; i < n; ++i)
     {
-        cin >> genre[i] >> artist[i];
+        cin >> genres[i] >> writers[i];
     }
 
-    if (n == 0)
-    {
-        cout << 0 << endl;
-        return;
-    }
-
-    vector adj(n, vector<int>(n));
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            if (i == j) continue;
-            if (genre[i] == genre[j] || artist[i] == artist[j])
-            {
-                adj[i][j] = 1;
-            }
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
         }
     }
-
-    vector dp(1 << n, vector(n, 0));
-    for (int i = 0; i < n; ++i)
-    {
-        dp[1 << i][i] = 1;
-    }
-
-    int max_len = 1;
-    for (int mask = 1; mask < (1 << n); ++mask)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            if (dp[mask][i] > 0)
-            {
-                for (int j = 0; j < n; ++j)
-                {
-                    if (!(mask & (1 << j)) && adj[i][j])
-                    {
-                        const int next_mask = mask | (1 << j);
-                        dp[next_mask][j] = max(dp[next_mask][j], dp[mask][i] + 1);
-                        max_len = max(max_len, dp[next_mask][j]);
-                    }
-                }
-            }
-        }
-    }
-
-    cout << n - max_len << endl;
 }
 
 int main()
@@ -73,5 +31,4 @@ int main()
     {
         solve();
     }
-    return 0;
 }
