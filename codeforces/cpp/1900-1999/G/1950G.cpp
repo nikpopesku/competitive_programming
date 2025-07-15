1e7106760101903d2c9f8214404beeb9b7428127
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-15 11:31:11 +0800

    codeforces/cpp/1900-1999/G/1950G.cpp

--- codeforces/cpp/1900-1999/G/1950G.cpp
+++ codeforces/cpp/1900-1999/G/1950G.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void solve() {
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<string> genres(n);
+    vector<string> writers(n);
+
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> genres[i] >> writers[i];
+    }
 }
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
     while (t--)
