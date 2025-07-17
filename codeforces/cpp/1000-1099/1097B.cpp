699fe3dce4f32740ba2baeb848c5facb7f23688d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-17 14:22:31 +0800

    codeforces/cpp/1000-1099/1097B.cpp

--- codeforces/cpp/1000-1099/1097B.cpp
+++ codeforces/cpp/1000-1099/1097B.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> v[i];
+    }
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
+    solve();
 }
