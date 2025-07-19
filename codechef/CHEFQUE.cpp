54dad6a995a07bf62d86fde3754c75130ab5274a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-19 16:03:23 +0800

    codechef/CHEFQUE.cpp

--- codechef/CHEFQUE.cpp
+++ codechef/CHEFQUE.cpp
@@ -11,38 +11,35 @@ int main()
     long long q, s1, a, b;
     cin >> q >> s1 >> a >> b;
     long long s = s1 % MAX;
-
+    long long sum = 0;
     bitset<MAX> bit;
 
     if (s1 % 2 == 1)
     {
-        bit |= 1 << s1;
-    }
-    else
-    {
-        bit &= ~(1 << s1);
+        sum += s1 / 2;
     }
 
     for (long long i = 2; i <= q; ++i)
     {
         s = (a * s + b) % MAX;
 
-        if (s % 2 == 1)
+        if (s % 2 == 1 && bit ^ 1 << (s / 2))
         {
             bit |= 1 << s;
+            sum += s / 2;
         }
-        else
+        else if (s % 2 == 0 && bit & 1 << (s / 2))
         {
             bit &= ~(1 << s);
+            sum -= s / 2;
         }
     }
 
-    long long sum = 0;
 
-    for (long long i = 0; i < MAX; ++i)
-    {
-        if (bit & 1 << i) sum += i;
-    }
+    // for (long long i = 0; i < MAX; ++i)
+    // {
+    //     if (bit & 1 << i) sum += i;
+    // }
 
     cout << sum << "\n";
 }
