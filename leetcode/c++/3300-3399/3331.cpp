53f49cecc67d3193cd7af43385f769d192151a0a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-23 13:49:35 +0800

    leetcode/c++/3300-3399/3331.cpp - own reproduce

--- leetcode/c++/3300-3399/3331.cpp
+++ leetcode/c++/3300-3399/3331.cpp
@@ -8,46 +8,20 @@ class Solution
 public:
     vector<int> findSubtreeSizes(const vector<int>& parent, const string& s)
     {
-        const int n = static_cast<int>(parent.size());
-        vector<vector<int>> children(n);
+        vector<vector<int>> children(parent.size());
 
-        // Build adjacency from initial parent info
-        for (int i = 1; i < n; ++i)
+        for (int i = 1; i < parent.size(); ++i)
         {
             children[parent[i]].push_back(i);
         }
 
-        vector ans(n, 0);
-        vector<vector<int>> d(26);
-
-        // d[ch] will contain stack of current ancestors whose character is 'a'+ch
-        dfs(0, -1, ans, s, children, d);
-        return ans;
-    }
-
-    void dfs(const int node, const int fa, vector<int>& ans, const string& s, const vector<vector<int>>& children,
-             vector<vector<int>>& d)
-    {
-        ans[node] = 1;
-        const int c = s[node] - 'a';
-        d[c].push_back(node);
-
-        for (const int ch : children[node])
-        {
-            dfs(ch, node, ans, s, children, d);
-        }
-
-        // The closest ancestor with same char is second last element in d[c];
-        // if none, use fa
-        int closestAncestorWithSameChar = (d[c].size() > 1) ? d[c][d[c].size() - 2] : fa;
-
-        if (closestAncestorWithSameChar != -1)
-        {
-            ans[closestAncestorWithSameChar] += ans[node];
-        }
-
-        d[c].pop_back();
+        calc(0, -1, s);
     };
+
+    void calc(const int node, int parent, const string& s)
+    {
+        int c = s[node] - 'a';
+    }
 };
 
 
