8c5381e8243a66f2b2d3889363433fd20057bb51
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-23 13:58:30 +0800

    leetcode/c++/3300-3399/3331.cpp - own reproduce

--- leetcode/c++/3300-3399/3331.cpp
+++ leetcode/c++/3300-3399/3331.cpp
@@ -15,12 +15,33 @@ public:
             children[parent[i]].push_back(i);
         }
 
-        calc(0, -1, s);
+        vector answer(parent.size(), 0);
+        vector<vector<int>> stack(26);
+
+        dfs(0, -1, s, children, answer, stack);
+
+        return answer;
     };
 
-    void calc(const int node, int parent, const string& s)
+    void dfs(const int node, int parent, const string& s, vector<vector<int>>& children, vector<int>& answer, vector<vector<int>>& stack)
     {
-        int c = s[node] - 'a';
+        const int c = s[node] - 'a';
+        stack[c].push_back(node);
+        answer[node] += 1;
+
+        for (const auto child : children[node])
+        {
+            dfs(child, node, s, children, answer, stack);
+        }
+
+        int nearestCommonAncestor = stack[c].size() > 1 ? stack[c][stack[c].size() - 2] : parent;
+
+        if (nearestCommonAncestor != -1)
+        {
+            answer[nearestCommonAncestor] += answer[node];
+        }
+
+        stack.pop_back();
     }
 };
 
@@ -29,7 +50,7 @@ int main()
 {
     Solution s;
 
-    vector a = {-1,0,0,1,1,1};
+    vector a = {-1, 0, 0, 1, 1, 1};
 
     for (const vector response = s.findSubtreeSizes(a, "abaabc"); auto i : response)
     {
