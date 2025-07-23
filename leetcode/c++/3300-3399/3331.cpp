ba23f717b7aae4e0f618574610950748adbf548a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-23 12:57:27 +0800

    leetcode/c++/3300-3399/3331.cpp

--- leetcode/c++/3300-3399/3331.cpp
+++ leetcode/c++/3300-3399/3331.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findSubtreeSizes(vector<int>& parent, string s) {
+    vector<int> findSubtreeSizes(vector<int>& parent, string s)
+    {
+        for (int i = 0; i < parent.size(); ++i)
+        {
+            int current = parent[i];
+            while (current != -1 && s[current] != s[i])
+            {
+                current = parent[current];
+            }
 
+            if (s[current] == s[i])
+            {
+                parent[i] = current;
+            }
+        }
     }
 };
