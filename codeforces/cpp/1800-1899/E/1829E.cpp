3bf221b8cc389b7b1b956ba167f424a99d6bf901
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-27 12:56:57 +0800

    codeforces/cpp/1800-1899/E/1829E.cpp

--- codeforces/cpp/1800-1899/E/1829E.cpp
+++ codeforces/cpp/1800-1899/E/1829E.cpp
@@ -4,17 +4,19 @@
 
 using namespace std;
 
-int dfs(int i, int j, set<pair<int, int>>& visited, vector<vector<int>>& v, int& n, int& m)
+#define ll long long
+
+ll dfs(ll i, ll j, set<pair<ll, ll>>& visited, vector<vector<ll>>& v, ll& n, ll& m)
 {
     visited.insert({i, j});
-    int response = v[i][j];
+    ll response = v[i][j];
 
-    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<pair<ll, ll>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     for (auto [x, y] : directions)
     {
-        int new_x = i + x;
-        int new_y = j + y;
+        ll new_x = i + x;
+        ll new_y = j + y;
 
         if (new_x >= 0 and new_x < n and new_y >= 0 and new_y < m and !visited.contains({new_x, new_y}) and v[new_x][
             new_y] > 0)
@@ -28,22 +30,22 @@ int dfs(int i, int j, set<pair<int, int>>& visited, vector<vector<int>>& v, int&
 
 void solve()
 {
-    int n, m, max_value = 0;
+    ll n, m, max_value = 0;
     cin >> n >> m;
-    vector v(n, vector(m, 0));
-    set<pair<int, int>> visited;
+    vector v(n, vector(m, 0LL));
+    set<pair<ll, ll>> visited;
 
-    for (int i = 0; i < n; ++i)
+    for (ll i = 0; i < n; ++i)
     {
-        for (int j = 0; j < m; ++j)
+        for (ll j = 0; j < m; ++j)
         {
             cin >> v[i][j];
         }
     }
 
-    for (int i = 0; i < n; ++i)
+    for (ll i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n; ++j)
+        for (ll j = 0; j < n; ++j)
         {
             if (v[i][j] > 0 && !visited.contains({i, j}))
             {
@@ -57,7 +59,7 @@ void solve()
 
 int main()
 {
-    int t;
+    ll t;
     cin >> t;
     while (t--)
     {
