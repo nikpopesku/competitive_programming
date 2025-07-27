11b731c5b19772d508345239a4005805b5670d58
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-27 11:42:58 +0800

    codeforces/cpp/1800-1899/B/1829B.cpp

---
+++ codeforces/cpp/1800-1899/B/1829B.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+using namespace std;
+
+void solve()
+{
+    int n, value;
+    cin >> n;
+    int max_blank_space = 0, current = 0;;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> value;
+
+        if (value == 0)
+        {
+            ++current;
+        } else
+        {
+            max_blank_space = max(max_blank_space, current);
+            current = 0;
+        }
+    }
+
+    cout << max_blank_space <<"\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
