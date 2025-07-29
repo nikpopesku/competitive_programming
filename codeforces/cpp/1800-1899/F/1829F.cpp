6e8d7c00d0082dcb0ff5b389eadb02b2c0a41b91
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-29 12:52:39 +0800

    codeforces/cpp/1800-1899/F/1829F.cpp

--- codeforces/cpp/1800-1899/F/1829F.cpp
+++ codeforces/cpp/1800-1899/F/1829F.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void solve()
 {
+    int n, m, a, b;
+    cin >> n >> m;
+    int degree[200];
+    for (int & i : degree) i = 0;
 
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> a >> b;
+        ++degree[a];
+        ++degree[b];
+    }
 }
 
 int main()
