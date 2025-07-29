6b9313921008811be52a391dbde53e2007fd2640
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-29 13:18:34 +0800

    codeforces/cpp/1800-1899/F/1829F.cpp

--- codeforces/cpp/1800-1899/F/1829F.cpp
+++ codeforces/cpp/1800-1899/F/1829F.cpp
@@ -1,20 +1,59 @@
 #include <iostream>
-#include <vector>
+#include <map>
+#include <ranges>
 
 using namespace std;
+
+constexpr int VERTICES = 200;
+constexpr int EDGES = 1000;
 
 void solve()
 {
     int n, m, a, b;
     cin >> n >> m;
-    int degree[200];
-    for (int & i : degree) i = 0;
+    int degree[VERTICES];
+    map<int, int> mp;
+    for (int& i : degree) i = 0;
 
     for (int i = 0; i < m; ++i)
     {
         cin >> a >> b;
         ++degree[a];
         ++degree[b];
+    }
+
+    int leaves = 0;
+
+    for (int i : degree)
+    {
+        if (i == 1)
+        {
+            ++leaves;
+        }
+        else if (i > 1)
+        {
+            ++mp[i];
+        }
+    }
+
+    if (mp.size() == 2)
+    {
+        for (auto [fst, snd] : mp)
+        {
+            if (snd == 1)
+            {
+                cout << fst << " " << leaves / fst << "\n";
+                return;
+            }
+        }
+    }
+    else
+    {
+        for (const auto snd : mp | views::values)
+        {
+            cout << snd - 1 << " " << leaves / (snd - 1) << "\n";
+            return;
+        }
     }
 }
 
