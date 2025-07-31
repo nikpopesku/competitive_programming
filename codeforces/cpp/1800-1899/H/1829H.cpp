beccdd2fff36a3b888c4a1a12dd08ade7f3c2dfe
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-31 14:20:23 +0800

    codeforces/cpp/1800-1899/H/1829H.cpp

--- codeforces/cpp/1800-1899/H/1829H.cpp
+++ codeforces/cpp/1800-1899/H/1829H.cpp
@@ -5,14 +5,16 @@ constexpr int mod = 1e9 + 7;
 
 void solve()
 {
-    int n, x;
-    cin >> n >> x;
+    int n, k;
+    cin >> n >> k;
     vector<int> a(n + 1);
     vector dp(n + 1, vector(1 << 6, 0));
+
     for (int i = 1; i <= n; ++i)
     {
         cin >> a[i];
-        for (int mask = 0; mask < (1 << 6); ++mask)
+
+        for (int mask = 0; mask < 1 << 6; ++mask)
         {
             dp[i][mask] = (dp[i][mask] + dp[i - 1][mask]) % mod;
             dp[i][mask & a[i]] = (dp[i][mask & a[i]] + dp[i - 1][mask]) % mod;
@@ -20,15 +22,18 @@ void solve()
 
         dp[i][a[i]] = (dp[i][a[i]] + 1) % mod;
     }
-    int ans = 0;
-    for (int mask = 0; mask < (1 << 6); ++mask)
+
+    int answer = 0;
+
+    for (int mask = 0; mask < 1 << 6; ++mask)
     {
-        if (__builtin_popcount(mask) == x)
+        if (__builtin_popcount(mask) == k)
         {
-            ans = (ans + dp[n][mask]) % mod;
+            answer = (answer + dp[n][mask]) % mod;
         }
     }
-    cout << ans << "\n";
+
+    cout << answer << "\n";
 }
 
 int32_t main()
