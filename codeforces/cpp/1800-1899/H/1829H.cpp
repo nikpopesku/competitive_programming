94055d06a4c814d97ed56c061599465fe65c6708
npopesku <nikolay.popesku@unbiased.co.uk>
2025-07-31 13:34:10 +0800

    codeforces/cpp/1800-1899/H/1829H.cpp

--- codeforces/cpp/1800-1899/H/1829H.cpp
+++ codeforces/cpp/1800-1899/H/1829H.cpp
@@ -1,27 +1,51 @@
-#include <iostream>
-#include <vector>
-
+#include "bits/stdc++.h"
 using namespace std;
 
 #define ll long long
 
-constexpr ll MOD = 1e9 + 7;
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
+
+const int mod = 1e9 + 7;
 
 void solve()
 {
-    ll n, k;
-    cin >> n >> k;
-    vector<ll> v(n);
-
-    for (ll i = 0; i < n; ++i)
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n + 1);
+    vector<vector<int>> dp(n + 1, vector<int>(1 << 6, 0));
+    for (int i = 1; i <= n; ++i)
     {
-        cin >> v[i];
+        cin >> a[i];
+        for (int mask = 0; mask < (1 << 6); ++mask)
+        {
+            dp[i][mask] += dp[i - 1][mask];
+            if (dp[i][mask] >= mod) dp[i][mask] -= mod;
+            dp[i][mask & a[i]] += dp[i - 1][mask];
+            if (dp[i][mask & a[i]] >= mod) dp[i][mask & a[i]] -= mod;
+        }
+        dp[i][a[i]] = (dp[i][a[i]] + 1) % mod;
     }
+    int ans = 0;
+    for (int mask = 0; mask < (1 << 6); ++mask)
+    {
+        if (__builtin_popcount(mask) == x)
+        {
+            ans = (ans + dp[n][mask]) % mod;
+        }
+    }
+    cout << ans << "\n";
 }
 
-int main()
+int32_t main()
 {
-    int t;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
     cin >> t;
     while (t--)
     {
