32fd8800b6a00f1270f97f7f955cffbde978e7a6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-02 12:16:56 +0800

    codeforces/cpp/1900-1999/C/1971C.cpp

---
+++ codeforces/cpp/1900-1999/C/1971C.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
