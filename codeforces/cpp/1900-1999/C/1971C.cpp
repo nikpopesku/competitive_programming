43960e562dde624857e54f036653472aadf8e58b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-02 12:29:22 +0800

    codeforces/cpp/1900-1999/C/1971C.cpp

--- codeforces/cpp/1900-1999/C/1971C.cpp
+++ codeforces/cpp/1900-1999/C/1971C.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
@@ -8,7 +9,27 @@ void solve()
 
     cin >> a >> b >> c >> d;
 
+    bool condition = false;
+    set<int> s1, s2;
 
+    for (int i = 1; i <= 12; ++i)
+    {
+        if (i != a && i != b)
+        {
+            if (condition)
+            {
+                s1.insert(i);
+            } else
+            {
+                s2.insert(i);
+            }
+        } else
+        {
+            condition = !condition;
+        }
+    }
+
+    cout << ((s1.contains(c) && s2.contains(d)) || (s1.contains(d) && s2.contains(c)) ? "YES\n" : "NO\n");
 }
 
 int main()
