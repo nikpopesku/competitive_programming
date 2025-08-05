efab76c7e6d9d0ac3e3a86217dfc1291420c6275
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-05 11:42:55 +0800

    codeforces/cpp/1900-1999/E/1971E.cpp

--- codeforces/cpp/1900-1999/E/1971E.cpp
+++ codeforces/cpp/1900-1999/E/1971E.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void solve()
 {
+    int n, k, q, value;
+    cin >> n >> k >> q;
+    vector a(k, 0);
+    vector b(k, 0);
+
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < q; ++i)
+    {
+        cin >> value;
+    }
 }
 
 int main()
