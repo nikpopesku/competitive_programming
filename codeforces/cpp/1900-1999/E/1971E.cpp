0a3c37297b74ce9fc6c669ff503ed1e8b79180e8
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-05 11:54:29 +0800

    codeforces/cpp/1900-1999/E/1971E.cpp

--- codeforces/cpp/1900-1999/E/1971E.cpp
+++ codeforces/cpp/1900-1999/E/1971E.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+int find(const int value, const vector& a, const int& n)
+{
+    int left = 0, right = n;
+
+    while (left < right)
+    {
+        int m = left + (right - left) / 2;
+
+        if (a[m] > value)
+        {
+            right = m - 1;
+        } else
+        {
+            left = m;
+        }
+    }
+
+    return left;
+}
 
 void solve()
 {
@@ -23,6 +44,16 @@ void solve()
     for (int i = 0; i < q; ++i)
     {
         cin >> value;
+
+        int index = find(value, a, n);
+        int response = a[index];
+
+        if (index < n)
+        {
+            response += trunc((a[index + 1] - a[index]) / (b[index + 1] - b[index]) * (value - a[index]));
+        }
+
+        cout << response << "\n";
     }
 }
 
