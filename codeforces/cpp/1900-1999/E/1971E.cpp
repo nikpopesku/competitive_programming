3f33a3cc39c8a1403289a00fac5c8567d9657275
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-05 12:05:02 +0800

    codeforces/cpp/1900-1999/E/1971E.cpp

--- codeforces/cpp/1900-1999/E/1971E.cpp
+++ codeforces/cpp/1900-1999/E/1971E.cpp
@@ -51,7 +51,7 @@ void solve()
 
         if (index < a.size() - 1)
         {
-            response += trunc((a[index + 1] - a[index]) / (b[index + 1] - b[index]) * (value - a[index]));
+            response += static_cast<int>(trunc((a[index + 1] - a[index]) / (b[index + 1] - b[index]) * (value - a[index])));
         }
 
         cout << response << " ";
