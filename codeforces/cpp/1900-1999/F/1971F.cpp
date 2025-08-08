1b91c76cb7be43912bd11ea5baa59ade746a8647
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-08 13:39:27 +0800

    codeforces/cpp/1900-1999/F/1971F.cpp

--- codeforces/cpp/1900-1999/F/1971F.cpp
+++ codeforces/cpp/1900-1999/F/1971F.cpp
@@ -9,8 +9,29 @@ void solve()
     int r;
     cin >> r;
 
-    int a[VAL];
-    for (int& i : a) i = 0;
+    // int a[VAL];
+    // for (int& i : a) i = 0;
+    int x = r;
+    int y = 0;
+
+    int response = 0;
+
+    while (x > 0)
+    {
+        int val = x * x + y * y;
+        if (val >= r * r && val < (r + 1) * (r + 1))
+        {
+            ++response;
+            ++y;
+        }
+        else
+        {
+            --x;
+            --y;
+        }
+    }
+
+    cout << 4 * response << "\n";
 }
 
 int32_t main()
