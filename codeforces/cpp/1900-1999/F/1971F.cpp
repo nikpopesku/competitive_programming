6df7cfb4747d0d642f36b2cdd9d3d92c3c9cee97
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-08 13:29:28 +0800

    codeforces/cpp/1900-1999/F/1971F.cpp

---
+++ codeforces/cpp/1900-1999/F/1971F.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int r;
+    cin >> r;
+
+    
+}
+
+int32_t main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
