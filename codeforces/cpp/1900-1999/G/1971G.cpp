3727e7b349c2c0b1a2cf6a9aa26ccc25ec79e28d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-09 14:00:03 +0800

    codeforces/cpp/1900-1999/G/1971G.cpp

--- codeforces/cpp/1900-1999/G/1971G.cpp
+++ codeforces/cpp/1900-1999/G/1971G.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+    }
 }
 
 int main()
