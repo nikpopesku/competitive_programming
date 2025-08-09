999c91e35079f21413c4453731be297775fd9db6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-09 14:49:56 +0800

    codeforces/cpp/1900-1999/G/1971G.cpp

--- codeforces/cpp/1900-1999/G/1971G.cpp
+++ codeforces/cpp/1900-1999/G/1971G.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <map>
 
 using namespace std;
 
@@ -7,11 +9,53 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(n);
+    vector<int> a(n), response(n);
+    int group_counter = 0;
+    vector<set<int>> positions;
+    vector<multiset<int>> values;
+    map<int, int> mp;
 
     for (int i = 0; i < n; ++i)
     {
         cin >> a[i];
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (!mp.contains(a[i]))
+        {
+            int value = a[i];
+            mp[value] = group_counter;
+            value = value >> 2;
+            value = value << 2;
+
+            mp[value] = group_counter;
+            mp[value + 1] = group_counter;
+            mp[value + 2] = group_counter;
+            mp[value + 3] = group_counter;
+            ++group_counter;
+        }
+
+        positions[mp[a[i]]].insert(i);
+        values[mp[a[i]]].insert(a[i]);
+    }
+
+    for (int i = 0; i < group_counter; ++i)
+    {
+        auto it_values = values[i].begin();
+        auto it_positions = positions[i].begin();
+
+        while (it_values != values[i].end())
+        {
+            response[*it_positions] = *it_values;
+            ++it_values;
+            ++it_positions;
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << response[i] << " ";
     }
 }
 
