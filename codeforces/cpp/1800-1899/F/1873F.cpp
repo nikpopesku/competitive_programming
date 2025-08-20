1557ff1bb32f0cde3f6363d64e7bb186d7266c6c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-20 11:41:40 +0800

    codeforces/cpp/1800-1899/F/1873F.cpp

---
+++ codeforces/cpp/1800-1899/F/1873F.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    vector<int> h(n);
+
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < n; ++i) cin >> h[i];
+
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
