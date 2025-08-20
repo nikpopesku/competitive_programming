3d19737ddbbbb29ea7b977d2e9784295b8d20e2a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-20 21:44:45 +0800

    codeforces/cpp/1800-1899/F/1873F.cpp

--- codeforces/cpp/1800-1899/F/1873F.cpp
+++ codeforces/cpp/1800-1899/F/1873F.cpp
@@ -12,11 +12,31 @@ void solve()
 
     vector<int> a(n);
     vector<int> h(n);
+    int max_value = 0;
+    long long current_a = 0;
 
     for (int i = 0; i < n; ++i) cin >> a[i];
-    for (int i = 0; i < n; ++i) cin >> h[i];
+    int current_max = a[0] <= k ? 1 : 0;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> h[i];
+        if (i > 0 && h[i - 1] % h[i] == 0 && current_a + a[i] <= k)
+        {
+            current_a += a[i];
+            ++current_max;
 
+            max_value = max(max_value, current_max);
+        }
+        else
+        {
+            current_max = a[i] <= k ? 1 : 0;
+            current_a = a[i] <= k ? a[i] : 0;
+        }
+    }
 
+    max_value = max(max_value, current_max);
+
+    cout << max_value << "\n";
 }
 
 int main()
