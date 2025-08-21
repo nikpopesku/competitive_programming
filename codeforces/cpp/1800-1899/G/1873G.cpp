6ed167d8cd536ef30834a1b467faba6e0e3d5c31
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-21 13:11:35 +0800

    codeforces/cpp/1800-1899/G/1873G.cpp

---
+++ codeforces/cpp/1800-1899/G/1873G.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+#define ll long long
+
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    int i = 0;
+    int response = 0;
+    while (i <= s.size() - 2)
+    {
+        if (s[i] == 'A' && s[i + 1] == 'B')
+        {
+            ++response;
+            s[i] = 'B';
+            s[i + 1] = 'C';
+            i = max(0, i - 1);
+            continue;
+        }
+
+        if (s[i] == 'B' and s[i + 1] == 'A')
+        {
+            ++response;
+            s[i] = 'C';
+            s[i + 1] = 'B';
+            ++i;
+            continue;
+        }
+
+        ++i;
+    }
+
+    cout << response << "\n";
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+}
