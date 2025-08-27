7267b64a61904af997607b6757706275001e082a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-27 12:09:40 +0800

    codeforces/cpp/1800-1899/G/1873G.cpp

--- codeforces/cpp/1800-1899/G/1873G.cpp
+++ codeforces/cpp/1800-1899/G/1873G.cpp
@@ -1,57 +1,55 @@
-#include <iostream>
-#include <vector>
-
-#define ll long long
+#include <bits/stdc++.h>
 
 using namespace std;
+
+constexpr int MAX = 200'007;
+constexpr int MOD = 1'000'000'007;
 
 void solve()
 {
     string s;
     cin >> s;
-
-    int i = 0;
-    int response = 0;
-
-    if (s.size() < 2)
+    int n = s.length(), cnt = 0;
+    bool all = (s[0] == 'B' || s[n - 1] == 'B');
+    for (int i = 0; i < n - 1; i++)
     {
-        cout << "0\n";
+        if (s[i] == s[i + 1] && s[i] == 'B') { all = true; }
+    }
+    vector<int> lens;
+    int curr = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'A') { curr++; }
+        else
+        {
+            if (curr != 0) { lens.push_back(curr); }
+            curr = 0;
+        }
+    }
+    if (curr != 0) { lens.push_back(curr); }
+    ranges::sort(lens);
+
+    if (lens.empty())
+    {
+        cout << 0 << '\n';
         return;
     }
 
-    while (i <= s.size() - 2)
+    int tot = 0;
+    if (all) { tot += lens[0]; }
+    for (int i = 1; i < lens.size(); i++)
     {
-        if (s[i] == 'A' && s[i + 1] == 'B')
-        {
-            ++response;
-            s[i] = 'B';
-            s[i + 1] = 'C';
-            i = max(0, i - 1);
-            continue;
-        }
-
-        if (s[i] == 'B' and s[i + 1] == 'A')
-        {
-            ++response;
-            s[i] = 'C';
-            s[i + 1] = 'B';
-            ++i;
-            continue;
-        }
-
-        ++i;
+        tot += lens[i];
     }
-
-    cout << response << "\n";
+    cout << tot << '\n';
 }
 
 int main()
 {
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
-        solve();
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) { solve(); }
+    // solve();
 }
