b6569a6c06af74a5581e7c2a90bbb1746a563b5a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-28 13:20:52 +0800

    codeforces/cpp/1800-1899/H/1873H.cpp

--- codeforces/cpp/1800-1899/H/1873H.cpp
+++ codeforces/cpp/1800-1899/H/1873H.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 void solve()
 {
+    int n, a, b;
+    cin >> n >> a >> b;
+    int u, v;
+    map<int, vector<int>> adj;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 }
 
 
