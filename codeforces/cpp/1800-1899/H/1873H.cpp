2aec116ba4018c4d0e17822dc265f5937a94d17c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-08-28 13:11:54 +0800

    codeforces/cpp/1800-1899/H/1873H.cpp

---
+++ codeforces/cpp/1800-1899/H/1873H.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tt;
+    cin >> tt;
+    for (int i = 1; i <= tt; i++) { solve(); }
+}
