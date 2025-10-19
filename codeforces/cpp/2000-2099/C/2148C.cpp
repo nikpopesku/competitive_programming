5ab61f147ee0e519ac3b55543bec5b499a8790d2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-19 13:33:08 +0800

    codeforces/cpp/2000-2099/C/2148C.cpp

---
+++ codeforces/cpp/2000-2099/C/2148C.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<int> a(n);
+        vector<int> b(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> a[i] >> b[i];
+        }
+
+        cout << n + m << "\n";
+    }
+
+    return 0;
+}
