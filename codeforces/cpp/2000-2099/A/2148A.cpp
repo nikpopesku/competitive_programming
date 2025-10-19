0aeac3d943dce68c01474badd8c61422b7365edc
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-19 12:56:49 +0800

    codeforces/cpp/2000-2099/A/2148A.cpp

---
+++ codeforces/cpp/2000-2099/A/2148A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for (auto i = 0; i < t; i++)
+    {
+        int x, n;
+        cin >> x >> n;
+        cout << (n % 2 == 0 ? "0" : x) << "\n";
+    }
+
+    return 0;
+}
