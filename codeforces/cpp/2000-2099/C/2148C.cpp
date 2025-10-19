00534c368a08adcd2d32895705b3356dc4a6abc9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-19 17:55:03 +0800

    codeforces/cpp/2000-2099/C/2148C.cpp

--- codeforces/cpp/2000-2099/C/2148C.cpp
+++ codeforces/cpp/2000-2099/C/2148C.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+int calc_response(const int& a, const int& b, int& response, int& current_side, int& current_time)
+{
+    if (int min_val = current_side != b ? 1 : 0; a >= current_time + min_val)
+    {
+        const int val = a - current_time - min_val;
+        response += val / 2 * 2;
+        current_time = a - min_val;
+    }
+
+    if (a > current_time)
+    {
+        ++response;
+    }
+
+    current_time = a;
+    current_side = b;
+
+    return response;
+}
+
 int main()
 {
     int t;
@@ -12,33 +32,19 @@ int main()
     {
         int n, m;
         cin >> n >> m;
-        vector<int> a(n);
-        vector<int> b(n);
+        int a, b;
         int current_side = 0;
         int current_time = 0;
         int response = 0;
 
         for (int i = 0; i < n; ++i)
         {
-            cin >> a[i] >> b[i];
-            --a[i];
+            cin >> a >> b;
 
-            int min_val = current_side != b[i] ? 1 : 0;
-            if (a[i] >= current_time + min_val)
-            {
-                const int val = a[i] - current_time - min_val;
-                response += val / 2 * 2;
-                current_time = a[i] - min_val;
-            }
-
-            if (a[i] > current_time)
-            {
-                ++response;
-            }
-
-            current_time = a[i];
-            current_side = b[i];
+            response = calc_response(a, b, response, current_side, current_time);
         }
+
+        response = calc_response(a, b, response, current_side, current_time);
 
         cout << response << "\n";
     }
