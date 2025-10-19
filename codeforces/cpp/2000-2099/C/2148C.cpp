cf161b8558030d03402fc069f974492518762ee3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-19 15:02:52 +0800

    codeforces/cpp/2000-2099/C/2148C.cpp

--- codeforces/cpp/2000-2099/C/2148C.cpp
+++ codeforces/cpp/2000-2099/C/2148C.cpp
@@ -14,9 +14,34 @@ int main()
         cin >> n >> m;
         vector<int> a(n);
         vector<int> b(n);
+        int current_side = 0;
+        int current_time = 0;
+        int response = 0;
+
         for (int i = 0; i < n; ++i)
         {
             cin >> a[i] >> b[i];
+
+            if (a[i] - 1 >= current_time + 2)
+            {
+                response += a[i] - 1 - current_time;
+                a[i] -= current_time - 1;
+            }
+
+            if (a[i] > 0)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    response += current_side != b[i] ? 2 : 1;
+                }
+                else if (current_side != b[i])
+                {
+                    ++response;
+                }
+            }
+
+            current_time = a[i] - 1;
+            current_side = b[i];
         }
 
         cout << n + m << "\n";
