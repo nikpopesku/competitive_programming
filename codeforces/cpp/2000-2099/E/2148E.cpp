baa6208bcaafcee341c11686422f2983af446945
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-21 14:25:47 +0800

    codeforces/cpp/2000-2099/E/2148E.cpp

--- codeforces/cpp/2000-2099/E/2148E.cpp
+++ codeforces/cpp/2000-2099/E/2148E.cpp
@@ -5,13 +5,11 @@
 using namespace std;
 
 
-int main()
-{
+int main() {
     int t;
     cin >> t;
 
-    while (t--)
-    {
+    while (t--) {
         int n, k;
         cin >> n >> k;
         vector<int> a(n);
