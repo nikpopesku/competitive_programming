59f21126a7c38d6bbeedd10f81f34d1901754fcd
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-21 14:25:37 +0800

    codeforces/cpp/2000-2099/E/2148E.cpp

--- codeforces/cpp/2000-2099/E/2148E.cpp
+++ codeforces/cpp/2000-2099/E/2148E.cpp
@@ -12,49 +12,13 @@ int main()
 
     while (t--)
     {
-        int n, value;
-        cin >> n;
-        int current_state = 0;
-        multiset<int> s;
-        long long response = 0;
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
 
-        for (int i = 0; i < n; ++i)
-        {
-            cin >> value;
-            if (value % 2 == 0)
-            {
-                response += value;
-            }
-            else
-            {
-                s.insert(value);
-            }
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
         }
-
-        if (s.empty())
-        {
-            cout << "0\n";
-
-            continue;
-        }
-
-        while (!s.empty())
-        {
-            if (!current_state)
-            {
-                const int largest = *prev(s.end());
-                s.erase(prev(s.end()));
-                response += largest;
-                current_state = 1;
-            }
-            else
-            {
-                s.erase(s.begin());
-                current_state = 0;
-            }
-        }
-
-        cout << response << "\n";
     }
 
     return 0;
