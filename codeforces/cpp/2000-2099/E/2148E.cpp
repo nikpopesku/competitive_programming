2e2a31b25c4e673c4ab3bcf452cf852e305233ad
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-21 14:42:16 +0800

    codeforces/cpp/2000-2099/E/2148E.cpp

--- codeforces/cpp/2000-2099/E/2148E.cpp
+++ codeforces/cpp/2000-2099/E/2148E.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -12,9 +13,19 @@ int main() {
         int n, k;
         cin >> n >> k;
         vector<int> a(n);
+        map<int, int> mp;
 
         for (int i = 0; i < n; ++i) {
             cin >> a[i];
+            mp[a[i]] += 1;
+        }
+
+        for (auto &elem: mp) {
+            if (elem.second % k != 0) {
+                cout << "0\n";
+
+                continue;
+            }
         }
     }
 
