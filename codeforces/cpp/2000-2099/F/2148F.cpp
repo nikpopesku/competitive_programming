4b43afd5a14de027f22fcfd40768a66b5d6c16d7
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-24 12:08:11 +0800

    codeforces/cpp/2000-2099/F/2148F.cpp

---
+++ codeforces/cpp/2000-2099/F/2148F.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+void solve() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc = 1;
+    cin >> tc;
+    while (tc--) solve();
+}
