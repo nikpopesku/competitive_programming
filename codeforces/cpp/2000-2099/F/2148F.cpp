2994e9e03f2ebef422b4c184126c059b7a2fe27e
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-24 12:21:38 +0800

    codeforces/cpp/2000-2099/F/2148F.cpp

--- codeforces/cpp/2000-2099/F/2148F.cpp
+++ codeforces/cpp/2000-2099/F/2148F.cpp
@@ -6,6 +6,26 @@ using namespace std;
 #define ll long long
 
 void solve() {
+    int n, k, value;
+    cin >> n;
+    vector<int> response;
+    int current_length = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> k;
+        for (int j = 0; j < k; ++j) {
+            cin >> value;
+            if (j + 1 > current_length) {
+                response.push_back(value);
+                ++current_length;
+            } else {
+                response[j] = value;
+            }
+        }
+    }
+
+    for (const auto &e: response) {
+        cout << e << " ";
+    }
 }
 
 int main() {
