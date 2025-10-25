aa08f28380f5c6779fcdbbcd2af33c2a6c649fa1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-25 16:59:47 +0800

    codeforces/cpp/2000-2099/F/2148F.cpp

--- codeforces/cpp/2000-2099/F/2148F.cpp
+++ codeforces/cpp/2000-2099/F/2148F.cpp
@@ -14,51 +14,49 @@ ostream& operator<<(ostream& out, vector<T>& a)
 
 void solve()
 {
-    int n;
+    int n, max_k = 0;
     cin >> n;
-    vector<vector<int>> g(n), relevant;
-    int max_k = 0;
+    vector<vector<int>> numbers(n), g;
 
     for (int i = 0; i < n; ++i)
     {
         int k;
         cin >> k;
-        g[i].resize(k);
-        ckmax(max_k, k);
+        max_k = max(max_k, k);
 
         for (int j = 0; j < k; ++j)
         {
-            cin >> g[i][j];
-            if (static_cast<int>(relevant.size()) == j) relevant.emplace_back();
-            relevant[j].push_back(i);
+            cin >> numbers[i][j];
+            if (g[j].size() == j)
+            {
+                g[j].emplace_back();
+            }
+            g[j].push_back(i);
         }
     }
 
-    vector<int> lex_min(max_k), rank(n, -1);
+    vector<int> lex_min, rank(n, -1);
 
     for (int i = max_k - 1; i >= 0; --i)
     {
         vector<array<int, 3>> cur;
-        for (const auto& j : relevant[i])
+
+        for (const auto& j : g[i])
         {
-            cur.push_back({g[j][i], rank[j], j});
+            cur.push_back({numbers[j][i], rank[i], j});
         }
+
         sort(cur.begin(), cur.end());
         lex_min[i] = cur[0][2];
         int rk = 0;
-        for (auto& j : cur) rank[j[2]] = rk++;
+        for (auto & c: cur)
+        {
+            rank[c[2]] = rk++;
+        }
     }
 
     vector<int> ans;
 
-    while (static_cast<int>(ans.size()) < max_k)
-    {
-        const int tmp = static_cast<int>(ans.size());
-        auto& v = g[lex_min[tmp]];
-        for (int i = tmp; i < static_cast<int>(v.size()); ++i) ans.push_back(v[i]);
-    }
-
-    assert(static_cast<int>(ans.size()) == max_k);
     cout << ans << "\n";
 }
 
