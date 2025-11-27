e3bf8463f58012ee2259c37e8fd0d1597ec6cc9d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-27 20:04:51 +0800

    cses.fi/1000-1099/1070.cpp

---
+++ cses.fi/1000-1099/1070.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 3) {
+        cout << "NO SOLUTION\n";
+
+        return 0;
+    }
+
+    int i, j;
+    for (i = 1, j = n / 2 + 1; i <= n / 2; ++i, ++j) {
+        cout << j << " " << i << " ";
+    }
+
+    if (n % 2 == 1) {
+        cout << j;
+    }
+}
