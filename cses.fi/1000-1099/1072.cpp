1a7522e1ba1c3973a4c1364491d6264a96cf1ac5
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-28 19:35:27 +0800

    cses.fi/1000-1099/1072.cpp

--- cses.fi/1000-1099/1072.cpp
+++ cses.fi/1000-1099/1072.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
+    map<int, int> mp;
+
+    for (int i = 1; i <= n; ++i) {
+        mp[i] = 0;
+    }
 
     vector<pair<int, int> > directions = {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
+    vector<pair<int, int> > directions2 = {{1, 2}, {2, 1}, {1, -2}, {2, -1}};
+    int response = 0;
+
+    for (int x = 0; x < n; ++x) {
+        for (int y = 0; y < n; ++y) {
+            int count = 0;
+            for (auto &[fst, snd]: directions2) {
+                pair nd = {x + fst, y + snd};
+                if (nd.first < n && nd.second >= 0 && nd.second < n) {
+                    ++count;
+                }
+            }
+
+            response += n * n - (x * n + y + count);
+        }
+    }
+
+    cout << response << "\n";
 }
