d2ef2049660bdbc5dae49dc485454397f6e9f8da
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-28 19:57:46 +0800

    cses.fi/1000-1099/1072.cpp

--- cses.fi/1000-1099/1072.cpp
+++ cses.fi/1000-1099/1072.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> n;
 
     for (int i = 1; i <= n; ++i) {
-        long long total = static_cast<long long>(i) * i * (static_cast<long long>(i) * i - 1) / 2;
+        const long long total = static_cast<long long>(i) * i * (static_cast<long long>(i) * i - 1) / 2;
         long long attacking = 0;
         if (i > 2) {
             attacking = 4LL * (i - 1) * (i - 2);
