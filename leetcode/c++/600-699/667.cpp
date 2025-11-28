ae63916710f45bad6629a79b50b97ce142e33c43
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-28 11:52:19 +0800

    leetcode/c++/600-699/667.cpp

--- leetcode/c++/600-699/667.cpp
+++ leetcode/c++/600-699/667.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> constructArray(int n, int k) {
+        vector<int> v;
+        backtrack(v, k, 0);
+    }
 
+private:
+    vector<int> response;
+
+    void backtrack(vector<int> v, int k, int n) {
+        if (static_cast<int>(v.size()) == n && Solution::check(v) == k) {
+            response = v;
+
+            return;
+        }
+    }
+
+
+    static int check(const vector<int> &v) {
+        set<int> s;
+        for (int i = 1; i < v.size(); ++i) {
+            s.insert(abs(v[i] - v[i - 1]));
+        }
+
+        return s.count();
     }
 };
 
@@ -16,7 +39,7 @@ int main() {
     int n, k;
     cin >> n >> k;
 
-    for (const auto & e: s.constructArray(n, k)) {
+    for (const auto &e: s.constructArray(n, k)) {
         cout << e << " ";
     }
 
