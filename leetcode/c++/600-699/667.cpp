c5d881d88abf927f253bc459be9e09c1f5cf7d49
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-28 11:39:59 +0800

    leetcode/c++/600-699/667.cpp

---
+++ leetcode/c++/600-699/667.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> constructArray(int n, int k) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    int n, k;
+    cin >> n >> k;
+
+    for (const auto & e: s.constructArray(n, k)) {
+        cout << e << " ";
+    }
+
+    return 0;
+}
