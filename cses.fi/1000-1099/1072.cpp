7101de81b245b8c01f93f7dc56dfa065ffc79551
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-28 18:04:20 +0800

    cses.fi/1000-1099/1072.cpp

--- cses.fi/1000-1099/1072.cpp
+++ cses.fi/1000-1099/1072.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
+
+    vector<pair<int, int> > directions = {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}};
 }
