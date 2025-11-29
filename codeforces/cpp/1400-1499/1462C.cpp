098aa1260a659124969a9c1313de7f64874284c4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-29 12:44:27 +0800

    codeforces/cpp/1400-1499/1462C.cpp

---
+++ codeforces/cpp/1400-1499/1462C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x;
+        cin >> x;
+
+        unordered_set<int> s;
+        int response = 0;
+
+        for (int num = 1; num <= x; ++num) {
+            int i = num;
+
+            while (i) {
+                int digit = i % 10;
+
+                i = i / 10;
+
+                if (s.contains(digit)) {
+                    response = -1;
+                    break;
+                }
+
+                s.insert(digit);
+                response += digit;
+            }
+
+            if (response == num) {
+                cout << i << "\n";
+                break;
+            }
+
+            response = -1;
+        }
+
+        if (response == -1) {
+            cout << "-1\n";
+        }
+    }
+}
