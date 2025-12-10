97c139bc82b979f6b14b71912cb534a55731e1b9
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-10 12:04:08 +0800

    leetcode/c++/700-799/771_jewels_and_stones.cpp

--- leetcode/c++/700-799/771_jewels_and_stones.cpp
+++ leetcode/c++/700-799/771_jewels_and_stones.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
+    int numJewelsInStones(const string& jewels, const string& stones) {
+        unordered_set<int> s;
 
+        for (const auto &j: jewels) {
+            s.insert(j);
+        }
+
+        int counter = 0;
+
+        for (const auto &st: stones) {
+            if (s.contains(st)) {
+                ++counter;
+            }
+        }
+
+        return counter;
     }
 };
+
 int main() {
     auto s = Solution();
     cout << s.numJewelsInStones("aA", "aAAbbbb") << endl;
