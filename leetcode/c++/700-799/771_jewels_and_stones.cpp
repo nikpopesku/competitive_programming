189b5d52b15ddc46b87e9245373f57e3cc4aa349
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-10 11:59:46 +0800

    leetcode/c++/700-799/771_jewels_and_stones.cpp

---
+++ leetcode/c++/700-799/771_jewels_and_stones.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+
+    }
+};
+int main() {
+    auto s = Solution();
+    cout << s.numJewelsInStones("aA", "aAAbbbb") << endl;
+    cout << s.numJewelsInStones("z", "ZZ") << endl;
+}
