b1ee8288bf9dea0ae3d0ed975d5afbc58aa3292b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-10 12:08:48 +0800

    leetcode/c++/300-399/387_first_unique_character.cpp

---
+++ leetcode/c++/300-399/387_first_unique_character.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+    cout << s.firstUniqChar("leetcode") << endl;
+    cout << s.firstUniqChar("loveleetcode") << endl;
+    cout << s.firstUniqChar("aabb") << endl;
+}
