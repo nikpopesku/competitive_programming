2a19b4c5d27144b6aaa73f6a1730e38dd0b25845
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-11 12:17:58 +0800

    codeforces/cpp/600-699/665C.cpp

---
+++ codeforces/cpp/600-699/665C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    for (int i = 1; i < static_cast<int>(s.size()) - 1; ++i) {
+        if (s[i - 1] == s[i] || s[i] == s[i + 1]) {
+            for (char c = 'a'; c < 'z'; ++c) {
+                if (c != s[i - 1] && c != s[i + 1]) {
+                    s[i] = c;
+                    break;
+                }
+            }
+        }
+    }
+
+    cout << s << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
