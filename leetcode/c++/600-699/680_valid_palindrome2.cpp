777eb3c74fe44f41e90e26352ec5dd362c7755ce
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-11 12:57:42 +0800

    leetcode/c++/600-699/680_valid_palindrome2.cpp

--- leetcode/c++/600-699/680_valid_palindrome2.cpp
+++ leetcode/c++/600-699/680_valid_palindrome2.cpp
@@ -6,24 +6,29 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int left = 0, right = static_cast<int>(s.size()) - 1;
-        int counter = 0;
 
-        while (left <= right && counter <= 1) {
+        while (left < right) {
+            if (s[left] == s[right]) {
+                ++left;
+                --right;
+            } else {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+
+        return true;
+    }
+
+private:
+    static bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
             if (s[left] != s[right]) {
-                if (s[left + 1] == s[right]) {
-                    ++left;
-                } else {
-                    --right;
-                }
-
-                ++counter;
-                continue;
+                return false;
             }
             ++left;
             --right;
         }
-
-        return counter <= 1;
+        return true;
     }
 };
 
