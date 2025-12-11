e021e9fa23886bedb3d9892231b3326a65ace0d1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-11 13:03:32 +0800

    leetcode/c++/600-699/680_valid_palindrome2.cpp

--- leetcode/c++/600-699/680_valid_palindrome2.cpp
+++ leetcode/c++/600-699/680_valid_palindrome2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool validPalindrome(string s) {
+    bool validPalindrome(const string &s) {
         int left = 0, right = static_cast<int>(s.size()) - 1;
 
         while (left < right) {
@@ -12,7 +12,7 @@ public:
                 ++left;
                 --right;
             } else {
-                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+                return isPalindrome(left + 1, right, s) || isPalindrome(left, right - 1, s);
             }
         }
 
@@ -20,14 +20,16 @@ public:
     }
 
 private:
-    static bool isPalindrome(const string& s, int left, int right) {
+    static bool isPalindrome(int left, int right, const string &s) {
         while (left < right) {
             if (s[left] != s[right]) {
                 return false;
             }
+
             ++left;
             --right;
         }
+
         return true;
     }
 };
@@ -38,7 +40,10 @@ int main() {
     cout << s.validPalindrome("aba") << endl;
     cout << s.validPalindrome("abca") << endl;
     cout << s.validPalindrome("abc") << endl;
-    cout << s.validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga") << endl;
+    cout << s.validPalindrome(
+                "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")
+            <<
+            endl;
 
     return 0;
 }
