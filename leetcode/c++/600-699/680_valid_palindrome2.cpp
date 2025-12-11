a7e8fb256296870eb59ccf91d3faaf283b43f302
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-11 12:49:16 +0800

    leetcode/c++/600-699/680_valid_palindrome2.cpp

--- leetcode/c++/600-699/680_valid_palindrome2.cpp
+++ leetcode/c++/600-699/680_valid_palindrome2.cpp
@@ -5,17 +5,31 @@ using namespace std;
 class Solution {
 public:
     bool validPalindrome(string s) {
+        int left = 0, right = static_cast<int>(s.size()) - 1;
+        int counter = 0;
 
+        while (left <= right && counter <= 1) {
+            if (s[left] != s[right]) {
+                if (s[left + 1] == s[right]) {
+                    ++left;
+                } else {
+                    --right;
+                }
+
+                ++counter;
+            }
+        }
+
+        return counter <= 1;
     }
 };
 
 int main() {
     Solution s;
 
-    cout << s.isPalindrome("A man, a plan, a canal: Panama") << endl;
-    cout << s.isPalindrome("race a car") << endl;
-    cout << s.isPalindrome(" ") << endl;
-    cout << s.isPalindrome("0P") << endl;
+    cout << s.validPalindrome("aba") << endl;
+    cout << s.validPalindrome("abca") << endl;
+    cout << s.validPalindrome("abc") << endl;
 
     return 0;
 }
