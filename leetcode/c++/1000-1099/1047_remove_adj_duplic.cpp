f278f3f45c3354bcdd356924ae6fee84b976b40a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-15 11:19:21 +0800

    leetcode/c++/1000-1099/1047_remove_adj_duplic.cpp

---
+++ leetcode/c++/1000-1099/1047_remove_adj_duplic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.validPalindrome("aba") << endl;
+    cout << s.validPalindrome("abca") << endl;
+    cout << s.validPalindrome("abc") << endl;
+    cout << s.validPalindrome(
+                "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")
+            <<
+            endl;
+
+    return 0;
+}
