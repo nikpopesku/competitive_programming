852eac01b910bead649b437a010383ae3d5b51e3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-15 12:03:14 +0800

    leetcode/c++/1000-1099/1021_remove_outermost_par.cpp

--- leetcode/c++/1000-1099/1021_remove_outermost_par.cpp
+++ leetcode/c++/1000-1099/1021_remove_outermost_par.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
+    string removeOuterParentheses(const string& s) {
+        string result;
+        result.reserve(s.size()); // Reserve memory to avoid reallocations
+        int depth = 0;
 
+        for (const char c : s) {
+            if (c == '(') {
+                if (depth++ > 0) { // Increment after check
+                    result += c;
+                }
+            } else { // c == ')'
+                if (--depth > 0) { // Decrement after check
+                    result += c;
+                }
+            }
+        }
+
+        return result;
     }
 };
 
