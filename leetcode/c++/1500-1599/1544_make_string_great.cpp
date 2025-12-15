0e1211844c0e9725c080477c997a258adffc2c8b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-15 11:35:59 +0800

    leetcode/c++/1500-1599/1544_make_string_great.cpp

---
+++ leetcode/c++/1500-1599/1544_make_string_great.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+
+    }
+};
+int main() {
+    Solution s;
+
+    cout << s.makeGood("leEeetcode") << endl;
+    cout << s.makeGood("abBAcC") << endl;
+    cout << s.makeGood("s") << endl;
+
+    return 0;
+}
