ed0a873580d8beb8d5e42fac16df6d4744297525
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-15 11:42:31 +0800

    leetcode/c++/1500-1599/1544_make_string_great.cpp

--- leetcode/c++/1500-1599/1544_make_string_great.cpp
+++ leetcode/c++/1500-1599/1544_make_string_great.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
 
 class Solution {
 public:
-    string makeGood(string s) {
+    string makeGood(const string &s) {
+        vector<int> v;
 
+        for (const auto &c: s) {
+            if (!v.empty() && v.back() != c && (tolower(v.back()) == c || v.back() == tolower(c))) {
+                v.pop_back();
+            } else {
+                v.push_back(c);
+            }
+        }
+
+        string t(v.begin(), v.end());
+
+        return t;
     }
 };
+
 int main() {
     Solution s;
 
