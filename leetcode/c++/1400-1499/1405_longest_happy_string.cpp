b34986000d2873b4fc88976008c8ba8a294b5ec2
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-18 11:47:26 +0800

    leetcode/c++/1400-1499/1405_longest_happy_string.cpp

---
+++ leetcode/c++/1400-1499/1405_longest_happy_string.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string reorganizeString(const string &s) {
+        unordered_map<char, int> mp;
+        string response;
+        response.reserve(s.size());
+
+        for (auto &c: s) {
+            ++mp[c];
+        }
+
+        priority_queue<pair<int, char> > pq;
+
+        for (auto [fst, snd]: mp) {
+            if (snd >= (s.size() + (s.size() % 2 == 0 ? 2 : 3)) / 2) {
+                return "";
+            }
+
+            pq.emplace(snd, fst);
+        }
+
+
+        while (!pq.empty()) {
+            pair<int, char> first = {}, second = {};
+
+            first = pq.top();
+            response += first.second;
+            pq.pop();
+
+
+            if (pq.empty()) {
+                break;
+            }
+
+
+            second = pq.top();
+            pq.pop();
+            response += second.second;
+
+            if (--first.first > 0) {
+                pq.emplace(first);
+            }
+
+
+            if (--second.first > 0) {
+                pq.emplace(second);
+            }
+        }
+
+        return response;
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.reorganizeString("aab") << endl;
+    cout << s.reorganizeString("aaab") << endl;
+
+    return 0;
+}
