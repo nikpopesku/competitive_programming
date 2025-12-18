a8522a251c73c9fa9057f422a88f2759ad6ffd1a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-18 12:01:27 +0800

    leetcode/c++/1400-1499/1405_longest_happy_string.cpp

--- leetcode/c++/1400-1499/1405_longest_happy_string.cpp
+++ leetcode/c++/1400-1499/1405_longest_happy_string.cpp
@@ -8,9 +8,40 @@ using namespace std;
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char> > pq;
 
+        pq.push({a, 'a'});
+        pq.push({b, 'b'});
+        pq.push({c, 'c'});
+
+        string response;
+
+        while (!pq.empty()) {
+            auto p1 = pq.top();
+            pq.pop();
+
+            if (pq.empty()) {
+                response += p1.first > 1 ? p1.second + p1.second : p1.second;
+                break;
+            }
+
+            auto p2 = pq.top();
+            pq.pop();
+
+            const int v1 = p1.first - 2 >= p2.first ? 2 : 1;
+            response += v1 == 2 ? p1.second + p1.second : p1.second;
+
+            const int v2 = p2.first - 2 >= pq.top().first ? 2 : 1;
+            response += v2 == 2 ? p2.second + p2.second : p2.second;
+
+            pq.push({p1.first - v1, p1.second});
+            pq.push({p2.first - v2, p2.second});
+        }
+
+        return response;
     }
 };
+
 int main() {
     Solution s;
 
