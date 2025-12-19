a01f25d5a41e0f6e96ae151f18beb6db0013fc5c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-19 11:58:14 +0800

    leetcode/c++/400-499/409_longest_palindrome.cpp

--- leetcode/c++/400-499/409_longest_palindrome.cpp
+++ leetcode/c++/400-499/409_longest_palindrome.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 #include <vector>
 
 
@@ -7,7 +8,28 @@ using namespace std;
 class Solution {
 public:
     int longestPalindrome(string s) {
+        unordered_map<char, int> mp;
 
+        for (auto & c: s) {
+            ++mp[c];
+        }
+
+        int response = 0;
+        bool has_odd = false;
+
+        for (auto &e : mp) {
+            if (!has_odd && e.second % 2 == 1) {
+                has_odd = true;
+            }
+
+            response += e.second % 2 == 1 ? e.second - 1: e.second;
+        }
+
+        if (has_odd) {
+            ++response;
+        }
+
+        return response;
     }
 };
 
