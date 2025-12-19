1f22f9bcc57fed05740004e9b7db18e750cbc58d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-19 16:26:16 +0800

    leetcode/c++/200-299/214_shortest_palindrome.cpp

---
+++ leetcode/c++/200-299/214_shortest_palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.shortestPalindrome("aacecaaa") << endl;
+    cout << s.shortestPalindrome("abcd") << endl;
+
+    return 0;
+}
