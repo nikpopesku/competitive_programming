dd024be9c16f5b69aa05a0d9bf12cd4206930941
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-19 11:52:12 +0800

    leetcode/c++/400-499/409_longest_palindrome.cpp

---
+++ leetcode/c++/400-499/409_longest_palindrome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.longestPalindrome("abccccdd") << endl;
+    cout << s.longestPalindrome("a") << endl;
+
+    return 0;
+}
