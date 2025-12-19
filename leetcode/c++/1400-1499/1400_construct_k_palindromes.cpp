fd61148aed1093e06935298d0335b8c907d6a124
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-19 12:04:36 +0800

    leetcode/c++/400-499/409_longest_palindrome.cpp

---
+++ leetcode/c++/1400-1499/1400_construct_k_palindromes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    cout << s.canConstruct("annabelle", 2) << endl;
+    cout << s.canConstruct("leetcode", 3) << endl;
+    cout << s.canConstruct("true", 4) << endl;
+
+    return 0;
+}
