09ff5e33d5ada9741bbd2f71b75209c65e0c9342
npopesku <nikolay.popesku@unbiased.co.uk>
2025-12-19 16:38:55 +0800

    leetcode/c++/200-299/214_shortest_palindrome.cpp

--- leetcode/c++/200-299/214_shortest_palindrome.cpp
+++ leetcode/c++/200-299/214_shortest_palindrome.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <ranges>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     string shortestPalindrome(string s) {
+        unordered_map<char, int> mp;
+        for (auto &c: s) {
+            ++mp[c];
+        }
 
+        int odd = 0;
+
+        for (auto &val: mp | views::values) {
+            if (val % 2 == 1) {
+                ++odd;
+            }
+        }
+
+        string response;
+
+        for (int i = static_cast<int>(s.size()) - 1; i >= s.size() - odd + 1; ++i) {
+            response += s[i];
+        }
+
+        return response + s;
     }
 };
 
 int main() {
     Solution s;
 
-    cout << s.shortestPalindrome("aacecaaa") << endl;
-    cout << s.shortestPalindrome("abcd") << endl;
+    cout << s.shortestPalindrome("aacecaaa") << endl; //aaacecaaa
+    cout << s.shortestPalindrome("abcd") << endl; //dcbabcd
 
     return 0;
 }
