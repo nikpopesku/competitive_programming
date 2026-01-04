f7feda66a4dbdda1e3c29061d332ab501154e093
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-04 14:07:01 +0200

    leetcode/c++/300-399/316_remove_dup_letters.cpp

--- leetcode/c++/300-399/316_remove_dup_letters.cpp
+++ leetcode/c++/300-399/316_remove_dup_letters.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -7,7 +8,29 @@ using namespace std;
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+        vector<char> v;
+        unordered_map<char, vector<int> > mp;
+        const size_t n = s.size();
 
+        for (int i = 0; i < n; ++i) {
+            mp[s[i]].push_back(i);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (v.empty()) {
+                v.push_back(s[i]);
+                continue;
+            }
+            auto occ = mp[v[v.size() - 1]];
+            while (s[i] < v[v.size() - 1] && occ[occ.size() - 1] > i) {
+                v.pop_back();
+                occ = mp[v[v.size() - 1]];
+            }
+        }
+
+        string ss(v.begin(), v.end());
+
+        return ss;
     }
 };
 
