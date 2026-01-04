0aadd7a9384a89f87ac4151d1bfaf54918106dc7
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-04 12:12:55 +0200

    leetcode/c++/400-499/402_remove_k_digits.cpp

--- leetcode/c++/400-499/402_remove_k_digits.cpp
+++ leetcode/c++/400-499/402_remove_k_digits.cpp
@@ -10,7 +10,7 @@ public:
         vector<char> st;
 
         for (const auto &c: num) {
-            if (k > 0 && !st.empty() && st[st.size() - 1] > c) {
+            while (k > 0 && !st.empty() && st[st.size() - 1] > c) {
                 st.pop_back();
                 --k;
             }
@@ -36,10 +36,10 @@ public:
 int main() {
     Solution s;
 
-    // cout << s.removeKdigits("1432219", 3) << endl; //1219
-    // cout << s.removeKdigits("10200", 1) << endl; //200
-    // cout << s.removeKdigits("10", 2) << endl; //0
-    // cout << s.removeKdigits("9", 1) << endl; //0
+    cout << s.removeKdigits("1432219", 3) << endl; //1219
+    cout << s.removeKdigits("10200", 1) << endl; //200
+    cout << s.removeKdigits("10", 2) << endl; //0
+    cout << s.removeKdigits("9", 1) << endl; //0
     cout << s.removeKdigits("33526221184202197273", 19) << endl; //0
 
     return 0;
