7883e6dafcaa3ff85395a576f353d41e2a9c6913
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-25 17:50:14 +0800

    leetcode/c++/1300-1399/1312_minimum_insertion.cpp

--- leetcode/c++/1300-1399/1312_minimum_insertion.cpp
+++ leetcode/c++/1300-1399/1312_minimum_insertion.cpp
@@ -15,6 +15,25 @@ public:
         for (int i = 0; i < n; ++i) {
             dp[i][i] = 1;
         }
+
+        for (int i = 1; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                dp[i][i + 1] = 2;
+            }
+        }
+
+        for (int len = 3; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1] - n;
     }
 };
 
