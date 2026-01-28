fa516586e45b7320523e967b3ae98e8d5f0c4ffe
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-28 13:50:31 +0800

    leetcode/c++/1700-1799/1791_center_star_graph.cpp

--- leetcode/c++/1700-1799/1791_center_star_graph.cpp
+++ leetcode/c++/1700-1799/1791_center_star_graph.cpp
@@ -8,8 +8,12 @@ using namespace std;
 
 class Solution {
 public:
-    int findCenter(vector<vector<int>>& edges) {
+    int findCenter(const vector<vector<int>>& edges) {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) {
+            return edges[0][0];
+        }
 
+        return edges[0][1];
     }
 };
 
