a44560b76a3eee58365e3748a42a61aceee7eebe
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-29 12:20:22 +0800

    lintcode/1245_all_o1_data_structure.cpp

---
+++ lintcode/1245_all_o1_data_structure.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class AllOne {
+public:
+    /**
+     * @param key: the element given to be added
+     * @return: nothing
+     */
+    void inc(const string &key) {
+        ++mp[key];
+    }
+
+    /**
+     * @param key: pop an element from the queue
+     * @return: nothing
+     */
+    void dec(const string &key) {
+        if (mp[key] == 1) {
+            delete mp[key];
+        } else {
+            --mp[key];
+        }
+    }
+
+    /**
+     * @return: nothing
+     */
+    string getMaxKey() {
+        // write your code here
+    }
+
+    /**
+     * @return: nothing
+     */
+    string getMinKey() {
+        // write your code here
+    }
+private:
+    unordered_map<string, int> mp;
+};
+
+
+int main() {
+    auto s = Solution();
+
+    const vector<vector<int> > edges = {{0, 1}, {0, 2}};
+    cout << s.countComponents(3, edges) << endl; //1
+
+    const vector<vector<int> > edges2 = {{0, 1}, {1, 2}, {2, 3}, {4, 5}};
+    cout << s.countComponents(6, edges2) << endl; //2
+}
