18277aa169105cd4f9e94edcc46dd4134cedf4f5
npopesku <nikolay.popesku@unbiased.co.uk>
2026-01-30 11:16:14 +0800

    lintcode/1245_all_o1_data_structure.cpp

--- lintcode/1245_all_o1_data_structure.cpp
+++ lintcode/1245_all_o1_data_structure.cpp
@@ -1,93 +1,61 @@
-#include <iterator>
+#include <string>
+#include <iostream>
 #include <list>
 #include <set>
-#include <string>
 #include <unordered_map>
 
 using namespace std;
 
-/**
- * O(1) AllOne: doubly-linked list of "buckets" (count -> set of keys).
- * - List ordered by count ascending → first bucket = min, last = max.
- * - GetMinKey/GetMaxKey return lexicographically smallest in that bucket (set::begin()).
- * - mp[key] = iterator to the bucket containing key.
- */
 class AllOne {
 public:
-    void inc(const string &key) {
-        if (mp.count(key) == 0) {
-            // New key: count becomes 1
-            if (buckets.empty() || buckets.front().count > 1) {
-                buckets.push_front(Bucket{1, {key}});
-            } else {
-                buckets.front().keys.insert(key);
-            }
-            mp[key] = buckets.begin();
-            return;
-        }
-
-        auto it = mp[key];
-        int cur = it->count;
-        it->keys.erase(key);
-        auto nxt = std::next(it);
-
-        if (nxt == buckets.end() || nxt->count > cur + 1) {
-            mp[key] = buckets.insert(nxt, Bucket{cur + 1, {key}});
-        } else {
-            nxt->keys.insert(key);
-            mp[key] = nxt;
-        }
-
-        if (it->keys.empty()) {
-            buckets.erase(it);
-        }
+    /**
+     * @param key: the element given to be added
+     * @return: nothing
+     */
+    void inc(string &key) {
+        // write your code here
     }
 
-    void dec(const string &key) {
-        if (mp.count(key) == 0) return;
-
-        auto it = mp[key];
-        int cur = it->count;
-        it->keys.erase(key);
-
-        if (cur == 1) {
-            mp.erase(key);
-        } else {
-            auto prev = it;
-            --prev;
-            if (it == buckets.begin() || prev->count < cur - 1) {
-                mp[key] = buckets.insert(it, Bucket{cur - 1, {key}});
-            } else {
-                prev->keys.insert(key);
-                mp[key] = prev;
-            }
-        }
-
-        if (it->keys.empty()) {
-            buckets.erase(it);
-        }
+    /**
+     * @param key: pop an element from the queue
+     * @return: nothing
+     */
+    void dec(string &key) {
+        // write your code here
     }
 
+    /**
+     * @return: nothing
+     */
     string getMaxKey() {
-        if (buckets.empty()) return "";
-        return *buckets.back().keys.begin();
+        if (mp.empty()) {
+            return "";
+        }
+
+        return *buckets.end()->keys.begin();
     }
 
+    /**
+     * @return: nothing
+     */
     string getMinKey() {
-        if (buckets.empty()) return "";
-        return *buckets.front().keys.begin();
-    }
+        if (mp.empty()) {
+            return "";
+        }
 
+        return *buckets.begin()->keys.begin();
+    }
 private:
     struct Bucket {
         int count;
         set<string> keys;
     };
+
     list<Bucket> buckets;
     unordered_map<string, list<Bucket>::iterator> mp;
 };
 
-#include <iostream>
+
 
 int main() {
     // //["hello","hello"]
