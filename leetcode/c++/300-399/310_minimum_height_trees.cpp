53afe5628d53ded24a7e51bce914593b1e098efd
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-01 13:11:06 +0800

    leetcode/c++/300-399/30_minimum_height_trees.cpp

---
+++ leetcode/c++/300-399/310_minimum_height_trees.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<vector<int>> a = [[1,0],[1,2],[1,3]]
+    for (auto elem : s.findMinHeightTrees(3, a)) {
+        cout << elem << ' ';
+    }
+}
