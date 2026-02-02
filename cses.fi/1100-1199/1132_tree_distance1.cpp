c0e0d524c09515c2f3559e275e0db53db5fa36ad
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-02 21:30:59 +0800

    cses.fi/1100-1199/1132_tree_distance1.cpp

--- cses.fi/1100-1199/1132_tree_distance1.cpp
+++ cses.fi/1100-1199/1132_tree_distance1.cpp
@@ -1,31 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int calc(const int node, int parent, vector<int> &distance, map<int, vector<int> > &adj) {
-    int best1 = 0;
-    int best2 = 0;
-
-    for (const auto nei: adj[node]) {
-        if (nei == parent) {
-            continue;
-        }
-
-        int dist = calc(nei, node, distance, adj) + 1;
-
-        if (dist > best1) {
-            best2 = best1;
-            best1 = dist;
-        } else if (dist > best2) {
-            best2 = dist;
+static int bfs_farthest(int start, const vector<vector<int>> &adj, vector<int> *dist_out) {
+    int n = static_cast<int>(adj.size()) - 1;
+    vector<int> dist(n + 1, -1);
+    queue<int> q;
+    dist[start] = 0;
+    q.push(start);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        for (int nei : adj[node]) {
+            if (dist[nei] != -1) {
+                continue;
+            }
+            dist[nei] = dist[node] + 1;
+            q.push(nei);
         }
     }
 
-    distance[node] = max(distance[node], best1 + best2);
-
-    return best1 + best2;
+    int best_node = start;
+    for (int i = 1; i <= n; ++i) {
+        if (dist[i] > dist[best_node]) {
+            best_node = i;
+        }
+    }
+    if (dist_out != nullptr) {
+        *dist_out = std::move(dist);
+    }
+    return best_node;
 }
-
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -35,18 +40,19 @@ int main() {
     int n, a, b;
     cin >> n;
 
-    map<int, vector<int> > adj;
-    vector distance(n + 1, 0);
-
+    vector<vector<int>> adj(n + 1);
     for (int i = 1; i < n; ++i) {
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
 
-    calc(1, 0, distance, adj);
+    vector<int> dist_a, dist_b;
+    int a_end = bfs_farthest(1, adj, nullptr);
+    int b_end = bfs_farthest(a_end, adj, &dist_a);
+    bfs_farthest(b_end, adj, &dist_b);
 
     for (int i = 1; i <= n; ++i) {
-        cout << distance[i] << ' ';
+        cout << max(dist_a[i], dist_b[i]) << (i == n ? '\n' : ' ');
     }
 }
