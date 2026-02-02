567fcacd7359627cb11899700f5ef3d450cf3eb2
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-02 17:13:19 +0800

    cses.fi/1100-1199/1132_tree_distance1.cpp

--- cses.fi/1100-1199/1132_tree_distance1.cpp
+++ cses.fi/1100-1199/1132_tree_distance1.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+void calc() {
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,11 +15,15 @@ int main() {
     cin >> n;
 
     vector<vector<int> > adj;
+    vector<int> distance(n, 0);
 
     for (int i = 1; i < n; ++i) {
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
+    }
+ for (int i = 0; i < n; ++i) {
+        cout << distance[i] << ' ';
     }
 
     cout << "1\n";
