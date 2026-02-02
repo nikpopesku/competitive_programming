6c297124f241d7f71f415f412ff63987446fba9b
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-02 16:58:19 +0800

    cses.fi/1100-1199/1132_tree_distance1.cpp

--- cses.fi/1100-1199/1132_tree_distance1.cpp
+++ cses.fi/1100-1199/1132_tree_distance1.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 
 
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n, u, v;
+    int n, a, b;
     cin >> n;
 
-    cout << maxdiameter << "\n";
+    vector<vector<int> > adj;
+ for (int i = 1; i < n; ++i) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    cout << "1\n";
 }
