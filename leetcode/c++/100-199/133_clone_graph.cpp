70a28380d13525810bb58f2f6c2884cd338b8b05
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-02 11:09:45 +0800

    leetcode/c++/100-199/133_clone_graph.cpp

---
+++ leetcode/c++/100-199/133_clone_graph.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+
+    }
+};
+
+int main() {
+    Solution s; //{1}
+    vector<vector<int> > a = {{1, 0}, {1, 2}, {1, 3}};
+    vector<vector<int> > c = {{0, 1}, {0, 2}, {0, 3}, {3, 4}, {4, 5}};
+    for (const auto elem: s.findMinHeightTrees(6, c)) {
+        cout << elem << ' ';
+    }
+}
