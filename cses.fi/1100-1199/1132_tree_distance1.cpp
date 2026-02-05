1b766cdd56242ddd02bf7c271e5bc1aa59e14ae8
npopesku <nikolay.popesku@unbiased.co.uk>
2026-02-05 16:33:28 +0800

    cses.fi/1100-1199/1132_tree_distance1.cpp

--- cses.fi/1100-1199/1132_tree_distance1.cpp
+++ cses.fi/1100-1199/1132_tree_distance1.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+int furthest_node(int start, vector<vector<int> > &adj, vector<int> *dist) {
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -15,5 +18,15 @@ int main() {
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
+    }
+
+    int a_end, b_end;
+    vector<int> a_dist, b_dist;
+    a_end = furthest_node(1, adj, nullptr);
+    b_end = furthest_node(a_end, adj, &a_dist);
+    furthest_node(b_end, adj, &b_dist);
+
+    for (int i = 1; i <= n; ++i) {
+        cout << max(a_dist[i], b_dist[i]) << " ";
     }
 }
