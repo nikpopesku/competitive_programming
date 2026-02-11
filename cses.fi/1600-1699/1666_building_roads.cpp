16da17d3ada9958831e89d5d344ec80ee94fd4a5
npopesku <nic1k@yandex.com>
2026-02-11 15:32:12 +0800

    cses.fi/1600-1699/1666_building_roads.cpp

--- cses.fi/1600-1699/1666_building_roads.cpp
+++ cses.fi/1600-1699/1666_building_roads.cpp
@@ -1,7 +1,40 @@
 #include<iostream>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
+class DisjointSetUnion {
+public:
+    explicit DisjointSetUnion(const int size): size(size + 1), parent(size + 1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int index) const {
+        while (index != parent[index]) {
+            index = parent[index];
+        }
+
+        return index;
+    }
+
+    bool unite(const int node_a, const int node_b) {
+        int parent_a = find(node_a);
+        int parent_b = find(node_b);
+
+        if (parent_a == parent_b) return false;
+
+        if (size[parent_a] < size[parent_b]) swap(parent_a, parent_b);
+
+        parent[parent_b] = parent_a;
+        size[parent_a] += size[parent_b];
+
+        return true;
+    }
+private:
+    vector<int> size;
+    vector<int> parent;
+};
 
 int main()
 {
@@ -9,6 +42,15 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n, m;
+    int n, m, u, v;
     cin >> n >> m;
+    vector adj(n, vector<int>());
+    DisjointSetUnion dsu(n);
+
+    for (int i = 0; i < m; ++i) {
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        dsu.unite(u, v);
+    }
 }
