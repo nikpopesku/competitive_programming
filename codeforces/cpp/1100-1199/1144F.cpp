7b25f30817164121cc4a081471529cd62e0502e0
npopesku <nic1k@yandex.com>
2026-02-14 13:56:30 +0800

    codeforces/cpp/1100-1199/1144F.cpp

--- codeforces/cpp/1100-1199/1144F.cpp
+++ codeforces/cpp/1100-1199/1144F.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -8,12 +9,38 @@ int main() {
     cin >> n >> m;
 
     vector adj(n + 1, vector<int>());
+    vector color(n + 1, -1);
 
     for (int i = 0; i < m; ++i) {
         cin >> u >> v;
 
         adj[u].push_back(v);
         adj[v].push_back(u);
+    }
+
+    color[1] = 0;
+    stack<int> st;
+    st.push(1);
+
+    while (!st.empty()) {
+        const auto elem = st.top();
+        const int nei_color = color[elem] == 1 ? 0 : 1;
+        st.pop();
+
+        for (auto nei: adj[elem]) {
+
+            if (color[nei] != -1 && color[nei] != nei_color) {
+                cout << "NO\n";
+                return 0;
+            }
+
+            if (color[nei] != -1 && color[nei] == nei_color) {
+                continue;
+            }
+
+            st.push(nei);
+            color[nei] = nei_color;
+        }
     }
 
 
