41472c1ff2fad42fd5f7a514754d4a99b1e8c87c
npopesku <nic1k@yandex.com>
2026-02-14 14:16:28 +0800

    atcoder/200-299/d/abc286_d_money_in_hand.cpp

--- atcoder/200-299/d/abc286_d_money_in_hand.cpp
+++ atcoder/200-299/d/abc286_d_money_in_hand.cpp
@@ -1,17 +1,14 @@
 #include<iostream>
 
 using namespace std;
-int N, M;
-int A[2000];
-long dp[2020];
 
 int main() {
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) cin >> A[i];
-    for (int j = 0; j <= M; j++) dp[j] = -1e18;
-    dp[0] = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = M; j--;)dp[j + 1] = max(dp[j + 1], dp[j] + (j + 1) * (long) A[i]);
+    int N, X;
+    int A[50];
+    int B[50];
+
+    cin >> N >> X;
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i] >> B[i];
     }
-    cout << dp[M] << endl;
 }
