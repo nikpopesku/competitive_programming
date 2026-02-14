c6e0df5e674cd44a3d49e4e7dc8dfce7a2a0e2f3
npopesku <nic1k@yandex.com>
2026-02-14 13:28:19 +0800

    codeforces/cpp/1100-1199/1144F.cpp

---
+++ codeforces/cpp/1100-1199/1144F.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, m, u, v;
+    cin >> n >> m;
+
+    vector adj(n+1, vector<int>());
+
+    for (int i = 0; i < m; ++i) {
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+
+    return 0;
+}
