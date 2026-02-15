4c87bfec022628a57b4b24a3375a894b29013b0c
npopesku <nic1k@yandex.com>
2026-02-15 13:47:36 +0800

    codeforces/cpp/1100-1199/1139C_edgy_trees.cpp

--- codeforces/cpp/1100-1199/1139C_edgy_trees.cpp
+++ codeforces/cpp/1100-1199/1139C_edgy_trees.cpp
@@ -48,13 +48,12 @@ private:
 int main() {
     int n, k, u, v, x;
     cin >> n >> k;
-    vector adj(n + 1, vector<int>());
+    DisjointSetUnion dsu(n);
 
     for (int i = 1; i <= n - 1; ++i) {
         cin >> u >> v >> x;
         if (x == 0) {
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            dsu.unionit(u, v);
         }
     }
 
