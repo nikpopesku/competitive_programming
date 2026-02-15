48afa57dc1572e25e7a95e8d5ad67dca0a1a2acd
npopesku <nic1k@yandex.com>
2026-02-15 13:43:23 +0800

    codeforces/cpp/1100-1199/1139C_edgy_trees.cpp

--- codeforces/cpp/1100-1199/1139C_edgy_trees.cpp
+++ codeforces/cpp/1100-1199/1139C_edgy_trees.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class DisjointSetUnion {
 public:
-    DisjointSetUnion(const int size): size(size + 1), parent(size + 1) {
+    explicit DisjointSetUnion(const int size): size(size + 1, 1), parent(size + 1) {
         iota(parent.begin(), parent.end(), 0);
     }
 
@@ -18,6 +18,24 @@ public:
         parent[node] = find(parent[node]);
 
         return parent[node];
+    }
+
+    bool unionit(int a, int b) {
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if (parent_a == parent_b) {
+            return false;
+        }
+
+        if (size[parent_a] < size[parent_b]) {
+            swap(parent_a, parent_b);
+        }
+
+        parent[parent_b] = parent_a;
+        size[parent_a] += size[parent_b];
+
+        return true;
     }
 private:
     vector<int> size;
