695c6622dccab6b2190d47f81bc636f5d8db3897
npopesku <nic1k@yandex.com>
2026-02-15 12:45:41 +0800

    cses.fi/1600-1699/1676_road_construction.cpp

--- cses.fi/1600-1699/1676_road_construction.cpp
+++ cses.fi/1600-1699/1676_road_construction.cpp
@@ -9,6 +9,8 @@ class DisjointSetUnion {
 public:
     explicit DisjointSetUnion(int size): size(size+1), parent(size+1) {
         iota(parent.begin(), parent.end(), 0);
+        maxsize = 1;
+        number_components = size;
     }
 
     int find(const int node) {
@@ -36,11 +38,25 @@ public:
         parent[parent_b] = parent_a;
         size[parent_a] += size[parent_b];
 
+        maxsize = max(maxsize, size[parent_a]);
+        --number_components;
+
         return true;
     }
+
+    int get_maxsize() const {
+        return maxsize;
+    }
+
+    int get_number_components() const {
+        return number_components;
+    }
+
 private:
     vector<int> size;
     vector<int> parent;
+    int maxsize;
+    int number_components;
 };
 
 
@@ -53,12 +69,19 @@ int main() {
     cin >> n >> m;
     vector adj(n+1, vector<int>());
     DisjointSetUnion dsu(n);
+    vector<pair<int, int>> response(m);
 
     for (int i = 0; i < m; ++i) {
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
         dsu.unionit(a, b);
+
+        response[i] = {dsu.get_number_components(), dsu.get_maxsize()};
+    }
+
+    for (auto [fst, snd]: response) {
+        cout << fst << ' ' << snd << '\n';
     }
 
 }
