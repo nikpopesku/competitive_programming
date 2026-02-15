c7f2333b04a6ade46ca1f70ee3cab6447131de7d
npopesku <nic1k@yandex.com>
2026-02-15 12:32:00 +0800

    cses.fi/1600-1699/1676_road_construction.cpp

--- cses.fi/1600-1699/1676_road_construction.cpp
+++ cses.fi/1600-1699/1676_road_construction.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
+#include <numeric>
+#include <vector>
 
 
 using namespace std;
+
+class DisjointSetUnion {
+public:
+    DisjointSetUnion(int size): size(size+1), parent(size+1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(const int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        parent[node] = find(parent[node]);
+
+        return parent[node];
+    }
+private:
+    vector<int> size;
+    vector<int> parent;
+};
 
 
 int main() {
