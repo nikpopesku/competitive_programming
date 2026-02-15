f8f08faa0520b044baea6d4794ba004d2bb47d64
npopesku <nic1k@yandex.com>
2026-02-15 21:09:30 +0800

    leetcode/c++/100-199/105_construct_from_preorder_inorder.cpp

---
+++ leetcode/c++/100-199/105_construct_from_preorder_inorder.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {;
+
+
+int main() {
+    auto s = Solution();
+    cout << s.numDistinct("rabbbit", "rabbit") << endl;
+}
