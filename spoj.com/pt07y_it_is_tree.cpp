883a57e51cbec3f8c7aa4f8090912c752664dd6c
npopesku <nic1k@yandex.com>
2026-02-15 19:53:22 +0800

    spoj.com/pt07y_it_is_tree.cpp

--- spoj.com/pt07y_it_is_tree.cpp
+++ spoj.com/pt07y_it_is_tree.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    int N, M;
+    int N, M, u, v;
     cin >> N >> M;
+    vector adj(N+1, vector<int>());
+
+    for (int i = 0; i < M; ++i) {
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
     if (M != N - 1) {
         cout << "NO\n";
 
         return 0;
     }
+
+    stack<int> st;
+    st.push(1);
+    vector visited(N+1, false);
+    visited[1] = true;
+
+    while (!st.empty()) {
+        const int elem = st.top();
+        st.pop();
+
+        for (auto nei: adj[elem]) {
+            if (!visited[nei]) {
+                st.push(nei);
+                visited[nei] = true;
+            }
+        }
+    }
+
+    for (const auto elem: visited) {
+        if (!elem) {
+            cout << "NO\n";
+
+            return 0;
+        }
+    }
+
+    cout << "YES\n";
 }
