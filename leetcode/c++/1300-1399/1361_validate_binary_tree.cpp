ff1a9a9616411350200ea2446eaf75a59a8f0889
npopesku <nic1k@yandex.com>
2026-02-16 11:41:13 +0800

    leetcode/c++/100-199/105_construct_from_preorder_inorder.cpp

---
+++ leetcode/c++/1300-1399/1361_validate_binary_tree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> indegree(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (leftChild[i] != -1) {
+                ++indegree[leftChild[i]];
+            }
+
+            if (rightChild[i] != -1) {
+                ++indegree[rightChild[i]];
+            }
+        }
+
+        int count_zero = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (indegree[i] > 1) {
+                return false;
+            }
+
+            if (indegree[i] == 0) {
+                ++count_zero;
+            }
+        }
+
+        return count_zero == 1;
+    }
+};
+
+int main() {
+    Solution s;
+
+    return 0;
+}
