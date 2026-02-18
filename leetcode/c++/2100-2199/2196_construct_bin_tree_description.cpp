386860f276f644670741d4de70bfcfa4f88d5f6b
npopesku <nic1k@yandex.com>
2026-02-18 12:33:55 +0800

    leetcode/c++/2100-2199/2196_construct_bin_tree_description.cpp

--- leetcode/c++/2100-2199/2196_construct_bin_tree_description.cpp
+++ leetcode/c++/2100-2199/2196_construct_bin_tree_description.cpp
@@ -28,8 +28,8 @@ public:
 int main() {
     Solution s;
 
-    vector<vector<int>> descriptions = {{20,15,1},{20,17,0},{50,20,1},{50,80,0},{80,19,1}}
-    cout << boolalpha << s.createBinaryTree(descriptions);
+    vector<vector<int>> descriptions = {{20,15,1},{20,17,0},{50,20,1},{50,80,0},{80,19,1}};
+    s.createBinaryTree(descriptions);
 
     return 0;
 }
