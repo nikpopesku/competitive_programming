d5961d343b32f31f73888f6e291442bd857bbf18
npopesku <nic1k@yandex.com>
2026-02-18 15:08:28 +0800

    leetcode/c++/1700-1799/1743_restore_array.cpp

--- leetcode/c++/1700-1799/1743_restore_array.cpp
+++ leetcode/c++/1700-1799/1743_restore_array.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
@@ -8,22 +9,33 @@ using namespace std;
 class Solution {
 public:
     vector<int> restoreArray(const vector<vector<int> > &adjacentPairs) {
-        unordered_map<int, vector<int> > degree;
+        unordered_map<int, vector<int> > adj;
 
         for (const auto &p: adjacentPairs) {
-            degree[p[0]].push_back(p[1]);
-            degree[p[1]].push_back(p[0]);
+            adj[p[0]].push_back(p[1]);
+            adj[p[1]].push_back(p[0]);
         }
 
         const int n = static_cast<int>(adjacentPairs.size()) + 1;
 
         vector<int> response(n);
-        for (auto &[fst, snd]: degree) {
+        for (auto &[fst, snd]: adj) {
             if (snd.size() == 1) {
                 response[0] = fst;
                 break;
             }
         }
+
+        int current = adj[response[0]][0];
+        int counter = 0;
+        while (++counter < n) {
+            response[counter] = current;
+
+            current = adj[current][0] != response[counter - 1] ? adj[current][0] : adj[current][1];
+        }
+
+
+        return response;
     }
 };
 
@@ -31,7 +43,9 @@ int main() {
     Solution s;
 
     vector<vector<int> > descriptions = {{2, 1}, {3, 4}, {3, 2}};
-    s.restoreArray(descriptions);
+    for (auto & elem: s.restoreArray(descriptions)) {
+        cout << elem << ' ';
+    }
 
     return 0;
 }
