3f59972ee983d6b37dd891ee26acededcca27b0b
npopesku <nic1k@yandex.com>
2026-02-18 13:06:37 +0800

    leetcode/c++/1700-1799/1743_restore_array.cpp

---
+++ leetcode/c++/1700-1799/1743_restore_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ranges>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<vector<int> > descriptions = {{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}};
+    s.createBinaryTree(descriptions);
+
+    return 0;
+}
