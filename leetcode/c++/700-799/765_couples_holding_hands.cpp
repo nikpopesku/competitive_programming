0000e3a68f659dc23d9db813237afcccccc83d91
npopesku <nic1k@yandex.com>
2026-02-20 11:46:30 +0800

    leetcode/c++/700-799/765_couples_holding_hands.cpp

---
+++ leetcode/c++/700-799/765_couples_holding_hands.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSwapsCouples(vector<int> &row) {
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    vector<int> row = {0, 2, 1, 3};
+    cout << s.minSwapsCouples(row) << endl;
+}
+
+}
