2bc69596ce3d1e8e6ae5a7b419c031ff7202a17a
npopesku <nic1k@yandex.com>
2026-02-22 11:34:31 +0800

    leetcode/c++/700-799/765_couples_holding_hands.cpp

--- leetcode/c++/700-799/765_couples_holding_hands.cpp
+++ leetcode/c++/700-799/765_couples_holding_hands.cpp
@@ -1,3 +1,4 @@
+#include <complex>
 #include <iostream>
 #include <vector>
 #include <unordered_map>
@@ -14,17 +15,24 @@ public:
             mp[row[i]]  = i;
         }
 
-        for (int i = 0; i < n; i += 2) {
+        int counter = 0;
 
+        for (int i = 0; i < n; i += 2) {
+            if (abs(mp[i] - mp[i+1]) > 1) {
+                ++counter;
+            }
         }
+
+        return counter;
     }
 };
 
 int main() {
     auto s = Solution();
 
-    vector<int> row = {0, 2, 1, 3};
+    vector<int> row = {0, 2, 1, 3}; //1
     cout << s.minSwapsCouples(row) << endl;
-}
 
+    vector<int> row2 = {3,2,0,1}; //0
+    cout << s.minSwapsCouples(row2) << endl;
 }
