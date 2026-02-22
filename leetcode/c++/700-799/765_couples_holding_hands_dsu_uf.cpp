0352e2786098ff2fb95cfa1a81ec9adc270a7e84
npopesku <nic1k@yandex.com>
2026-02-22 12:23:04 +0800

    leetcode/c++/700-799/765_couples_holding_hands_dsu_uf.cpp

--- leetcode/c++/700-799/765_couples_holding_hands_dsu_uf.cpp
+++ leetcode/c++/700-799/765_couples_holding_hands_dsu_uf.cpp
@@ -6,10 +6,37 @@ using namespace std;
 
 class DisjointUnionSet {
 public:
-    DisjointUnionSet(const int size) : size(size + 1), parent(size + 1) {
+    explicit DisjointUnionSet(const int size) : size(size + 1), parent(size + 1) {
         iota(parent.begin(), parent.end(), 0);
     }
 
+    int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        parent[node] = find(parent[node]);
+
+        return parent[node];
+    }
+
+    bool unionit(int a, int b) {
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if (parent_a == parent_b) {
+            return false;
+        }
+
+        if (size[parent_a] < size[parent_b]) {
+            swap(parent_a, parent_b);
+        }
+
+        parent[parent_b] = parent_a;
+        size[parent_a] += size[parent_b];
+
+        return true;
+    }
 private:
     vector<int> size;
     vector<int> parent;
