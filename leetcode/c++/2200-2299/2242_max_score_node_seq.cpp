fd61a45a76d6c14458c136a77735e181ae15d381
npopesku <nic1k@yandex.com>
2026-02-23 11:58:23 +0800

    leetcode/c++/2200-2299/2242_max_score_node_seq.cpp

--- leetcode/c++/2200-2299/2242_max_score_node_seq.cpp
+++ leetcode/c++/2200-2299/2242_max_score_node_seq.cpp
@@ -1,44 +1,39 @@
 #include <iostream>
-#include <list>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    int maximumScore(const vector<int> &scores, const vector<vector<int> > &edges) {
+    int maximumScore(const vector<int> &scores, const vector<vector<int>> &edges) {
         const int n = static_cast<int>(scores.size());
-        vector adj(n, list<int>());
+        vector<vector<int>> top(n); // top-3 neighbors by score
 
         for (auto &e: edges) {
-            if (adj[e[0]].empty()) {
-                adj[e[0]].push_back(e[1]);
-                continue;
+            for (auto [u, v]: {pair{e[0], e[1]}, pair{e[1], e[0]}}) {
+                top[u].push_back(v);
+                sort(top[u].begin(), top[u].end(),
+                     [&](int a, int b){ return scores[a] > scores[b]; });
+                if (top[u].size() > 3) top[u].pop_back();
             }
-            for (auto it = adj[e[0]].begin(); it != adj[e[0]].end(); ++it) {
-                if (scores[*it] <= scores[e[1]]) {
-                    adj[e[0]].insert(it, e[1]);
+        }
+
+        int ans = -1;
+
+        for (auto &e: edges) {
+            const int u = e[0], v = e[1];
+            const int cur = scores[u] + scores[v];
+            for (const int a: top[u]) {
+                if (a == v) continue;
+                for (const int d: top[v]) {
+                    if (d == u || d == a) continue;
+                    ans = max(ans, cur + scores[a] + scores[d]);
                 }
             }
         }
 
-        int best_response = -1;
-
-        for (auto &e: edges) {
-            const int cur = scores[e[0]] + scores[e[1]];
-
-            for (const auto &a: adj[e[0]]) {
-                if (a != e[1]) {
-                    for (const auto &d: adj[e[1]]) {
-                        if (d != e[0] && d != a) {
-                            best_response = max(best_response, cur + scores[a] + scores[d]);
-                        }
-                    }
-                }
-            }
-        }
-
-        return best_response;
+        return ans;
     }
 };
 
@@ -46,10 +41,14 @@ int main() {
     auto s = Solution();
 
     const vector scores = {5, 2, 9, 8, 4};
-    const vector<vector<int> > edges = {{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}};
+    const vector<vector<int>> edges = {{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}};
     cout << s.maximumScore(scores, edges) << endl; //24
 
     const vector scores2 = {9, 20, 6, 4, 11, 12};
-    const vector<vector<int> > edges2 = {{0, 3}, {5, 3}, {2, 4}, {1, 3}};
+    const vector<vector<int>> edges2 = {{0, 3}, {5, 3}, {2, 4}, {1, 3}};
     cout << s.maximumScore(scores2, edges2) << endl; //-1
+
+    const vector scores3 = {1, 1, 1, 10, 10};
+    const vector<vector<int>> edges3 = {{0, 3}, {1, 3}, {2, 4}, {3, 4}, {0, 1}};
+    cout << s.maximumScore(scores3, edges3) << endl; //22
 }
