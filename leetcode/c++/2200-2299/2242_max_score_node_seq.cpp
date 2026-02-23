f1d8ccbfcb1ce72e4692a380de74fd08203fe293
npopesku <nic1k@yandex.com>
2026-02-23 11:33:08 +0800

    leetcode/c++/2200-2299/2242_max_score_node_seq.cpp

--- leetcode/c++/2200-2299/2242_max_score_node_seq.cpp
+++ leetcode/c++/2200-2299/2242_max_score_node_seq.cpp
@@ -6,15 +6,14 @@ using namespace std;
 
 class Solution {
 public:
-    int maximumScore(vector<int>& scores, vector<vector<int>>& edges) {
-
+    int maximumScore(vector<int> &scores, vector<vector<int> > &edges) {
     }
 };
 
 int main() {
     auto s = Solution();
 
-    vector<int> scores = {5,2,9,8,4};
-    vector<vector<int> > edges = {{0,1},{1,2},{2,3},{0,2},{1,3},{2,4}};
+    vector<int> scores = {5, 2, 9, 8, 4};
+    vector<vector<int> > edges = {{0, 1}, {1, 2}, {2, 3}, {0, 2}, {1, 3}, {2, 4}};
     cout << s.maximumScore(scores, edges) << endl; //0
 }
