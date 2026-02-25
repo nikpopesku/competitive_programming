c9fe6f4d98176f797fd51003a8f59bd0a61b5c70
npopesku <nic1k@yandex.com>
2026-02-25 14:32:41 +0800

    leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp

--- leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp
+++ leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <vector>
 
 using namespace std;
+
+class DisjointUnionSet {
+public:
+    explicit DisjointUnionSet(int size): size(size + 1, 1), parent(size + 1) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+
+    int find(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+
+        parent[node] = find(parent[node]);
+
+        return parent[node];
+    }
+
+    bool unionit(int a, int b) {
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if (parent_a == parent_b) {
+            return false;
+        }
+
+        if (size[parent_a] < size[parent_b]) {
+            swap(parent_a, parent_b);
+        }
+
+        parent[parent_b] = parent_a;
+        size[parent_a] += size[parent_b];
+
+        return true;
+    }
+private:
+    vector<int> parent;
+    vector<int> size;
+};
 
 class Solution {
 public:
