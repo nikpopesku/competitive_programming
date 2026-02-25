34a66e6371d5c51d428d631f8753cc259ebe290a
npopesku <nic1k@yandex.com>
2026-02-25 14:10:28 +0800

    leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp

--- leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp
+++ leetcode/c++/1600-1699/1627_graph_connec_threshold.cpp
@@ -14,6 +14,20 @@ int main() {
 
     vector<vector<int>> queries = {{1, 4}, {2, 5}, {3, 6}};
     for (auto e: s.areConnected(6, 2, queries)) {
-        cout << boolalpha << e << ' ';
+        cout << boolalpha << e << ' '; //{false,false,true}
+    }
+
+    cout << '\n';
+
+    vector<vector<int>> queries2 = {{4,5},{3,4},{3,2},{2,6},{1,3}};
+    for (auto e: s.areConnected(6, 0, queries2)) {
+        cout << boolalpha << e << ' '; //{true,true,true,true,true}
+    }
+
+    cout << '\n';
+
+    vector<vector<int>> queries3 = {{4,5},{4,5},{3,2},{2,3},{3,4}};
+    for (auto e: s.areConnected(5, 1, queries3)) {
+        cout << boolalpha << e << ' '; //{true,true,true,true,true}
     }
 }
