cea24dfc45da1d8ae5f9d81818b909cdd4ef1b42
npopesku <nic1k@yandex.com>
2026-02-26 16:30:42 +0800

    codeforces/cpp/1000-1099/1087D.cpp

---
+++ codeforces/cpp/1000-1099/1087D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
