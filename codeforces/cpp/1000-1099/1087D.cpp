2e4c7567dd7e455c7224d3410325c5029253d824
npopesku <nic1k@yandex.com>
2026-02-26 16:32:56 +0800

    codeforces/cpp/1000-1099/1087D.cpp

--- codeforces/cpp/1000-1099/1087D.cpp
+++ codeforces/cpp/1000-1099/1087D.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void solve() {
-    int n;
-    cin >> n;
+    int n, s, a, b;
+    cin >> n >> s;
+    vector adj(n + 1, vector<int>());
+
+    for (int i = 1; i <= n - 1; ++i) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
 }
 
 int main() {
