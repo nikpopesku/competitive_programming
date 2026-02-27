cf28577a56ad1d31f45d1a38a49005290a53bd23
npopesku <nic1k@yandex.com>
2026-02-27 16:30:02 +0800

    leetcode/c++/1400-1499/1489_crit_pseudo_crit_edge_mst.cpp

--- leetcode/c++/1400-1499/1489_crit_pseudo_crit_edge_mst.cpp
+++ leetcode/c++/1400-1499/1489_crit_pseudo_crit_edge_mst.cpp
@@ -61,7 +61,7 @@ public:
                 mst.push_back(e);
             }
 
-            if (mst.size() == n -1) {
+            if (mst.size() == n - 1) {
                 break;
             }
         }
@@ -71,7 +71,7 @@ public:
 int main() {
     auto s = Solution();
 
-    vector<vector<string> > edges = {{0,1,1},{1,2,1},{2,3,2},{0, 3, 2}, {0, 4, 3}, {3, 4, 3}, {1, 4, 6}};
+    vector<vector<string> > edges = {{0, 1, 1}, {1, 2, 1}, {2, 3, 2}, {0, 3, 2}, {0, 4, 3}, {3, 4, 3}, {1, 4, 6}};
 
     for (auto &e: s.findCriticalAndPseudoCriticalEdges(5, edges)) {
         cout << e << ' '; //[[0,1],[2,3,4,5]]
