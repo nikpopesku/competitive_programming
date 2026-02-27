92f63281314836d0605610abdd513b2bc438832f
npopesku <nic1k@yandex.com>
2026-02-27 10:54:45 +0800

    leetcode/c++/300-399/332_reconstruct_itinerary.cpp

--- leetcode/c++/300-399/332_reconstruct_itinerary.cpp
+++ leetcode/c++/300-399/332_reconstruct_itinerary.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,21 +9,38 @@ class Solution {
 public:
     vector<string> findItinerary(const vector<vector<string> > &tickets) {
         unordered_map<string, vector<string> > adj;
+        unordered_map<string, bool> visited;
 
         for (auto &e: tickets) {
             adj[e[0]].push_back(e[1]);
             adj[e[1]].push_back(e[0]);
+            visited[e[0]] = false;
+            visited[e[1]] = false;
         }
 
         vector<string> stack;
 
         stack.push_back("JFK");
+        visited["JFJ"] = true;
         vector<string> response;
 
         while (!stack.empty()) {
             string elem = stack[0];
             response.push_back(elem);
+
+            vector<string> neighbours;
+            for (auto & nei: adj[elem]) {
+                if (!visited[nei]) {
+                    neighbours.push_back(nei);
+                }
+
+                ranges::sort(neighbours);
+            }
             stack.clear();
+            if (!neighbours.empty()) {
+                stack.push_back(neighbours[0]);
+                visited[neighbours[0]] = true;
+            }
         }
 
         return response;
