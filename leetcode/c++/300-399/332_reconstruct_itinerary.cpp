6be2993ee0e1018cab7ff712b531c32ca60ea916
npopesku <nic1k@yandex.com>
2026-02-27 11:28:06 +0800

    leetcode/c++/300-399/332_reconstruct_itinerary.cpp

--- leetcode/c++/300-399/332_reconstruct_itinerary.cpp
+++ leetcode/c++/300-399/332_reconstruct_itinerary.cpp
@@ -9,17 +9,33 @@ using namespace std;
 class Solution {
 public:
     vector<string> findItinerary(const vector<vector<string> > &tickets) {
-        unordered_map<string, vector<string>> adj;
+        unordered_map<string, vector<string> > adj;
 
-        for (auto&e: tickets) {
+        for (auto &e: tickets) {
             adj[e[0]].push_back(e[1]);
         }
 
         for (auto &dest: adj | views::values) {
             sort(dest.rbegin(), dest.rend());
         }
+
+        vector<string> result;
+
+        dfs(adj, result, "JFK");
+        ranges::reverse(result);
+
+        return result;
     }
 
+private:
+    void dfs(unordered_map<string, vector<string> > &adj, vector<string> &result, const string &u) {
+        while (adj[u].empty()) {
+            auto e = adj[u].back();
+            adj[u].pop_back();
+            dfs(adj, result, e);
+        }
+ result.push_back(u);
+    }
 };
 
 int main() {
