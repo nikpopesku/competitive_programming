12c195b6c5782edae9bb407225ec3cc82adc0ad7
npopesku <nic1k@yandex.com>
2026-02-27 11:30:54 +0800

    leetcode/c++/300-399/332_reconstruct_itinerary.cpp

---
+++ leetcode/c++/1400-1499/1489_crit_pseudo_crit_edge_mst.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <ranges>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> findCriticalAndPseudoCriticalEdges(int n, vector<vector<int>>& edges) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    vector<vector<string> > edges = {{0,1,1},{1,2,1},{2,3,2},{0,3,2},{0,4,3},{3,4,3},{1,4,6}};
+
+    for (auto &e: s.findCriticalAndPseudoCriticalEdges(5, edges)) {
+        cout << e << ' '; [[0,1],[2,3,4,5]]
+    }
+
+    // cout << '\n';
+}
