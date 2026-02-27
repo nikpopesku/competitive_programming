349c6c55a3d9c3a6d2bf4424d8d087ab82e976a6
npopesku <nic1k@yandex.com>
2026-02-27 11:07:51 +0800

    leetcode/c++/300-399/332_reconstruct_itinerary.cpp

--- leetcode/c++/300-399/332_reconstruct_itinerary.cpp
+++ leetcode/c++/300-399/332_reconstruct_itinerary.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ class Solution {
 public:
     vector<string> findItinerary(const vector<vector<string> > &tickets) {
         unordered_map<string, vector<string> > adj;
-        unordered_map<pair<string, string>, bool> visited;
+        set<pair<string, string>> visited;
 
         for (auto &e: tickets) {
             adj[e[0]].push_back(e[1]);
@@ -35,7 +36,7 @@ public:
             stack.clear();
             if (!neighbours.empty()) {
                 stack.push_back(neighbours[0]);
-                visited[pair{elem, neighbours[0]}] = true;
+                visited.insert(pair{elem, neighbours[0]});
             }
         }
 
@@ -46,13 +47,13 @@ public:
 int main() {
     auto s = Solution();
 
-    // vector<vector<string> > tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
-    //
-    // for (auto &e: s.findItinerary(tickets)) {
-    //     cout << e << ' ';
-    // }
-    //
-    // cout << '\n';
+    vector<vector<string> > tickets = {{"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"}};
+
+    for (auto &e: s.findItinerary(tickets)) {
+        cout << e << ' '; //["JFK","MUC","LHR","SFO","SJC"]
+    }
+
+    cout << '\n';
 
     vector<vector<string> > tickets2 = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
 
