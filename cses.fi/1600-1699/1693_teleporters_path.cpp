a5256dbd05e4bb1f9903afe3aaa2d21d5651aeb8
npopesku <nic1k@yandex.com>
2026-03-02 12:20:43 +0800

    cses.fi/1600-1699/1693_teleporters_path.cpp

--- cses.fi/1600-1699/1693_teleporters_path.cpp
+++ cses.fi/1600-1699/1693_teleporters_path.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+void dfs(vector<int>& response, int node, vector<vector<int>>& adj) {
+    while (!adj[node].empty()) {
+        auto e = adj[node].back();
+        adj[node].pop_back();
+
+        dfs(response, e, adj);
+    }
+    response.push_back(node);
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -9,9 +19,9 @@ int main() {
 
     int n, m, a, b;
     cin >> n >> m;
-    vector adj(n+1, vector<int>());
-    vector<int> indegree (n+1, 0);
-    vector<int> outdegree (n+1, 0);
+    vector adj(n + 1, vector<int>());
+    vector<int> indegree(n + 1, 0);
+    vector<int> outdegree(n + 1, 0);
 
     for (int i = 0; i < m; ++i) {
         cin >> a >> b;
@@ -21,17 +31,46 @@ int main() {
     }
 
     vector<int> response;
-    ranges::reverse(response);
+
+
+    for (int i = 1; i <= n; ++i) {
+        const int diff = outdegree[i] - indegree[i];
+        if (i == 1 && diff != 1) {
+            cout << "IMPOSSIBLE\n";
+
+            return 0;
+        }
+
+        if (i == n && diff != -1) {
+            cout << "IMPOSSIBLE\n";
+
+            return 0;
+        }
+
+        if (i != 1 && i != n && diff != 0) {
+            cout << "IMPOSSIBLE\n";
+
+            return 0;
+        }
+    }
 
     if (response[0] != 1 || response.back() != n) {
         cout << "IMPOSSIBLE\n";
+
+        return 0;
     }
 
     if (static_cast<int>(response.size()) != m + 1) {
         cout << "IMPOSSIBLE\n";
+
+        return 0;
     }
 
-    for (auto &i: response) {
-       cout << i << ' ';
+
+    dfs(response, 1, adj);
+    ranges::reverse(response);
+
+    for (const auto &i: response) {
+        cout << i << ' ';
     }
 }
