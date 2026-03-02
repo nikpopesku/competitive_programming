56f56a52a747925fc51384a9e4b7b96dad19d1ba
npopesku <nic1k@yandex.com>
2026-03-02 11:38:23 +0800

    cses.fi/1600-1699/1693_teleporters_path.cpp

---
+++ cses.fi/1600-1699/1693_teleporters_path.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m, a, b;
+    cin >> n >> m;
+    vector adj(n+1, vector<int>());
+
+    for (int i = 0; i < m; ++i) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+    }
+}
