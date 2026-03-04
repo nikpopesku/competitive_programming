c1b7af5ac17e28122b37277901b3c0943d92b88c
npopesku <nic1k@yandex.com>
2026-03-04 13:51:35 +0800

    leetcode/c++/800-899/847_shortest_path_visit_nodes.cpp

---
+++ leetcode/c++/800-899/847_shortest_path_visit_nodes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int> > &graph) {
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    vector<vector<int> > graph = {{1, 2, 3}, {0}, {0}, {0}};
+    cout << s.shortestPathLength(graph) << endl; //0
+}
