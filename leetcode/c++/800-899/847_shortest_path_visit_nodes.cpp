b260ce12a87036dad5e2920c643a712c7cad0219
npopesku <nic1k@yandex.com>
2026-03-04 14:02:13 +0800

    leetcode/c++/800-899/847_shortest_path_visit_nodes.cpp

--- leetcode/c++/800-899/847_shortest_path_visit_nodes.cpp
+++ leetcode/c++/800-899/847_shortest_path_visit_nodes.cpp
@@ -8,11 +8,11 @@ using namespace std;
 class Solution {
 public:
     int shortestPathLength(vector<vector<int> > &graph) {
-        queue<pair<int, int>> q;
+        queue<pair<int, int> > q;
         int n = static_cast<int>(graph.size());
 
         for (int i = 0; i < n; ++i) {
-            q.push({i, 1<<i});
+            q.push({i, 1 << i});
         }
     }
 };
