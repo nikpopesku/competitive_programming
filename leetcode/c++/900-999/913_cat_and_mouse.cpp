ff0edd72bdc0aa8b0060a6c7adcf1cc4059dcd44
npopesku <nic1k@yandex.com>
2026-03-05 11:29:16 +0800

    leetcode/c++/900-999/913_cat_and_mouse.cpp

--- leetcode/c++/900-999/913_cat_and_mouse.cpp
+++ leetcode/c++/900-999/913_cat_and_mouse.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <numeric>
 #include <queue>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
@@ -14,15 +15,20 @@ public:
         queue<tuple<int, int, int>> q;
 
         q.push({1, 2, MOUSE_MOVE});
+        int count_moves = 0;
+        unordered_map<tuple<int, int, int>, bool> visited;
 
         while (!q.empty()) {
             auto [mouse_position, cat_position, last_move] = q.front();
             q.pop();
             int next_move = last_move == MOUSE_MOVE ? CAT_MOVE : MOUSE_MOVE;
 
-            for (auto nei: graph[next_move]) {
-                if (next_move == MOUSE_MOVE and nei == 0) continue;
+            for (const auto new_move: graph[next_move]) {
+                if (next_move == CAT_MOVE and new_move == 0) continue;
+                auto state = next_move == MOUSE_MOVE ? tuple{new_move, cat_position, next_move} : tuple{mouse_position, new_move, next_move};
             }
+
+            ++count_moves;
         }
     }
 };
