dd11f7a44e1b8d6c4e26724576bb26776212accc
npopesku <nic1k@yandex.com>
2026-03-05 11:49:15 +0800

    leetcode/c++/900-999/913_cat_and_mouse.cpp

--- leetcode/c++/900-999/913_cat_and_mouse.cpp
+++ leetcode/c++/900-999/913_cat_and_mouse.cpp
@@ -73,6 +73,6 @@ int main() {
     vector<vector<int> > graph = {{2, 5}, {3}, {0, 4, 5}, {1, 4, 5}, {2, 3}, {0, 2, 3}};
     cout << s.catMouseGame(graph) << endl; // 0
 
-    vector<vector<int> > graph2 = {{1,3},{0},{3},{0,2}};
+    vector<vector<int> > graph2 = {{1, 3}, {0}, {3}, {0, 2}};
     cout << s.catMouseGame(graph2) << endl; // 1
 }
