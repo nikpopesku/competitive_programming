2589a6ee5207648a2dea66a689a7f5a8ca1c4f90
npopesku <nic1k@yandex.com>
2026-03-05 11:26:00 +0800

    leetcode/c++/900-999/913_cat_and_mouse.cpp

--- leetcode/c++/900-999/913_cat_and_mouse.cpp
+++ leetcode/c++/900-999/913_cat_and_mouse.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <numeric>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
+constexpr int MOUSE_MOVE = 1;
+constexpr int CAT_MOVE = 2;
+
 class Solution {
 public:
-    int catMouseGame(vector<vector<int> > &graph) {
+    int catMouseGame(const vector<vector<int> > &graph) {
+        queue<tuple<int, int, int>> q;
+
+        q.push({1, 2, MOUSE_MOVE});
+
+        while (!q.empty()) {
+            auto [mouse_position, cat_position, last_move] = q.front();
+            q.pop();
+            int next_move = last_move == MOUSE_MOVE ? CAT_MOVE : MOUSE_MOVE;
+
+            for (auto nei: graph[next_move]) {
+                if (next_move == MOUSE_MOVE and nei == 0) continue;
+            }
+        }
     }
 };
 
