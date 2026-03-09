3583401d5230cd3c752252834b77a75c86c71ba6
npopesku <nic1k@yandex.com>
2026-03-09 11:56:30 +0800

    neetcode.io/valid_tree.cpp

--- neetcode.io/valid_tree.cpp
+++ neetcode.io/valid_tree.cpp
@@ -1,11 +1,42 @@
 #include <vector>
 #include <iostream>
+#include <stack>
 
 using namespace std;
+
+dfs(int node) {
+
+}
 
 class Solution {
 public:
     bool validTree(int n, vector<vector<int> > &edges) {
+        if (edges.size() != n - 1) {
+            return false;
+        }
+
+        vector visited(n, false);
+        stack<int> st;
+        st.push(0);
+        visited[0] = true;
+
+        while (!st.empty()) {
+            const auto e = st.top();
+            st.pop();
+
+            for (auto nei: edges[e]) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    st.push(nei);
+                }
+            }
+        }
+
+        for (auto e: visited) {
+            if (e == false) return false;
+        }
+
+        return true;
     }
 };
 
