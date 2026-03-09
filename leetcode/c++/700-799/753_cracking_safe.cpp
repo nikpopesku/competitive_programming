369b1f1af3651591fca972a0627a98191fc69d4e
npopesku <nic1k@yandex.com>
2026-03-09 11:29:24 +0800

    leetcode/c++/700-799/753_cracking_safe.cpp

--- leetcode/c++/700-799/753_cracking_safe.cpp
+++ leetcode/c++/700-799/753_cracking_safe.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <stack>
 #include <unordered_map>
 #include <vector>
 
@@ -8,7 +9,26 @@ using namespace std;
 class Solution {
 public:
     string crackSafe(int n, int k) {
+        stack<string> st;
+        string s;
+        for (int i = 0; i < n - 1; ++i) s += '0';
+        st.push(s);
+        string path;
 
+        while (!st.empty()) {
+            string node = st.top();
+
+            if (!node.empty()) {
+                auto d = next_digit(node);
+                next = node[1:] + d;      // slide window
+                st.push(next);
+            } else {
+                st.pop();
+                path += node;             // dead end → collect it
+            }
+        }
+
+        return path;
     }
 };
 
