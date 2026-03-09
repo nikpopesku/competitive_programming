9c2e1448c3f61ea5728bc7c3cd1be01ead14e74e
npopesku <nic1k@yandex.com>
2026-03-09 12:01:16 +0800

    neetcode.io/valid_tree.cpp

--- neetcode.io/valid_tree.cpp
+++ neetcode.io/valid_tree.cpp
@@ -11,6 +11,13 @@ public:
             return false;
         }
 
+        vector adj(n, vector<int>());
+
+        for (auto &e: edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
         vector visited(n, false);
         stack<int> st;
         st.push(0);
@@ -20,7 +27,7 @@ public:
             const auto e = st.top();
             st.pop();
 
-            for (auto nei: edges[e]) {
+            for (auto nei: adj[e]) {
                 if (!visited[nei]) {
                     visited[nei] = true;
                     st.push(nei);
@@ -40,5 +47,8 @@ int main() {
     auto s = Solution();
 
     vector<vector<int> > edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
-    cout << s.validTree(1, 2) << '\n'; //10
+    cout << boolalpha << s.validTree(5, edges) << '\n'; //true
+
+    vector<vector<int> > edges2 = {{0, 1}, {1, 2}, {2, 3}, {1, 3}, {1, 4}};
+    cout << boolalpha << s.validTree(5, edges2) << '\n'; //false
 }
