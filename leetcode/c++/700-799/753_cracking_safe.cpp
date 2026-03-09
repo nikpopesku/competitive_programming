a8345729174bda6602f22ee60d9146a10bd090d5
npopesku <nic1k@yandex.com>
2026-03-09 11:11:46 +0800

    leetcode/c++/700-799/753_cracking_safe.cpp

---
+++ leetcode/c++/700-799/753_cracking_safe.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string crackSafe(int n, int k) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << s.crackSafe(1, 2) << '\n'; //10
+    cout << s.crackSafe(2, 2) << '\n'; //01100
+}
