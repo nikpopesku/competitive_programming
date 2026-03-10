946335c499503cb285fddd4b530cc238284cdee7
npopesku <nic1k@yandex.com>
2026-03-10 11:49:35 +0800

    neetcode.io/count-connected-components.cpp

--- neetcode.io/count-connected-components.cpp
+++ neetcode.io/count-connected-components.cpp
@@ -46,8 +46,8 @@ int main() {
     auto s = Solution();
 
     vector<vector<int> > edges = {{0, 1}, {1, 2}, {3, 4}};
-    cout << boolalpha << s.countComponents(5, edges) << '\n'; //1
+    cout << boolalpha << s.countComponents(5, edges) << '\n'; //2
 
     vector<vector<int> > edges2 = {{0, 1}, {1, 2}, {2, 3}, {3, 4}};
-    cout << boolalpha << s.countComponents(5, edges2) << '\n'; //2
+    cout << boolalpha << s.countComponents(5, edges2) << '\n'; //1
 }
