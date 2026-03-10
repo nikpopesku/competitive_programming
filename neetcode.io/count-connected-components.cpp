895e87cd7a6d485e44771a7da5a1dc2a3b9de72b
npopesku <nic1k@yandex.com>
2026-03-10 11:48:53 +0800

    neetcode.io/count-connected-components.cpp

--- neetcode.io/count-connected-components.cpp
+++ neetcode.io/count-connected-components.cpp
@@ -7,6 +7,37 @@ using namespace std;
 class Solution {
 public:
     int countComponents(int n, vector<vector<int> > &edges) {
+        vector adj(n, vector<int>());
+
+        for (auto &e: edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector visited(n, false);
+        stack<int> st;
+        int counter = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                st.push(i);
+                visited[i] = true;
+                ++counter;
+
+                while (!st.empty()) {
+                    auto e = st.top();
+                    st.pop();
+
+                    for (auto &nei: adj[e]) {
+                        if (!visited[nei]) {
+                            st.push(nei);
+                            visited[nei] = true;
+                        }
+                    }
+                }
+            }
+        }
+ return counter;
     }
 };
 
