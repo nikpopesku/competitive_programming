b4d00aed9166ac09f9519b9034f8d24149130e26
npopesku <nic1k@yandex.com>
2026-03-11 16:25:19 +0800

    atcoder/200-299/d/abc286_d_money_in_hand.cpp

--- atcoder/200-299/d/abc286_d_money_in_hand.cpp
+++ atcoder/200-299/d/abc286_d_money_in_hand.cpp
@@ -5,33 +5,10 @@ using namespace std;
 
 int main() {
     int N, X;
-    int A[50];
-    int B[50];
+    int A[50], B[50];
 
     cin >> N >> X;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0 ; i < N; ++i) {
         cin >> A[i] >> B[i];
     }
-
-    vector dp(X + 1, false);
-    dp[0] = true;
-
-    for (int i = 0; i < N; ++i) {
-        for (int j = X; j >= 0; --j) {
-            if (dp[j]) {
-                for (int k = 1; k <= B[i]; ++k) {
-                    if (j + A[i] * k <= X) {
-                        dp[j + A[i] * k] = true;
-                    }
-                    if (j + A[i] * k == X) {
-                        cout << "Yes\n";
-
-                        return 0;
-                    }
-                }
-            }
-        }
-    }
-
-    cout << "No\n";
 }
