b1af6d8668b0afe8593175debb922e3be267c221
npopesku <nic1k@yandex.com>
2026-03-11 16:34:06 +0800

    atcoder/200-299/d/abc286_d_money_in_hand.cpp

--- atcoder/200-299/d/abc286_d_money_in_hand.cpp
+++ atcoder/200-299/d/abc286_d_money_in_hand.cpp
@@ -14,4 +14,22 @@ int main() {
 
     vector<bool> dp(X+1, false);
     dp[0] = true;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j  = X; j >= 0; --j) {
+            if (dp[j]) {
+                for (int k = 1; k <= B[i]; ++k) {
+                    if (j + A[i] * k <= X) {
+                        dp[j + A[i] * k] = true;
+                    }
+                    if (j + A[i] * k == X) {
+                        cout << "Yes\n";
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "No\n";
 }
