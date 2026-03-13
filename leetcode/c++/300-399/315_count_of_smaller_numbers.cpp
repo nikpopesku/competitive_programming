ef0df9b5e15a4751cb62d79c2f486a42f226e54b
npopesku <nic1k@yandex.com>
2026-03-13 13:50:58 +0800

    leetcode/c++/300-399/315_count_of_smaller_numbers.cpp

--- leetcode/c++/300-399/315_count_of_smaller_numbers.cpp
+++ leetcode/c++/300-399/315_count_of_smaller_numbers.cpp
@@ -3,10 +3,40 @@
 
 using namespace std;
 
+class Bit {
+public:
+    Bit(int sz): n(sz), tree(sz + 1) {
+
+    }
+
+    int query(int index) {
+
+    }
+
+    void update(int index, const int delta) {
+        ++index;
+
+        while (index <= n) {
+            tree[index] += delta;
+            index += index & -index;
+        }
+    }
+private:
+    int n;
+    vector<int> tree;
+};
+
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
+        int maxval = nums[0];
+        for (auto &e: nums) maxval = max(maxval, e);
+        Bit bt(maxval);
 
+        for (int i = maxval; i >= 0; --i) {
+            int val = bt.query(nums[i] - 1);
+            bt.update(i);
+        }
     }
 };
 
