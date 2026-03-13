f9ac0b3bcc8ea7a1e76c1dbc56971876751fc774
npopesku <nic1k@yandex.com>
2026-03-13 13:20:31 +0800

    leetcode/c++/300-399/315_count_of_smaller_numbers.cpp

---
+++ leetcode/c++/300-399/315_count_of_smaller_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+
+    }
+};
+
+
+
+
+int main() {
+    vector nums = {5,2,6,1};
+    for (auto na = Solution(); auto &i: na.countSmaller(nums)) {
+        cout << i << "\n";
+    }
+
+}
