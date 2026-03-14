9da91706facd11756640cf3e86fa8ea9667298e6
npopesku <nic1k@yandex.com>
2026-03-14 12:02:05 +0800

    leetcode/c++/300-399/315_count_of_smaller_numbers.cpp

--- leetcode/c++/400-499/493_reverse_pairs.cpp
+++ leetcode/c++/400-499/493_reverse_pairs.cpp
@@ -47,5 +47,8 @@ public:
 int main() {
     vector nums = {1, 3, 2, 3, 1};
     auto na = Solution();
-    cout << na.reversePairs(nums) << "\n";
+    cout << na.reversePairs(nums) << "\n"; //2
+
+    vector nums2 = {2, 4, 3, 5, 1};
+    cout << na.reversePairs(nums2) << "\n"; //3
 }
