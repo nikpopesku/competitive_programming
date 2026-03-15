32aa9159c43870bf4dedc82dbe6c7aa74d544a0e
npopesku <nic1k@yandex.com>
2026-03-15 15:17:20 +0800

    leetcode/c++/300-399/315_count_of_smaller_numbers.cpp

--- leetcode/c++/400-499/493_reverse_pairs.cpp
+++ leetcode/c++/400-499/493_reverse_pairs.cpp
@@ -4,57 +4,14 @@
 
 using namespace std;
 
-class Bit {
-public:
-    explicit Bit(const int sz) : n(sz), tree(sz + 1, 0) {
-    }
 
-    void update(int index) {
-        while (index <= n) {
-            ++tree[index];
-            index += index & -index;
-        }
-    }
-
-    [[nodiscard]] int query(int index) const {
-        int response = 0;
-
-        while (index > 0) {
-            response += tree[index];
-            index -= index & -index;
-        }
-
-        return response;
-    }
-
-private:
-    int n;
-    vector<int> tree;
-};
 
 class Solution {
 public:
-    int reversePairs(const vector<int> &nums) {
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
-        sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
-        const int n = static_cast<int>(sorted.size());
+    int reversePairs(vector<int>& nums) {
 
-        Bit bt(n);
-        const int m = static_cast<int>(nums.size());
-        int response = 0;
-
-        for (int i = m - 1; i >= 0; --i) {
-            response += bt.query(lower_bound(sorted.begin(), sorted.end(), nums[i], [](const int x, const int target) {
-                return 2LL * x < target;
-            }) - sorted.begin());
-            bt.update(lower_bound(sorted.begin(), sorted.end(), nums[i]) - sorted.begin() + 1);
-        }
-
-        return response;
     }
 };
-
 
 int main() {
     vector nums = {1, 3, 2, 3, 1};
