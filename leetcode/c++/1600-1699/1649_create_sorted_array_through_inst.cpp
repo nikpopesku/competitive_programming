6e89f4bdbf886aab26a7c9390f155bcb0102e804
npopesku <nic1k@yandex.com>
2026-03-15 15:32:35 +0800

    leetcode/c++/1600-1699/1649_create_sorted_array_through_inst.cpp

--- leetcode/c++/1600-1699/1649_create_sorted_array_through_inst.cpp
+++ leetcode/c++/1600-1699/1649_create_sorted_array_through_inst.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    int createSortedArray(vector<int>& instructions) {
-
+    int createSortedArray(vector<int> &instructions) {
     }
 };
 
 int main() {
-    vector nums = {1, 3, 2, 3, 1};
+    vector nums = {1, 5, 6, 2};
     auto na = Solution();
-    cout << na.reversePairs(nums) << "\n"; //2
+    cout << na.createSortedArray(nums) << "\n"; //1
 
-    vector nums2 = {2, 4, 3, 5, 1};
-    cout << na.reversePairs(nums2) << "\n"; //3
+    vector nums2 = {1, 2, 3, 6, 5, 4};
+    cout << na.createSortedArray(nums2) << "\n"; //3
+
+    vector nums3 = {1, 3, 3, 3, 2, 4, 2, 1, 2};
+    cout << na.createSortedArray(nums4) << "\n"; //4
 }
