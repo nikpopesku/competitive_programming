003a1765ac95fbfb7ef583c173e926d4e2860552
npopesku <nic1k@yandex.com>
2026-03-16 13:10:17 +0800

    codeforces/cpp/1400-1499/1430E_string_reversal.cpp

--- codeforces/cpp/1400-1499/1430E_string_reversal.cpp
+++ codeforces/cpp/1400-1499/1430E_string_reversal.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -36,4 +37,20 @@ int main() {
     string s;
     cin >> n;
     cin >> s;
+
+    string t = s;
+    ranges::reverse(t);
+
+    vector<int> p(n);
+    for (char c = 'a'; c <= 'z'; ++c) {
+        vector<int> let_s, let_t;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == c) let_s.push_back(i);
+            if (t[i] == c) let_t.push_back(i);
+        }
+
+        for (int i = 0; i < static_cast<int>(let_s.size()); ++i) {
+            p[let_s[i]] = let_t[i];
+        }
+    }
 }
