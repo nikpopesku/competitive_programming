2e9b55891718a4f6ccd80abf4ce9c68d6fe3ecba
npopesku <nic1k@yandex.com>
2026-03-16 13:02:38 +0800

    codeforces/cpp/1400-1499/1430E_string_reversal.cpp

--- codeforces/cpp/1400-1499/1430E_string_reversal.cpp
+++ codeforces/cpp/1400-1499/1430E_string_reversal.cpp
@@ -3,10 +3,37 @@
 
 using namespace std;
 
+class Bit {
+public:
+    explicit Bit(const int sz) : n(sz), tree(sz + 1) {
+    }
+
+    void update(int index) {
+        while (index <= n) {
+            ++tree[index];
+            index += index & -index;
+        }
+    }
+
+    int query(int index) const {
+        int sum = 0;
+
+        while (index > 0) {
+            sum += tree[index];
+            index -= index & -index;
+        }
+
+        return sum;
+    }
+
+private:
+    int n;
+    vector<int> tree;
+};
+
 int main() {
     int n;
     string s;
     cin >> n;
     cin >> s;
-
 }
