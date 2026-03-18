bfcd6d57f84c7a080ae0bc17baa182066433400f
npopesku <nic1k@yandex.com>
2026-03-18 12:06:52 +0800

    leetcode/c++/1500-1599/1504_count_submatrices_with_all_ones.cpp

--- leetcode/c++/1500-1599/1504_count_submatrices_with_all_ones.cpp
+++ leetcode/c++/1500-1599/1504_count_submatrices_with_all_ones.cpp
@@ -19,17 +19,29 @@ public:
             }
         }
 
-        int xxxx = 5;
+        int answer = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int min_value = h[i][j];
+                for (int k = 0; k <= j; ++k) {
+                    min_value = min(min_value, h[i][k]);
+                }
+                answer += min_value;
+            }
+        }
+
+        return answer;
     }
 };
 
 int main() {
     Solution s;
 
-    vector<vector<int> > mat = {{1, 0, 1}, {1, 1, 0}, {1, 1, 0}};
+    const vector<vector<int> > mat = {{1, 0, 1}, {1, 1, 0}, {1, 1, 0}};
     cout << s.numSubmat(mat) << endl; //13
 
-    vector<vector<int> > mat2 = {{0, 1, 1, 0}, {0, 1, 1, 1}, {1, 1, 1, 0}};
+    const vector<vector<int> > mat2 = {{0, 1, 1, 0}, {0, 1, 1, 1}, {1, 1, 1, 0}};
     cout << s.numSubmat(mat2) << endl; //24
 
     return 0;
