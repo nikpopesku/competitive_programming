1639b9aba1465c517e47761b877b162b809c21a1
npopesku <nic1k@yandex.com>
2026-03-19 11:48:56 +0800

    spoj.com/matsum.cpp

---
+++ spoj.com/matsum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+void solve() {
+    int N;
+    cin >> N;
+    vector v(N, vector<int>(N));
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
