0b33f7f2bb89aefe91cb7ab4168d98ae074b0155
npopesku <nic1k@yandex.com>
2026-03-19 11:55:10 +0800

    spoj.com/matsum.cpp

--- spoj.com/matsum.cpp
+++ spoj.com/matsum.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+class Bit {
+public:
+    explicit Bit(const int sz) : n(sz), tree(n + 1, vector<int>(n + 1)) {
+    }
+
+    void update(int x, int y, int val) {
+    }
+
+    int range(const int x1, const int y1, const int x2, const int y2) {
+        return query(x2, y2) + query(x1 - 1, y1 - 1) - query(x2, y1 - 1) - query(x1 - 1, y2);
+    }
+
+private:
+    int n;
+    vector<vector<int> > tree;
+
+    int query(int x1, int y) {
+        int sum = 0;
+
+        return sum;
+    }
+};
+
 void solve() {
     int N, x1, y1, x2, y2, val;
     cin >> N;
