911f144ff908752de9f25da4000755a3075f189a
npopesku <nic1k@yandex.com>
2026-03-19 11:50:56 +0800

    spoj.com/matsum.cpp

--- spoj.com/matsum.cpp
+++ spoj.com/matsum.cpp
@@ -5,9 +5,21 @@
 using namespace std;
 
 void solve() {
-    int N;
+    int N, x1, y1, x2, y2, val;
     cin >> N;
     vector v(N, vector<int>(N));
+
+    string type;
+
+    while (type != "END") {
+        cin >> type;
+
+        if (type == "SET") {
+            cin >> x1 >> y1 >> val;
+        } else {
+            cin >> x1 >> y1 >> x2 >> y2;
+        }
+    }
 }
 
 int main() {
