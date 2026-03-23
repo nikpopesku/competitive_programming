4d70d3e0c05d84975bb6c5b37fb2a2566badab9b
npopesku <nic1k@yandex.com>
2026-03-23 11:05:30 +0800

    codeforces/cpp/000-099/61E_enemy_is_weak.cpp

---
+++ codeforces/cpp/000-099/61E_enemy_is_weak.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+}
