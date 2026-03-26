86efccab433a9fb799c4f73f69c0e35b583af151
npopesku <nic1k@yandex.com>
2026-03-26 16:11:20 +0800

    cses.fi/1600-1699/1649_dynamic_range_min_query.cpp

--- cses.fi/1600-1699/1649_dynamic_range_min_query.cpp
+++ cses.fi/1600-1699/1649_dynamic_range_min_query.cpp
@@ -2,6 +2,28 @@
 #include <vector>
 using namespace std;
 
+class SegTree {
+public:
+    SegTree(vector<int> arr): n(static_cast<int>(arr.size())), tree(n * 4) {
+        build(arr, 1, 0, n-1);
+    }
+private:
+    int n;
+    vector<int> tree;
+
+    void build(vector<int>& arr, const int index, const int lo, const int hi) {
+        if (lo == hi) {
+            tree[index] = arr[lo];
+            return;
+        }
+
+        const int mid = (lo + hi) / 2;
+        build(arr, index, lo, mid);
+        build(arr, index, mid + 1, hi);
+        tree[index] = min(tree[2 * index], tree[2 * index + 1]);
+    }
+};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -12,7 +34,11 @@ int main() {
     vector<int> v(n);
     for (int &x: v) cin >> x;
 
+    SegTree st(v);
+
     while (q--) {
         cin >> type >> a >> b;
+
+        if (type == 1)
     }
 }
