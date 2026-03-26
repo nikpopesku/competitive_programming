9935977e216dac86240feaadedfc0171a245e06f
npopesku <nic1k@yandex.com>
2026-03-26 15:08:03 +0800

    cses.fi/1600-1699/1648_dynamic_range_min_query.cpp

--- cses.fi/1600-1699/1648_dynamic_range_min_query.cpp
+++ cses.fi/1600-1699/1648_dynamic_range_min_query.cpp
@@ -5,9 +5,42 @@ using namespace std;
 
 #define ll long long
 
+class Bit {
+public:
+    explicit Bit(const int sz): n(sz), tree(n+1, 0) {
+
+    }
+
+    void update(int index) {
+
+    }
+
+    int range(const int a, const int b) {
+        return query(b) - query(a - 1);
+    }
+private:
+    int n;
+    vector<int> tree;
+
+    int query(int index) {
+        return 4;
+    }
+};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    int n, q, type, a, b;
+    cin >> n >> q;
+    vector<int> v;
+
+    for (int i =0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < q; ++i) {
+        cin >> type >> a >> b;
+    }
 }
