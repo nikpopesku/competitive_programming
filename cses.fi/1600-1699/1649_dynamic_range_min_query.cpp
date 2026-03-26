f91da0bdc257c96efe0b18c586e5cb5350c610d1
npopesku <nic1k@yandex.com>
2026-03-26 16:06:14 +0800

    cses.fi/1600-1699/1649_dynamic_range_min_query.cpp

--- cses.fi/1600-1699/1649_dynamic_range_min_query.cpp
+++ cses.fi/1600-1699/1649_dynamic_range_min_query.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, q;
+    int n, q, type, a, b;
     cin >> n >> q;
 
+    vector<int> v(n);
+    for (int &x: v) cin >> x;
+
+    while (q--) {
+        cin >> type >> a >> b;
+    }
 }
