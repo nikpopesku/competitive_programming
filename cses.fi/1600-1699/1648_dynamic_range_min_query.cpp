10d454d90b7fe2076d5c0329169a0ffa0c0c18f7
npopesku <nic1k@yandex.com>
2026-03-26 15:11:45 +0800

    cses.fi/1600-1699/1648_dynamic_range_min_query.cpp

--- cses.fi/1600-1699/1648_dynamic_range_min_query.cpp
+++ cses.fi/1600-1699/1648_dynamic_range_min_query.cpp
@@ -7,17 +7,22 @@ using namespace std;
 
 class Bit {
 public:
-    explicit Bit(const int sz): n(sz), tree(n+1, 0) {
-
+    explicit Bit(const int sz) : n(sz), tree(n + 1, 0) {
     }
 
-    void update(int index) {
+    void update(int index, const int val) {
+        const int delta = val - tree[index];
 
+        while (index <= n) {
+            tree[index] += delta;
+            index += index & -index;
+        }
     }
 
     int range(const int a, const int b) {
         return query(b) - query(a - 1);
     }
+
 private:
     int n;
     vector<int> tree;
@@ -32,12 +37,14 @@ int main() {
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int n, q, type, a, b;
+    int n, q, type, a, b, val;
     cin >> n >> q;
-    vector<int> v;
 
-    for (int i =0; i < n; ++i) {
-        cin >> v[i];
+    Bit bt(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> val;
+        bt.update(i + 1, val);
     }
 
     for (int i = 0; i < q; ++i) {
