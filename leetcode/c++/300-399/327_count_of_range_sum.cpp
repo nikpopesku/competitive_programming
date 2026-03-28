1c39fc3b7756ba4c8e73ebd05880bb71c577b620
npopesku <nic1k@yandex.com>
2026-03-28 11:55:06 +0800

    leetcode/c++/300-399/327_count_of_range_sum.cpp

--- leetcode/c++/300-399/327_count_of_range_sum.cpp
+++ leetcode/c++/300-399/327_count_of_range_sum.cpp
@@ -4,6 +4,23 @@
 
 using namespace std;
 
+class Bit {
+public:
+    explicit Bit (const int sz): n(sz), tree(sz + 1, 0) {
+
+    }
+
+    void update(int index) {
+        while (index <= n) {
+            ++tree[index];
+            index += index & -index;
+        }
+    }
+private:
+    int n;
+    vector<int> tree;
+};
+
 class Solution {
 public:
     int countRangeSum(const vector<int> &nums, const int lower, const int upper) {
