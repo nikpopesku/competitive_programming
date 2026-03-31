192cc9451fd6ca5d127b1c6b85d5f25a0c497c07
npopesku <nic1k@yandex.com>
2026-03-31 11:20:24 +0800

    leetcode/c++/2400-2499/2407_longest_increasing_subsequence_ii.cpp

--- leetcode/c++/2400-2499/2407_longest_increasing_subsequence_ii.cpp
+++ leetcode/c++/2400-2499/2407_longest_increasing_subsequence_ii.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 class SegTree {
 public:
-    SegTree(int sz): n(sz), tree(n * 4, 0) {
+    explicit SegTree(const int sz): n(sz), tree(n * 4, 0) {
 
     }
 
-    void update(int pos, int val) {
+    void update(const int pos, const int val) {
         update(1, 1, n, pos, val);
     }
 
-    int query(int l, int r) {
+    int query(const int l, const int r) {
         return query(1, 1, n, l, r);
     }
 private:
     int n;
     vector<int> tree;
 
-    void update(const int node, const int nl, const int nr, int pos, int val) {
+    void update(const int node, const int nl, const int nr, const int pos, const int val) {
         if (nl == nr) {
             tree[node] = max(tree[node], val);
 
@@ -35,7 +35,7 @@ private:
         tree[node] = max(tree[node * 2], tree[node * 2 + 1]);
     }
 
-    int query(int node, int nl, int nr, int l, int r) {
+    int query(int node, int nl, const int nr, const int l, const int r) {
         if (r < nl || l > nr) {
             return 0;
         }
