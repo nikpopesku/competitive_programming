59cd39b52ada0aae5c5a45424a7664e5b13cf75e
npopesku <nic1k@yandex.com>
2026-04-02 12:54:59 +0800

    codeforces/cpp/200-300/276C_little_girl_max_sum.cpp

--- codeforces/cpp/200-300/276C_little_girl_max_sum.cpp
+++ codeforces/cpp/200-300/276C_little_girl_max_sum.cpp
@@ -9,44 +9,4 @@ int main()
     // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int n, q, l, r;
-    cin >> n >> q;
-    vector<long long> a(n);
-
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
-
-    vector<long long> count(n + 1, 0);
-
-    for (int i = 0; i < q; ++i)
-    {
-        cin >> l >> r;
-
-        count[l - 1]++;
-
-        if (r < n)
-        {
-            --count[r];
-        }
-    }
-
-    for (int i = 1; i < n; ++i)
-    {
-        count[i] += count[i - 1];
-    }
-
-    ranges::sort(a);
-    ranges::sort(count.begin(), count.begin() + n);
-
-    long long response = 0;
-
-    for (int i = 0; i < n; ++i)
-    {
-        response += a[i] * count[i];
-    }
-
-    cout << response << "\n";
 }
