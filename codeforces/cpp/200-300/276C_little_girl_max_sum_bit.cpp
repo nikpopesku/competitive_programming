df9779030f80a478e4bb6bded0baa09c9e14cb86
npopesku <nic1k@yandex.com>
2026-04-03 10:57:57 +0800

    codeforces/cpp/200-300/276C_little_girl_max_sum_bit.cpp

---
+++ codeforces/cpp/200-300/276C_little_girl_max_sum_bit.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define  ll long long
+
+class Bit {
+public:
+    Bit (int sz): n(sz), tree(n+1, 0) {
+
+    }
+
+    void update(int l, int r) {
+
+    }
+private:
+    int n;
+    vector<int> tree;
+};
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, q, l, r;
+    cin >> n >> q;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    Bit bt(n);
+
+    while (q--) {
+        cin >> l >> r;
+        bt.update(l, r);
+    }
+}
