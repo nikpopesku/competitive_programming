e68644a77b90c06bb94d6852fee94add2511a3b7
npopesku <nic1k@yandex.com>
2026-04-05 12:21:50 +0800

    leetcode/c++/700-799/775_global_local_inv.cpp

--- leetcode/c++/700-799/775_global_local_inv.cpp
+++ leetcode/c++/700-799/775_global_local_inv.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool isIdealPermutation(const vector<int>& nums) {
+    bool isIdealPermutation(const vector<int> &nums) {
         for (int i = 0; i < nums.size(); ++i) {
             if (abs(i - nums[i]) > 1) {
                 return false;
@@ -20,10 +20,9 @@ public:
 int main() {
     auto s = Solution();
 
-    vector<int> v = {1,0,2};
+    vector<int> v = {1, 0, 2};
     cout << boolalpha << s.isIdealPermutation(v) << '\n';
 
-    vector<int> v2 = {1,2,0};
+    vector<int> v2 = {1, 2, 0};
     cout << boolalpha << s.isIdealPermutation(v2) << '\n';
-
 }
