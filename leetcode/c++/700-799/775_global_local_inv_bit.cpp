9b595280b801be91fb08bb2495061f73c4cc8a0d
npopesku <nic1k@yandex.com>
2026-04-05 13:10:28 +0800

    leetcode/c++/700-799/775_global_local_inv_bit.cpp

--- leetcode/c++/700-799/775_global_local_inv_bit.cpp
+++ leetcode/c++/700-799/775_global_local_inv_bit.cpp
@@ -9,6 +9,7 @@ public:
     }
 
     void update(int index) {
+        ++index;
         while (index <= n) {
             ++tree[index];
             index += index & -index;
@@ -17,6 +18,7 @@ public:
 
     [[nodiscard]] int query(int index) const {
         int sm = 0;
+        ++index;
 
         while (index > 0) {
             sm += tree[index];
@@ -34,18 +36,24 @@ private:
 class Solution {
 public:
     bool isIdealPermutation(const vector<int> &nums) {
-        Bit bt(2000000 + 2);
         const int n = static_cast<int>(nums.size());
 
         int global = 0;
         int local = 0;
 
+        vector<int> v = nums;
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(), v.end()), v.end());
+        const int m = static_cast<int>(v.size());
+        Bit bt(m);
+
         for (int i = 0; i < n; ++i) {
             if (i < n - 1 && nums[i] > nums[i + 1]) {
                 ++local;
             }
-            global += bt.query(nums[i]);
-            bt.update(nums[i]);
+            const int index = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+            global += bt.query(index);
+            bt.update(index);
         }
 
         return global > local;
