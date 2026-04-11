87553c57334e8c0476340a48cbce638ba1a958a6
Nikolai Popesku <nic1k@yandex.com>
2026-04-11 11:13:26 +0800

    leetcode/c++/math_quest/q2_smalles_int_div_by_k.cpp

--- leetcode/c++/math_quest/q2_smalles_int_div_by_k.cpp
+++ leetcode/c++/math_quest/q2_smalles_int_div_by_k.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <set>
 
 
 using namespace std;
 
+#define ll long long
 
 class Solution {
 public:
     int smallestRepunitDivByK(int k) {
+        set<ll> uniq;
 
+        ll num = 1;
+
+        while (true) {
+            int md = num % k;
+            if (md == 0) {
+                return num;
+            }
+
+
+            if (uniq.contains(md)) {
+                return -1;
+            }
+
+            uniq.insert(md);
+            num = num << 1;
+            num += 1;
+        }
     }
 };
 
@@ -15,4 +35,6 @@ int main() {
     auto s = Solution();
 
     cout << s.smallestRepunitDivByK(1) << '\n';
+    cout << s.smallestRepunitDivByK(2) << '\n';
+    cout << s.smallestRepunitDivByK(3) << '\n';
 }
