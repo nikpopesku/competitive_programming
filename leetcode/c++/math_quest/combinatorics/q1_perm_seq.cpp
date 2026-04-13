0b5ff2871845691210eafc5b53fd35a37d4b6659
Nikolai Popesku <nic1k@yandex.com>
2026-04-13 11:47:16 +0800

    leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp

--- leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
+++ leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
@@ -11,18 +11,16 @@ public:
         string response;
         int block_size = 1;
         for (int i = 1; i < n; ++i) block_size *= i;
-        int m = n;
 
         while (k) {
             int index = k / block_size;
             if (k % block_size > 0) ++index;
             response += to_string(digits[index - 1]);
             digits.erase(digits.begin() + index - 1);
-            --m;
             if (block_size <= k) {
                 k -= block_size;
             }
-            block_size /= m;
+            block_size /= static_cast<int>(digits.size());
         }
 
         response += to_string(digits[0]);
@@ -35,6 +33,7 @@ public:
 int main() {
     auto s = Solution();
 
+    cout << s.getPermutation(1, 1) << '\n'; //213
     cout << s.getPermutation(3, 3) << '\n'; //213
     cout << s.getPermutation(4, 9) << '\n'; //2314
     cout << s.getPermutation(3, 1) << '\n'; //123
