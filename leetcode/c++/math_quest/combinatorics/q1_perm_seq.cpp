a19127f581d55bea6415d698596d1aadadd05ce8
Nikolai Popesku <nic1k@yandex.com>
2026-04-13 11:09:55 +0800

    leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp

--- leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
+++ leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
@@ -4,30 +4,14 @@ using namespace std;
 
 class Solution {
 public:
-    int smallestRepunitDivByK(int k) {
-        if (k % 2 == 0 || k % 5 == 0) {
-            return -1;
-        }
+    string getPermutation(int n, int k) {
 
-        int remainder = 0;
-
-        for (int len = 1; len <= k; ++len) {
-            remainder = (remainder * 10 + 1) % k;
-            if (remainder == 0) {
-                return len;
-            }
-        }
-
-        return -1;
     }
 };
-
 int main() {
     auto s = Solution();
 
-    cout << s.smallestRepunitDivByK(1) << '\n';
-    cout << s.smallestRepunitDivByK(2) << '\n';
-    cout << s.smallestRepunitDivByK(3) << '\n';
-    cout << s.smallestRepunitDivByK(5) << '\n';
-    cout << s.smallestRepunitDivByK(23) << '\n';
+    cout << s.getPermutation(3, 3) << '\n'; //213
+    cout << s.getPermutation(4, 9) << '\n'; //2314
+    cout << s.getPermutation(3, 1) << '\n'; //123
 }
