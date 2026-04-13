be1fef3e4fe955f5b931a9a60636a19a4adce15b
Nikolai Popesku <nic1k@yandex.com>
2026-04-13 11:36:56 +0800

    leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp

--- leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
+++ leetcode/c++/math_quest/combinatorics/q1_perm_seq.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        vector<int> digits = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        digits.resize(n);
+        string response;
+        int block_size = 1;
+        for (int i = 1; i < n; ++i) block_size *= i;
+        int m = n;
 
+        while (k) {
+            int index = k / block_size;
+            if (k % block_size > 0) ++index;
+            response += to_string(digits[index - 1]);
+            digits.erase(digits.begin() + index);
+            --m;
+            k -= block_size;
+            block_size /= m;
+        }
+
+
+        return response;
     }
 };
+
 int main() {
     auto s = Solution();
 
     cout << s.getPermutation(3, 3) << '\n'; //213
-    cout << s.getPermutation(4, 9) << '\n'; //2314
-    cout << s.getPermutation(3, 1) << '\n'; //123
+    // cout << s.getPermutation(4, 9) << '\n'; //2314
+    // cout << s.getPermutation(3, 1) << '\n'; //123
 }
