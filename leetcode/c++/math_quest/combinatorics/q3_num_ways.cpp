6c545f76dd84e4aa13631ecd4356387b7aa5a58a
Nikolai Popesku <nic1k@yandex.com>
2026-04-15 13:01:08 +0800

    leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp

--- leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp
+++ leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp
@@ -5,21 +5,23 @@ using namespace std;
 
 constexpr int MOD = 1e9 + 7;
 
+#define ll long long
+
 class Solution {
 public:
-    int rearrangeSticks(int n, int k) {
-        vector dp(n + 1, vector(k + 1, 0));
-        for (int i = 0; i <= min(n, k); ++i) {
+    int rearrangeSticks(const int n, const int k) {
+        vector dp(n + 1, vector(k + 1, 0LL));
+        for (ll i = 0; i <= min(n, k); ++i) {
             dp[i][i] = 1;
         }
 
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= min(i, k); ++j) {
+        for (ll i = 1; i <= n; ++i) {
+            for (ll j = 1; j <= min(i, static_cast<ll>(k)); ++j) {
                 dp[i][j] = (dp[i - 1][j - 1] + (i - 1) * dp[i - 1][j] % MOD) % MOD;
             }
         }
 
-        return dp.back().back();
+        return static_cast<int>(dp.back().back());
     }
 };
 
@@ -27,4 +29,5 @@ int main() {
     auto s = Solution();
 
     cout << s.rearrangeSticks(3, 2) << '\n';
+    cout << s.rearrangeSticks(20, 11) << '\n';
 }
