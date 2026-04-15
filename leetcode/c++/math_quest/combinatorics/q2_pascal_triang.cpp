f33507ac0821c2e6287998784844ff543284ec52
Nikolai Popesku <nic1k@yandex.com>
2026-04-15 11:19:07 +0800

    leetcode/c++/math_quest/combinatorics/q2_pascal_triang.cpp

--- leetcode/c++/math_quest/combinatorics/q2_pascal_triang.cpp
+++ leetcode/c++/math_quest/combinatorics/q2_pascal_triang.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> generate(const int numRows) {
-        vector<vector<int>> response;
+    vector<vector<int> > generate(const int numRows) {
+        vector<vector<int> > response;
 
         for (int row = 0; row < numRows; ++row) {
             vector<int> row_vec;
             for (int col = 0; col <= row; ++col) {
-                if (row == 0 || col == 0) {
+                if (col == 0 || col == row) {
                     row_vec.push_back(1);
                 } else {
-                    row_vec.push_back(response.back()[col - 1]  + response.back()[col]);
+                    row_vec.push_back(response.back()[col - 1] + response.back()[col]);
                 }
             }
             response.push_back(row_vec);
@@ -27,7 +27,7 @@ public:
 int main() {
     auto s = Solution();
 
-    for (auto e: s.generate(5)) {
+    for (auto e: s.generate(1)) {
         for (auto x: e) cout << x << ' ';
         cout << '\n';
     }
