b1551a511703ad831f4df47dd02efc6dbb9f88fe
Nikolai Popesku <nic1k@yandex.com>
2026-04-15 12:57:47 +0800

    leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp

--- leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp
+++ leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp
@@ -10,6 +10,14 @@ public:
         for (int i = 0; i <= min(n, k); ++i) {
             dp[i][i] = 1;
         }
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= min(i, k); ++j) {
+                dp[i][j] = dp[i-1][j-1] + (i - 1) * dp[i-1][j];
+            }
+        }
+
+        return dp.back().back();
     }
 };
 
