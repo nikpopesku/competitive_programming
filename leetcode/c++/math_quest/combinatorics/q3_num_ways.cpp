7e680a749bb08a9363360ec4223a749ee3713c26
Nikolai Popesku <nic1k@yandex.com>
2026-04-15 11:25:44 +0800

    leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp

---
+++ leetcode/c++/math_quest/combinatorics/q3_num_ways.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rearrangeSticks(int n, int k) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << s.rearrangeSticks(3, 2) << '\n';
+}
