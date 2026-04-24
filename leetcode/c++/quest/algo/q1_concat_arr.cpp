1f5f028670554ee63b45387b11e2362d5e9bdbb9
Nikolai Popesku <nic1k@yandex.com>
2026-04-24 11:33:15 +0800

    leetcode/c++/quest/algo/q1_concat_arr.cpp

---
+++ leetcode/c++/quest/algo/q1_concat_arr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+
+        ans.resize
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << s.getConcatenation(1, 1) << '\n'; //213
+}
