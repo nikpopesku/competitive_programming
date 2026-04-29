85ae07bb170393eb99c49cb7bb8ce2a100fbc155
Nikolai Popesku <nic1k@yandex.com>
2026-04-29 12:29:24 +0800

    leetcode/c++/quest/algo/array2/q2_nums_smaller.cpp

--- leetcode/c++/quest/algo/array2/q2_nums_smaller.cpp
+++ leetcode/c++/quest/algo/array2/q2_nums_smaller.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int> &nums) {
-        vector v(10, 0);
+        vector v(101, 0);
         const int n = static_cast<int>(nums.size());
         for (const auto x: nums) ++v[x];
         for (int i = 1; i < 10; ++i) v[i] += v[i - 1];
@@ -30,5 +30,8 @@ int main() {
     cout << '\n';
 
     for (vector<int> nums = {7, 7, 7, 7}; const auto e: s.smallerNumbersThanCurrent(nums)) cout << e << ' ';
+    cout << '\n';
+
+    for (vector<int> nums = {5, 0, 10, 0, 10, 6}; const auto e: s.smallerNumbersThanCurrent(nums)) cout << e << ' ';
     cout << '\n';
 }
