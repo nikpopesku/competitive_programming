82a1462dc4545e1ee586b4a5c49a4a1fecdb28fa
Nikolai Popesku <nic1k@yandex.com>
2026-04-30 12:39:26 +0800

    leetcode/c++/quest/algo/stack/q1_build_arr_with_stack.cpp

---
+++ leetcode/c++/quest/algo/stack/q1_build_arr_with_stack.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    for (vector<int> nums = {1,3}; const auto e: s.buildArray(nums, 3)) cout << e << ' ';
+    cout << '\n';
+}
