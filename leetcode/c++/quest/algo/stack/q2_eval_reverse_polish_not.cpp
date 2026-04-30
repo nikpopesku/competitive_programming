b408267c6d8910728ce27acd7ff98e8dd8adb5b8
Nikolai Popesku <nic1k@yandex.com>
2026-04-30 13:09:24 +0800

    leetcode/c++/quest/algo/stack/q1_build_arr_with_stack.cpp

--- leetcode/c++/quest/algo/stack/q2_eval_reverse_polish_not.cpp
+++ leetcode/c++/quest/algo/stack/q2_eval_reverse_polish_not.cpp
@@ -35,7 +35,7 @@ int main() {
     vector<string> tokens1 = {"2", "1", "+", "3", "*"};
     cout << s.evalRPN(tokens1) << '\n';
 
-    vector<string> tokens2 = {"2", "1", "+", "3", "*"};
+    vector<string> tokens2 = {"4", "13", "5", "/", "+"};
     cout << s.evalRPN(tokens2) << '\n';
 
     vector<string> tokens3 = {"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
