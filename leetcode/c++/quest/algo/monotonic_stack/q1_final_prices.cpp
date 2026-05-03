756fabec0e4f8984cee862dc75fd152dcacd0845
Nikolai Popesku <nic1k@yandex.com>
2026-05-03 12:50:45 +0800

    leetcode/c++/quest/algo/monotonic_stack/q1_final_prices.cpp

--- leetcode/c++/quest/algo/monotonic_stack/q1_final_prices.cpp
+++ leetcode/c++/quest/algo/monotonic_stack/q1_final_prices.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> finalPrices(vector<int> &prices) {
+    vector<int> finalPrices(const vector<int> &prices) {
+        const int n = static_cast<int>(prices.size());
+        vector final_prices = prices;
+        stack<int> st;
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && prices[st.top()] > prices[i]) {
+                const int index = st.top();
+                st.pop();
+                final_prices[index] = final_prices[index] - prices[i];
+            }
+            st.push(i);
+        }
+
+        return final_prices;
     }
 };
 
