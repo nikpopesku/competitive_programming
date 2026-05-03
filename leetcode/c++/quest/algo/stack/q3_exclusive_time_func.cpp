2b1a92996df9f45f79e5bc732e4b378a7c237304
Nikolai Popesku <nic1k@yandex.com>
2026-05-03 12:14:08 +0800

    leetcode/c++/quest/algo/stack/q3_exclusive_time_func.cpp

--- leetcode/c++/quest/algo/stack/q3_exclusive_time_func.cpp
+++ leetcode/c++/quest/algo/stack/q3_exclusive_time_func.cpp
@@ -49,6 +49,7 @@ int main() {
 
     for (vector<string> logs = {"0:start:0", "1:start:2", "1:end:5", "0:end:6"}; const auto e: s.exclusiveTime(2, logs))
         cout << e << ' ';
+    
     cout << '\n';
 
     for (vector<string> logs2 = {"0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"}; const auto e:
