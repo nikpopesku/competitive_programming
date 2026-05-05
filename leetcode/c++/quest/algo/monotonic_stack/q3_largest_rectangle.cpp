f144a57bf24880016b6251e02056e68433bc7de8
Nikolai Popesku <nic1k@yandex.com>
2026-05-05 12:00:26 +0800

    leetcode/c++/quest/algo/monotonic_stack/q3_largest_rectangle.cpp

--- leetcode/c++/quest/algo/monotonic_stack/q3_largest_rectangle.cpp
+++ leetcode/c++/quest/algo/monotonic_stack/q3_largest_rectangle.cpp
@@ -7,16 +7,16 @@ using namespace std;
 class Solution {
 public:
     int largestRectangleArea(vector<int> &heights) {
-        int n = static_cast<int>(heights.size());
         int max_area = 0;
+        const int n = static_cast<int>(heights.size());
         stack<int> st;
 
         for (int i = 0; i <= n; ++i) {
-            int h = (i == n) ? 0 : heights[i];
+            int h = i < n ? heights[i] : 0;
             while (!st.empty() && heights[st.top()] > h) {
-                int height = heights[st.top()];
+                const int height = heights[st.top()];
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
+                const int width = st.empty() ? i : i - st.top() - 1;
                 max_area = max(max_area, height * width);
             }
             st.push(i);
