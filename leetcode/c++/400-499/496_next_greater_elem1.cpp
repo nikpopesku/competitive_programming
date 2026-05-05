69330758883503dbe506a2365b58e6b42ddf3e7f
Nikolai Popesku <nic1k@yandex.com>
2026-05-05 12:18:39 +0800

    leetcode/c++/400-499/496_next_greater_elem1.cpp

--- leetcode/c++/400-499/496_next_greater_elem1.cpp
+++ leetcode/c++/400-499/496_next_greater_elem1.cpp
@@ -6,12 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2) {
     }
 };
 
 int main() {
-    for (vector<int> nums1 = {4,1,2}, vector<int> nums2 = {1,3,4,2}; const auto e: s.nextGreaterElement(nums1, nums2)) cout << e << ' ';
+    auto s = Solution();
+
+    for (vector<int> nums1 = {4, 1, 2}, nums2 = {1, 3, 4, 2}; const auto e: s.nextGreaterElement(nums1, nums2))
+        cout << e << ' ';
+    cout << '\n';
+
+    for (vector<int> nums1 = {2, 4}, nums2 = {1, 2, 3, 4}; const auto e: s.nextGreaterElement(nums1, nums2))
+        cout << e << ' ';
     cout << '\n';
 }
