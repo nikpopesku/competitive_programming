25d8b460f3d3cd50001462839fbb38ba69ca4bd2
Nikolai Popesku <nic1k@yandex.com>
2026-05-07 12:19:19 +0800

    leetcode/c++/900-999/901_online_stock_span.cpp

---
+++ leetcode/c++/900-999/901_online_stock_span.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+
+class StockSpanner {
+public:
+    StockSpanner() {
+
+    }
+
+    int next(int price) {
+
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
+int main() {
+    auto s = Solution();
+
+    for (vector<int> nums1 = {1, 2, 1}; const auto e: s.nextLargerNodes(nums1))
+        cout << e << ' ';
+    cout << '\n';
+
+    for (vector<int> nums1 = {1, 2, 3, 4, 3}; const auto e: s.nextLargerNodes(nums1))
+        cout << e << ' ';
+    cout << '\n';
+}
