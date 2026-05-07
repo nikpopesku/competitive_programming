73452fd5e56cd9c166ebb8721841b23cc686b129
Nikolai Popesku <nic1k@yandex.com>
2026-05-07 12:15:45 +0800

    leetcode/c++/1000-1099/1019_next_greater_node_in_linked_list.cpp

--- leetcode/c++/1000-1099/1019_next_greater_node_in_linked_list.cpp
+++ leetcode/c++/1000-1099/1019_next_greater_node_in_linked_list.cpp
@@ -6,31 +6,58 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {
+    }
+
+    ListNode(int x) : val(x), next(nullptr) {
+    }
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {
+    }
+};
+
 class Solution {
 public:
-    vector<int> nextLargerNodes(ListNode* head) {
+    vector<int> nextLargerNodes(ListNode *head) {
+        vector<int> nums;
+        auto node = head;
 
+        while (node != nullptr) {
+            nums.push_back(node->val);
+            node = node->next;
+        }
+
+        int n = static_cast<int>(nums.size());
+        vector<int> response(n, 0);
+        stack<int> st;
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) {
+                int index = st.top();
+                st.pop();
+                response[index] = nums[i];
+            }
+
+            st.push(i);
+        }
+
+        return response;
     }
 };
 
 int main() {
     auto s = Solution();
 
-    for (vector<int> nums1 = {1, 2, 1}; const auto e: s.nextGreaterElements(nums1))
+    for (vector<int> nums1 = {1, 2, 1}; const auto e: s.nextLargerNodes(nums1))
         cout << e << ' ';
     cout << '\n';
 
-    for (vector<int> nums1 = {1, 2, 3, 4, 3}; const auto e: s.nextGreaterElements(nums1))
+    for (vector<int> nums1 = {1, 2, 3, 4, 3}; const auto e: s.nextLargerNodes(nums1))
         cout << e << ' ';
     cout << '\n';
 }
