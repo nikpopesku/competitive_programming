4bb302dddd89c444a09244b02273dc10adae2141
Nikolai Popesku <nic1k@yandex.com>
2026-05-07 12:06:52 +0800

    leetcode/c++/500-599/503_next_greater_elem2.cpp

--- leetcode/c++/500-599/503_next_greater_elem2.cpp
+++ leetcode/c++/500-599/503_next_greater_elem2.cpp
@@ -9,17 +9,32 @@ using namespace std;
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int> &nums) {
+        int n = static_cast<int>(nums.size());
+        vector<int> response(n, -1);
+        stack<int> st;
+
+        for (int i = 0; i < 2 * n; ++i) {
+            int j = i % n;
+            while (!st.empty() && nums[st.top()] < nums[j]) {
+                const int index = st.top();
+                st.pop();
+                response[index] = nums[j];
+            }
+            st.push(j);
+        }
+
+        return response;
     }
 };
 
 int main() {
     auto s = Solution();
 
-    for (vector<int> nums1 = {1, 2, 1}; const auto e: s.nextGreaterElement(nums1))
+    for (vector<int> nums1 = {1, 2, 1}; const auto e: s.nextGreaterElements(nums1))
         cout << e << ' ';
     cout << '\n';
 
-    for (vector<int> nums1 = {1, 2, 3, 4, 3}; const auto e: s.nextGreaterElement(nums1))
+    for (vector<int> nums1 = {1, 2, 3, 4, 3}; const auto e: s.nextGreaterElements(nums1))
         cout << e << ' ';
     cout << '\n';
 }
