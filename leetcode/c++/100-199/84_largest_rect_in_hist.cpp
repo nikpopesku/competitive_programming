30e3b2f542a9aaa104cbe8dd0c56839fd16fe443
Nikolai Popesku <nic1k@yandex.com>
2026-05-08 12:26:06 +0800

    leetcode/c++/100-199/84_largest_rect_in_hist.cpp

---
+++ leetcode/c++/100-199/84_largest_rect_in_hist.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int> &heights) {
+        int max_area = 0;
+        int n = static_cast<int>(heights.size());
+        stack<int> st;
+
+        for (int i = 0; i <= n; ++i) {
+            int h = i < n ? heights[i] : 0;
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i + 1: i - st.top() - 1;
+                max_area = max(max_area, height * width);
+            }
+        }
+
+        return max_area;
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+
+    vector nums = {9, 3, 15, 20, 7};
+    cout << s.largestRectangleArea(nums) << endl;
+
+    vector nums2 = {2, 4};
+    cout << s.largestRectangleArea(nums2) << endl;
+}
