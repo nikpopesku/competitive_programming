500b3a74b22c58fbee1e5bb94062a4a933f0ece5
Nikolai Popesku <nic1k@yandex.com>
2026-05-22 12:45:36 +0800

    cses.fi/1100-1199/1147_max_building1.cpp

---
+++ cses.fi/1100-1199/1147_max_building1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+        }
+    }
+}
