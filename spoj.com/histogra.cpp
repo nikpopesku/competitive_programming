dbadbfe47fbcec59828039a71ee7b904013c29c2
Nikolai Popesku <nic1k@yandex.com>
2026-05-23 11:42:25 +0800

    spoj.com/histogra.cpp

--- spoj.com/histogra.cpp
+++ spoj.com/histogra.cpp
@@ -24,8 +24,12 @@ int main() {
         }
 
         for (int i = 0; i <= n; ++i) {
-            while (!st.empty() && rectangles[st.top()] >= rectangles[i]) {
-
+            int h = i == n ? 0 : rectangles[i];
+            while (!st.empty() && rectangles[st.top()] >= h) {
+                const int height = rectangles[i];
+                st.pop();
+                const int width = st.empty() ? i : i - st.top() - 1;
+                max_area = max(max_area, width * height);
             }
 
             st.push(i);
