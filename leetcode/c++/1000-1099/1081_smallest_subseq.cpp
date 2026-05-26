2e95b0eb4859a5d9ff982e6c63668f02960bc51e
Nikolai Popesku <nic1k@yandex.com>
2026-05-26 12:25:48 +0800

    leetcode/c++/1000-1099/1081_smallest_subseq.cpp

--- leetcode/c++/1000-1099/1081_smallest_subseq.cpp
+++ leetcode/c++/1000-1099/1081_smallest_subseq.cpp
@@ -8,29 +8,28 @@ using namespace std;
 
 class Solution {
 public:
-    string removeDuplicateLetters(string s) {
+    string smallestSubsequence(string s) {
         vector<char> v;
-        unordered_map<char, int> lastOccurrence;
+        unordered_map<char, int> last_occurrence;
         set<char> st;
-        const size_t n = s.size();
+        int n = static_cast<int>(s.size());
 
         for (int i = 0; i < n; ++i) {
-            lastOccurrence[s[i]] = i;
+            last_occurrence[s[i]] = i;
         }
 
         for (int i = 0; i < n; ++i) {
-            if (st.contains(s[i])) {
-                continue;
-            }
+            if (st.contains(s[i])) continue;
 
-            while (!v.empty() && s[i] < v.back() && lastOccurrence[v.back()] > i) {
+            while (!v.empty() && v.back() > s[i] && last_occurrence[s[i]] > i) {
                 st.erase(v.back());
                 v.pop_back();
             }
 
-            v.push_back(s[i]);
             st.insert(s[i]);
+            v.push_back(s[i]);
         }
+
 
         string ss(v.begin(), v.end());
 
@@ -41,8 +40,8 @@ public:
 int main() {
     Solution s;
 
-    cout << s.removeDuplicateLetters("bcabc") << endl; //abc
-    cout << s.removeDuplicateLetters("cbacdcbc") << endl; //acdb
+    cout << s.smallestSubsequence("bcabc") << endl; //abc
+    cout << s.smallestSubsequence("cbacdcbc") << endl; //acdb
 
     return 0;
 }
