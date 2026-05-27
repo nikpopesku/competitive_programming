93bebddfc70eba086678cab391d9ec4de8561531
Nikolai Popesku <nic1k@yandex.com>
2026-05-27 15:43:30 +0800

    codeforces/cpp/700-799/797C_minimal_string.cpp

--- codeforces/cpp/700-799/797C_minimal_string.cpp
+++ codeforces/cpp/700-799/797C_minimal_string.cpp
@@ -3,20 +3,21 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     string s;
     cin >> s;
+    const int n = static_cast<int>(s.size());
 
     string t, u;
-    const int n = static_cast<int>(s.size());
 
     for (int i = 0; i < n; ++i) {
         while (!t.empty() && t.back() <= s[i]) {
             u += t.back();
         }
+
+        t += s[i];
     }
 
     cout << u;
