22ac25a87a9d3912a21e9106ef7010597516bd97
Nikolai Popesku <nic1k@yandex.com>
2026-05-27 12:29:34 +0800

    codeforces/cpp/700-799/797C_minimal_string.cpp

---
+++ codeforces/cpp/700-799/797C_minimal_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    string u;
+    cin >> u;
+    string s, t;
+
+    cout << t;
+}
