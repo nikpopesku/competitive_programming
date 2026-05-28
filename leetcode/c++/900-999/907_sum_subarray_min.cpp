97f5c9a5f929d2500079a4053e086e7179e2b8ac
Nikolai Popesku <nic1k@yandex.com>
2026-05-28 12:25:19 +0800

    leetcode/c++/900-999/907_sum_subarray_min.cpp

--- leetcode/c++/900-999/907_sum_subarray_min.cpp
+++ leetcode/c++/900-999/907_sum_subarray_min.cpp
@@ -7,10 +7,16 @@ class Solution {
 public:
     int sumSubarrayMins(const vector<int> &arr) {
         const int n = static_cast<int>(arr.size());
-        vector<int> mn(n + 1, 0);
+        vector<int> mn_right(n + 1, 0);
+        vector<int> mn_left(n, 0);
+        mn_left[0] = arr[0];
 
         for (int i = n - 1; i >= 0; --i) {
-            mn[i] = min(mn[i + 1], arr[i]);
+            mn_right[i] = min(mn_right[i + 1], arr[i]);
+        }
+
+        for (int i = 1; i < n; ++i) {
+            mn_left[i] = min(mn_left[i], arr[i]);
         }
     }
 };
