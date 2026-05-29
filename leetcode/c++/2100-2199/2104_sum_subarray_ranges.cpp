e8e555604fa49bf32709c4f9140d637c58ed3707
Nikolai Popesku <nic1k@yandex.com>
2026-05-29 12:20:00 +0800

    leetcode/c++/2100-2199/2104_sum_subarray_ranges.cpp

---
+++ leetcode/c++/2100-2199/2104_sum_subarray_ranges.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ranges>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int> &nums) {
+    }
+};
+
+int main() {
+    Solution s;
+
+    vector<vector<int> > nums = {1, 2, 3};
+    cout << s.subArrayRanges(nums) << '\n';
+
+    return 0;
+}
