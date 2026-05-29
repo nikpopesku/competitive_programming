7402368b4df492826a4413c4e907a6b900ce0e19
Nikolai Popesku <nic1k@yandex.com>
2026-05-29 15:45:20 +0800

    leetcode/c++/2100-2199/2104_sum_subarray_ranges.cpp

--- leetcode/c++/2100-2199/2104_sum_subarray_ranges.cpp
+++ leetcode/c++/2100-2199/2104_sum_subarray_ranges.cpp
@@ -32,7 +32,7 @@ public:
                 const int index = st_max.top();
                 st_max.pop();
                 max_right[index] = i - index;
-                max_left[index] = st_max.empty() ? index : index - st_max.top();
+                max_left[index] = st_max.empty() ? index + 1 : index - st_max.top();
             }
 
             st_min.push(i);
@@ -50,15 +50,14 @@ public:
             const int index = st_max.top();
             st_max.pop();
             max_right[index] = n - index;
-            max_left[index] = st_max.empty() ? index : index - st_max.top();
+            max_left[index] = st_max.empty() ? index + 1 : index - st_max.top();
         }
 
 
         ll response = 0;
 
         for (int i = 0; i < n; ++i) {
-            // response = response + static_cast<ll>(nums[i]) * (max_left[i] * max_right[i] - min_left[i] * min_right[i]);
-            response = response + static_cast<ll>(nums[i]) * min_left[i] * min_right[i];
+            response = response + static_cast<ll>(nums[i]) * (max_left[i] * max_right[i] - min_left[i] * min_right[i]);
         }
 
         return response;
@@ -68,16 +67,11 @@ public:
 int main() {
     Solution s;
 
-    // vector<int> nums = {1, 2, 3};
-    // cout << s.subArrayRanges(nums) << '\n';
-    //
-    // vector<int> nums2 = {4, -2, -3, 4, 1};
-    // cout << s.subArrayRanges(nums2) << '\n';
+    vector<int> nums = {1, 2, 3};
+    cout << s.subArrayRanges(nums) << '\n';
 
-    vector<int> arr1 = {3, 1, 2, 4};
-    cout << s.subArrayRanges(arr1) << endl; // Expected: 17
+    vector<int> nums2 = {4, -2, -3, 4, 1};
+    cout << s.subArrayRanges(nums2) << '\n';
 
-    vector<int> arr2 = {11, 81, 94, 43, 3};
-    cout << s.subArrayRanges(arr2) << endl; // Expected: 444
     return 0;
 }
