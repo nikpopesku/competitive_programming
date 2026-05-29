def2416fa627ad7fe08f427a991a964fbd307c2e
Nikolai Popesku <nic1k@yandex.com>
2026-05-29 12:29:21 +0800

    leetcode/c++/900-999/907_sum_subarray_min.cpp

--- leetcode/c++/900-999/907_sum_subarray_min.cpp
+++ leetcode/c++/900-999/907_sum_subarray_min.cpp
@@ -4,39 +4,19 @@
 
 using namespace std;
 
-constexpr int MODULO = 1e9 + 7;
+const
 
 class Solution {
 public:
-    int sumSubarrayMins(const vector<int> &arr) {
-        const int n = static_cast<int>(arr.size());
-        stack<int> st;
-        vector left_count(n, 1);
-        vector right_count(n, 1);
-
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-                const int index = st.top();
-                st.pop();
-
-                right_count[index] = i - index;
-                left_count[index] = st.empty() ? index + 1 : index - st.top();
-            }
-            st.push(i);
-        }
-
-        while (!st.empty()) {
-            const int index = st.top();
-            st.pop();
-
-            right_count[index] = n - index;
-            left_count[index] = st.empty() ? index + 1 : index - st.top();
-        }
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = static_cast<int>(arr.size());
+        vector<int> left_count(n, 1);
+        vector<int> right_count(n, 1);
 
         long long response = 0;
 
         for (int i = 0; i < n; ++i) {
-            response = (response + (static_cast<long long>(arr[i]) * left_count[i] % MODULO) * right_count[i] % MODULO) % MODULO;
+            response = response + (static_cast<long long>(arr[i]) * left_count[i] * right_count[i]
         }
 
         return static_cast<int>(response);
