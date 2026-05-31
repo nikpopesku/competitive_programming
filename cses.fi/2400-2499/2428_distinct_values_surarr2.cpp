0683bfc672d5f4bd826a0c4da56312b7fc117769
Nikolai Popesku <nic1k@yandex.com>
2026-05-31 13:06:48 +0800

    cses.fi/2400-2499/2428_distinct_values_surarr2.cpp

--- cses.fi/2400-2499/2428_distinct_values_surarr2.cpp
+++ cses.fi/2400-2499/2428_distinct_values_surarr2.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-constexpr int VALUES = 1e9;
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -24,18 +22,26 @@ int main() {
     ranges::sort(sorted_x);
     sorted_x.erase(ranges::unique(sorted_x).begin(), sorted_x.end());
     const int sz = static_cast<int>(sorted_x.size());
-    vector val(sz, false);
+    vector val(sz, 0);
+    int current_total = 0;
 
     while (r < n) {
         const int sorted_pos_r = static_cast<int>(ranges::lower_bound(sorted_x, x[r]) - sorted_x.begin());
 
-        while (val[sorted_pos_r] || r - l + 1 > k) {
+        while (val[sorted_pos_r] > 0 || current_total > k) {
             ++l;
             const int sorted_pos_l = static_cast<int>(ranges::lower_bound(sorted_x, x[l - 1]) - sorted_x.begin());
-            val[sorted_pos_l] = false;
+            --val[sorted_pos_l];
+            if (val[sorted_pos_l] == 0) {
+                --current_total;
+            }
         }
 
-        val[sorted_pos_r] = true;
+        ++val[sorted_pos_r];
+        if (val[sorted_pos_r] == 1) {
+            ++current_total;
+        }
+
         response = response + r - l + 1;
 
         r += 1;
