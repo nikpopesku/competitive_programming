6dd912c14f521879e2169613ceee2e4681153cde
Nikolai Popesku <nic1k@yandex.com>
2026-05-31 12:20:43 +0800

    cses.fi/2400-2499/2428_distinct_values_surarr2.cpp

--- cses.fi/2400-2499/2428_distinct_values_surarr2.cpp
+++ cses.fi/2400-2499/2428_distinct_values_surarr2.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+constexpr int VALUES = 1e9;
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -15,4 +17,22 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> x[i];
     }
+
+    int l = 0, r = 0;
+    int response = 0;
+    bool val[VALUES] = {false};
+
+    while (r < n) {
+        while (val[x[r]]) {
+            ++l;
+            val[x[l-1]] = false;
+        }
+
+        val[x[r]] = true;
+        response += r - l + 1;
+
+        r += 1;
+    }
+
+    return response;
 }
