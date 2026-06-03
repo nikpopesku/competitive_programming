43248ceec424afe1b2a2a4cba38a8f3253acfcd6
Nikolai Popesku <nic1k@yandex.com>
2026-06-03 12:09:25 +0800

    leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp

--- leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp
+++ leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp
@@ -39,4 +39,7 @@ int main() {
 
     vector<int> arr = {6, 2, 4};
     cout << s.mctFromLeafValues(arr) << endl; // 32
+
+    vector<int> arr2 = {4, 11};
+    cout << s.mctFromLeafValues(arr2) << endl; // 44
 }
