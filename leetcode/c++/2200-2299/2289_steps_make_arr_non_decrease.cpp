17b6e37f4eb145bfc420cd3c73f249052d9d8df0
Nikolai Popesku <nic1k@yandex.com>
2026-06-03 12:18:14 +0800

    leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp

--- leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp
+++ leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 class Solution {
 public:
-    int totalSteps(vector<int>& nums) {
-
+    int totalSteps(vector<int> &nums) {
     }
 };
 
@@ -15,9 +14,9 @@ public:
 int main() {
     auto s = Solution();
 
-    vector<int> arr = {6, 2, 4};
-    cout << s.mctFromLeafValues(arr) << endl; // 32
+    vector<int> arr = {5, 3, 4, 4, 7, 3, 6, 11, 8, 5, 11};
+    cout << s.totalSteps(arr) << endl; // 3
 
-    vector<int> arr2 = {4, 11};
-    cout << s.mctFromLeafValues(arr2) << endl; // 44
+    vector<int> arr2 = {4, 5, 7, 7, 13};
+    cout << s.mctFromLeafValues(arr2) << endl; // 0
 }
