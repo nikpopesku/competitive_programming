609843fd3eb85bc64cb8c14a20dc2588604ea7de
Nikolai Popesku <nic1k@yandex.com>
2026-06-03 12:07:31 +0800

    leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp

--- leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp
+++ leetcode/c++/1100-1199/1130_min_cost_tree_leaf_values.cpp
@@ -22,6 +22,13 @@ public:
             st.push(i);
         }
 
+        while (st.empty()) {
+            const int index = st.top();
+            st.pop();
+            const int product = !st.empty() ? arr[index] * arr[st.top()] : 0;
+            response += product;
+        }
+
         return response;
     }
 };
