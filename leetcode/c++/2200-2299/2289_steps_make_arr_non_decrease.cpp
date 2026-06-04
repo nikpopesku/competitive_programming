ecd65d0ba008837dd8add5f18cdb1bfa3243d3df
Nikolai Popesku <nic1k@yandex.com>
2026-06-04 12:21:59 +0800

    leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp

--- leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp
+++ leetcode/c++/2200-2299/2289_steps_make_arr_non_decrease.cpp
@@ -7,6 +7,29 @@ using namespace std;
 class Solution {
 public:
     int totalSteps(vector<int> &nums) {
+        stack<int> st;
+        int n = static_cast<int>(nums.size());
+        int response = 0;
+        bool flag = true;
+
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < n; ++i) {
+                while (!st.empty() && nums[st.top()] < nums[i]) {
+                    if (!flag) {
+                        ++response;
+                        flag = true;
+                    }
+                    int index = st.top();
+                    st.pop();
+
+                }
+
+                st.push(i);
+            }
+        }
+
+        return response;
     }
 };
 
