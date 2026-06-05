1dc9816b60d4137260f1fea861f50bf3479e928f
Nikolai Popesku <nic1k@yandex.com>
2026-06-05 15:29:53 +0800

    leetcode/c++/000-099/49_group_anagrams.cpp

--- leetcode/c++/000-099/49_group_anagrams.cpp
+++ leetcode/c++/000-099/49_group_anagrams.cpp
@@ -16,20 +16,17 @@ public:
 int main() {
     auto s = Solution();
 
-    vector<string> st = {"eat","tea","tan","ate","nat","bat"};
-    for (auto &e: s.groupAnagrams(st)) {
+    for (vector<string> st = {"eat","tea","tan","ate","nat","bat"}; auto &e: s.groupAnagrams(st)) {
         for (auto &x: e) cout << x << ' ';
     }
     cout << '\n';
 
-    vector<string> st2 = {""};
-    for (auto &e: s.groupAnagrams(st2)) {
+    for (vector<string> st2 = {""}; auto &e: s.groupAnagrams(st2)) {
         for (auto &x: e) cout << x << ' ';
     }
     cout << '\n';
 
-    vector<string> st3 = {""};
-    for (auto &e: s.groupAnagrams(st3)) {
+    for (vector<string> st3 = {""}; auto &e: s.groupAnagrams(st3)) {
         for (auto &x: e) cout << x << ' ';
     }
     cout << '\n';
