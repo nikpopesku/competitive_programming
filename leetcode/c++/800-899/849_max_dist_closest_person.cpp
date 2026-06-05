c52cb8b260bd7735867a66aec54fcba10b7e9e22
Nikolai Popesku <nic1k@yandex.com>
2026-06-05 14:37:27 +0800

    leetcode/c++/800-899/849_max_dist_closest_person.cpp

--- leetcode/c++/800-899/849_max_dist_closest_person.cpp
+++ leetcode/c++/800-899/849_max_dist_closest_person.cpp
@@ -8,6 +8,30 @@ using namespace std;
 class Solution {
 public:
     int maxDistToClosest(vector<int> &seats) {
+        int n = static_cast<int>(seats.size());
+        int distance = 0;
+        vector closest(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (seats[i] == 1) {
+                distance = 0;
+            } else {
+                closest[i] = ++distance;
+            }
+        }
+
+        int max_distance = 0;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (seats[i] == 1) {
+                distance = 0;
+            } else {
+                closest[i] = min(closest[i], ++distance);
+                max_distance = max(max_distance, closest[i]);
+            }
+        }
+
+        return max_distance;
     }
 };
 
@@ -19,4 +43,7 @@ int main() {
 
     vector<int> seats2 = {1, 0, 0, 0};
     cout << s.maxDistToClosest(seats2) << endl; //2
+
+    vector<int> seats3 = {0, 1};
+    cout << s.maxDistToClosest(seats3) << endl; //2
 }
