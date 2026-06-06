22c97a1f29e01bf4f40f5f5a0085d82a6e4c2312
Nikolai Popesku <nic1k@yandex.com>
2026-06-06 12:57:08 +0800

    leetcode/c++/000-099/3_longet_subs_without_repeat.cpp

--- leetcode/c++/000-099/3_longet_subs_without_repeat.cpp
+++ leetcode/c++/000-099/3_longet_subs_without_repeat.cpp
@@ -8,20 +8,20 @@ class Solution {
 public:
     int lengthOfLongestSubstring(const string &s) {
         if (s.empty()) return 0;
-        vector v(26, false);
+        vector v(256, false);
 
         const int n = static_cast<int>(s.size());
         int left = 0, right = 0;
         int max_length = 0;
 
         while (right < n) {
-            while (v[s[right] - 'a']) {
+            while (v[s[right]]) {
                 ++left;
-                v[s[left - 1] - 'a'] = false;
+                v[s[left - 1]] = false;
             }
 
             max_length = max(max_length, right - left + 1);
-            v[s[right] - 'a'] = true;
+            v[s[right]] = true;
             ++right;
         }
 
