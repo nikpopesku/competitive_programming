bd05e9698f841715f07b62fedba0d7e6ce4069f7
Nikolai Popesku <nic1k@yandex.com>
2026-06-07 11:53:58 +0800

    leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair

--- leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair.cpp
+++ leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <bitset>
 
 using namespace std;
 
 class Solution {
 public:
-    bool consecutiveSetBits(int n) {
+    bool consecutiveSetBits(const int n) {
+        const unsigned int n1 = static_cast<unsigned int>(n);
+        unsigned int m = ~n1;
+        const int bit_length = bit_width(n1);
+        int counter = -1;
 
+        while (m > 0) {
+            ++counter;
+            if (m & 1) {
+                break;
+            }
+            m = m >> 1;
+        }
+
+        return counter == 0 || counter == bit_length - 1;
     }
 };
 
