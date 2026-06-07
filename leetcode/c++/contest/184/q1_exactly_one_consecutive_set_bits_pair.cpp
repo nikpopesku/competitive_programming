d491439824b619a891d9a997c203af9c2355772b
Nikolai Popesku <nic1k@yandex.com>
2026-06-07 11:32:55 +0800

    leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair

---
+++ leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool consecutiveSetBits(int n) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << boolalpha << s.consecutiveSetBits(6) << endl; //true
+    cout << boolalpha << s.consecutiveSetBits(5) << endl; //false
+}
