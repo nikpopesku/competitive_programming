39869061de32fd0884527b4de2fc0c58908fb77c
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 12:16:25 +0800

    leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp

--- leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp
+++ leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp
@@ -11,12 +11,12 @@ public:
         vector conver(n, 0);
         conver[0] = 2 * arr[0];
         vector<long long> prefix_response(n);
-        prefix_response = conver[0];
+        prefix_response[0] = conver[0];
 
         for (int i = 1; i < n; ++i) {
             max_val[i] = max(max_val[i - 1], arr[i]);
             conver[i] = arr[i] + max_val[i];
-            prefix_response[i] = prefix_response[i-1] + conver[i];
+            prefix_response[i] = prefix_response[i - 1] + conver[i];
         }
 
         return prefix_response;
@@ -28,10 +28,12 @@ int main() {
 
     // [4,10,24,36,56]
     for (vector<int> nums{2, 3, 7, 5, 10}; const auto &e: sol.findPrefixScore(nums)) {
-        cout << e << "\n";
+        cout << e << ' ';
     }
+    cout << '\n';
+
     // [2,4,8,16,32,64]
     for (vector<int> nums{1, 1, 2, 4, 8, 16}; const auto &e: sol.findPrefixScore(nums)) {
-        cout << e << "\n";
+        cout << e << ' ';
     }
 }
