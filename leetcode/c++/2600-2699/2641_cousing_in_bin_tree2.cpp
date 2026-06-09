0d588898d8bfe5918d460e448d6bda2fc86e65ed
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 12:19:52 +0800

    leetcode/c++/2600-2699/2641_cousing_in_bin_tree2.cpp

---
+++ leetcode/c++/2600-2699/2641_cousing_in_bin_tree2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+
+    }
+};
+
+int main() {
+    auto sol = Solution();
+
+    // [4,10,24,36,56]
+    for (vector<int> nums{2, 3, 7, 5, 10}; const auto &e: sol.findPrefixScore(nums)) {
+        cout << e << ' ';
+    }
+    cout << '\n';
+
+    // [2,4,8,16,32,64]
+    for (vector<int> nums{1, 1, 2, 4, 8, 16}; const auto &e: sol.findPrefixScore(nums)) {
+        cout << e << ' ';
+    }
+}
