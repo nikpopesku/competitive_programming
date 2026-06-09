022262fe951ca2176d1dcb1d21a8b6b6a4a4c135
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 12:28:57 +0800

    leetcode/c++/2600-2699/2641_cousing_in_bin_tree2.cpp

--- leetcode/c++/2600-2699/2641_cousing_in_bin_tree2.cpp
+++ leetcode/c++/2600-2699/2641_cousing_in_bin_tree2.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <stack>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
+    }
+};
+
 class Solution {
 public:
-    TreeNode* replaceValueInTree(TreeNode* root) {
+    TreeNode *replaceValueInTree(TreeNode *root) {
+        stack<TreeNode*> st;
+        st.push(root);
+        unordered_map<int, unordered_map<TreeNode, int> > mp;
 
+        while (!st.empty()) {
+            auto node = st.top();
+            st.pop();
+        }
     }
 };
 
