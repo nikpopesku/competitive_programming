d8ef59807ad7af7c2f4a673ed324a9f69a3eeb5a
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 12:02:59 +0800

    leetcode/c++/2600-2699/2639_find_width_columns_grid.cpp

---
+++ leetcode/c++/2600-2699/2639_find_width_columns_grid.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findColumnWidth(vector<vector<int>>& grid) {
+        const int n = static_cast<int>(grid.size());
+        const int m = static_cast<int>(grid[0].size());
+        vector<int> column_width(m, 0);
+
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                int val = abs(grid[i][j]);
+                int width = val != 0 ? 0 : 1;
+
+                while (val) {
+                    ++width;
+                    val /= 10;
+                }
+
+                if (grid[i][j] < 0) ++width;
+
+                column_width[j] = max(column_width[j], width);
+            }
+
+
+        }
+
+        return column_width;
+    }
+};
