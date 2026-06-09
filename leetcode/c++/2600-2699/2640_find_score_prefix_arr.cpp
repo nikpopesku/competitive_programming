c665248d762677f97a446912d2f8e3eaa73ded1e
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 12:18:01 +0800

    leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp

--- leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp
+++ leetcode/c++/2600-2699/2640_find_score_prefix_arr.cpp
@@ -7,15 +7,15 @@ class Solution {
 public:
     vector<long long> findPrefixScore(vector<int> &arr) {
         const int n = static_cast<int>(arr.size());
-        vector<int> max_val = arr;
+        int max_val = arr[0];
         vector conver(n, 0);
         conver[0] = 2 * arr[0];
         vector<long long> prefix_response(n);
         prefix_response[0] = conver[0];
 
         for (int i = 1; i < n; ++i) {
-            max_val[i] = max(max_val[i - 1], arr[i]);
-            conver[i] = arr[i] + max_val[i];
+            max_val = max(max_val, arr[i]);
+            conver[i] = arr[i] + max_val;
             prefix_response[i] = prefix_response[i - 1] + conver[i];
         }
 
