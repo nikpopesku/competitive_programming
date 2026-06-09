9f444a790240e09afdf483f1c073c3e0260ae47d
Nikolai Popesku <nic1k@yandex.com>
2026-06-09 11:59:55 +0800

    leetcode/c++/2600-2699/2639_find_width_columns_grid.cpp

--- leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair.cpp
+++ leetcode/c++/contest/184/q1_exactly_one_consecutive_set_bits_pair.cpp
@@ -8,7 +8,7 @@ public:
     vector<int> findColumnWidth(vector<vector<int>>& grid) {
         const int n = static_cast<int>(grid.size());
         const int m = static_cast<int>(grid[0].size());
-        vector<int> column_width(n, 0);
+        vector<int> column_width(m, 0);
 
 
         for (int i = 0; i < n; ++i) {
