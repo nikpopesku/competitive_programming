3398c64f8eac5878cd370c4fbf4a2e4522313766
Nikolai Popesku <nic1k@yandex.com>
2026-06-10 13:41:34 +0800

    leetcode/c++/quest/math/prime_numbers/q3_closest_prime.cpp

---
+++ leetcode/c++/quest/math/geometry/q1_proj_area.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& grid) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << s.countPrimes(10) << '\n'; //4
+    cout << s.countPrimes(0) << '\n'; //0
+    cout << s.countPrimes(1) << '\n'; //0
+    cout << s.countPrimes(2) << '\n'; //0
+}
