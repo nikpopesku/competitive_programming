e2392a9d5a2535f98578e674fd22cc5461843a24
Nikolai Popesku <nic1k@yandex.com>
2026-06-10 14:21:36 +0800

    leetcode/c++/quest/math/geometry/q1_proj_area.cppgpp

--- leetcode/c++/quest/math/geometry/q1_proj_area.cpp
+++ leetcode/c++/quest/math/geometry/q1_proj_area.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 class Solution {
 public:
-    int projectionArea(vector<vector<int>>& grid) {
-
+    int projectionArea(vector<vector<int> > &grid) {
     }
 };
 
 int main() {
     auto s = Solution();
 
-    cout << s.countPrimes(10) << '\n'; //4
-    cout << s.countPrimes(0) << '\n'; //0
-    cout << s.countPrimes(1) << '\n'; //0
-    cout << s.countPrimes(2) << '\n'; //0
+    vector<vector<int> > grid = {{1, 2}, {3, 4}};
+    cout << s.projectionArea(grid) << '\n'; //17
+
+    vector<vector<int> > grid2 = {{2}};
+    cout << s.projectionArea(grid2) << '\n'; //5
 }
