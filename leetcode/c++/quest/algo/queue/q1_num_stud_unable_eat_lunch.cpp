5446e3bb2beaf73ab731a78c9c5efd65763fe4be
Nikolai Popesku <nic1k@yandex.com>
2026-06-16 12:06:53 +0800

    leetcode/c++/quest/algo/queue/q1_num_stud_unable_eat_lunch.cpp

--- leetcode/c++/quest/algo/queue/q1_num_stud_unable_eat_lunch.cpp
+++ leetcode/c++/quest/algo/queue/q1_num_stud_unable_eat_lunch.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -6,6 +8,19 @@ using namespace std;
 class Solution {
 public:
     int countStudents(vector<int> &students, vector<int> &sandwiches) {
+        stack<int> sand;
+        queue<int> q;
+        int count_students_left = static_cast<int>(students.size());
+
+        for (int i = static_cast<int>(sandwiches.size() - 1); i >= 0; --i) sand.push(sandwiches[i]);
+        for (int i = 0; i < count_students_left; ++i) q.push(students[i]);
+
+
+        while (!q.empty()) {
+
+        }
+
+        return count_students_left;
     }
 };
 
