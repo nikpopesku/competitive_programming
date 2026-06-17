a8ce828ee3b6f68d434d09bc30b7ed68cf5481a1
Nikolai Popesku <nic1k@yandex.com>
2026-06-17 14:29:59 +0800

    leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp

--- leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp
+++ leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp
@@ -6,16 +6,23 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-
+    vector<vector<int> > kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k) {
     }
 };
 
 int main() {
     auto s = Solution();
-    vector<int> stones = {2, 7, 4, 1, 8, 1};
-    cout << s.lastStoneWeight(stones) << '\n';
+    vector<int> nums1 = {1, 7, 11};
+    vector<int> nums2 = {2, 4, 6};
+    for (auto &x: s.kSmallestPairs(nums1, nums2, 3)) {
+        for (auto &y: x) cout << y << ' ';
+    }
+    cout << '\n';
 
-    vector<int> stones2 = {1};
-    cout << s.lastStoneWeight(stones2) << '\n';
+    vector<int> nums12 = {1, 1, 2};
+    vector<int> nums22 = {1, 2, 3};
+    for (auto &x: s.kSmallestPairs(nums12, nums22, 2)) {
+        for (auto &y: x) cout << y << ' ';
+    }
+    cout << '\n';
 }
