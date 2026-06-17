2c10c6b1cf2a94b53d582f557c08d1bfaf450475
Nikolai Popesku <nic1k@yandex.com>
2026-06-17 14:34:13 +0800

    leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp

--- leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp
+++ leetcode/c++/quest/algo/heap/q2_find_k_pairs_with_smallest_sum.cpp
@@ -6,7 +6,21 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int> > kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k) {
+    vector<vector<int> > kSmallestPairs(const vector<int> &nums1, const vector<int> &nums2, int k) {
+        priority_queue<pair<int, vector<int> > > pq;
+
+        for (const auto &n1: nums1) {
+            for (const auto &n2: nums2) {
+                pq.push({-n1 - n2, vector{n1, n2}});
+            }
+        }
+
+        vector<vector<int> > pairs_with_smallest_sum(k);
+
+        for (int i = 0; i < k; ++i) {
+            pairs_with_smallest_sum[k] = pq.top().second;
+            pq.pop();
+        }
     }
 };
 
@@ -15,14 +29,14 @@ int main() {
     vector<int> nums1 = {1, 7, 11};
     vector<int> nums2 = {2, 4, 6};
     for (auto &x: s.kSmallestPairs(nums1, nums2, 3)) {
-        for (auto &y: x) cout << y << ' ';
+        for (const auto &y: x) cout << y << ' ';
     }
     cout << '\n';
 
     vector<int> nums12 = {1, 1, 2};
     vector<int> nums22 = {1, 2, 3};
     for (auto &x: s.kSmallestPairs(nums12, nums22, 2)) {
-        for (auto &y: x) cout << y << ' ';
+        for (const auto &y: x) cout << y << ' ';
     }
     cout << '\n';
 }
