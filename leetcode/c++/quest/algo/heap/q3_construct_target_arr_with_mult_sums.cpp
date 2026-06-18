5981f4f92b803e88d8e6af199c40f4f56f445231
Nikolai Popesku <nic1k@yandex.com>
2026-06-18 12:17:58 +0800

    leetcode/c++/quest/algo/heap/q3_construct_target_arr_with_mult_sums.cpp

--- leetcode/c++/quest/algo/heap/q3_construct_target_arr_with_mult_sums.cpp
+++ leetcode/c++/quest/algo/heap/q3_construct_target_arr_with_mult_sums.cpp
@@ -7,10 +7,35 @@ using namespace std;
 class Solution {
 public:
     bool isPossible(const vector<int> &target) {
-        priority_queue<int> pq;
-        for (const auto &t: target) pq.push(-t);
+        priority_queue<pair<int, int> > pq;
+        const int n = static_cast<int>(target.size());
 
+        for (int i = 0; i < n; ++i) {
+            pq.emplace(-target[i], i);
+        }
 
+        vector<int> arr(n, 1);
+        int current_sum = n;
+
+        while (!pq.empty()) {
+            int elem = pq.top().first;
+            int i = pq.top().second;
+            pq.pop();
+
+            if (current_sum > elem) {
+                return false;
+            }
+
+            if (current_sum <= elem) {
+                const int temp = arr[i];
+                arr[i] = current_sum;
+                current_sum = 2 * current_sum - temp;
+
+                if (current_sum < elem) {
+                    pq.emplace(elem, i);
+                }
+            }
+        }
     }
 };
 
