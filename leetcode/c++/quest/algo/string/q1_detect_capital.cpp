a4cb1aec9bb6be6b6759066bd12b4341b788866d
Nikolai Popesku <nic1k@yandex.com>
2026-06-24 12:19:00 +0800

    leetcode/c++/quest/algo/string/q1_detect_capital.cpp

---
+++ leetcode/c++/quest/algo/string/q1_detect_capital.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.detectCapitalUse("USA") << '\n'; //true
+    cout << s.detectCapitalUse("FlaG") << '\n'; //false
+}
