b43695312ac77ad5695e0a0e04779e9bd44b2f74
Nikolai Popesku <nic1k@yandex.com>
2026-06-24 13:03:21 +0800

    leetcode/c++/quest/algo/string/q1_detect_capital.cpp

--- leetcode/c++/quest/algo/string/q2_license_key_formatting.cpp
+++ leetcode/c++/quest/algo/string/q2_license_key_formatting.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
+        const int n = static_cast<int>(s.size());
+        string current_group;
+        string formatted_key;
 
+        for (int i = n; i >= 0; --i) {
+            if (s[i] == '-') continue;
+            if (current_group.size() == k) {
+                formatted_key = current_group + (formatted_key.size() > 0 ? "-" : "");
+                current_group = "";
+            }
+
+            current_group = to_string(toupper(s[i])) + current_group;
+        }
+
+        formatted_key = current_group + (formatted_key.size() > 0 ? "-" : "");
+
+        return formatted_key;
     }
 };
 
