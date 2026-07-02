e3c5ac07a57ceaabf2154019f1883de92bbe4179
Nikolai Popesku <nic1k@yandex.com>
2026-07-02 13:04:32 +0800

    leetcode/c++/quest/algo/string/q3_masking_personal_info.cpp

---
+++ leetcode/c++/quest/algo/string/q3_masking_personal_info.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string maskPII(string s) {
+        if (const int at = get_at(s); at != -1) {
+            string masked_string = tolower(s[0]) + "*****" + tolower(s[at - 1]);
+            for (int i = at + 1; i < static_cast<int>(s.size()); ++i) {
+                masked_string += tolower(s[i]);
+            }
+
+            return masked_string;
+        }
+    }
+
+private:
+    int get_at(string &s) {
+        for (int i = 0; i < static_cast<int>(s.size()); ++i) {
+            if (s[i] == '@') return i;
+        }
+
+        return -1;
+    }
+};
+
+
+int main() {
+    auto s = Solution();
+    cout << s.maskPII("LeetCode@LeetCode.com") << '\n'; //"l*****e@leetcode.com"
+    cout << s.maskPII("AB@qq.com") << '\n'; //"a*****b@qq.com"
+}
