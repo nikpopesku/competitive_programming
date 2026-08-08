33e001dfbb7ce71592bdbd42e17e691e839c8f87
Nikolai Popesku <nic1k@yandex.com>
2026-08-08 13:04:42 +0800

    leetcode/c++/300-399/372_super_pow.cpp

--- leetcode/c++/300-399/372_super_pow.cpp
+++ leetcode/c++/300-399/372_super_pow.cpp
@@ -15,8 +15,10 @@ public:
 
 
         for (int i = 0; i < b.size(); ++i) {
+            bool first = true;
             for (auto val: {i != 0 ? 9 : 0, b[i]}) {
-                aa = a;
+                aa = first ? res : static_cast<ll>(a);
+                first = false;
                 while (val > 0) {
                     if (val & 1) {
                         res = res * aa % MD;
