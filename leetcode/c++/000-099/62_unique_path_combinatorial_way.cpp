c2861c2623e907d73c7411331cc94021bee28029
Nikolai Popesku <nic1k@yandex.com>
2026-08-08 19:14:31 +0800

    leetcode/c++/000-099/62_unique_path_combinatorial_way.cpp

--- leetcode/c++/000-099/62_unique_path_combinatorial_way.cpp
+++ leetcode/c++/000-099/62_unique_path_combinatorial_way.cpp
@@ -13,19 +13,9 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int response = 1;
-        set<int> st;
-        for (int i = 2; i < n; ++i) st.insert(i);
 
-        for (int i = m; i <= m + n - 2; ++i) {
-            response *= i;
-            vector<int> dividers{};
-            for (auto &elem: st) {
-                if (response % elem == 0) {
-                    response /= elem;
-                    dividers.push_back(elem);
-                }
-            }
-            for (auto &elem: dividers) st.erase(elem);
+        for (int i = 1; i < n; ++i) {
+            response = response * (i + m - 1) / i;
         }
 
         return response;
