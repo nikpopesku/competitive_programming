031cefa3a5a1360929eb8bcd23b684200689690e
Nikolai Popesku <nic1k@yandex.com>
2026-08-08 12:05:07 +0800

    leetcode/c++/300-399/372_super_pow.cpp

--- leetcode/c++/300-399/372_super_pow.cpp
+++ leetcode/c++/300-399/372_super_pow.cpp
@@ -6,19 +6,18 @@ using namespace std;
 
 class Solution {
 public:
-    int superPow(int a, vector<int>& b) {
-
+    int superPow(int a, vector<int> &b) {
     }
 };
 
 int main() {
     auto s = Solution();
-    vector<int> v {3};
+    vector<int> v{3};
     std::cout << s.superPow(2, v) << std::endl;
 
-    vector<int> v2 {1, 0};
+    vector<int> v2{1, 0};
     std::cout << s.superPow(2, v2) << std::endl;
 
-    vector<int> v3 {4,3,3,8,5,2};
+    vector<int> v3{4, 3, 3, 8, 5, 2};
     std::cout << s.superPow(1, v3) << std::endl;
 }
