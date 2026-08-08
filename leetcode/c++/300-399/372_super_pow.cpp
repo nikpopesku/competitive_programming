11d79711fb445442f26d7bb0c6ddb8c0ebf0a272
Nikolai Popesku <nic1k@yandex.com>
2026-08-08 12:28:35 +0800

    leetcode/c++/300-399/372_super_pow.cpp

--- leetcode/c++/300-399/372_super_pow.cpp
+++ leetcode/c++/300-399/372_super_pow.cpp
@@ -4,9 +4,30 @@
 
 using namespace std;
 
+constexpr int MD = 1337;
+
 class Solution {
 public:
     int superPow(int a, vector<int> &b) {
+        int res = 1;
+
+
+        for (int & i : b) {
+            for (auto val: {10, i}) {
+                while (val > 0) {
+                    if (val & 1) {
+                        res = res * val % MD;
+                        --val;
+                    } else {
+                        val >>= 1;
+                        res = res * res % MD;
+                    }
+                }
+            }
+        }
+
+
+        return res;
     }
 };
 
