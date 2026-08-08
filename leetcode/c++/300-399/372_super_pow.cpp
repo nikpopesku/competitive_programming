f39234a168742e3bc180366e9b375fd58f2853b6
Nikolai Popesku <nic1k@yandex.com>
2026-08-08 12:50:38 +0800

    leetcode/c++/300-399/372_super_pow.cpp

--- leetcode/c++/300-399/372_super_pow.cpp
+++ leetcode/c++/300-399/372_super_pow.cpp
@@ -10,17 +10,18 @@ class Solution {
 public:
     int superPow(int a, vector<int> &b) {
         int res = 1;
+        int aa = a;
 
 
         for (int i = 0; i < b.size(); ++i) {
-            for (auto val: {i != 0 ? 10 : 0, b[i]}) {
+            for (auto val: {i != 0 ? 9 : 0, b[i]}) {
                 while (val > 0) {
                     if (val & 1) {
-                        res = res * a % MD;
+                        res = res * aa;
                         --val;
                     } else {
                         val >>= 1;
-                        res = res * res % MD;
+                        aa = aa * aa;
                     }
                 }
             }
@@ -33,12 +34,12 @@ public:
 
 int main() {
     auto s = Solution();
-    vector<int> v{3};
-    std::cout << s.superPow(2, v) << std::endl;
+    // vector<int> v{3};
+    // std::cout << s.superPow(2, v) << std::endl;
 
     vector<int> v2{1, 0};
     std::cout << s.superPow(2, v2) << std::endl;
 
-    vector<int> v3{4, 3, 3, 8, 5, 2};
-    std::cout << s.superPow(1, v3) << std::endl;
+    // vector<int> v3{4, 3, 3, 8, 5, 2};
+    // std::cout << s.superPow(1, v3) << std::endl;
 }
