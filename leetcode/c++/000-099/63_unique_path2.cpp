341c097b59eaf8a24a2d0c38ccc8e8721273d4a7
Nikolai Popesku <nic1k@yandex.com>
2026-08-11 12:23:02 +0800

    leetcode/c++/000-099/63_unique_path2.cpp

---
+++ leetcode/c++/000-099/63_unique_path2.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <stack>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    cout << s.uniquePathsWithObstacles(3, 7) << '\n';
+}
