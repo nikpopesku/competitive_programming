b8584bc170b79926b807a8a16ecf42a488f9516f
Nikolai Popesku <nic1k@yandex.com>
2026-08-14 12:12:11 +0800

    leetcode/c++/000-099/77_combinations.cpp

---
+++ leetcode/c++/000-099/77_combinations.cpp
@@ -0,0 +1,40 @@
+#include <print>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int> > combine(int n, int k) {
+        vector<vector<int> > combinations;
+
+        vector<int> current_combination;
+        backtrack(1, n, k, combinations, current_combination);
+
+        return combinations;
+    }
+
+private:
+    void backtrack(int start, int n, int k, vector<vector<int> > &combinations, vector<int> &current_combination) {
+        if (current_combination.size() == k) {
+            combinations.push_back(current_combination);
+
+            return;
+        }
+
+        if (n - start + 1 < k) return;
+
+        for (int i = start; i < n; ++i) {
+            current_combination.push_back(i);
+            backtrack(start + 1, n, k, combinations, current_combination);
+            current_combination.pop_back();
+        }
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    vector<vector<int> > obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+    print("{}", s.combine(4, 2));
+}
