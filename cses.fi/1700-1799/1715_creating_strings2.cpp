443085e9f5012320c5b296ade3a9e6a4da2bc939
Nikolai Popesku <nic1k@yandex.com>
2026-08-15 12:29:27 +0800

    cses.fi/1700-1799/1715_creating_strings2.cpp

--- cses.fi/1700-1799/1715_creating_strings2.cpp
+++ cses.fi/1700-1799/1715_creating_strings2.cpp
@@ -10,7 +10,7 @@ ll pow(ll a, ll b) {
     ll response = 1;
     while (b > 0) {
         if (b % 2 == 1) {
-            response *= a % MD;
+            response = response * a % MD;
             --b;
         } else {
             a = a * a % MD;
@@ -43,7 +43,7 @@ int main() {
 
     for (const ll i : occ) {
         if (i > 1) {
-            response *= pow(fact[i], MD - 2) % MD;
+            response = response * pow(fact[i], MD - 2) % MD;
         }
     }
 
