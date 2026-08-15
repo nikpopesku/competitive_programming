daf6b07baca92e1a7b7b80ec4a4526f8ba90a0d7
Nikolai Popesku <nic1k@yandex.com>
2026-08-15 12:16:17 +0800

    cses.fi/1700-1799/1715_creating_strings2.cpp

--- cses.fi/1700-1799/1715_creating_strings2.cpp
+++ cses.fi/1700-1799/1715_creating_strings2.cpp
@@ -6,21 +6,6 @@ using namespace std;
 
 constexpr ll MD = 1e9 + 7;
 
-ll pow(ll a, ll b) {
-    ll response = 1;
-    while (b > 0) {
-        if (b % 2 == 1) {
-            response *= a % MD;
-            --b;
-        } else {
-            a = a * a % MD;
-            b >>= 1;
-        }
-    }
-
-    return response;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -29,7 +14,7 @@ int main() {
     vector occ(26, 0);
     vector<ll> fact(1e6 + 1, 1);
 
-    for (int i = 1; i < fact.size(); ++i) {
+    for (ll i = 1; i < fact.size(); ++i) {
         fact[i] = fact[i - 1] * i % MD;
     }
 
@@ -39,11 +24,11 @@ int main() {
         ++occ[i - 'a'];
     }
 
-    int response = fact[s.size()];
+    ll response = fact[s.size()];
 
-    for (const int i : occ) {
+    for (const ll i : occ) {
         if (i > 1) {
-            response *= pow(fact[i], MD - 2) % MD;
+            response /= fact[i] % MD;
         }
     }
 
