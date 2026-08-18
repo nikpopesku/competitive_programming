7c2d450bd7e387b59ee2f59c0c88ba088180371f
Nikolai Popesku <nic1k@yandex.com>
2026-08-18 11:36:39 +0800

    leetcode/c++/000-099/96_uniq_bst.cpp

---
+++ leetcode/c++/000-099/96_uniq_bst.cpp
@@ -0,0 +1,18 @@
+#include <print>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+
+    }
+};
+
+int main() {
+    auto s = Solution();
+
+    print("{}\n", s.numTrees(3)); //5
+    print("{}\n", s.numTrees(1)); //1
+}
