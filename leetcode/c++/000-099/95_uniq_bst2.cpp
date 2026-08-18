75fd389f0ceb61795df20ea40bca22ee0ec5fa51
Nikolai Popesku <nic1k@yandex.com>
2026-08-18 11:59:57 +0800

    leetcode/c++/000-099/95_uniq_bst2.cpp

--- leetcode/c++/000-099/95_uniq_bst2.cpp
+++ leetcode/c++/000-099/95_uniq_bst2.cpp
@@ -7,21 +7,39 @@ using namespace std;
 
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<TreeNode*> generateTrees(int n) {
+ **/
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {
+    }
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {
     }
 };
+
+
+class Solution {
+public:
+    vector<TreeNode *> generateTrees(int n) {
+        vector<TreeNode *> v;
+        TreeNode *cur_variant {};
+        backtrack(v, cur_variant, 0, n);
+
+        return v;
+    }
+
+private:
+    void backtrack(vector<TreeNode *> &v, TreeNode *cur_variant, int start, int n) {
+    }
+};
+
 int main() {
     auto s = Solution();
 
