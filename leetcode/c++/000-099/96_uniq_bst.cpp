d454cd84b6cf0caa001fa9e404e861cc5e5f6e15
Nikolai Popesku <nic1k@yandex.com>
2026-08-18 11:48:18 +0800

    leetcode/c++/000-099/96_uniq_bst.cpp

--- leetcode/c++/000-099/96_uniq_bst.cpp
+++ leetcode/c++/000-099/96_uniq_bst.cpp
@@ -8,13 +8,19 @@ using namespace std;
 class Solution {
 public:
     int numTrees(int n) {
-        vector<ull> fact(2 * n + 1, 1);
+        vector<ull> fact(n + 1, 1);
 
         for (ull i = 2; i < fact.size(); ++i) {
             fact[i] = i * fact[i - 1];
         }
 
-        return fact[2 * n] / (n + 1) / fact[n] / fact[n];
+        ull response = 1;
+
+        for (ull i = 1; i <= n; ++i) {
+            response = response * (i + n) / i;
+        }
+
+        return static_cast<int>(response / (n + 1));
     }
 };
 
@@ -23,5 +29,5 @@ int main() {
 
     print("{}\n", s.numTrees(3)); //5
     print("{}\n", s.numTrees(1)); //1
-    print("{}\n", s.numTrees(19)); //1
+    print("{}\n", s.numTrees(19)); //1767263190
 }
