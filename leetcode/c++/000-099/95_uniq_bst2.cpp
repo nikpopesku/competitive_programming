931ebbbdac29d9eae162c737ceb6fda813ff101a
Nikolai Popesku <nic1k@yandex.com>
2026-08-20 12:11:39 +0800

    leetcode/c++/000-099/95_uniq_bst2.cpp

--- leetcode/c++/000-099/95_uniq_bst2.cpp
+++ leetcode/c++/000-099/95_uniq_bst2.cpp
@@ -28,8 +28,30 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<TreeNode*> generateTrees(int n) {
+    vector<TreeNode *> generateTrees(const int n) {
+        if (n == 0) return {};
 
+        return build(1, n);
+    }
+
+private:
+    vector<TreeNode *> build(const int start, const int end) {
+        if (start > end) return {nullptr};
+
+        vector<TreeNode *> result;
+
+        for (int r = start; r <= end; ++r) {
+            vector<TreeNode *> lefts = build(start, r - 1);
+            vector<TreeNode *> rights = build(r + 1, end);
+
+            for (const auto l : lefts) {
+                for (const auto rt: rights) {
+                    result.push_back(new TreeNode(r, l, rt));
+                }
+            }
+        }
+
+        return result;
     }
 };
 
