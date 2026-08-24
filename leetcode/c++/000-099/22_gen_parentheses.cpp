dfc38d0fc14a0abfdb597241e388836d29417400
Nikolai Popesku <nic1k@yandex.com>
2026-08-24 13:47:26 +0800

    leetcode/c++/000-099/22gen_parentheses.cpp

--- leetcode/c++/000-099/22_gen_parentheses.cpp
+++ leetcode/c++/000-099/22_gen_parentheses.cpp
@@ -1,6 +1,5 @@
 #include <string>
-#include <iostream>
-#include <stack>
+#include <print>
 #include <vector>
 
 using namespace std;
@@ -38,9 +37,6 @@ private:
 int main() {
     auto s = Solution();
 
-    vector nums = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-    cout << s.trap(nums) << endl; //6
-
-    vector nums2 = {4, 2, 0, 3, 2, 5};
-    cout << s.trap(nums2) << endl;
+    print("{}\n", s.generateParenthesis(3));
+    print("{}\n", s.generateParenthesis(1));
 }
