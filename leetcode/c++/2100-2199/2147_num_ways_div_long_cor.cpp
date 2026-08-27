c9249fbab1c01685247d5bfe4f4b88dd98890632
Nikolai Popesku <nic1k@yandex.com>
2026-08-27 13:19:14 +0800

    leetcode/c++/2100-2199/2147_num_ways_div_long_cor.cpp

--- leetcode/c++/2100-2199/2147_num_ways_div_long_cor.cpp
+++ leetcode/c++/2100-2199/2147_num_ways_div_long_cor.cpp
@@ -6,11 +6,33 @@
 
 using namespace std;
 
+#define ll long long
+
+constexpr ll MD = 1e9 + 7;
 
 class Solution {
 public:
     int numberOfWays(string corridor) {
+        ll cur_seats = 0;
+        ll cur_space = 0;
+        ll num_ways = 1;
 
+        for (const auto i: corridor) {
+            if (cur_seats == 2) {
+                if (i == 'P') {
+                    ++cur_space;
+                } else {
+                    num_ways = num_ways * (cur_space + 1) % MD;
+                    cur_space = 0;
+                    cur_seats = 0;
+                }
+            }
+            if (i == 'S' && cur_seats != 2) {
+                ++cur_seats;
+            }
+        }
+
+        cout << num_ways << '\n';
     }
 };
 
@@ -18,6 +40,8 @@ int main() {
     Solution s;
 
     s.numberOfWays("SSPPSPS"); //3
+    s.numberOfWays("PPSPSP"); //1
+    s.numberOfWays("S"); //0
 
     return 0;
 }
