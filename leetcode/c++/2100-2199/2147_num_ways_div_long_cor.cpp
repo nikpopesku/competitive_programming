ef814e58627a4f3d7caebe57dd43db7ad71c8d13
Nikolai Popesku <nic1k@yandex.com>
2026-08-27 12:46:19 +0800

    leetcode/c++/2100-2199/2147_num_ways_div_long_cor.cpp

---
+++ leetcode/c++/2100-2199/2147_num_ways_div_long_cor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ranges>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+
+    }
+};
+
+int main() {
+    Solution s;
+
+    s.numberOfWays("SSPPSPS"); //3
+
+    return 0;
+}
