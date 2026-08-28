25d078ff55abb9da14b7729e28077b943294f436
Nikolai Popesku <nic1k@yandex.com>
2026-08-28 12:01:50 +0800

    leetcode/c++/100-199/115_distinct_subsequences.cpp

--- leetcode/c++/100-199/115_distinct_subsequences.cpp
+++ leetcode/c++/100-199/115_distinct_subsequences.cpp
@@ -11,11 +11,11 @@ public:
         const int m = static_cast<int>(t.size());
 
         vector dp(n + 1, vector(m + 1, 0));
-        for (int i = 0; i < n; ++i) dp[i][0] = 1;
-        for (int i = 0; i < m; ++i) dp[0][i] = 0;
+        for (int i = 0; i <= n; ++i) dp[i][0] = 1;
+        for (int i = 1; i <= m; ++i) dp[0][i] = 0;
 
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
                 if (s[i] == t[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 } else {
