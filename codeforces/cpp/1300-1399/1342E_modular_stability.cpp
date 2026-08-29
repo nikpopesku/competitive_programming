d2abb878284f4f47bcecd5034814aa05b20ddc73
Nikolai Popesku <nic1k@yandex.com>
2026-08-29 12:55:54 +0800

    codeforces/cpp/1300-1399/1342E_modular_stability.cpp

--- codeforces/cpp/1300-1399/1342E_modular_stability.cpp
+++ codeforces/cpp/1300-1399/1342E_modular_stability.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,7 +7,33 @@ using namespace std;
 
 constexpr ll MD = 998244353;
 
+ll pow(ll a, ll b) {
+    ll response = 1;
+
+    while (b > 0) {
+        if (b % 2 == 1) {
+            response = response * a % MD;
+            --b;
+        } else {
+            a = a * a % MD;
+            b >>= 1;
+        }
+    }
+
+    return response;
+}
+
 int main() {
     ll n, k;
     cin >> n >> k;
+    ll num_stable_arr = 0;
+    vector<ll> fact(n + 1, 1);
+    for (ll i = 2; i <= n; ++i) fact[i] = i * fact[i - 1] % MD;
+
+    for (ll i = 1; i <= n / k; ++i) {
+        num_stable_arr = (num_stable_arr + fact[n / i - 1] * pow(fact[k - 1], MD - 2) % MD * pow(
+                              fact[n / i - k + 1], MD - 2) % MD) % MD;
+    }
+
+    cout << num_stable_arr << '\n';
 }
