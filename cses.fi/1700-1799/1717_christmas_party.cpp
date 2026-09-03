75f92d2da812bffdbe8169baa82f24242ceb73a1
Nikolai Popesku <nic1k@yandex.com>
2026-09-03 12:29:46 +0800

    cses.fi/1700-1799/1717_christmas_party.cpp

--- cses.fi/1700-1799/1717_christmas_party.cpp
+++ cses.fi/1700-1799/1717_christmas_party.cpp
@@ -10,4 +10,15 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    ll n;
+    cin >> n;
+    vector<ll> d(n + 1, 1);
+    d[2] = 2;
+
+    for (ll i = 3; i <= n; ++i) {
+        d[n] = (n - 1) * ((d[n - 2] + d[n - 1]) % MD) % MD;
+    }
+
+    cout << d[n] << '\n';
 }
